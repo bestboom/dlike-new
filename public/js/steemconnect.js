ed6bc3e8c954de5f20ec0e4d5ae0d8121a9b0444
bestboom <certseek@gmail.com>
2019-02-18 01:02:13 +0500

    update

--- public/js/steemconnect.js
+++ public/js/steemconnect.js
@@ -1,11 +1,28 @@
-var sc = require('steemconnect');
+//var sc = require('steemconnect');
 
-var api = sc.Initialize({
-  app: 'busy',
-  callbackURL: 'http://localhost:8000/demo/',
+var api = sc2.Initialize({
+  app: 'dlike.app',
+  callbackURL: 'http://localhost:8000/steemconnect/',
   accessToken: 'access_token',
-  scope: ['vote', 'comment']
+  scope: [ 'login', 'vote', 'comment', 'delete_comment', 'comment_options', 'custom_json' ],
 });
 
-var link = api.getLoginURL(state);
+api.me(function (err, res) {
+  console.log(err, res)
+});
+
+if ($.cookie("access_token") != null) {
+  api.setAccessToken($.cookie("access_token"));
+  api.me(function (err, result) {
+     console.log('/me', err, result); // DEBUG
+    if (!err) {
+      // Fill the steemconnect placeholder with results
+      steemconnect.user = result.account;
+      console.log(steemconnect.user);
+      steemconnect.metadata = JSON.stringify(result.user_metadata, null, 2);
+      steemconnect.profile_image = JSON.parse(steemconnect.user.json_metadata)['profile']['profile_image'];
+    }
+  });
+};
+var link = api.getLoginURL();
 console.log(link);
