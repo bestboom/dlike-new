b00c83eb18ea72bdcbc61f1d14c79136a324f5fe
bestboom <certseek@gmail.com>
2019-02-22 02:55:50 +0500

    Update mint.js

--- public/js/mint.js
+++ public/js/mint.js
@@ -27,9 +27,12 @@ $(document).ready(function(){
     });    
 	function _fetch(apiUrl,webUrl) {
         $.post(apiUrl,{url:webUrl},function(response){
+            console.log(response);
+            
             let res = JSON.parse(response);
-            window.location.replace("editDetails.php?url="+encodeURIComponent(res.url)+"&title="+encodeURIComponent(res.title)+"&imgUrl="+encodeURIComponent(res.imgUrl)+"&details="+encodeURIComponent(res.des));
+            window.location.replace("share.php?url="+encodeURIComponent(res.url)+"&title="+encodeURIComponent(res.title)+"&imgUrl="+encodeURIComponent(res.imgUrl)+"&details="+encodeURIComponent(res.des));
             console.log("Response array: "+res.url);
+
         });
     }
 	function isValidURL(url){
