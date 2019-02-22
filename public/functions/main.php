1fc0f3bd5409c9d355794e8ae64aed912c9f7494
bestboom <certseek@gmail.com>
2019-02-22 15:45:43 +0500

    update

---
+++ public/functions/main.php
@@ -0,0 +1,53 @@
+<?php
+function genBeneficiaries($tring)
+{
+    $arr_str = explode(",", $tring);
+    $beneficiaries = [];
+    if (!($arr_str === null or $arr_str === [""])) {
+        foreach ($arr_str as $tr) {
+            $xpld = explode(":", $tr);
+            $weight = (float)$xpld[1] * 100;
+            $weight = (int)$weight;
+            $beneficiary =
+                [
+                    "account" => $xpld[0],
+                    "weight" => $weight
+                ];
+            $beneficiaries[] = $beneficiary;
+        }
+    }
+    return $beneficiaries;
+}
+
+
+
+function getMaxPayout($user_status, $user_score) {
+    $max_accepted_payout = 300;
+    switch($user_status) {
+        case "elite":
+            $max_accepted_payout = 990;
+            break;
+        case "blacklisted":
+            $max_accepted_payout = 300;
+            break;
+        case "normal":
+            if ($user_score <= 3)
+                $max_accepted_payout = 300;
+            elseif ($user_score > 3 && $user_score <= 4)
+                $max_accepted_payout = 400;
+            elseif ($user_score > 4 && $user_score <= 5)
+                $max_accepted_payout = 500;
+            elseif ($user_score > 5 && $user_score <= 6)
+                $max_accepted_payout = 600;
+            elseif ($user_score > 6 && $user_score <= 7)
+                $max_accepted_payout = 700;
+            elseif ($user_score > 7 && $user_score <= 10)
+                $max_accepted_payout = 990;
+            else
+                $max_accepted_payout = 300;
+            break;
+        default:
+            $max_accepted_payout = 300;
+    }
+    return $max_accepted_payout;
+}
