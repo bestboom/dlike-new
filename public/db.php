a7cbd4b21ed9c45fe6c98905245a2a93ce3d06a4
bestboom <certseek@gmail.com>
2019-02-24 09:02:03 +0500

    Update db.php

--- public/db.php
+++ public/db.php
@@ -2,7 +2,9 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+echo getenv("AQ");
   # get the mongo db name out of the env
+echo getenv("MONGO_URL");
   $mongo_url = parse_url(getenv("MONGO_URL"));
   echo $dbname = str_replace("/", "", $mongo_url["path"]);
 
