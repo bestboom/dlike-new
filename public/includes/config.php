bcb0a5a1881af622717e121cab2910b8fb27679b
bestboom <certseek@gmail.com>
2019-02-24 20:13:55 +0500

    Update config.php

--- public/includes/config.php
+++ public/includes/config.php
@@ -14,12 +14,13 @@ echo '<br>';
 echo $ipst = long2ip($ip);
 echo '<br>';
 echo $_SESSION['token'] = sha1($ip);
-setcookie('token', $_SESSION['token'], time() + (86400 * 30), "/");
+
 echo '<br>';
 
 
 if(!isset($_COOKIE[token])) {
     echo "Cookie named '" . token . "' is not set!";
+  setcookie('token', $_SESSION['token'], time() + (86400 * 30), "/");
 } else {
     echo "Cookie '" . token . "' is set!<br>";
     echo "Value is: " . $_COOKIE[token];
