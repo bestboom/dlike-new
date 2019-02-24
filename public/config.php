141db5056a6df3de6ee0dd570b7e16525e8ab5cb
bestboom <certseek@gmail.com>
2019-02-24 17:08:17 +0500

    Update config.php

--- public/config.php
+++ public/config.php
@@ -17,21 +17,6 @@ if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
 
-$sql = "CREATE TABLE MyLikes (
-id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-username VARCHAR(30) NOT NULL,
-likes INT(20) NOT NULL,
-stars INT(20),
-userip VARCHAR(50),
-like_date TIMESTAMP
-)";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Table MyGuests created successfully";
-} else {
-    echo "Error creating table: " . $conn->error;
-}
-
 $sql = "INSERT INTO MyLikes (username, likes, stars, userip)
 VALUES ('John', 12 , 4, 12323423412)";
 
