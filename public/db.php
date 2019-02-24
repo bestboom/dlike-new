83aa5d492077da8e96818eeb1c3c79b0df391042
bestboom <certseek@gmail.com>
2019-02-24 09:36:24 +0500

    Update db.php

--- public/db.php
+++ public/db.php
@@ -2,11 +2,25 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-echo getenv("AQ");
   # get the mongo db name out of the env
-echo getenv("MONGO_URL");
   $mongo_url = parse_url(getenv("MONGO_URL"));
   echo $dbname = str_replace("/", "", $mongo_url["path"]);
 
+  # connect
+  $m   = new Mongo(getenv("MONGO_URL"));
+  $db  = $m->$dbname;
+  $col = $db->access;
 
+  # insert a document
+  $visit = array( "ip" => $_SERVER["HTTP_X_FORWARDED_FOR"] );
+  $col->insert($visit);
+
+  # print all existing documents
+  $data = $col->find();
+  foreach($data as $visit) {
+    echo "<li>" . $visit["ip"] . "</li>";
+  }
+
+  # disconnect
+  $m->close();
 ?>
