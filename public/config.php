06dce6efbd3c44e123891a19568a806188fb4fca
bestboom <certseek@gmail.com>
2019-02-24 13:17:25 +0500

    Update config.php

--- public/config.php
+++ public/config.php
@@ -1,4 +1,10 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $server = $url["host"];
 $username = $url["user"];
@@ -11,5 +17,5 @@ $config = array(
     'pw' => $password,
     'db' => $db 
 );
-
+echo 'looks its working';
 ?>
