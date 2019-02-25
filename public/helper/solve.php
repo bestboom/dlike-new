2593d73a1de0651f5a6a619f4ccf6fc347b8c570
bestboom <certseek@gmail.com>
2019-02-25 22:13:26 +0500

    update

--- public/helper/solve.php
+++ public/helper/solve.php
@@ -1,10 +1,14 @@
 <?php require 'includes/config.php';
 	require 'lib/solvemedialib.php';
-	if($_POST)
-		{ 
+
+
 			
 		echo $_POST['myRate'];
 		echo $_POST['likes_user'];
+
+		
+	if($_POST)
+		{ 
 			$solvemedia_response=solvemedia_check_answer($privkey,$_SERVER["REMOTE_ADDR"],
 			$_POST["adcopy_challenge"],$_POST["adcopy_response"],$hashkey);
 			if(!$solvemedia_response->is_valid)
