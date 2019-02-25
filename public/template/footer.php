5b427fd655850c4329dc580132f37dcf3db835cd
bestboom <certseek@gmail.com>
2019-02-26 00:35:33 +0500

    Update footer.php

--- public/template/footer.php
+++ public/template/footer.php
@@ -65,6 +65,7 @@
     <script src="https://momentjs.com/downloads/moment.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/steemconnect@latest"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.js"></script>
     <script src="./js/posts.js"></script>
     <script src="./js/steemconnect.js"></script>
 </body>
