8f70a198d68e7a54c233adf9bc287e8b6d35d972
bestboom <certseek@gmail.com>
2019-02-25 22:26:01 +0500

    update

--- public/helper/solve.php
+++ public/helper/solve.php
@@ -5,11 +5,12 @@ error_reporting(E_ALL);
 
 
 	require '../lib/solvemedialib.php';
-	require '../includes/config.php';
+	
 
 
 			
-		echo $_POST['myRate'];
+		echo $_POST['myRate1'];
+		echo '<br>';
 		echo $_POST['likes_user'];
 
 
@@ -18,7 +19,7 @@ error_reporting(E_ALL);
 			$solvemedia_response=solvemedia_check_answer($privkey,$_SERVER["REMOTE_ADDR"],
 			$_POST["adcopy_challenge"],$_POST["adcopy_response"],$hashkey);
 			if(!$solvemedia_response->is_valid)
-				{ echo 'looks good'; }
-			else { echo 'bad';}
+				{ echo 'captcha not solved'; }
+			else { echo 'its ok';}
 		}
 ?>
