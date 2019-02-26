0bd386490f3e65ffa2b0597e0482bdc1f0f976bb
jagdishkumarrj <jagdishkumarrj@gmail.com>
2019-02-27 01:54:09 +0530

    Update index.php

--- public/helper/index.php
+++ public/helper/index.php
@@ -3,7 +3,7 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-define('UPLOAD_DIR', '/app/public/images/');
+define('UPLOAD_DIR', '../images/');
 class DataGraber{
     private $title;
     private $thumbnail;
