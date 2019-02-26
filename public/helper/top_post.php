4edfcb321467085a4a8cdadfa699da080024088b
bestboom <certseek@gmail.com>
2019-02-26 20:36:08 +0500

    update

--- public/helper/top_post.php
+++ public/helper/top_post.php
@@ -15,3 +15,34 @@ if ($result->num_rows > 0) {
     <? }
 } 
 ?>
+<div class="col-md-6 post-thumb">
+                    <a href="#"><img src="" alt="img" class="card-img-top2 img-fluid" id="top_img"></a>
+                </div>
+                <div class="col-md-6 post-contnet-wrap">
+                    <span class="post-meta">30 NOV, 2019</span>
+                    <h4 class="post-title">
+                        <a href="#"><span id="top_title"></span></a>
+                    </h4>
+                    <p class="post-entry"></p>
+                    <div class="post-footer">
+                        <div class="post-author-block">
+                            <div class="author-thumb">
+                                <a href="#">
+                                    <img src="./images/post/authors/1.png" alt="img" class="img-responsive">
+                                </a>
+                            </div>.
+                            <div class="author-info">
+                                <h5>
+                                    <a href="#">
+                                        Nayn e Castro
+                                    </a>
+                                </h5>
+                                <a href="#">@excoin</a>
+                            </div>
+                        </div>
+                        <div class="post-comments">
+                            <img src="./images/post/cmnt-icon.png" alt="img" class="img-responsive">
+                            <a href="#">03 Comments</a>
+                        </div>
+                    </div>
+                </div>
--- public/index.php
+++ public/index.php
@@ -1,17 +1,9 @@
-<?php 
-    include('template/header.php'); 
-    require ('lib/solvemedialib.php');
-
-?>
+<?php include('template/header.php'); require ('lib/solvemedialib.php'); ?>
         <div class="container">
             <div class="offset-md-2 col-md-8">
                 <div class="banner-content">
                     <h2>Welcome To Dlike</h2>
-                    <p>
-                        Share What you like with community <br>
-                         Get rewarded if community likes your shares
-                    </p>
-
+                    <p>Share What you like with community <br>Get rewarded if community likes your shares</p>
                 </div>
             </div>
         </div>
@@ -20,37 +12,6 @@
         <div class="container">
             <article class="post-style-two post-full-width">
                 <?php include('helper/top_post.php'); ?>
-                <div class="col-md-6 post-thumb">
-                    <a href="#"><img src="" alt="img" class="card-img-top2 img-fluid" id="top_img"></a>
-                </div>
-                <div class="col-md-6 post-contnet-wrap">
-                    <span class="post-meta">30 NOV, 2019</span>
-                    <h4 class="post-title">
-                        <a href="#"><span id="top_title"></span></a>
-                    </h4>
-                    <p class="post-entry"></p>
-                    <div class="post-footer">
-                        <div class="post-author-block">
-                            <div class="author-thumb">
-                                <a href="#">
-                                    <img src="./images/post/authors/1.png" alt="img" class="img-responsive">
-                                </a>
-                            </div>.
-                            <div class="author-info">
-                                <h5>
-                                    <a href="#">
-                                        Nayn e Castro
-                                    </a>
-                                </h5>
-                                <a href="#">@excoin</a>
-                            </div>
-                        </div>
-                        <div class="post-comments">
-                            <img src="./images/post/cmnt-icon.png" alt="img" class="img-responsive">
-                            <a href="#">03 Comments</a>
-                        </div>
-                    </div>
-                </div>
             </article><!-- post-style-two -->
             <div class="row" id="content">
             </div>
