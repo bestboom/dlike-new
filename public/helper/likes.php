2760c855fbfed2bafb94b07c22890de63dcd3f76
bestboom <certseek@gmail.com>
2019-02-26 23:11:21 +0500

    Update likes.php

--- public/helper/likes.php
+++ public/helper/likes.php
@@ -8,44 +8,30 @@ require '../includes/config.php';
 
 
 
-$sqlt = "SELECT * FROM PostsLikes ORDER BY likes DESC LIMIT 1";
-$result = $conn->query($sqlt);
+
+
+$sql = "SELECT id, username, stars, userip, author, permlink, like_date FROM MyLikes";
+$result = $conn->query($sql);
 
 if ($result->num_rows > 0) {
     // output data of each row
     while($row = $result->fetch_assoc()) {
-        $top_auth = $row["author"];
-        $top_permlink = $row["permlink"];
+        echo "id: " . $row["id"]. " - Name: " . $row["username"]. " " . $row["stars"]. " " . $row["userip"]. " " . $row["author"]. " " . $row["permlink"]. " " . $row["like_date"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
 
-        ?>
-        <script>
-        	let topauthor = <?php echo json_encode($top_auth); ?>;
-        	let toppermlink = <?php echo json_encode($top_permlink); ?>;
+echo '<br>';
 
-        	steem.api.getContent(topauthor , toppermlink, function(err, res) {
-        		console.log(res);
+$sqlt = "SELECT id, username, stars, userip, author, permlink, like_date FROM MyLikes";
+$result = $conn->query($sql);
 
-            steem.api.getContent(topauthor , toppermlink, function(err, res) {
-                console.log(res);
-                let metadata = JSON.parse(res.json_metadata);
-                let img = new Image();
-                if (typeof metadata.image === "string"){
-                    img.src = metadata.image.replace("?","?");
-                    }else img.src = metadata.image[0];
-                    json_metadata = metadata;
-                    let category = metadata.category;
-                    if (category === undefined) { category = "dlike"; } else {category = metadata.category;}
-                    let post_description = metadata.body;
-                    let title = res.title;
-
-                    $('#top_title').html(title);
-                    $('.post-entry').html(category);
-                    $("#top_img").attr("src", img.src);
-
-        	});
-    	</script>
-
-    <? }
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["id"]. " - Name: " . $row["username"]. " " . $row["stars"]. " " . $row["userip"]. " " . $row["author"]. " " . $row["permlink"]. " " . $row["like_date"]. "<br>";
+    }
 } else {
     echo "0 results";
 }
