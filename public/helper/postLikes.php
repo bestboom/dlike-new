947b64e7ad1c40c2b589f59624bfffccfec2a255
jagdishkumarrj <jagdishkumarrj@gmail.com>
2019-02-27 23:46:53 +0530

    Update postLikes.php

--- public/helper/postLikes.php
+++ public/helper/postLikes.php
@@ -1,1 +1,11 @@
- 
+ <?php
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    
+    require '../includes/config.php';
+
+    $sql = "SELECT * FROM PostsLikes LIMIT 10";
+    $result = $conn->query($sql);
+    var_dump($result);
+?>
