6751aa1c41d5f90e4f741f3948941951da6aaa30
bestboom <certseek@gmail.com>
2019-03-01 15:37:43 +0500

    Update posts.js

--- public/js/posts.js
+++ public/js/posts.js
@@ -189,7 +189,7 @@ $(document).ready(function(){
         $('#top_title').html(title);
         $('.post_catg').html(category);
         $('.post-date').html(created_time);
-		$('.post-entry').text(post_body.substr(0,27)+'...');
+		$('.post-entry').text(post_body.text().substr(0,27)+'...');
         $('.tags').html(posttags);
 		$('#top_img').attr("src", img.src).show();
         $('.authThumb').attr("src", auth_img);
