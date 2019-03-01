d767067090d6fe5ccfb9d74f6d548d5d3cc738dd
bestboom <certseek@gmail.com>
2019-03-01 10:38:13 +0500

    update

--- public/helper/top_post.php
+++ public/helper/top_post.php
@@ -27,8 +27,8 @@ if ($result->num_rows > 0) {
                     </div>
                     
                     <h4 class="post-title"><a href="#"><span id="top_title"></span></a> </h4>
-                    <p class="post-entry data-max='27'></p>
-                    <div class="post-tag-block"><div class="tags"></div></div>
+                    <p class="post-entry"></p>
+                    <div class="post-tag-block-main"><div class="tags"></div></div>
                     <div class="post-footer">
                         <div class="post-author-block">
                             <div class="author-thumb"><a href="#"><img src="" alt="img" class="img-responsive authThumb"></a></div>
