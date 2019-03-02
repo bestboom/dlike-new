066a59538b199b46e73f3908482f3e7e84460f8c
jagdishkumarrj <jagdishkumarrj@gmail.com>
2019-03-03 00:55:01 +0530

    Update postmodal.php

--- public/template/modals/postmodal.php
+++ public/template/modals/postmodal.php
@@ -2,8 +2,7 @@
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
-
-var_dump($_POST);
+    
     $post_author = $_POST['postath'];
     $post_permlink = $_POST['postplink'];
 ?>
