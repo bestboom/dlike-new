816c58dea02a72bde11ba29136c567b3d0f26190
bestboom <certseek@gmail.com>
2019-03-02 22:54:35 +0500

    Update top_post.php

--- public/helper/top_post.php
+++ public/helper/top_post.php
@@ -22,7 +22,7 @@ if ($result->num_rows > 0) {
                     <div class="row d-flex justify-content-center">
                     <figure class="hov-item">
                         <img src="./images/post/dlike-hover.png" alt="img" class="img-responsive">
-                        <span class="hov_me" data-toggle="modal" data-permlink="<?php echo json_encode($top_permlink); ?>" data-author="<?php echo json_encode($top_auth); ?>">
+                        <span class="hov_me" data-toggle="modal" data-permlink="'<?php echo json_encode($top_permlink); ?>'" data-author="'<?php echo json_encode($top_auth); ?>'">
                         <figcaption class="hov-title">
                             <h5><?php echo $top_likes; ?></h5>
                         </figcaption>
@@ -43,10 +43,10 @@ if ($result->num_rows > 0) {
                             <div class="author-info"> <h5><a href="#" class="auth_name"></a></h5></div>
                         </div>
                         <div class="post-comments">
-                            <i class="fas fa-comments icon-pad"></i><a href="#">0 </a> Comments
+                            <i class="fas fa-comments icon-pad"></i><a href="#">03</a> Comments
                         </div>
                         <div>
-                            <a class="upvoting" data-toggle="modal" data-target="#upvoteModal" data-permlink="<?php echo json_encode($top_permlink); ?>'" data-author="<?php echo json_encode($top_auth); ?>"><i class="fas fa-chevron-circle-up"></i></a>&nbsp; | <i class="fas fa-dollar-sign dollar-pad"></i><span id="top_post_votes"></span>
+                            <a class="upvoting" data-toggle="modal" data-target="#upvoteModal" data-permlink="' + toppermlink + '" data-author="' + topauthor + '"><i class="fas fa-chevron-circle-up"></i></a>&nbsp; | <i class="fas fa-dollar-sign dollar-pad"></i><span id="top_post_votes"></span>
                         </div>
                     </div>
                 </div>
