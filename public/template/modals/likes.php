a5d669c200f0dd099a250990e8729ce0e77dd7bf
bestboom <certseek@gmail.com>
2019-03-02 20:41:11 +0500

    Update likes.php

--- public/template/modals/likes.php
+++ public/template/modals/likes.php
@@ -18,7 +18,7 @@
 ?>
 <div class="dvd-account-title">
     <h3>Do You Recomend this Share?</h3>
-    <p><span>Total Votes:  <!--<?php echo $likes; ?> -->  <div class="star-ratings-sprite">Ratings: <span style="" class="star-ratings-sprite-rating"></span></div> </span></p>
+    <p><span>Total Votes:   <div class="star-ratings-sprite">Ratings: <span style="" class="star-ratings-sprite-rating"></span></div> </span></p>
 </div>  
 <!--
 <? }
