aaa16ae6ade04bce38959e5c8b16c701a6ebd2f3
bestboom <certseek@gmail.com>
2019-03-02 19:40:22 +0500

    Update likes.php

--- public/template/modals/likes.php
+++ public/template/modals/likes.php
@@ -3,7 +3,7 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 
-    require '../../includes/config.php';
+    require './../includes/config.php';
 
         $author = isset($_GET["author"]) ? $_GET["author"] : "";
         $author = stripslashes( $author );
