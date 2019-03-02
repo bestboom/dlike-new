918907fa7dfa8d9704a4577f99e6d0c46485f37e
jagdishkumarrj <jagdishkumarrj@gmail.com>
2019-03-02 23:24:02 +0530

    Update postLikes.php

--- public/helper/postLikes.php
+++ public/helper/postLikes.php
@@ -12,12 +12,12 @@
 	} else {
 		$strReturn['status'] = 'OK';
 		$author = isset($_GET["author"]) ? $_GET["author"] : "";
-		//$author = stripslashes( $author );
+		$author = stripslashes( $author );
 		
 		$permlink = isset($_GET["permlink"]) ? $_GET["permlink"] : "";
-		//$permlink = stripslashes( $permlink );
+		$permlink = stripslashes( $permlink );
 		
-		$sql = "SELECT * FROM PostsLikes WHERE author = '$author' AND permlink = '$permlink' LIMIT 1";
+		$sql = "SELECT * FROM PostsLikes WHERE author = '"$author"' AND permlink = '"$permlink"' LIMIT 1";
 		$result = $conn->query($sql);
 
 		if ($result->num_rows > 0) {
