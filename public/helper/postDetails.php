e36e5526da9699973d8751d9fa2108023707d6c1
bestboom <certseek@gmail.com>
2019-03-03 01:34:00 +0500

    Update postDetails.php

--- public/helper/postDetails.php
+++ public/helper/postDetails.php
@@ -3,9 +3,22 @@
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 
-	echo $postauthor = $_POST['postath'];
-	echo $postpermlink = $_POST['postplink'];
+	 $postauthor = $_POST['postath'];
+	 $postpermlink = $_POST['postplink'];
 
 
 
 	?>
+
+
+	<script>
+ $(document).ready(function(){
+    let postauthor = <?php echo json_encode($postauthor); ?>;
+    let poatpermlink = <?php echo json_encode($postpermlink); ?>;
+
+
+steem.api.getContent(topauthor , toppermlink, function(err, res) {
+	console.log(res);
+});
+});
+</script>
