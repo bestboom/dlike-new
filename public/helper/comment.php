53889602367e8fd8e9013ab97f2306ea8a620bc7
bestboom <certseek@gmail.com>
2019-03-03 16:50:44 +0500

    Update comment.php

--- public/helper/comment.php
+++ public/helper/comment.php
@@ -1,4 +1,11 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+echo 'coment';
+
 if (isset($_POST["permlink"]) && isset($_POST["author"])){
 
 	echo $_POST["permlink"];
