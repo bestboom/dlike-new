5c51ba4c47994e181b04cd172a578a7d2722f856
bestboom <certseek@gmail.com>
2019-03-04 12:31:56 +0500

    Update upvotemodal.php

--- public/template/modals/upvotemodal.php
+++ public/template/modals/upvotemodal.php
@@ -2,10 +2,10 @@
                                                     <div class="container">
                                                         <div class="row d-flex justify-content-around">
                                                             <form action="helper/vote.php" method="POST">
-                                                                <input type="hidden" name="v_author" id="vote_author" value="" />
-                                                                <input type="hidden" name="v_permlink" id="vote_permlink" value="" />
-                                                                <input type="hidden" name="vote_value" id="vote_weight" value="" />
-                                                            <div class="col-sm-2 nopadding"><i class="fas fa-chevron-circle-up upme"></i></div>    
+                                                                <input type="text" name="v_author" id="vote_author" value="" />
+                                                                <input type="text" name="v_permlink" id="vote_permlink" value="" />
+                                                                <input type="text" name="vote_value" id="vote_weight" value="" />
+                                                            <div class="col-sm-2 nopadding"><button><i class="fas fa-chevron-circle-up upme"></i></button></div>    
                                                             <div class="col-sm-8 nopadding">
                                                                 <div class="row d-flex justify-content-around">
                                                                     <div class="col-sm-9"><input id="rs-range-line" class="rs-range" type="range" value="0" min="0" max="100"></div>
