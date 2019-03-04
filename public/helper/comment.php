c0c06d1a0a92b422c2d8de60aa48aba8f2186d1e
bestboom <certseek@gmail.com>
2019-03-04 06:43:08 +0500

    Update comment.php

--- public/helper/comment.php
+++ public/helper/comment.php
@@ -4,10 +4,14 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+if(isset($_COOKIE['access_token'])){
+echo $myval = $_COOKIE['access_token']);
+	}
+
 if (isset($_POST["post_permlink"]) && isset($_POST["post_author"])){
 
-	echo $_POST["user_at"];
-	echo '<br>';
+	//echo $_POST["user_at"];
+	echo 'okkkkkkkkkkkkkkk';
 	echo $_POST["post_permlink"];
 	echo '<br>';
 	echo $_POST["post_author"];
