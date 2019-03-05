3b4daa6670e63993ffa6e01390fb3a92b9795263
bestboom <certseek@gmail.com>
2019-03-05 17:49:15 +0500

    Update mint.js

--- public/js/mint.js
+++ public/js/mint.js
@@ -290,7 +290,7 @@ $('#content').on("click", ".post_detail", function() {
                     let a_p = "https://dlike.io/@"+$comment.author;
                     let $commentbody = $comment.body.replace(/<[\/]{0,1}(p)[^><]*>/ig,"");
 
-                $(".cmt_section").append('<li class="comment'+postpermlink +postauthor +'">\n' +
+                $(".cmt_section").append('<li class="comment">\n' +
                     '<div class="comment-wrap">\n' +
                     '<div class="comment-info">\n' +
                     '<div class="commenter-thumb"><img src="https://steemitimages.com/u/'+$comment.author+'/avatar" class="img-fluid" alt=""></div>\n' +
