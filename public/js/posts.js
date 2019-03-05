9894fbd92750b7ab013defcab77fb286a22e4d81
bestboom <certseek@gmail.com>
2019-03-05 10:25:43 +0500

    Update posts.js

--- public/js/posts.js
+++ public/js/posts.js
@@ -207,10 +207,10 @@ $(document).ready(function(){
 				$(".cmt_section").append('<li class="comment">\n' +
 					'<div class="comment-wrap">\n' +
 					'<div class="comment-info">\n' +
-					'<div class="commenter-thumb"><img src="./images/post/authors/9.png" class="img-fluid" alt=""></div>\n' +
+					'<div class="commenter-thumb"><img src="https://steemitimages.com/u/'+$comment.author+'/avatar" onerror="this.src='./images/post/authors/9.png" class="img-fluid'" alt=""></div>\n' +
 					'<div class="commenter-info">\n' +
-					'<span class="commenter-name">Lisa Parsons</span>\n' +
-					'<span class="date">28 Dec 2019</span>\n' +
+					'<span class="commenter-name">'+$comment.author+'</span>\n' +
+					'<span class="date">'+moment.utc($comment.created + "Z", "YYYY-MM-DD  h:mm:ss").fromNow()+'</span>\n' +
 					'</div>\n' +
 					'<div class="reply"><button type="button" class="reply-button">Reply</button></div>\n' +
 					'</div>\n' +
