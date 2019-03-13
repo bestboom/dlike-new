7f1d263d4bd493363eae9ba67c951fb2d8af2a5d
bestboom <certseek@gmail.com>
2019-03-13 09:43:25 +0500

    Update wallet.php

--- public/wallet.php
+++ public/wallet.php
@@ -20,7 +20,7 @@ $rowIt = $resultAmount->fetch_assoc();
             <? } else { ?>
                 <div class="row d-flex justify-content-around wallet-profile">
                     <div class="row">
-                        <span><img src="https://steemitimages.com/u/<?php echo $user_wallet; ?>/avatar" onerror="this.src='/images/post/8.png'" alt="img" class="img-responsive img-wallet"></span>
+                        <span><img src="https://steemitimages.com/u/<?php echo $user_wallet; ?>/avatar" onerror="this.src='/images/post/authors/8.png'" alt="img" class="img-responsive img-wallet"></span>
                         <h4><?php echo $user_wallet; ?></h4>  
                     </div>     
                     <div>
