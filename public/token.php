a17b06cc686f28b484222f623d6aa235e3206a9a
bestboom <certseek@gmail.com>
2019-03-16 18:15:05 +0500

    update

--- public/token.php
+++ public/token.php
@@ -1,92 +1,43 @@
+<?php include('includes/config.php'); ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel='favicon icon' type=image/x-icon href=images/favicon.ico />
   <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-    <link rel="stylesheet" type="text/css" href="https://www.lition.io/wp-content/cache/wpfc-minified/7bk9mmx3/c05tt.css" media="all"/>
-<link rel="stylesheet" type="text/css" href="https://www.lition.io/wp-content/cache/wpfc-minified/erntw1dh/c05tt.css" media="all"/>
-  <link rel="stylesheet" href="./assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="./assets/css/fontawesome-all.min.css">
-  <link rel="stylesheet" href="./assets/css/themify-icons.css">
-  <link rel="stylesheet" href="./assets/css/slick.css">
-  <link rel="stylesheet" href="./css/style.css">
-  <link rel="stylesheet" href="./css/responsive.css">
-  <link rel="stylesheet" href="./css/token.css">
-
-  <title>Excoin cryptowatch</title>
+  <link rel="stylesheet" type="text/css" href="https://www.lition.io/wp-content/cache/wpfc-minified/7bk9mmx3/c05tt.css" media="all"/>
+  <link rel="stylesheet" type="text/css" href="https://www.lition.io/wp-content/cache/wpfc-minified/erntw1dh/c05tt.css" media="all"/>
+  <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+  <link rel="stylesheet" href="/assets/css/fontawesome-all.min.css">
+  <link rel="stylesheet" href="/assets/css/themify-icons.css">
+  <link rel="stylesheet" href="/assets/css/slick.css">
+  <link rel="stylesheet" href="/css/style.css">
+  <link rel="stylesheet" href="/css/responsive.css">
+  <link rel="stylesheet" href="/css/token.css">
+  <title>Dlike</title>
 </head>
 <body>
     <div class="banner-block">
         <nav class="navbar main-nav navbar-expand-lg nav-tok">
             <div class="container">
-                <a class="navbar-brand" href="index.html">
-                    <img class="navbar-logo" src="images/logo.png" alt="..."/>
+                <a class="navbar-brand" href="/">
+                    <img class="navbar-logo" src="/images/logo.png" alt="Dlike"/>
                 </a>
                 <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav ml-auto">
-                        <li class="nav-item dropdown">
-                            <a class="nav-link" href="exchange-one.html">Exchange </a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item"><a class="nav-link" href="exchange-one.html">Exchange Default</a></li>
-                                <li class="nav-item"><a class="nav-link" href="exchange-two.html">Exchange Full</a></li>
-                                <li class="nav-item"><a class="nav-link" href="exchange-three.html">Exchange Dark</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item"><a class="nav-link" href="chart.html">Chart</a></li>
-                        <li class="nav-item"><a class="nav-link" href="features.html">Features</a></li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link" href="#">Support</a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item"><a class="nav-link" href="support.html">Support Default</a></li>
-                                <li class="nav-item"><a class="nav-link" href="support-details.html">Support Details</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link" href="#">News</a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item"><a class="nav-link" href="latest-news.html">Latest News</a></li>
-                                <li class="nav-item"><a class="nav-link" href="news-details.html">News Details</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link" href="#">More</a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item"><a class="nav-link" href="faq.html">Faq</a></li>
-                                <li class="nav-item"><a class="nav-link" href="how-work.html">How Work</a></li>
-                                <li class="nav-item"><a class="nav-link" href="contact.html">Contact Us</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item dropdown language-option">
-                            <a class="nav-link" href="#">
-                                <i class="fas fa-globe"></i> EN
-                            </a>
-                            <ul class="dropdown-menu">
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                            <i class="fab fa-buysellads"></i> US
-                                    </a>
-                                </li>
-                                <li class="nav-item">
-                                    <a class="nav-link" href="#">
-                                        <i class="fas fa-lira-sign"></i> UK
-                                    </a>
-                                </li>
-                            </ul>
-                        </li>
-                        <li class="nav-item button">
-                            <a class="btn nav-link" href="login.html">Login</a>
-                        </li>
-                        <li class="nav-item button active">
-                            <a class="btn nav-link" href="signup.html">Sign Up</a>
-                        </li>
+                      <li class="nav-item"><a class="nav-link" href="/share.php">SHARE</a></li>
+                      <li class="nav-item"><a class="nav-link" href="/explorer">Explorer</a></li>
+                      <li class="nav-item"><a class="nav-link" href="/token">Token</a></li>
+                      <li class="nav-item button active">
+                          <a class="btn nav-link log_link" href="#"><span class="img_profile"><img src="" id="user_img"  style="display: none;" class="rounded-circle img-fluid my_img"></span><span id="user_log"> Login</span></a>
+                      </li>
                     </ul>
                 </div>
             </div>
-            <!-- Modal -->
         </nav><!-- main-nav-block -->
         <div class="container">
             <div class="col-md-6">
