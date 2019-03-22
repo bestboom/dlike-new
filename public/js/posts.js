fe91437f73306973a0437306a4e3571d4e77fb3f
bestboom <certseek@gmail.com>
2019-03-22 22:05:35 +0500

    Update posts.js

--- public/js/posts.js
+++ public/js/posts.js
@@ -14,7 +14,7 @@ $(document).ready(function(){
 	let $tag, $limit, content = "#content";
 	let query = {
 		tag: "dlike",
-		limit: 98,
+		limit: 92,
 	};
 
 	steem.api.getDiscussionsByCreated(query, function (err, res) {
