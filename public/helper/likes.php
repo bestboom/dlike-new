0082fb91f829e57fc35c74d51ff6efd471e43a58
bestboom <certseek@gmail.com>
2019-03-26 00:28:44 +0500

    Update likes.php

--- public/helper/likes.php
+++ public/helper/likes.php
@@ -6,7 +6,7 @@ error_reporting(E_ALL);
 
 require '../includes/config.php';
 
-
+/*
 
 $sqlw = "DELETE FROM PostsLikes";
 
@@ -15,7 +15,7 @@ if ($conn->query($sqlw) === TRUE) {
 } else {
     echo "Error creating table: " . $conn->error;
 }
-/*
+
 $sqlm = "DELETE FROM transactions";
 
 if ($conn->query($sqlm) === TRUE) {
