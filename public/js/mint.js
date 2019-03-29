756d8625837c7a3a9ce0629e49f77ee11bfc4884
bestboom <certseek@gmail.com>
2019-03-29 13:09:01 +0500

    Update mint.js

--- public/js/mint.js
+++ public/js/mint.js
@@ -82,15 +82,15 @@ $(document).ready(function(){
         }
     }
     $('.shareme').click(function () {
-        let text_words = $.trim($('[name="description"]').val()).split(' ').filter(function(v){return v!==''}).length;
-        if(text_words < 30){
-            showModalError(
-                "Make Sure..",
-                "Write minimum 40 words to explain how this share is useful for community.",
-                ""
-                );
-            return false;  
-        }
+        //let text_words = $.trim($('[name="description"]').val()).split(' ').filter(function(v){return v!==''}).length;
+        //if(text_words < 30){
+        //    showModalError(
+        //        "Make Sure..",
+        //        "Write minimum 40 words to explain how this share is useful for community.",
+        //        ""
+        //        );
+        //    return false;  
+        //}
         if($('.catg').val() == "0"){
             $('.catg').css("border-color", "RED");
             showModalError(
