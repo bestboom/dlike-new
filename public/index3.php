8852d1b1cba6284c94ac84f6b7efa7055eb57dda
Naenara <naenara0409@outlook.com>
2019-04-28 02:16:27 +0800

    result-7

--- public/index3.php
+++ public/index3.php
@@ -119,130 +119,82 @@ require('lib/solvemedialib.php'); ?>
     </style>
     <div class="latest-post-section"  style="padding-top: 40px !important;">
         <div class="container">
-            <div class="row" id="content">
+            <div class="row" style="background: #eeeeee94;padding-top: 31px;margin-bottom: 20px;">
+                <div class="col-lg-8 col-md-6 postsMainDiv mainDivpostLike_2">
+                    <div class=" p-0">
+                        <div class="container p-0">
+                            <div class="row">
+                                <div class="offset-md-0 col-md-12">
+                                    <div class="testimonials-wrap">
+                                        <div id="testimonial" class="testimonial-slider p-0">
+                                            <div>
+                                                <div class="testimonial-block post_block">
+                                                    <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
+                                                         class="card-img-top img-fluid" style="margin: 0 !important;">
 
-                <div class="row" style="background: #eeeeee94;padding-top: 31px;margin-bottom: 20px;">
-                    <div class="col-lg-8 col-md-6 postsMainDiv mainDivpostLike_2">
-                        <div class=" p-0">
-                            <div class="container p-0">
-                                <div class="row">
-                                    <div class="offset-md-0 col-md-12">
-                                        <div class="testimonials-wrap">
-                                            <div id="testimonial" class="testimonial-slider p-0">
-                                                <div>
-                                                    <div class="testimonial-block post_block">
-                                                        <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
-                                                             class="card-img-top img-fluid" style="margin: 0 !important;">
-
-                                                        <a class="post_title">Top takeaways</a>
-                                                    </div>
+                                                    <a class="post_title">Top takeaways</a>
                                                 </div>
+                                            </div>
 
-                                                <div>
-                                                    <div class="testimonial-block post_block">
-                                                        <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
-                                                             class="card-img-top img-fluid" style="margin: 0 !important;">
+                                            <div>
+                                                <div class="testimonial-block post_block">
+                                                    <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
+                                                         class="card-img-top img-fluid" style="margin: 0 !important;">
 
-                                                        <a class="post_title">Top takeaways</a>
-                                                    </div>
+                                                    <a class="post_title">Top takeaways</a>
                                                 </div>
+                                            </div>
 
-                                                <div>
-                                                    <div class="testimonial-block post_block">
-                                                        <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
-                                                             class="card-img-top img-fluid" style="margin: 0 !important;">
+                                            <div>
+                                                <div class="testimonial-block post_block">
+                                                    <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
+                                                         class="card-img-top img-fluid" style="margin: 0 !important;">
 
-                                                        <a class="post_title">Top takeaways</a>
-                                                    </div>
+                                                    <a class="post_title">Top takeaways</a>
                                                 </div>
+                                            </div>
 
-                                                <div>
-                                                    <div class="testimonial-block post_block">
-                                                        <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
-                                                             class="card-img-top img-fluid" style="margin: 0 !important;">
+                                            <div>
+                                                <div class="testimonial-block post_block">
+                                                    <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
+                                                         class="card-img-top img-fluid" style="margin: 0 !important;">
 
-                                                        <a class="post_title">Top takeaways</a>
-                                                    </div>
+                                                    <a class="post_title">Top takeaways</a>
                                                 </div>
+                                            </div>
 
-                                                <div>
-                                                    <div class="testimonial-block post_block">
-                                                        <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
-                                                             class="card-img-top img-fluid" style="margin: 0 !important;">
+                                            <div>
+                                                <div class="testimonial-block post_block">
+                                                    <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
+                                                         class="card-img-top img-fluid" style="margin: 0 !important;">
 
-                                                        <a class="post_title">Top takeaways</a>
-                                                    </div>
+                                                    <a class="post_title">Top takeaways</a>
                                                 </div>
+                                            </div>
 
-                                                <div>
-                                                    <div class="testimonial-block post_block">
-                                                        <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
-                                                             class="card-img-top img-fluid" style="margin: 0 !important;">
+                                            <div>
+                                                <div class="testimonial-block post_block">
+                                                    <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
+                                                         class="card-img-top img-fluid" style="margin: 0 !important;">
 
-                                                        <a class="post_title">Top takeaways</a>
-                                                    </div>
+                                                    <a class="post_title">Top takeaways</a>
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                 </div>
                             </div>
-                        </div><!-- testimonial-section -->
-                    </div>
+                        </div>
+                    </div><!-- testimonial-section -->
+                </div>
 
-                    <div class="col-lg-4 col-md-6 postsMainDiv mainDivpostLike_2">
+                <div class="col-lg-4 col-md-6 postsMainDiv mainDivpostLike_2">
 
-                        <article class="post-style-two">
-                            <div class="post-thumb" style="border: none;">
-                                <div class="row pl-3">
-                                    <div class="col-4">
-                                        <span>Top Rated</span>
-                                    </div>
-                                    <div class="col-8 pl-0">
-                                        <hr style="margin-top: 12px;margin-bottom: 5px;border-top: 6px solid #7F7F7F;">
-                                    </div>
-                                </div>
-                            </div>
-
-                            <div class="post-thumb" style="border: none;background: white">
-
-                                <div class="row">
-                                    <div class="col-4 p-2 pl-4" style="height: 95px;">
-                                        <img src="https://dotesports-media.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2019/04/26143424/London_Spitfire_wins_5-26-18.jpg"
-                                             alt="London Spitfire signs Quatermain | Dot Esports"
-                                             style="height: 100%;" class="card-img-top img-fluid">
-                                    </div>
-
-                                    <div class="col-8">
-                                        <a style="color: black;font-size: 12px;">The mysterious Imperial Treasures Of Japan</a>
-                                        <div class="row mt-2">
-                                            <div class="col-1 pt-1">
-                                                <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/AAywGC0.img?h=16&amp;w=16&amp;m=6&amp;q=60&amp;u=t&amp;o=t&amp;l=f&amp;f=png"
-                                                     class="loaded">
-                                            </div>
-
-                                            <div class="col-4">
-                                                <span class="sourcename">CNN</span>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <!-- <a class="post_detail text-center" data-toggle="modal"
-                                    data-target="#postModal"
-                                    data-permlink="london-spitfire-signs-quatermain-dot-esports"
-                                    data-author="miercoles">
-                                     <img src="https://dotesports-media.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2019/04/26143424/London_Spitfire_wins_5-26-18.jpg" alt="London Spitfire signs Quatermain | Dot Esports"
-                                          style="height: 135px;" class="card-img-top img-fluid">
-                                 </a>-->
-                            </div>
-
-                        </article>
-
+                    <article class="post-style-two">
                         <div class="post-thumb" style="border: none;">
                             <div class="row pl-3">
                                 <div class="col-4">
-                                    <span>Advertiseme</span>
+                                    <span>Top Rated</span>
                                 </div>
                                 <div class="col-8 pl-0">
                                     <hr style="margin-top: 12px;margin-bottom: 5px;border-top: 6px solid #7F7F7F;">
@@ -250,18 +202,67 @@ require('lib/solvemedialib.php'); ?>
                             </div>
                         </div>
 
-                        <article class="post-style-two" style="height: 250px;background: #6b2525;margin-bottom: 40px;">
+                        <div class="post-thumb" style="border: none;background: white">
 
+                            <div class="row">
+                                <div class="col-4 p-2 pl-4" style="height: 95px;">
+                                    <img src="https://dotesports-media.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2019/04/26143424/London_Spitfire_wins_5-26-18.jpg"
+                                         alt="London Spitfire signs Quatermain | Dot Esports"
+                                         style="height: 100%;" class="card-img-top img-fluid">
+                                </div>
 
-                            <div class="post-thumb">
+                                <div class="col-8">
+                                    <a style="color: black;font-size: 12px;">The mysterious Imperial Treasures Of Japan</a>
+                                    <div class="row mt-2">
+                                        <div class="col-1 pt-1">
+                                            <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/AAywGC0.img?h=16&amp;w=16&amp;m=6&amp;q=60&amp;u=t&amp;o=t&amp;l=f&amp;f=png"
+                                                 class="loaded">
+                                        </div>
 
+                                        <div class="col-4">
+                                            <span class="sourcename">CNN</span>
+                                        </div>
+                                    </div>
+
+                                </div>
                             </div>
+                            <!-- <a class="post_detail text-center" data-toggle="modal"
+                                data-target="#postModal"
+                                data-permlink="london-spitfire-signs-quatermain-dot-esports"
+                                data-author="miercoles">
+                                 <img src="https://dotesports-media.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2019/04/26143424/London_Spitfire_wins_5-26-18.jpg" alt="London Spitfire signs Quatermain | Dot Esports"
+                                      style="height: 135px;" class="card-img-top img-fluid">
+                             </a>-->
+                        </div>
 
+                    </article>
 
-                        </article>
-
+                    <div class="post-thumb" style="border: none;">
+                        <div class="row pl-3">
+                            <div class="col-4">
+                                <span>Advertiseme</span>
+                            </div>
+                            <div class="col-8 pl-0">
+                                <hr style="margin-top: 12px;margin-bottom: 5px;border-top: 6px solid #7F7F7F;">
+                            </div>
+                        </div>
                     </div>
+
+                    <article class="post-style-two" style="height: 250px;background: #6b2525;margin-bottom: 40px;">
+
+
+                        <div class="post-thumb">
+
+                        </div>
+
+
+                    </article>
+
                 </div>
+            </div>
+
+            <div class="row" id="content">
+
 
             </div>
 
