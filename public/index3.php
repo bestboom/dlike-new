35041fb87d1da5debb04f7bcf61e89a65fbe08b4
Naenara <naenara0409@outlook.com>
2019-04-27 21:55:48 +0800

    result

--- public/index3.php
+++ public/index3.php
@@ -1,52 +1,228 @@
-<?php include('template/header2.php'); require ('lib/solvemedialib.php'); ?>
-    <?php if($_COOKIE['username'] == ''){ ?>
-<div class="banner-content home-connect">
-                    <div class="news-headline-block">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-1 col-md-2">
-                                <i class="fas fa-volume-up vol"></i>
-                            </div>
-                            <div class="col-lg-11 col-md-10">
-                                <div class="news-headlines-block">
-                                    <div class="news-headlines-slider ticker">
-                                        <ul>
-                                            <li>Dlike will soon start token sale for steemians with huge bonus.</li>
-                                        </ul>
-                                    </div><!-- news-headlines-slider -->
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-            <? } else { ?>
-                <div class="banner-content home-connect">
-                    <div class="news-headline-block">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-1 col-md-2">
-                                <i class="fas fa-volume-up vol"></i>
-                            </div>
-                            <div class="col-lg-11 col-md-10">
-                                <div class="news-headlines-block">
-                                    <div class="news-headlines-slider ticker">
-                                        <ul>
-                                            <li>Dlike will soon start token sale for steemians with huge bonus.</li>
-                                        </ul>
-                                    </div><!-- news-headlines-slider -->
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-            <? } ?>
+<?php include('template/header2.php');
+require('lib/solvemedialib.php'); ?>
+    <!-- --><?php /*if($_COOKIE['username'] == ''){ */ ?>
+<?php if (1) { ?>
+    <div class="banner-content home-connect">
+    <div class="news-headline-block">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-1 col-md-2">
+                <i class="fas fa-volume-up vol"></i>
+            </div>
+            <div class="col-lg-11 col-md-10">
+                <div class="news-headlines-block">
+                    <div class="news-headlines-slider ticker">
+                        <ul>
+                            <li>Dlike will soon start token sale for steemians with huge bonus.</li>
+                        </ul>
+                    </div><!-- news-headlines-slider -->
+                </div>
             </div>
         </div>
+    </div>
+<?php } else { ?>
+    <div class="banner-content home-connect">
+    <div class="news-headline-block">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-1 col-md-2">
+                <i class="fas fa-volume-up vol"></i>
+            </div>
+            <div class="col-lg-11 col-md-10">
+                <div class="news-headlines-block">
+                    <div class="news-headlines-slider ticker">
+                        <ul>
+                            <li>Dlike will soon start token sale for steemians datawith huge bonus.</li>
+                        </ul>
+                    </div><!-- news-headlines-slider -->
+                </div>
+            </div>
+        </div>
+    </div>
+<?php } ?>
+    </div>
+    </div>
     </div><!-- sub-header -->
+
+    <style>
+        .slick-list.draggable{
+            min-height: 563px !important;
+            margin-bottom: 100px;
+        }
+
+        .testimonial-block>img{
+            height: 35.1rem;
+            width: 62.2rem;
+        }
+
+        .testimonial-slider button.slick-prev {
+            left: 0px;
+            z-index: 2222;
+        }
+
+        .testimonial-slider button.slick-next{
+            right: 0px;
+        }
+
+        .testimonial-slider button.slick-next:before, .testimonial-slider button.slick-prev:before {
+            color: white;
+        }
+
+        .slick-next.slick-arrow{
+            padding-left: 15px;
+            background: #303030 !important;
+        }
+
+        .slick-prev.slick-arrow{
+            padding-left: 15px;
+            background: #303030 !important;
+        }
+
+        .testimonial-slider button.slick-next, .testimonial-slider button.slick-prev{
+            width: 35px;
+            height: 56px;
+            background: red;
+        }
+
+        .post_block{
+            position: relative;
+        }
+
+        .post_title{
+            position: absolute;
+            top: 80%;
+            left: 8.5%;
+            color: white;
+            font-size: 27px;
+        }
+
+        .testimonial-slider .slick-dots{
+            bottom: 24px;
+        }
+
+        .testimonial-slider .slick-dots li button{
+            background: #666666;
+        }
+
+        .testimonial-slider .slick-dots li.slick-active button {
+            height: 14px;
+            width: 14px;
+            background: #FFFFFF;
+        }
+
+    </style>
     <div class="latest-post-section">
+        <div class="container">
+            <div class="row" id="content">
+                <div class="col-12 col-lg-8">
+                    <div class=" p-0">
+                        <div class="container">
+                            <div class="row">
+                                <div class="offset-md-0 col-md-12">
+                                    <div class="testimonials-wrap">
+                                        <div id="testimonial" class="testimonial-slider p-0">
+                                            <div>
+                                                <div class="testimonial-block post_block">
+                                                    <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
+                                                         class="card-img-top img-fluid" style="margin: 0 !important;">
+
+                                                    <a class="post_title">Top takeaways</a>
+                                                </div>
+                                            </div>
+
+                                            <div>
+                                                <div class="testimonial-block post_block">
+                                                    <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
+                                                         class="card-img-top img-fluid" style="margin: 0 !important;">
+
+                                                    <a class="post_title">Top takeaways</a>
+                                                </div>
+                                            </div>
+
+                                            <div>
+                                                <div class="testimonial-block post_block">
+                                                    <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
+                                                         class="card-img-top img-fluid" style="margin: 0 !important;">
+
+                                                    <a class="post_title">Top takeaways</a>
+                                                </div>
+                                            </div>
+
+                                            <div>
+                                                <div class="testimonial-block post_block">
+                                                    <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
+                                                         class="card-img-top img-fluid" style="margin: 0 !important;">
+
+                                                    <a class="post_title">Top takeaways</a>
+                                                </div>
+                                            </div>
+
+                                            <div>
+                                                <div class="testimonial-block post_block">
+                                                    <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
+                                                         class="card-img-top img-fluid" style="margin: 0 !important;">
+
+                                                    <a class="post_title">Top takeaways</a>
+                                                </div>
+                                            </div>
+
+                                            <div>
+                                                <div class="testimonial-block post_block">
+                                                    <img src="https://static-global-s-msn-com.akamaized.net/img-resizer/tenant/amp/entityid/BBm5UN9.img?h=368&w=622&m=6&q=60&u=t&o=t&l=f&f=jpg&x=673&y=288"
+                                                         class="card-img-top img-fluid" style="margin: 0 !important;">
+
+                                                    <a class="post_title">Top takeaways</a>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- testimonial-section -->
+                </div>
+
+                <div class="col-12 col-lg-4 pl-5 pr-5">
+                    <div class="row">
+                        <div class="col-lg-12 col-md-12 postsMainDiv mainDivpostLike_1 p-0" postlikes="0"
+                             postnumber="1">
+
+                            <article class="post-style-two">
+
+
+                                <div class="post-thumb">
+                                    <a class="post_detail text-center" data-toggle="modal"
+                                       data-target="#postModal"
+                                       data-permlink="london-spitfire-signs-quatermain-dot-esports"
+                                       data-author="miercoles">
+                                        <img
+                                                src="https://dotesports-media.nyc3.cdn.digitaloceanspaces.com/wp-content/uploads/2019/04/26143424/London_Spitfire_wins_5-26-18.jpg"
+                                                alt="London Spitfire signs Quatermain | Dot Esports"
+                                                style="height: 250px;"
+                                                class="card-img-top img-fluid">
+                                    </a>
+                                </div>
+
+
+                            </article>
+                        </div>
+                    </div>
+
+                    <div class="row" style="height: 250px;background: #6b2525;margin-bottom: 40px;">
+
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+
+    <div class="latest-post-section pt-0">
         <div class="container">
             <div class="row  align-items-center h-100 post_select">
                 <div class="row col-md-3 justify-content-center">
-                        <h4 class="lab_post orderByLatest activeOrderBy">Latest</h4>
-                        <h4 class="lab_post orderByTopRated">Top Rated</h4>
+                    <h4 class="lab_post orderByLatest activeOrderBy">Latest</h4>
+                    <h4 class="lab_post orderByTopRated">Top Rated</h4>
                 </div>
                 <div class="col-md-9 lay">&nbsp;</div>
             </div>
@@ -54,5 +230,5 @@
             </div>
         </div>
     </div>
-<?php include('template/modals/modal.php'); ?>    
-<?php include('template/footer.php'); ?>
+<?php include('template/modals/modal.php'); ?>
+<?php include('template/footer3.php'); ?>
