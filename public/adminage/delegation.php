9459f24891988021f5d264b50df3a41c73526670
Chirag Zunzvadia <jaihind530@gmail.com>
2019-04-30 01:48:32 +0530

    Update delegation.php

--- public/adminage/delegation.php
+++ public/adminage/delegation.php
@@ -1,4 +1,5 @@
 <?php include('head.php'); ?>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
