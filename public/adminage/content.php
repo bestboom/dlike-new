cc1d05468cbd692be5df98b0a07648020c7e177c
bestboom <certseek@gmail.com>
2019-04-30 00:11:59 +0500

    Update content.php

--- public/adminage/content.php
+++ public/adminage/content.php
@@ -3,16 +3,16 @@
             <div class="row">
                 <div class="col-md-4">
                     <div class="support-category-block">
+                    	<a href="delegation.php">
                         <img src="../images/others/21.png" alt="img" class="img-responsive">
                         <h4>Delegations</h4>
+                    	</a>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="support-category-block">
-                    	<a href="delegation.php">
                         	<img src="../images/others/22.png" alt="img" class="img-responsive">
                         	<h4>Contact Support</h4>
-                    	</a>
                     </div>
                 </div>
                 <div class="col-md-4">
