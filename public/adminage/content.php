fb58746d84713849c61895fb3226da054a1eacb1
bestboom <certseek@gmail.com>
2019-04-30 00:06:36 +0500

    Update content.php

--- public/adminage/content.php
+++ public/adminage/content.php
@@ -3,120 +3,22 @@
             <div class="row">
                 <div class="col-md-4">
                     <div class="support-category-block">
-                        <img src="./images/others/21.png" alt="img" class="img-responsive">
-                        <h4>Ask a Question</h4>
+                        <img src="../images/others/21.png" alt="img" class="img-responsive">
+                        <h4>Delegations</h4>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="support-category-block">
-                        <img src="./images/others/22.png" alt="img" class="img-responsive">
+                        <img src="../images/others/22.png" alt="img" class="img-responsive">
                         <h4>Contact Support</h4>
                     </div>
                 </div>
                 <div class="col-md-4">
                     <div class="support-category-block">
-                        <img src="./images/others/23.png" alt="img" class="img-responsive">
+                        <img src="../images/others/23.png" alt="img" class="img-responsive">
                         <h4>Submit a request</h4>
                     </div>
                 </div>
             </div>
         </div>
     </div><!-- support-category-section -->
-    <div class="container">
-        <div class="support-list-section">
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="support-list-block">
-                        <h5>ACCOUNT</h5>
-                        <ul class="support-list">
-                            <li><a href="#">How to Sign Up</a></li>
-                            <li><a href="#">Have Not Received Confirmation Email</a></li>
-                            <li><a href="#">Activation Link Invalid or Expired</a></li>
-                            <li><a href="#">How to Change  Email Address</a></li>
-                            <li><a href="#">How to Delete your Account?</a></li>
-                            <li class="colord"><a href="#">+5 Questions</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="support-list-block">
-                        <h5>Your Wallet</h5>
-                        <ul class="support-list">
-                            <li><a href="#">Wallet confirmation</a></li>
-                            <li><a href="#">Wallet address</a></li>
-                            <li><a href="#">How to Delete your Account?</a></li>
-                            <li><a href="#">Buying with someone else's wallet</a></li>
-                            <li><a href="#">Sending bitcoins to another wallet</a></li>
-                            <li class="colord"><a href="#">+2 Questions</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="support-list-block">
-                        <h5>SECURITY</h5>
-                        <ul class="support-list">
-                            <li><a href="#">How to Sign Up</a></li>
-                            <li><a href="#">Buying with someone else's wallet</a></li>
-                            <li><a href="#">Common scams</a></li>
-                            <li><a href="#">How is my bank account information protected?</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="others-support-list-section">
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="others-support-list-block">
-                        <h5>Wallet Service</h5>
-                        <ul class="others-support-list">
-                            <li><a href="#">Supported Digital Currencies</a></li>
-                            <li><a href="#">How do I send digital currency to another wallet?</a></li>
-                            <li><a href="#">How do I receive digital currency ?</a></li>
-                            <li><a href="#">How to Delete your Account?</a></li>
-                            <li><a href="#">Buying with someone else's wallet</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="others-support-list-block">
-                        <h5>Verification</h5>
-                        <ul class="others-support-list">
-                            <li><a href="#">Required quality for ID verification</a></li>
-                            <li><a href="#">Accepted identification</a></li>
-                            <li><a href="#">Payment Methods for UK Customers</a></li>
-                            <li><a href="#">Documents uploaded</a></li>
-                            <li class="colord"><a href="#">+4 Questions</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="others-support-list-block">
-                        <h5>Mobile App</h5>
-                        <ul class="others-support-list">
-                            <li><a href="#">How to download app</a></li>
-                            <li><a href="#">Update your mobile app</a></li>
-                            <li><a href="#">Phone support scam</a></li>
-                            <li><a href="#">Documents uploaded</a></li>
-                            <li><a href="#">Accepted credit/debit cards</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <div class="colto-section">
-        <div class="container">
-            <div class="colto-content-wrap">
-                <div class="colto-content">
-                    <h3>Getting started</h3>
-                    <p>We provide professional and secure trading services for investors</p>
-                </div>
-                <div class="colto-btn-group">
-                    <button class="btn callto-btn">Log In</button>
-                    <span>or</span>
-                    <button class="btn callto-btn">Create Account</button>
-                </div>
-            </div>
-        </div>
-    </div><!-- colto-section -->
