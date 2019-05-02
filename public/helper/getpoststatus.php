7f69c0b3fbc050a06cdc23f2aa3dfdf358286f93
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-02 17:54:58 +0530

    Create getpoststatus.php

---
+++ public/helper/getpoststatus.php
@@ -0,0 +1,19 @@
+<?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(0);
+    	require '../includes/config.php';
+	  
+    $sql = "SELECT * FROM poststatus";
+		$result = $conn->query($sql);
+		if ($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				$strReturn['permlink'][] = $row['permlink'];
+				$strReturn['status'][] = $row['status'];
+			}
+		} else {
+			$strReturn['permlink'] = 0;
+		}
+    $strReturn['status'] = 'OK';
+  	echo json_encode($strReturn);die;
+?>
