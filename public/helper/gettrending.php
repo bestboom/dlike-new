8afa09dfe42d01c139fc1e5588a8ffc210f75349
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-03 17:10:08 +0530

    Create gettrending.php

---
+++ public/helper/gettrending.php
@@ -0,0 +1,25 @@
+<?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(0);
+    	require '../includes/config.php';
+	  
+    	$sql = "SELECT * FROM posttags order by tagcount,id DESC";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		$html = '<div class="colxs-1 trendingword">Trending Now ></div>';
+		$counter = 0; 
+		while($row = $result->fetch_assoc()) {
+			if (strpos($row['tagname'], 'dlike') === false && $counter < 10) {
+				$html .= '<div class="colxs-1"><a href="'.$row['tagname'].'">'.$row['tagname'].'</a></div>';
+				++$counter;
+			}
+			
+		}
+		$strReturn['status'] = 'OK';
+		$strReturn['html'] .= $html;
+	} else {
+		$strReturn['status'] = 'error';
+	}
+  	echo json_encode($strReturn);die;
+?>
