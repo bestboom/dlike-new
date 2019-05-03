9fd2de9c31474a0b1f64a1e983044f732fc709fc
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-03 16:42:49 +0530

    Update submit_post.php

--- public/helper/submit_post.php
+++ public/helper/submit_post.php
@@ -1,7 +1,7 @@
 <?php
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
-error_reporting(0);
+error_reporting(E_ALL);
 require '../includes/config.php';
 require_once "../helper/publish_post.php";
 include('../functions/main.php');
