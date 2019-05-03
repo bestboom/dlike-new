e1f69a89e42ba4187b5dad167761a61541913687
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-03 17:52:13 +0530

    Update tags.php

---
+++ public/tags.php
@@ -0,0 +1,89 @@
+<?php include('template/header2.php'); require ('lib/solvemedialib.php'); ?>
+    <?php if($_COOKIE['username'] == ''){ ?>
+        <div class="container">
+            <div class="row home-banner">
+                <div class="col-md-3 main main_offer">
+                    <div class="daily">
+                        <div class="daily_box"><p class="daily_start">10</p><p class="daily_mid">STEEM</p><p class="daily_end">Daily</p></div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="banner-content">
+                        <h2>Dlike - Where You Get Liked</h2>
+                        <p>Share What you like with community <br>Get rewarded if community likes your shares</p>
+                        <h5>Daily Top Recommended Post (most dlikes) is Rewarded By Dlike</h5>
+                        <h5>Anyone can recommend a post for which steem account is not compulsory</h5>
+                        <!-- <h2 class="annc">Starting 11th March</h2> -->
+                    </div>
+                </div>
+                <div class="col-md-3 main_offer">
+                    <div class="daily">
+                        <div class="daily_box"><p class="daily_start">2500</p><p class="daily_mid">DLike Tokens</p><p class="daily_end">Daily</p></div>
+                    </div>
+                </div>
+             </div>
+         </div>
+            <? } else { ?>
+                <div class="banner-content home-connect">
+                    <div class="news-headline-block">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-1 col-md-2">
+                                <i class="fas fa-volume-up vol"></i>
+                            </div>
+                            <div class="col-lg-11 col-md-10">
+                                <div class="news-headlines-block">
+                                    <div class="news-headlines-slider ticker">
+                                        <ul>
+                                            <li>Dlike will soon start token sale for steemians with huge bonus.</li>
+                                        </ul>
+                                    </div><!-- news-headlines-slider -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            <? } ?>
+            </div>
+        </div>
+    </div><!-- sub-header -->
+    <div class="latest-post-section">
+        <div class="container">
+	    
+            <div class="row  align-items-center h-100 post_select">
+                <div class="row col-md-3 justify-content-center">
+                        <h4 class="lab_post">Posts</h4>
+                </div>
+                <div class="col-md-9 lay">&nbsp;</div>
+            </div>
+            
+            <div class="row" id="content">
+            </div>
+		
+        </div>
+    </div>
+<?php include('template/modals/modal.php'); ?>    
+<?php include('template/footer2.php'); ?>
+<style>
+.showcursor{cursor:pointer;}
+</style>
+<script>
+
+    	$(document).ready(function(){
+      
+      var tagname = '<?php echo $_GET['tag'];?>';
+      alert(tagname);
+      return false;
+	    $.ajax({
+		type: "POST",
+		url: '/helper/gettrending.php',
+		dataType: 'json',
+		success: function(response) {
+		    if(response.status == "OK") {
+			$(".appendtrending").html(response.html);
+		    }
+		}
+	    });
+
+	});
+
+</script>
