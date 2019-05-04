866f317939515eb19e8630ea592ef79705c66fe5
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-04 13:52:57 +0530

    Update tags.php

--- public/tags.php
+++ public/tags.php
@@ -62,7 +62,7 @@
         </div>
     </div>
 <?php include('template/modals/modal.php'); ?>    
-<?php include('template/footer.php'); ?>
+<?php include('template/footer2.php'); ?>
 <style>
 .showcursor{cursor:pointer;}
 </style>
