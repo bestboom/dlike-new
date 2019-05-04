6d0dc7ff8078e6a00a4382248b2ec502036c6e41
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-04 15:19:08 +0530

    Update gettposts.php

--- public/helper/gettposts.php
+++ public/helper/gettposts.php
@@ -1,18 +1,42 @@
 <?php
+if(isset($_POST['tagname']) && $_POST['tagname'] != "") {
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(0);
     	require '../includes/config.php';
 	  
-    	$sql = "SELECT * FROM poststatus where permlink = '".$_POST['permlink']."'";
+    	$sql = "SELECT postid FROM posttags where tagname = '".$_POST['tagname']."'";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc()) {
-			$strReturn['setstatus'] = $row['status'];
+			$postid = $row['postid'];
+		}
+		$sql1 = "SELECT postid FROM posttags where tagname = '".$_POST['tagname']."'";
+		$result1 = $conn->query($sql1);
+		if ($result1->num_rows > 0) {
+			while($row1 = $result1->fetch_assoc()) {
+				
+				$json_metadata = json_decode($row1['json_metadata'],true);
+				
+				
+				$data['username'] = $row1['username'];
+				$data['created_at'] = $row1['created_at'];
+				$data['category'] = $json_metadata['category'];
+				$data['permlink'] = $row1['permlink'];
+				$data['thumbnail'] = $json_metadata['image'];
+				$data['metatags'] = $row1['username'];
+				$data['title'] = $row1['title'];
+				$data['exturl'] = $json_metadata['url'];
+				
+				$strReturn['data_row'][] = $data;
+			}
 			$strReturn['status'] = 'OK';
 		}
+		
+		
 	} else {
 		$strReturn['status'] = 'error';
 	}
   	echo json_encode($strReturn);die;
+}
 ?>
