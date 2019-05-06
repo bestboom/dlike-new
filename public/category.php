9fb8431c1d46f3f36cfb290379db8c4013077694
bestboom <certseek@gmail.com>
2019-05-06 10:55:41 +0500

    up

---
+++ public/category.php
@@ -0,0 +1,243 @@
+<?php include('template/header2.php'); ?>
+    <?php if($_COOKIE['username'] == ''){ ?>
+        <div class="container">
+            <div class="row home-banner">
+                <div class="col-md-3 main main_offer">
+                    <div class="daily">
+                        <div class="daily_box"><p class="daily_start">10</p><p class="daily_mid">STEEM</p><p class="daily_end">Daily</p></div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="banner-content">
+                        <h2>Dlike - Where You Get Liked</h2>
+                        <p>Share What you like with community <br>Get rewarded if community likes your shares</p>
+                        <h5>Daily Top Recommended Post (most dlikes) is Rewarded By Dlike</h5>
+                        <h5>Anyone can recommend a post for which steem account is not compulsory</h5>
+                        <!-- <h2 class="annc">Starting 11th March</h2> -->
+                    </div>
+                </div>
+                <div class="col-md-3 main_offer">
+                    <div class="daily">
+                        <div class="daily_box"><p class="daily_start">2500</p><p class="daily_mid">DLike Tokens</p><p class="daily_end">Daily</p></div>
+                    </div>
+                </div>
+             </div>
+         </div>
+            <? } else { ?>
+                <div class="banner-content home-connect">
+                    <div class="news-headline-block">
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-lg-1 col-md-2">
+                                <i class="fas fa-volume-up vol"></i>
+                            </div>
+                            <div class="col-lg-11 col-md-10">
+                                <div class="news-headlines-block">
+                                    <div class="news-headlines-slider ticker">
+                                        <ul>
+                                            <li>Dlike will soon start token sale for steemians with huge bonus.</li>
+                                        </ul>
+                                    </div><!-- news-headlines-slider -->
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+            <? } ?>
+            </div>
+        </div>
+    </div><!-- sub-header -->
+    <div class="latest-post-section">
+		<div class="container">
+	    	<div class="row" id="catposts"></div>
+		</div>
+    </div>
+  
+<?php include('template/footer2.php'); ?>
+<script>
+	$(document).ready(function(){
+      	var catname = '<?php echo $_GET['cat'];?>';
+
+      	let $tag, $limit, catposts = "#catcontent";
+	let query = {
+		tag: "dlike",
+		limit: 48,
+	};
+
+	steem.api.getDiscussionsByCreated(query, function (err, res) {
+		//console.log(res);
+		res.forEach(($post, i) => {
+			let metadata;
+			if ($post.json_metadata && $post.json_metadata.length > 0){
+				metadata = JSON.parse($post.json_metadata);
+			}
+		//get meta tags
+			let steemTags = metadata.tags;
+			let dlikeTags = steemTags.slice(2);
+			let metatags = dlikeTags.map(function (meta) { if (meta) return '<a href="/tags/'+meta+'"> #' + meta + ' </a>' });
+			let category = metadata.category;
+			let exturl = metadata.url;
+
+			var currentPostNumber = i;
+			var currentLikesDivElement = 'postLike_' + i;
+			if(metadata && metadata.community == "dlike" && metadata.category == catname){
+				getTotalcomments($post.author,$post.permlink);
+
+				// get image here
+				let img = new Image();
+				if (typeof metadata.image === "string") {
+					if (metadata.image.indexOf("https://dlike") >= 0){
+					img.src = metadata.image.replace("?","%3f");
+					}else{
+					 img.src = metadata.image;
+					}
+				} else {
+					if (!metadata.image || metadata.image[0] === undefined) {
+					img.src = "https://dlike.io/images/default-img.jpg";
+					} else {
+					img.src = metadata.image[0];
+					}
+				}
+
+				//get time
+				let activeDate = moment.utc($post.created + "Z", 'YYYY-MM-DD  h:mm:ss').fromNow();
+
+				//Get the body
+				let body;
+				if($post && $post.body && $post.body != undefined){
+					try {
+					body = $post.body;
+					body = body.split(/\n\n#####\n\n/);
+					body = body[1];
+					body = body.replace(/#([^\s]*)/g,'');
+					//body = $post.body.replace(/<(.|\n)*?>/g, '');
+					}catch(err) {
+					body = "";
+					}
+				}else{
+					body = "";
+				}
+
+				//image or youtube
+				let thumbnail = '<img src="' + img.src + '" alt="' + $post.title + '" class="card-img-top img-fluid">';
+
+				var getLocation = function(href) {
+					var l = document.createElement("a");
+					l.href = href;
+					return l;
+				};
+				var url = getLocation(metadata.url);
+				var youtubeAnchorTagVariableClass = '';
+				if(url.hostname == 'www.youtube.com' || url.hostname == 'youtube.com' || url.hostname == 'youtu.be' || url.hostname == 'www.youtu.be'){
+					//alert(url);
+					youtubeAnchorTagVariableClass = 'youtubeAnchorTagVariableClass_' + i;
+					if(url.search != ''){
+						let query = url.search.substr(1); //remove ? from begning
+						query = query.split('&')
+						for (i in query){
+							let splited = query[i].split('=');
+							if(splited[0] == 'v'){
+								thumbnail = '<iframe src="https://www.youtube.com/embed/' + splited[1] + '" class="card-img-top img-fluid" style="overflow:hidden;" scrolling="no" frameborder="0" allowfullscreen></iframe>';
+							}
+						}
+					}else{
+						thumbnail = '<iframe src="https://www.youtube.com/embed/' + url.pathname + '" class="card-img-top img-fluid" style="overflow:hidden;" scrolling="no" frameborder="0" allowfullscreen></iframe>';
+					}
+				}
+
+				//check comments
+				function getTotalcomments(thisAutor,thisPermlink){
+					//Conting the comments (just the dlike ones)
+					steem.api.getContentReplies(thisAutor,thisPermlink, function(err, result) {
+						let totalDlikeComments = 0;  
+						result.forEach(comment =>{
+						let metadata;
+							if (comment.json_metadata && comment.json_metadata.length > 0){
+								metadata = JSON.parse(comment.json_metadata);
+							}
+							if(metadata && metadata.community == "dlike"){
+								totalDlikeComments +=1;    
+							}
+						});
+						$("#DlikeComments" + thisPermlink + thisAutor).html(totalDlikeComments);
+					});
+				}
+
+				var adduserhtml = "";
+				if(c_username == "dlike") {
+					adduserhtml += '<a class="userstatus_icon'+$post.permlink +$post.author +' showcursor" onclick="return openuser_popup(this)" data-permlink="' + $post.permlink + '" data-author="' + $post.author + '" data-category="' + category + '"><i class="fa fa-check-square" class="user_status'+$post.permlink +$post.author +'"></i></a>';
+				}
+	
+		$(catcontent).append('<div class="col-lg-4 col-md-6 postsMainDiv mainDiv'+ currentLikesDivElement +'" postLikes="0" postNumber="'+ currentPostNumber +'">\n' +
+					'\n' +
+					'<article class="post-style-two">\n' +
+					'\n' +
+					'<div class="post-contnet-wrap-top">\n' +
+					'\n' +
+					'<div class="post-footer">\n' +
+					'\n' +
+					'<div class="post-author-block">\n' +
+					'\n' +
+					'<div class="author-thumb"><a href="#"><img src="https://steemitimages.com/u/' + $post.author + '/avatar" alt="img" class="img-responsive"></a></div>\n' +
+					'\n' +
+					'<div class="author-info">\n' +
+					'\n' +
+					'<h5><a href="#">' + $post.author + '</a><div class="time">' + activeDate + '</div></h5>\n' +
+					'\n' +    
+					'</div>\n' +
+					'\n' + 
+					'</div>\n' +
+					'\n' +
+					'<div class="post-comments"><span class="post-meta">' + category + '</span></div>\n' +
+					'\n' +
+					'</div>\n' +
+					'\n' +
+					'</div>\n' + 
+					'\n' +
+					'<div class="post-thumb"><a class="post_detail" data-toggle="modal" data-target="#postModal" data-permlink="' + $post.permlink + '" data-author="' + $post.author + '">' + thumbnail + '</a></div>\n' + 
+					'\n' +
+					'<div class="post-contnet-wrap">\n' +
+					'\n' +
+					'<div class="row d-flex justify-content-center hov-it"><div class="hov-item"><img src="./images/post/dlike-hover.png" alt="img" class="img-responsive"><span class="hov_me" data-toggle="modal" data-target="" data-permlink="' + $post.permlink + '" data-author="' + $post.author + '"><div class="hov-txt"><h5><span id="hov-num" class="commentsDiv' + currentLikesDivElement + '"></span></h5></div></span></div></div>\n' +
+                    '\n' +
+					'<h4 class="post-title"><a href="' + exturl + '" target="_blank">' + $post.title + '</a></h4>\n' +
+					'\n' +
+					'<p class="post-entry post-tags">' + metatags + '</p>\n' +
+					'\n' +
+					'<div class="post-footer">\n' +
+					'<div class="post-author-block">\n' +
+					'<div class="author-info"><i class="fas fa-dollar-sign"></i><span>&nbsp;' + $post.pending_payout_value.substr(0, 4) + '</span> | <i class="fas fa-comments"></i>&nbsp;<span id="DlikeComments'+$post.permlink +$post.author +'">0</span></div>\n' +
+					'</div>\n' +
+					'<div class="post-comments"><a class="upvoting" data-toggle="modal" data-target="#upvoteModal" data-permlink="' + $post.permlink + '" data-author="' + $post.author + '"><i class="fas fa-chevron-circle-up" id="vote_icon'+$post.permlink +$post.author +'"></i></a><span>&nbsp; | ' + $post.active_votes.length + ' Votes</span></div>\n' +
+					'</div>\n' +
+					'</div>\n' +
+				'</article></div>');
+
+				getTotalLikes($post.author,$post.permlink, currentLikesDivElement);
+
+        		let author = $post.author;
+        		let permlink = $post.permlink;				
+
+    		//check if voted
+    		steem.api.getActiveVotes($post.author, $post.permlink, function(err, result) {
+                //console.log(result);
+                    if(result === Array) {
+                    	var voterList = result;
+                   	} else {
+                       	var voterList = [];
+                    }
+                    if(!(voterList === Array)) {
+                       	voterList = [];
+                    }
+                    var voterList = result;
+                for (let j = 0; j < voterList.length; j++) {
+                	if (voterList[j].voter == username) { 
+                		$("#vote_icon" + permlink + author).css("color", "RED"); 
+                		$('#vote_icon' + permlink + author).click(function(){return false;});
+                		$('#vote_icon' + permlink + author).hover(function() {toastr.error('hmm... Already Upvoted');})
+                	}
+                }                        
+    		});
+    	}
+		});
+
+</script>
