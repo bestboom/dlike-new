3d4dc0b416fe0cb413aa72f8fb2f8ef685944561
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-06 18:23:46 +0530

    Update content.php

--- public/adminage/content.php
+++ public/adminage/content.php
@@ -23,26 +23,32 @@
                 </div>
 
                 <div class="col-md-4">
-                    <div class="support-users-block">
+                    <div class="support-category-block">
+                        <a href="users.php">
                         <i class="fa fa-user" aria-hidden="true"></i>
 
                         <h4>Users</h4>
+                        </a>
                     </div>
                 </div>
 
                 <div class="col-md-4">
-                    <div class="support-post-block">
+                    <div class="support-category-block">
+                        <a href="posts.php">
                         <i class="fa fa-sticky-note-o" aria-hidden="true"></i>
 
                         <h4>Posts</h4>
+                        </a>
                     </div>
                 </div>
 
                 <div class="col-md-4">
-                    <div class="support-fpost-block">
+                    <div class="support-category-block">
+                        <a href="featuredposts.php">
                         <i class="fa fa-sticky-note-o" aria-hidden="true"></i>
 
                         <h4>Featured Posts</h4>
+                        </a>
                     </div>
                 </div>
             </div>
