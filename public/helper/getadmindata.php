d6350a9468291350af42c18e20be818359baded7
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-06 23:14:24 +0530

    Update getadmindata.php

--- public/helper/getadmindata.php
+++ public/helper/getadmindata.php
@@ -1,1 +1,22 @@
+<?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(0);
+  require '../includes/config.php';
+	
+  if(isset($_POST['data']) && $_POST['data'] == "users"){
+    $table = "steemposts"; 
+  }
+  $sql = "SELECT * FROM $table";
+	$result = $conn->query($sql);
 
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$strReturn['html_data'][] = $row;
+		}
+    $strReturn['status'] = 'OK';
+	} else {
+		$strReturn['status'] = 'error';
+	}
+  	echo json_encode($strReturn);die;
+?>
