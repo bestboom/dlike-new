894de74cdd812c5b529397e028251b6723fe2148
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-06 23:28:03 +0530

    Update getadmindata.php

--- public/helper/getadmindata.php
+++ public/helper/getadmindata.php
@@ -7,14 +7,18 @@
   if(isset($_POST['data']) && $_POST['data'] == "users"){
     $table = "steemposts"; 
   }
-  $sql = "SELECT * FROM $table";
+  	$sql = "SELECT * FROM $table";
 	$result = $conn->query($sql);
 
 	if ($result->num_rows > 0) {
 		while($row = $result->fetch_assoc()) {
-			$strReturn['html_data'][] = $row;
+			if(isset($_POST['data']) && $_POST['data'] == "users"){
+				$dataset['username'] = $row['username'];
+				$dataset['status'] = $row['status'];
+				$strReturn['html_data'][] = $dataset;
+			}
 		}
-    $strReturn['status'] = 'OK';
+    		$strReturn['status'] = 'OK';
 	} else {
 		$strReturn['status'] = 'error';
 	}
