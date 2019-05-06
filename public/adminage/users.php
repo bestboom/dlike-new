9a67f42955113b5726ebac92d535d2a9b6d7db1e
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-07 03:31:27 +0530

    Update users.php

--- public/adminage/users.php
+++ public/adminage/users.php
@@ -98,7 +98,7 @@ $(document).ready(function(){
 					result_html += '<tr><td>'+result_data[i]['username']+'</td><td>'+result_data[i]['status']+'</td><td><a href="javascript:" onclick="return openuser_popup(this)" class="btn btn-small btn-primary" data-author="'+result_data[i]['username']+'" data-status="'+result_data[i]['status']+'" >'+action_var+'</a></td></tr>';
 				}
 
-				result_html += '</tbody></table><script>$(/''#user_table/'').DataTable({});</script>';
+				result_html += "</tbody></table><script>$('#user_table').DataTable({});</script>";
 				$("#show_results").html(result_html);
 			}
 		}
