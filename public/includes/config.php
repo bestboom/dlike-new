cfd719ca55156558e757db60603d98eb0c494155
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-06 12:01:14 +0530

    Update config.php

--- public/includes/config.php
+++ public/includes/config.php
@@ -5,9 +5,9 @@ $privkey=getenv('privkey');
 $hashkey=getenv('hashkey');
 
 $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
-echo $server = $url["host"]; 
-$username = $url["user"];
-$password = $url["pass"];
+$server = $url["host"]; 
+echo $username = $url["user"]; echo "<br>";
+echo $password = $url["pass"];
 $db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
