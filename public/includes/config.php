eb1ac7ae88db2ebc6c980bdcfcdd640b1fad216e
bestboom <certseek@gmail.com>
2019-05-06 11:40:45 +0500

    Update config.php

--- public/includes/config.php
+++ public/includes/config.php
@@ -8,7 +8,7 @@ $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 $server = $url["host"]; 
 $username = $url["user"]; 
 $password = $url["pass"];
-echo $db = substr($url["path"], 1);
+$db = substr($url["path"], 1);
 
 $conn = new mysqli($server, $username, $password, $db);
 
