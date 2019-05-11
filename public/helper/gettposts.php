771aed5974c651c9e20def42fa3464ad48c26fa7
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-11 13:08:27 +0530

    Update gettposts.php

--- public/helper/gettposts.php
+++ public/helper/gettposts.php
@@ -38,7 +38,7 @@ if(isset($_POST['tagname']) && $_POST['tagname'] != "") {
 if(isset($_REQUEST['catname']) && $_REQUEST['catname'] != "") {
 
 	
-	$sql1 = 'SELECT * FROM steemposts WHERE json_metadata RLIKE ''\"category":"[[:<:]]'.$_REQUEST['catname'].'[[:>:]]"'\' order by created_at DESC';
+	echo $sql1 = 'SELECT * FROM steemposts WHERE json_metadata RLIKE ''\"category":"[[:<:]]'.$_REQUEST['catname'].'[[:>:]]"'\' order by created_at DESC';die;
 	$result1 = $conn->query($sql1);
 	if ($result1->num_rows > 0) {
 		while($row1 = $result1->fetch_assoc()) {
