21f94c08c369a02ff9e26d45d88a4bdb66e9c06e
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-11 17:19:43 +0530

    Update save_admin_data.php

--- public/helper/save_admin_data.php
+++ public/helper/save_admin_data.php
@@ -1,18 +1,10 @@
 <?php
         ini_set('display_errors', 1);
         ini_set('display_startup_errors', 1);
-        error_reporting(E_ALL);
+        error_reporting(0);
         require '../includes/config.php';
         if(isset($_POST['tag']) && $_POST['tag'] == "events"){
-            if(!is_dir("/Uploads/events/")) {
-                mkdir("/Uploads/events/", 0777);
-            }
-                
-
-            // Move the uploaded file
-            move_uploaded_file($_FILES["upload"]["tmp_name"], "/images/others/". "e_".$_FILES["upload"]["name"]);
-die;
-            $file_name = "Uploads/events/". $_FILES["upload"]["name"];
+            $file_name = $_POST['upload'];
             $title = $_POST['title'];
             $tags = $_POST['tags'];
             
