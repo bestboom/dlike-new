7cc72676d95742f67f22ac035ca0e3bcad07d71c
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-11 15:06:50 +0530

    Update gettposts.php

--- public/helper/gettposts.php
+++ public/helper/gettposts.php
@@ -1,225 +1,84 @@
-<?php include('template/header2.php');  ?>
-    <?php if($_COOKIE['username'] == ''){ ?>
-        <div class="container">
-            <div class="row home-banner">
-                <div class="col-md-3 main main_offer">
-                    <div class="daily">
-                        <div class="daily_box"><p class="daily_start">10</p><p class="daily_mid">STEEM</p><p class="daily_end">Daily</p></div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="banner-content">
-                        <h2>Dlike - Where You Get Liked</h2>
-                        <p>Share What you like with community <br>Get rewarded if community likes your shares</p>
-                        <h5>Daily Top Recommended Post (most dlikes) is Rewarded By Dlike</h5>
-                        <h5>Anyone can recommend a post for which steem account is not compulsory</h5>
-                        <!-- <h2 class="annc">Starting 11th March</h2> -->
-                    </div>
-                </div>
-                <div class="col-md-3 main_offer">
-                    <div class="daily">
-                        <div class="daily_box"><p class="daily_start">2500</p><p class="daily_mid">DLike Tokens</p><p class="daily_end">Daily</p></div>
-                    </div>
-                </div>
-             </div>
-         </div>
-            <? } else { ?>
-                <div class="banner-content home-connect">
-                    <div class="news-headline-block">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-1 col-md-2">
-                                <i class="fas fa-volume-up vol"></i>
-                            </div>
-                            <div class="col-lg-11 col-md-10">
-                                <div class="news-headlines-block">
-                                    <div class="news-headlines-slider ticker">
-                                        <ul>
-                                            <li>Dlike will soon start token sale for steemians with huge bonus.</li>
-                                        </ul>
-                                    </div><!-- news-headlines-slider -->
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-            <? } ?>
-            </div>
-        </div>
-    </div><!-- sub-header -->
-    
-    
-    <div class="latest-post-section">
-	<div class="container">
-	    <div class="row  align-items-center h-100 myloader" style="margin-bottom:30px;">
-                <div class="row col-md-3 justify-content-center">
-                        <h4 class="lab_post"><?php echo $_GET['tag'];?></h4>
-                </div>
-                <div class="col-md-9 lay">&nbsp;</div>
-            </div>
-	    <div id="loader">Loading</div>
-	    <div class="row" id="contentposts">
-	    </div>
-	</div>
-    </div>
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(0);
+require '../includes/config.php';
 
-<div class="modal fade" id="upvoteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-sm" role="document">
-        <div class="modal-content mybody">
-            <?php include('template/modals/upvotemodal.php'); ?>
-        </div>
-    </div>
-</div> 
-<?php include('template/footer2.php'); ?>
-<style>
-.showcursor{cursor:pointer;}
-</style>
-<script>
+function timeago($date) {
+   $timestamp = strtotime($date);	
+   
+   $strTime = array("second", "minute", "hour", "day", "month", "year");
+   $length = array("60","60","24","30","12","10");
 
-    	$(document).ready(function(){
-			
-		$("#loader").show();
-		
-      
-		  var tagname = '<?php echo $_GET['tag'];?>';
+   $currentTime = time();
+   if($currentTime >= $timestamp) {
+		$diff     = time()- $timestamp;
+		for($i = 0; $diff >= $length[$i] && $i < count($length)-1; $i++) {
+		$diff = $diff / $length[$i];
+		}
+
+		$diff = round($diff);
+		$addif = "";
+		if($diff>1) {
+			$addif = "s";
+		}
+		return $diff . " " . $strTime[$i] . $addif." ago ";
+   }
+}
 	
-			$.ajax({
-			type: "POST",
-			url: '/helper/gettposts.php',
-			data:{'tagname':tagname},
-			dataType: 'json',
-			success: function(response) {
-			    if(response.status == "OK") {
-				var resulthtml = response.data_row;
-				
-				//$(".total_posts").html(resulthtml.length+' posts found, <a style="color: #1652f0;" href="/tags/'+tagname+'">#'+tagname+'</a>');
-				for(i=0;i<resulthtml.length;i++) {
-
-				    var responsehtml = '';
-				    var currentPostNumber = i;
-				    var currentLikesDivElement = 'postLike_' + i;
-
-				   var timstamp = resulthtml[i]['created_at'];
-				   var permlink = resulthtml[i]['permlink'];
-				   var username = resulthtml[i]['username'];
-
-				   responsehtml = '<div class="col-lg-4 col-md-6 postsMainDiv mainDiv '+currentLikesDivElement+'" postLikes="0" postNumber="'+currentPostNumber+'" id="article_'+permlink+'">\n' +
-					    '\n' +
-					    '<article class="post-style-two">\n' +
-					    '\n' +
-					    '<div class="post-contnet-wrap-top">\n' +
-					    '\n' +
-					    '<div class="post-footer">\n' +
-					    '\n' +
-					    '<div class="post-author-block">\n' +
-					    '\n' +
-					    '<div class="author-thumb"><a href="#"><img src="https://steemitimages.com/u/' + username + '/avatar" alt="img" class="img-responsive"></a></div>\n' +
-					    '\n' +
-					    '<div class="author-info">\n' +
-					    '\n' +
-					    '<h5><a href="#">' + username + '</a><div class="time" id="articletime_'+permlink+'">'+timstamp+'</div></h5>\n' +
-					    '\n' +    
-					    '</div>\n' +
-					    '\n' + 
-					    '</div>\n' +
-					    '\n' +
-					    '<div class="post-comments"><span class="post-meta"></span></div>\n' +
-					    '\n' +
-					    '</div>\n' +
-					    '\n' +
-					    '</div>\n' + 
-					    '\n' +
-					    '<div class="post-thumb"><a class="post_detail" data-toggle="modal" data-target="#postModal" data-permlink="' + permlink + '" data-author="' + username + '"></a></div>\n' + 
-					    '\n' +
-					    '<div class="post-contnet-wrap">\n' +
-					    '\n' +
-					    '<h4 class="post-title"><a href="" target="_blank"></a></h4>\n' +
-					    '\n' +
-					    '<p class="post-entry post-tags"></p>\n' +
-					    '\n' +
-					    '<div class="post-footer">\n' +
-					    '<div class="post-author-block">\n' +
-					    '<div class="author-info"><i class="fas fa-dollar-sign"></i><span class="pending_payout_value"></span> | <i class="fas fa-comments"></i>&nbsp;<span id="DlikeComments'+permlink +username +'">0</span></div>\n' +
-					    '</div>\n' +
-					    '<div class="post-comments"><a class="upvoting" data-toggle="modal" data-target="#upvoteModal" data-permlink="' + permlink + '" data-author="' + username + '"><i class="fas fa-chevron-circle-up" id="vote_icon'+permlink +username +'"></i></a><span class="active_votes">&nbsp; |  Votes</span></div>\n' +
-					    '</div>\n' +
-					    '</div>\n' +
-				    '</article></div>';
-
-
-
-				   $("#contentposts").append(responsehtml);
-
-				    steem.api.getContent(username , permlink, function(err, res) {
-
-					let metadata = JSON.parse(res.json_metadata);
-					let img = new Image();
-					if (typeof metadata.image === "string"){
-						img.src = metadata.image.replace("?","?");
-					} else {
-						img.src = metadata.image[0];
-					}
-					json_metadata = metadata;
-					let category = metadata.category;
-					if (category === undefined) { category = "dlike"; } else {category = metadata.category;};
-					let steemTags = metadata.tags;
-					let dlikeTags = steemTags.slice(2);
-					let posttags = dlikeTags.map(function (meta) { if (meta) return '<a href="/tags/'+ meta +'">' + meta + ' </a>' });
-					let post_description = metadata.body;
-
+if(isset($_POST['tagname']) && $_POST['tagname'] != "") {
 	
-					let title = res.title;
-					//let created = res.created;
-					let created = timstamp;
-					let created_time = moment.utc(created + "Z", 'YYYY-MM-DD  h:mm:ss').fromNow();
-					let author = res.author;
-					let auth_img = "https://steemitimages.com/u/" + author + "/avatar";
-					
-					 var username = author;
-				    var created_at = created;
-				    var permlink = res.permlink;
-				    var metatags =  posttags;
-				    var exturl =   metadata.url;;
+	  
+    	$sql = "SELECT postid FROM posttags where tagname = '".$_POST['tagname']."'";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$postid = $row['postid'];
+		}
+		$sql1 = "SELECT * FROM steemposts where id IN (".$postid.") order by created_at DESC";
+		$result1 = $conn->query($sql1);
+		if ($result1->num_rows > 0) {
+			while($row1 = $result1->fetch_assoc()) {
+			    
+				$json_metadata = json_decode($row1['json_metadata'],true);
+				$data['username'] = $row1['username'];
+				$data['permlink'] = $row1['permlink'];
+				$data['created_at'] = timeago($row1['created_at']);
 
-				    var thumbnail = '<img src="' + metadata.image + '" alt="' + title + '" class="card-img-top img-fluid">';
-
-
-					    
-				   
-				    
-
-				    $('#article_'+permlink+' span.post-meta').html(category);
-				    $('#article_'+permlink+' a.post_detail').html(thumbnail);
-				    $('#article_'+permlink+' h4.post-title a').attr('href',exturl);
-				    $('#article_'+permlink+' h4.post-title a').html(title);
-				    $('#article_'+permlink+' p.post-tags').html(metatags);
-				    $('#article_'+permlink+' span.pending_payout_value').html(res.pending_payout_value.substr(0, 4));
-				    $('#article_'+permlink+' span.active_votes').html("&nbsp; | "+res.active_votes.length+" Votes");
-				    
-				    
-				    
-				    
-				    
-				    
-				    
-				});
-				   
-				    
-				    
-				    
-				}
-				$("#loader").hide();
-				$(".myloader").css('display','flex');
-				
-				
-			    }
-			    else {
-				$("#loader").hide();
-				$(".myloader").css('display','flex');
-				$("#contentposts").append("No posts found.");
-			    }
+				$strReturn['data_row'][] = $data;
 			}
-			});
+			$strReturn['status'] = 'OK';
+		}
+		
+		
+	} else {
+		$strReturn['status'] = 'error';
+	}
+  	echo json_encode($strReturn);die;
+}
 
-		});
+if(isset($_REQUEST['catname']) && $_REQUEST['catname'] != "") {
 
-</script>
+	
+	$sql1 = 'SELECT json_metadata,username,permlink,created_at FROM steemposts order by id DESC';
+	$result1 = $conn->query($sql1);
+	if ($result1->num_rows > 0) {
+		while($row1 = $result1->fetch_assoc()) {
+			
+			$json_metadata = json_decode($row1['json_metadata'],true);
+			if(strtolower($json_metadata['category']) == strtolower($_REQUEST['catname'])) {
+				$data['username'] = $row1['username'];
+				$data['permlink'] = $row1['permlink'];
+				$data['created_at'] = timeago($row1['created_at']);
+				$strReturn['data_row'][] = $data;
+			}
+		}
+		$strReturn['status'] = 'OK';
+	}
+	else {
+		$strReturn['status'] = 'error';
+	}
+  	echo json_encode($strReturn);die;
+
+}
+?>
