95dd944fde0ebf36563cf82acba378fd74c6af06
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-14 00:15:14 +0530

    Update ads.php

--- public/adminage/ads.php
+++ public/adminage/ads.php
@@ -10,24 +10,22 @@
 	  <div id="show_results"></div>
   </div>
 
-  <div class="modal fade" id="PostStatusModal" role="dialog">
+  <div class="modal fade" id="adStatusModal" role="dialog">
 	<div class="modal-dialog">
 	
 	  <!-- Modal content-->
 	  <div class="modal-content">
 		    <div class="modal-body text-center">
-			    <input type="hidden" id="p_username" />
-			    <input type="hidden" id="p_permlink" />
-			    <input type="hidden" id="p_category" />
-			    <p>What would you think about this post?</p>
-			    <select class="form-control" id="status_select">
-			    <option value="">Please select</option>
-			    <option value="Rejected">Rejected</option>
-			    <option value="Low Level">Low Level</option>
-			    <option value="High Level">High Level</option>
-			    </select>
+			    <input type="hidden" id="a_id" />
+			    <label for="Tags" class="">Ad HTML:</label>
+			    <textarea class="form-control" id="ad_html" placeholder="Enter html" name="ad_html"></textarea>
 			    <br>
-			    <p><input type="button" id="savepoststatus" class="btn btn-primary" value="Save it"/></p>
+			    <label for="Title">Status:</label>
+			    <input type="radio" name="status" id="status_1" value="1"> Active
+    
+			    <input type="radio" name="status" id="status_0"value="0"> Inactive
+    
+			    <p><input type="button" id="saveadstatus" class="btn btn-primary" value="Save it"/></p>
 				    
 		    </div>
 		    <div class="modal-footer">
@@ -45,111 +43,69 @@
 <script src="//rawgit.com/carlo/jquery-base64/master/jquery.base64.min.js"></script>
 <style>table.dataTable thead th{    border-bottom: 1px solid #dee2e6;}.dataTables_length{display:none !important;}.dataTables_wrapper .dataTables_filter input{    display: block;width: auto;padding: .375rem .75rem;font-size: 1rem;line-height: 1.5;color: #495057;background-color: #fff;background-clip: padding-box;border: 1px solid #ced4da;border-radius: .25rem;transition: border-color .15s ease-in-out,box-shadow .15s ease-in-out;}table.dataTable.no-footer{    border-bottom: 1px solid #dee2e6;}</style>
 <script>
-	function openevent_popup(self){
-		var permlink = $(self).data('permlink');
-		var author = $(self).data('author');
-		var category = $(self).data('category');
+	function openad_popup(self){
+		var ad_html = $(self).data('adhtml');
 		var status = $(self).data('status');
+		var a_id =  $(self).data('id');
 		
-		$("#p_username").val(author);
-		$("#p_permlink").val(permlink);
-		$("#p_category").val(category);
-		$("#status_select").val(status);
+		$("#a_id").val(a_id);
+		$("#ad_html").val(ad_html);
+		$("#status_"+status).prop("checked", true);
 		
-		$("#PostStatusModal").modal('show');
+		$("#adStatusModal").modal('show');
     }
     
 $(document).ready(function(){
 	$("#loader").show();
 	
-	var savepoststatus=$('#savepoststatus');
-	var events_show_status = $('#events_show_status');
-	events_show_status.click(function(){
-		$("#loader").show();
-		var option = $(this).data('option');
-		var setclass="bg-danger";
-		var r_class="bg-success";
-		var setoption="disable";
-		
-		$.ajax({
-		    type: "POST",
-		    url: '/helper/save_admin_data.php',
-		    data:{'tag':'settings','type':'events','option':option},
-		    dataType: 'json',
-		    success: function(response) {
-				$("#loader").hide();
-			if(response.status == "OK") {
-				
-			    toastr.success(response.message);
-			    if(option == "disable") {
-					setclass = "bg-success";
-					setoption="enable";
-					r_class = "bg-danger";
-					settoption = "Enable";
-				}
-				else {
-					setclass = "bg-danger";
-					setoption="disable";
-					r_class = "bg-success";
-					settoption = "Disable";
-				}
-			    events_show_status.addClass(setclass);
-			    events_show_status.removeClass(r_class);
-			    events_show_status.data('option',setoption);
-			    events_show_status.text(settoption);
-			    
-				
-			}
-			else {
-			    toastr.error(response.message);
-			    return false;
-			}
-		    },
-		    error: function() {
-				$("#loader").hide();
-				toastr.error('Error occured');
-			    return false;
-		    }
-	    });
-	});
-	savepoststatus.click(function(){
-		$("#loader").show();
-	    var p_username = $("#p_username").val();
-	    var p_permlink = $("#p_permlink").val();
-	    var p_category = $("#p_category").val();
-	    var p_status = $("#status_select").val();
-	    if(p_status == ""){
+	var saveadstatus=$('#savepoststatus');
+
+	
+	saveadstatus.click(function(){
+	    $("#loader").show();
+	    var a_ad_html = $("#ad_html").val();
+	    var a_status = $("#status").val();
+	    var a_id = $("#a_id").val();
+	    if(a_status == ""){
 		alert("Please select status.");
 		return false;
 	    }
 	    
 	    $.ajax({
 		    type: "POST",
-		    url: '/helper/poststatus.php',
-		    data:{'p_username':p_username,'p_permlink':p_permlink,'p_category':p_category,'p_status':p_status},
+		    url: '/helper/save_admin_data.php',
+		    data:{'tag':'ads','a_id':a_id,'a_status':a_status,'a_ad_html':a_ad_html},
 		    dataType: 'json',
 		    success: function(response) {
-				$("#loader").hide();
+			$("#loader").hide();
 			if(response.status == "OK") {
 			    toastr.success(response.message);
-			    $('#PostStatusModal').modal('hide');
+			    $('#adStatusModal').modal('hide');
 			    
-			    var all_status = p_status;
-			    $("#post_"+p_username).text('Edit Status');
-			    $("#post_"+p_username).data('status',all_status);
-			    $("#post_s_"+p_username).html(all_status);
+
+			    var status = "Inactive";
+			    if(a_status == "1"){
+				status = "Active";
+			    }
+			    
+
+			    $("#status_"+a_id).html(status);
+			    $("#ad_html_"+a_id).html(a_ad_html);
+			    $("#ad_"+a_id).data('adhtml',a_ad_html);
+			    $("#ad_"+a_id).data('status',a_status);
+			    
 			    
 				
 			}
 			else {
-			    $('#PostStatusModal').modal('hide');
+			    $('#adStatusModal').modal('hide');
 			    toastr.error(response.message);
 			    return false;
 			}
 		    },
 		    error: function() {
 				$("#loader").hide();
-			$('#PostStatusModal').modal('hide');
+			$('#adStatusModal').modal('hide');
 			 toastr.error('Error occured');
 			    return false;
 		    }
@@ -173,7 +129,12 @@ $(document).ready(function(){
 				for(i=0;i<result_data.length;i++){
 					ad_html = $.base64.decode(result_data[i]['ad_html']);
 					var action_var = "Edit";
-					result_html += '<tr><td>'+ad_html+'</td><td>'+result_data[i]['status']+'</td><td><a href="javascript:"  id="ad_'+result_data[i]['id']+'" onclick="return openad_popup(this)" class="btn btn-small btn-primary"  data-adhtml="'+result_data[i]['ad_html']+'" data-status="'+result_data[i]['status']+'" >'+action_var+'</a></td></tr>';
+					var status = "Inactive";
+					if(result_data[i]['status'] == "1"){
+					    status = "Active";
+					}
+					
+					result_html += '<tr><td id="ad_html_'+result_data[i]['id']+'">'+ad_html+'</td><td id="status_'+result_data[i]['id']+'">'+status+'</td><td><a href="javascript:"  id="ad_'+result_data[i]['id']+'" onclick="return openad_popup(this)" class="btn btn-small btn-primary"  data-adhtml="'+result_data[i]['ad_html']+'" data-id="'+result_data[i]['id']+'" data-status="'+result_data[i]['status']+'" >'+action_var+'</a></td></tr>';
 				}
 				result_html += '</tbody></table>';			    
 				$("#show_results").html(result_html);
