e5db708f47abf5aa8f3bc8c1b89b0bce420b8c58
Chirag Zunzvadia <jaihind530@gmail.com>
2019-05-13 16:08:50 +0530

    Update index3.php

--- public/index3.php
+++ public/index3.php
@@ -47,8 +47,25 @@ if ($result_s->num_rows > 0) {
     }
 }
 
-
-
+function get_client_ip() {
+    $ipaddress = '';
+    if (getenv('HTTP_CLIENT_IP'))
+        $ipaddress = getenv('HTTP_CLIENT_IP');
+    else if(getenv('HTTP_X_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_X_FORWARDED_FOR');
+    else if(getenv('HTTP_X_FORWARDED'))
+        $ipaddress = getenv('HTTP_X_FORWARDED');
+    else if(getenv('HTTP_FORWARDED_FOR'))
+        $ipaddress = getenv('HTTP_FORWARDED_FOR');
+    else if(getenv('HTTP_FORWARDED'))
+       $ipaddress = getenv('HTTP_FORWARDED');
+    else if(getenv('REMOTE_ADDR'))
+        $ipaddress = getenv('REMOTE_ADDR');
+    else
+        $ipaddress = 'UNKNOWN';
+    return $ipaddress;
+}
+$ip = get_client_ip();
 $current_city = file_get_contents('https://ipapi.co/' . $ip . '/city/');
             
 
