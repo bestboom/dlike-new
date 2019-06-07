a612249d6db1d77e9b2a56f097135994b924e09d
bestboom <certseek@gmail.com>
2019-06-07 22:57:06 +0500

    Update latest.php

--- public/latest.php
+++ public/latest.php
@@ -35,8 +35,8 @@ $sql1 = "SELECT json_metadata,username,permlink,title FROM steemposts ORDER BY i
 		while($row1 = $result1->fetch_assoc()) {
 			$json_metadata = json_decode($row1['json_metadata'],true);
 
-				echo $imgsrc = $json_metadata['image'];
-				echo $title = $row1['title']."<br>";
+				$imgsrc = $json_metadata['image'];
+				echo $title = "<img src="$imgsrc">.$row1['title']."<br>";
 				$category = $json_metadata['category'];
 				
 				$username = $row1['username']."<br>";
