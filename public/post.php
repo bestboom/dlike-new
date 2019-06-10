31bf03ed2e175dd03dcd5749f02358973725062e
bestboom <certseek@gmail.com>
2019-06-10 12:21:16 +0500

    Update post.php

--- public/post.php
+++ public/post.php
@@ -12,6 +12,44 @@ $auth = str_replace('@', '', $user);
                         <div class="blog-details-wrapper">
                             <div class="single-post-block">
                                 <h3 class="post-title"><a href="#"><span class="mod-title"></span></a></h3>
+
+            <div class="details-post-meta-block">
+                <div class="container">
+                    <div class="row">
+                        <div class="col">
+                            <div class="details-post-meta-block-wrap">
+                                <div class="post-author-block">
+                                    <div class="author-thumb">
+                                        <a href="#"><img src="" onerror="this.src='./images/post/authors/8.png'" alt="img" class="img-responsive mod-authThumb"></a>
+                                    </div>
+                                    <div class="author-info">
+                                        <h5> <a href="#" class="mod-auth"></a></h5>
+                                    </div>
+                                </div>
+                                <div class="post-tag-block">
+                                    <h5>Post Tag</h5>
+                                    <div class="tags mod-tags">
+                                    </div>
+                                </div><!-- post-tag-block -->
+                                <div class="post-share-block">
+                                    <h5>Share this</h5>
+                                    <ul class="social-share-list">
+                                        <li><a href="#" class="faceboox"><i class="fab fa-facebook-f"></i></a></li>
+                                        <li><a href="#" class="twitter"><i class="fab fa-twitter"></i></a></li>
+                                        <li><a href="#" class="google-plus"> <i class="fab fa-google-plus-g"></i></a></li>
+                                        <li><a href="#" class="linkdin"> <i class="fab fa-linkedin-in"></i></a></li>
+                                        <li> <a href="#" class="pinterest"><i class="fab fa-pinterest"></i></a></li>
+                                        <li><a href="#" class="instagram"><i class="fab fa-instagram"></i></a></li>
+                                    </ul>
+                                </div><!-- post-share-block -->
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+
+
                                 <div class="post-thumb-block">
                                     <img src="" onerror="this.src='/images/post/8.png'" alt="img" class="img-responsive mod-thumb">
                                 </div>
