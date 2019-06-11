59879e42d0b65e4e4e28495749bc79ac970f1140
bestboom <certseek@gmail.com>
2019-06-11 09:20:37 +0500

    Update header5.php

--- public/template/header5.php
+++ public/template/header5.php
@@ -6,14 +6,14 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Dlike is a blockchain based dApp where you share links from your own blog articles or any useful link that is informative for community and get rewarded if community likes your links with steem upvotes.">
-  <link rel='favicon icon' type=image/x-icon href=images/favicon.ico />
-  <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-  <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
-  <link rel="stylesheet" href="/assets/css/fontawesome-all.min.css">
-  <link rel="stylesheet" href="/assets/css/themify-icons.css">
-  <link rel="stylesheet" href="/assets/css/slick.css">
-  <link rel="stylesheet" href="/css/style.css">
-  <link rel="stylesheet" href="/css/responsive.css">
+    <link rel='favicon icon' type=image/x-icon href=images/favicon.ico />
+    <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
+    <link rel="stylesheet" href="/assets/css/fontawesome-all.min.css">
+    <link rel="stylesheet" href="/assets/css/themify-icons.css">
+    <link rel="stylesheet" href="/assets/css/slick.css">
+    <link rel="stylesheet" href="/css/style3.css">
+    <link rel="stylesheet" href="/css/responsive.css">
 </head>
 <body>
 <div id="mySidenav" class="sidenav" style="    z-index: 222222;">
