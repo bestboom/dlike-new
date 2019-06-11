360201ee194324d47000210119d41f02df538c5e
bestboom <certseek@gmail.com>
2019-06-11 09:17:12 +0500

    up

--- public/template/header5.php
+++ public/template/header5.php
@@ -23,11 +23,11 @@
     <a href="/docs/dlike-paper.pdf">Whitepaper</a>
 
 </div>
-<div class="">
+<div class="banner-block">
     <nav class="navbar main-nav navbar-expand-lg">
         <div class="container">
             <a class="navbar-brand" href="/">
-                <img class="navbar-logo" src="images/logo.png" alt="Dlike"/>
+                <img class="navbar-logo" src="/images/logo.png" alt="Dlike"/>
             </a>
             <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
