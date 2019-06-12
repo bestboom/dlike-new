d664c58814205fe9f1fa05c4e1cc6f9a0dad2036
bestboom <certseek@gmail.com>
2019-06-12 10:56:22 +0500

    Update index6.php

--- public/index6.php
+++ public/index6.php
@@ -1,1 +1,19 @@
-<script async src="https://appsha1.cointraffic.io//js/?wkey=hLMQzDKQgG"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+    <script src="/assets/js/popper.min.js"></script>
+    <script src="/assets/js/bootstrap.min.js"></script>
+    <script src="/assets/js/slick.min.js"></script>
+    <script src="/assets/js/jquery.peity.min.js"></script>
+    <script src="/assets/js/jquery.slimscroll.min.js"></script>
+    <script src="/js/custom.js"></script>
+    <script src="/js/mint.js"></script>
+    <script src="https://cdn.steemjs.com/lib/latest/steem.min.js"></script>
+    <script src="https://momentjs.com/downloads/moment.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/steemconnect@latest"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.js"></script>
+    <script src="/js/posts.js"></script>
+    <script src="/js/steemconnect.js"></script>
+    <script src="/js/toaster.js"></script>
+    <script src="./js/jquery.ticker.js"></script>
+    <script type="text/javascript"> $(function (){ $('.ticker').ticker(); });</script>
+    <script async src="https://appsha1.cointraffic.io//js/?wkey=hLMQzDKQgG"></script>
