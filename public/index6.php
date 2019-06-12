e24ddae45e5c712feb5d9834c6015d596d5daff1
bestboom <certseek@gmail.com>
2019-06-12 11:18:27 +0500

    Update index6.php

--- public/index6.php
+++ public/index6.php
@@ -4,11 +4,11 @@
 <head>
   <title>Dlike - Share To Get Rewarded</title>
   <meta charset="UTF-8">
-
+  <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="description" content="Dlike is a blockchain based dApp where you share links from your own blog articles or any useful link that is informative for community and get rewarded if community likes your links with steem upvotes.">
   <link rel='favicon icon' type=image/x-icon href=images/favicon.ico />
   <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-
+  <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
   <link rel="stylesheet" href="/assets/css/fontawesome-all.min.css">
   <link rel="stylesheet" href="/assets/css/themify-icons.css">
   <link rel="stylesheet" href="/assets/css/slick.css">
@@ -37,7 +37,7 @@
                 </div>
             </div>
         </nav><!-- main-nav-block -->
-
+    </div>    
 
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
     <script src="/assets/js/popper.min.js"></script>
