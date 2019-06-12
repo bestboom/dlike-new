3f491671dcee46ca07d6739ca69a9e1f386330b7
bestboom <certseek@gmail.com>
2019-06-12 11:36:33 +0500

    Update index6.php

--- public/index6.php
+++ public/index6.php
@@ -8,10 +8,8 @@
   <meta name="description" content="Dlike is a blockchain based dApp where you share links from your own blog articles or any useful link that is informative for community and get rewarded if community likes your links with steem upvotes.">
   <link rel='favicon icon' type=image/x-icon href=images/favicon.ico />
 
+  <link rel="stylesheet" href="/assets/css/bootstrap.min.css">
 
-  <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
-  <link rel="stylesheet" href="/assets/css/fontawesome-all.min.css">
-  <link rel="stylesheet" href="/assets/css/themify-icons.css">
   <link rel="stylesheet" href="/assets/css/slick.css">
   <link rel="stylesheet" href="/css/style.css">
   <link rel="stylesheet" href="/css/responsive.css">
