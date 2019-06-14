8376e2450599db901bd60d37159a970f3663575d
bestboom <certseek@gmail.com>
2019-06-14 17:20:25 +0500

    Update post.php

--- public/post.php
+++ public/post.php
@@ -18,6 +18,16 @@ function getUserIpAddr(){
 }
 
 echo 'User Real IP - '.getUserIpAddr();
+echo '<br>';
+
+if (!empty($_SERVER['HTTP_CLIENT_IP'])){
+  $ip=$_SERVER['HTTP_CLIENT_IP'];
+}elseif (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])){
+  $ip=$_SERVER['HTTP_X_FORWARDED_FOR'];
+}else{
+  $ip=$_SERVER['REMOTE_ADDR'];
+}
+echo $ip;
 ?>
 </div>
         <div class="container" style="padding-top: 40px;">
