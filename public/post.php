2d17ca8d878c599bedc53ea21757d101c5a6df4e
bestboom <certseek@gmail.com>
2019-06-14 16:27:13 +0500

    Update post.php

--- public/post.php
+++ public/post.php
@@ -4,7 +4,7 @@ $user = $_GET['user'];
 $auth = str_replace('@', '', $user);
 $sender =  $_COOKIE['username'];
 
-echo $myip = $_SERVER['REMOTE_ADDR'];
+echo $myip = $_SERVER['HTTP_CLIENT_IP']?$_SERVER['HTTP_CLIENT_IP']:($_SERVER['HTTP_X_FORWARDED_FOR']?$_SERVER['HTTP_X_FORWARDED_FOR']:$_SERVER['REMOTE_ADDR']);
 ?>
 </div>
         <div class="container" style="padding-top: 40px;">
