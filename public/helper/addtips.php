f032adddf2bb0a6a90e5b804a19a6168f56f32e7
bestboom <certseek@gmail.com>
2019-06-14 12:58:24 +0500

    Update addtips.php

--- public/helper/addtips.php
+++ public/helper/addtips.php
@@ -3,7 +3,7 @@
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 
-	require '/includes/config.php';
+	require '/./includes/config.php';
 
 	$saved_ip = $_COOKIE['usertoken'];
 	$receiver =  $_POST['tipauthor'];	
