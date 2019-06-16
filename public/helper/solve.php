8b9c178515d39e3f44bed56c592ede902629dfc7
bestboom <certseek@gmail.com>
2019-06-16 23:04:19 +0500

    Update solve.php

--- public/helper/solve.php
+++ public/helper/solve.php
@@ -42,45 +42,4 @@ if (isset($_POST["rec_author"]) && isset($_POST["rec_permlink"])){
                                     //} else {die('Some error');}
 };
 
-
-
-
-				$sqlm = "INSERT INTO MyLikes (username, stars, userip, author, permlink)
-						VALUES ('".$userval."', '".$rating."', '".$saved_ip."', '".$author."', '".$permlink."')";
-
-						if (mysqli_query($conn, $sqlm)) {
-
-			
-    								} else {
-    									/*echo "post not exists";*/
-    									$addPost = "INSERT INTO PostsLikes (author, permlink, likes, rating, lastUpdatedDate)
-													VALUES ('".$author."', '".$permlink."', '".$newLike ."', '".$rating."', '".date("Y-m-d h:m:s")."')";
-										$addPostQuery = $conn->query($addPost);
-									}
-
-    					echo '<div class="alert alert-success">Your Recomendation is Added</div>';
-    					echo '<script>document.getElementById("logsubmit").reset(); setTimeout(function(){location.reload();}, 1000);</script>';
-					} else {
-    				echo '<div class="alert alert-danger">There is some issue. Please Try Later!</div>';
-					}
-			
-		
-
-
-
 ?>
-
-
-
-        $checkPost = "SELECT * FROM PostsLikes WHERE author = '$author' and permlink = '$permlink'";
-                                $result = mysqli_query($conn, $checkPost);
-                                    if ($result->num_rows > 0) {
-                                        $row = $result->fetch_assoc();
-                                            $old_likes = $row['likes'];
-
-                                        $updatePost = "UPDATE PostsLikes SET likes = '$old_likes' + 1  WHERE author = '$author' and permlink = '$permlink'";
-                                        $updatePostQuery = $conn->query($updatePost);
-                                            if ($updatePostQuery === TRUE) {
-                                                //$addPost = "INSERT INTO MyLikes (username, stars, userip, author, permlink)
-                                                    //VALUES ('".$userval."', '".$rating."', '".$saved_ip."', '".$author."', '".$permlink."')";
-                                                    // $addPostQuery = $conn->query($addPost);
