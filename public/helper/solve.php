56f0ee8ec330189caf672b354d1157afe9377e80
bestboom <certseek@gmail.com>
2019-06-16 23:01:47 +0500

    Update solve.php

--- public/helper/solve.php
+++ public/helper/solve.php
@@ -17,6 +17,61 @@ if (isset($_POST["rec_author"]) && isset($_POST["rec_permlink"])){
 		$permlink =  $_POST['rec_permlink'];
 		$newLike = '1';
 
+
+		$sqlm = "INSERT INTO MyLikes (username, stars, userip, author, permlink)
+						VALUES ('".$userval."', '".$rating."', '".$saved_ip."', '".$author."', '".$permlink."')";
+
+						if (mysqli_query($conn, $sqlm)) {
+
+
+                                                                die(json_encode([
+                                                                'error' => false,
+                                                                'message' => 'Thankk You', 
+                                                                'data' => 'Recomnding'
+                                                                ]));
+
+                                            } else {
+                                                                die(json_encode([
+                                                                'error' => true,
+                                                                'message' => 'Sorry', 
+                                                                'data' => 'SOme ISSUE'
+                                                                ]));
+                                         
+                                            }
+                                       // }
+                                    //} else {die('Some error');}
+};
+
+
+
+
+				$sqlm = "INSERT INTO MyLikes (username, stars, userip, author, permlink)
+						VALUES ('".$userval."', '".$rating."', '".$saved_ip."', '".$author."', '".$permlink."')";
+
+						if (mysqli_query($conn, $sqlm)) {
+
+			
+    								} else {
+    									/*echo "post not exists";*/
+    									$addPost = "INSERT INTO PostsLikes (author, permlink, likes, rating, lastUpdatedDate)
+													VALUES ('".$author."', '".$permlink."', '".$newLike ."', '".$rating."', '".date("Y-m-d h:m:s")."')";
+										$addPostQuery = $conn->query($addPost);
+									}
+
+    					echo '<div class="alert alert-success">Your Recomendation is Added</div>';
+    					echo '<script>document.getElementById("logsubmit").reset(); setTimeout(function(){location.reload();}, 1000);</script>';
+					} else {
+    				echo '<div class="alert alert-danger">There is some issue. Please Try Later!</div>';
+					}
+			
+		
+
+
+
+?>
+
+
+
         $checkPost = "SELECT * FROM PostsLikes WHERE author = '$author' and permlink = '$permlink'";
                                 $result = mysqli_query($conn, $checkPost);
                                     if ($result->num_rows > 0) {
@@ -29,21 +84,3 @@ if (isset($_POST["rec_author"]) && isset($_POST["rec_permlink"])){
                                                 //$addPost = "INSERT INTO MyLikes (username, stars, userip, author, permlink)
                                                     //VALUES ('".$userval."', '".$rating."', '".$saved_ip."', '".$author."', '".$permlink."')";
                                                     // $addPostQuery = $conn->query($addPost);
-                                                                die(json_encode([
-                                                                'error' => false,
-                                                                'message' => 'Thankk You', 
-                                                                'data' => 'Upvoting'
-                                                                ]));
-
-                                            } else {
-                                                                die(json_encode([
-                                                                'error' => true,
-                                                                'message' => 'Sorry', 
-                                                                'data' => 'Already Upvoted'
-                                                                ]));
-                                         
-                                            }
-                                       // }
-                                    } else {die('Some error');}
-};
-?>
