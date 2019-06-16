2cf3a251d36e093435ed26e55b980bd9e0851b7e
bestboom <certseek@gmail.com>
2019-06-16 13:19:11 +0500

    Update recomend.php

--- public/template/modals/recomend.php
+++ public/template/modals/recomend.php
@@ -4,7 +4,7 @@
             <div class="row d-flex justify-content-around">
                 <input type="hidden" name="v_permlink" id="vote_permlink" value="" />
                 <input type="hidden" name="v_likes" id="vote_likes" value="" />   
-                <div class="col-sm-2 nopadding"><i class="fas fa-heart upme"></i></div>
+                <div class="col-sm-2 nopadding"><i class="fas fa-heart recomendme"></i></div>
                 <div class="col-sm-8 nopadding love-it">I Love This SHARE...</div> 
                 <div class="col-sm-2 nopadding">
                     <button type="button" class="close" data-dismiss="modal" aria-label="Close">x</button>
