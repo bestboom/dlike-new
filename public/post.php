a3213ceaeed2e5519074da56972be9283a9fc87e
bestboom <certseek@gmail.com>
2019-06-16 11:46:38 +0500

    Update post.php

--- public/post.php
+++ public/post.php
@@ -183,7 +183,7 @@ $sender =  $_COOKIE['username'];
                     </div>
                 </div>
             </div>  
-<div class="modal fade" id="likes" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
+<div class="modal fade" id="likesss" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
     <div class="modal-dialog modal-lg" role="document">
         <div class="modal-content">
             <?php include('template/modals/likes.php'); ?>
