bf12d59f944f9d546b5f3e21c3a06704572fdbcf
bestboom <certseek@gmail.com>
2019-06-16 11:59:24 +0500

    Update post.php

--- public/post.php
+++ public/post.php
@@ -183,9 +183,10 @@ $sender =  $_COOKIE['username'];
                     </div>
                 </div>
             </div>  
-<div class="modal fade" id="likesss" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
+<div class="modal fade" id="upvoteModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-sm" role="document">
+        <div class="modal-content mybody">
+            <?php include('template/modals/upvotemodal.php'); ?>
         </div>
     </div>
 </div>
