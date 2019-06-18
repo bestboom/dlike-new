589bf81e33c9f8161d41ee524b33ab6d5e9f75c8
bestboom <certseek@gmail.com>
2019-06-18 16:58:11 +0500

    up

--- public/template/modals/modal.php
+++ public/template/modals/modal.php
@@ -1,10 +1,3 @@
-<div class="modal fade" id="likes" tabindex="-1" role="dialog" aria-labelledby="modelTitleId" aria-hidden="true">
-    <div class="modal-dialog modal-lg" role="document">
-        <div class="modal-content">
-        	<?php include('template/modals/likes.php'); ?>
-        </div>
-    </div>
-</div>
 <div class="modal fade" id="recomendModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog modal-sm" role="document">
         <div class="modal-content mybody">
