58bd78a322da00beb26736eea2b4ef2dafe65c62
bestboom <certseek@gmail.com>
2019-06-19 22:27:03 +0500

    Update post.php

--- public/post.php
+++ public/post.php
@@ -120,7 +120,7 @@ $views = '1';
                 $resultvtip = mysqli_query($conn, $verifytip);
                 $rowvtip = $resultvtip->fetch_assoc();  
                 echo '<center><button class="btn btn-danger">You Already Tip This Post</button></center>';
-            }   
+            } else { echo 'good'; }    
 ?>
                                 
                             
