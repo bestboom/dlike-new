654a6611d9664165889636a8e3f91a71f1ca7c21
bestboom <certseek@gmail.com>
2019-06-20 15:14:09 +0500

    Update post.php

--- public/post.php
+++ public/post.php
@@ -303,6 +303,7 @@ function UpdateTime() {
     var cTime = new Date().getTime();
     console.log(cTime);
     var diff = cTime - sTime;
+    console.log(diff);
     var seconds = countDown - Math.floor(diff / 1000);
     if (seconds >= 0) {
         var minutes = Math.floor(seconds / 60);
