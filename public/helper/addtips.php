bca87afd8555f29ff7d91cea7ec9c6d42aec86e5
bestboom <certseek@gmail.com>
2019-06-21 10:35:47 +0500

    Update addtips.php

--- public/helper/addtips.php
+++ public/helper/addtips.php
@@ -19,7 +19,7 @@ if (isset($_POST["tipauthor"]) && isset($_POST["tippermlink"])){
 				echo '<div class="alert alert-danger">You Have already tip this post</div>';
 			} else {
 
-				$veriftime = "SELECT TimeStampDiff(SECOND,tip_time,Now()) AS lasttime FROM TipTop where sender = '$sender' order by tip_time DESC limit 1";
+				$verifytime = "SELECT TimeStampDiff(SECOND,tip_time,Now()) AS lasttime FROM TipTop where sender = '$sender' order by tip_time DESC limit 1";
                     $resulttime = $conn->query($verifytime);
                             $rowtime = $resulttime->fetch_assoc();
                                 $lasttip = $rowtime['lasttime']; 
@@ -28,16 +28,16 @@ if (isset($_POST["tipauthor"]) && isset($_POST["tippermlink"])){
                                 	echo '<script>setTimeout(function(){location.reload();}',
                                 } else {
 			
-			$sqlm = "INSERT INTO TipTop (sender, receiver, permlink, userip, tip_time)
-						VALUES ('".$sender."', '".$receiver."', '".$permlink."', '".$ip."', now())";
+								$sqlm = "INSERT INTO TipTop (sender, receiver, permlink, userip, tip_time)
+											VALUES ('".$sender."', '".$receiver."', '".$permlink."', '".$ip."', now())";
 				
-				if (mysqli_query($conn, $sqlm)) {
-					echo '<script>document.getElementById("tipsubmit").reset(); setTimeout(function(){location.reload();}, 1000);</script>';
-    				echo '<div class="alert alert-success">Tip is Successful</div>';
-				} else {
-    				echo '<div class="alert alert-danger">There is some issue. Please Try Later!</div>';
-				}
-			}
-		}
+								if (mysqli_query($conn, $sqlm)) {
+									echo '<script>document.getElementById("tipsubmit").reset(); setTimeout(function(){location.reload();}, 1000);</script>';
+    								echo '<div class="alert alert-success">Tip is Successful</div>';
+								} else {
+    								echo '<div class="alert alert-danger">There is some issue. Please Try Later!</div>';
+								}
+							}
+						}
 }			
 ?>
