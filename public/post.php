dda04fad899326d64609c5e8b6e46798c353a576
bestboom <certseek@gmail.com>
2019-06-22 13:31:22 +0500

    Update post.php

--- public/post.php
+++ public/post.php
@@ -36,15 +36,15 @@ $views = '1';
                     $postviews = '1';  
                 }      
         //tip total income
-                $post_inc = "SELECT * FROM TipTop where permlink = '$link' and author = '$auth'";
+                $post_inc = "SELECT SUM(tip1) As post_inc FROM TipTop where permlink = '$link' and receiver = '$auth'";
                     $result_inc = $conn->query($post_inc);
                         if ($result_inc->num_rows > 0) { 
                 //$sqli = "SELECT SUM(tip1) As post_inc FROM TipTop where permlink = '$link' and author = '$auth'";
                 //$resulti = $conn->query($sqli);
                     //if ($resulti->num_rows > 0) {
-                            echo 'incom';
-                        //$rowinc = mysqli_fetch_assoc($resulti);
-                        //$postincome = $rowinc["post_inc"];
+                           // echo 'incom';
+                        $rowinc = mysqli_fetch_assoc($resulti);
+                        $postincome = $rowinc["post_inc"];
                     }else{ $postincome = '0';}
 
 ?>
