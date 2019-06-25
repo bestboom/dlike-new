3402a358b801fec8c119bd80858cb86e5bda0e5d
bestboom <certseek@gmail.com>
2019-06-26 00:53:48 +0500

    Update addstake.php

--- public/helper/addstake.php
+++ public/helper/addstake.php
@@ -10,12 +10,14 @@ if (isset($_POST["stakemaount"]) && isset($_POST["stake_option"]) && isset($_POS
 	echo $period = $_POST["stake_option"];
 	echo $user = $_POST["staker"];
 
-$sqls = "SELECT amount FROM wallet where username='$user'"; 
-$resultAmount = $conn->query($sqls);
-if ($resultAmount->num_rows > 0) {
-$rowIt = $resultAmount->fetch_assoc();	
-echo (number_format($rowIt['amount']));
-} else {echo '<div class="alert alert-danger">You are not in balance!</div>';}
+	$sqls = "SELECT amount FROM wallet where username='$user'"; 
+	$resultAmount = $conn->query($sqls);
+		if ($resultAmount->num_rows > 0) {
+			$rowIt = $resultAmount->fetch_assoc();	
+			echo $user_bal = $rowIt['amount'];
+			else if ($amount > $user_bal){ echo '<div class="alert alert-danger">You do not have enough tokens!</div>';}
+			else {echo '<div class="alert alert-success">all looks good!</div>';}
+		} else {echo '<div class="alert alert-danger">You are not in balance!</div>';}
 
 
 
