ad56e20dd29bbf19861ee41f8dcbcc7b841baba5
bestboom <certseek@gmail.com>
2019-06-26 14:10:19 +0500

    Update addstake.php

--- public/helper/addstake.php
+++ public/helper/addstake.php
@@ -14,14 +14,16 @@ if (isset($_POST["stakemaount"]) && isset($_POST["stake_option"]) && isset($_POS
 	$resultAmount = $conn->query($sqls);
 		if ($resultAmount->num_rows > 0) {
 				$rowIt = $resultAmount->fetch_assoc();	
-				echo $user_bal = $rowIt['amount'];
+				$user_bal = $rowIt['amount'];
 			if ($amount > $user_bal){ echo '<div class="alert alert-danger">You do not have enough tokens!</div>';}
 			else {
 
 				$sqlm = "INSERT INTO staking (username, amount, period, start_time)
 					VALUES ('".$user."', '".$amount."', '".$period."', now())";
 
-				if (mysqli_query($conn, $sqlm)) {echo '<div class="alert alert-success">Staking done successfully</div>';}
+				if (mysqli_query($conn, $sqlm)) {echo '<div class="alert alert-success">Staking done successfully</div>';
+				echo '<script>document.getElementById("stake_sub").reset(); setTimeout(function(){location.reload();},2000);</script>';
+				}
 			}
 		} else {echo '<div class="alert alert-danger">Do not have token balance!</div>';}
 
