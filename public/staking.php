85c52163ba8bf39dd837a20acec89d38330cc46e
bestboom <certseek@gmail.com>
2019-06-28 09:21:19 +0500

    Update staking.php

--- public/staking.php
+++ public/staking.php
@@ -64,7 +64,7 @@ if(isset($_COOKIE['username']) && !empty($_COOKIE['username'])) { $staker =  $_C
                         <form action="" class="user-connected-from create-account-form" method="POST" id="stake_sub">   
                         <input type="hidden" name="staker" id="staking_user" value="<? echo $staker; ?>" />   
                             <div class="form-group">
-                                <input type="number" class="form-control" name="stakemaount" id="stakemaount" placeholder="Amount to Stake">
+                                <input type="number" class="form-control" name="stakemaount" id="stakemaount" placeholder="Amount to Stake (Min 500)">
                             </div>
                             <div class="form-group">
                                 <select class="form-control form-control-lg period" name="stake_option" id="stake">
