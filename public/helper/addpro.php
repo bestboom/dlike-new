4b8a17d282019471d19fc11df164571caa750aac
bestboom <certseek@gmail.com>
2019-06-29 15:31:23 +0500

    Update addpro.php

--- public/helper/addpro.php
+++ public/helper/addpro.php
@@ -1,3 +1,19 @@
 <?php 
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+require '../includes/config.php';
+echo $amount = '10000';
+if (isset($_POST["pro_user"]){
+	$user = $_POST["pro_user"];
+	$amount = '10000';
+	$sqls = "SELECT amount FROM wallet where username='$user'"; 
+	$resultAmount = $conn->query($sqls);
+		$rowIt = $resultAmount->fetch_assoc();	
+		$user_bal = $rowIt['amount'];
+	if ($amount > $user_bal){ echo '<div class="alert alert-danger">You do not have enough tokens!</div>';}
+	else {echo '<div class="alert alert-success">Looks Good</div>';}
+
+} else {echo '<div class="alert alert-danger">There is some issue. Please Try Later!</div>';}
 
 ?>
