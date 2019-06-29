91c762fb96840ece36b56c592f57884d1e948726
bestboom <certseek@gmail.com>
2019-06-29 14:22:59 +0500

    Create addpro.php

---
+++ public/helper/addpro.php
@@ -0,0 +1,18 @@
+<?php 
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+require '../includes/config.php';
+
+if (isset($_POST["pro_amt"]){
+	$amount = '10000';
+	$sqls = "SELECT amount FROM wallet where username='$user'"; 
+	$resultAmount = $conn->query($sqls);
+		$rowIt = $resultAmount->fetch_assoc();	
+		$user_bal = $rowIt['amount'];
+	if ($amount > $user_bal){ echo '<div class="alert alert-danger">You do not have enough tokens!</div>';}
+	else {echo '<div class="alert alert-success">Looks Good</div>';}
+
+} else {echo '<div class="alert alert-danger">There is some issue. Please Try Later!</div>';}
+
+?>
