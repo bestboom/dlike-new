a370885309c45adafd33bfdeb481f3e0a8a5d737
bestboom <certseek@gmail.com>
2019-06-30 11:18:06 +0500

    Update addpro.php

--- public/helper/addpro.php
+++ public/helper/addpro.php
@@ -4,15 +4,30 @@
 	error_reporting(E_ALL);
 require '../includes/config.php';
 
-if (isset($_POST["pro_user"])){
+if (isset($_POST["pro_user"]))
+{
 	$user = $_POST["pro_user"];
-	$amount = '70501';
+	$amount = '10000';
+
 	$sqls = "SELECT amount FROM wallet where username='$user'"; 
 	$resultAmount = $conn->query($sqls);
 		$rowIt = $resultAmount->fetch_assoc();	
 		$user_bal = $rowIt['amount'];
-	if ($amount > $user_bal){ echo '<div class="alert alert-danger">You do not have enough tokens!</div>';}
-	else {echo '<div class="alert alert-success">Looks Good</div>';}
+
+	if ($amount > $user_bal)
+	{ 
+		echo '<div class="alert alert-danger">You do not have enough tokens!</div>';
+	}
+	else 
+	{
+		$sqlm = "INSERT INTO prousers (username, amount, buy_time)
+					VALUES ('".$user."', '".$amount."', now())";
+
+		if (mysqli_query($conn, $sqlm))
+		{
+			echo '<div class="alert alert-success">Looks Good</div>';
+		}
+	}
 
 } else {echo '<div class="alert alert-danger">There is some issue. Please Try Later!</div>';}
 
