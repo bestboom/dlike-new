13d8cfe3c043b9b9c258b0f2645436048efc5c35
bestboom <certseek@gmail.com>
2019-06-30 10:59:54 +0500

    Update likes.php

--- public/helper/likes.php
+++ public/helper/likes.php
@@ -7,6 +7,20 @@ error_reporting(E_ALL);
 require '../includes/config.php';
 
 
+$sql = "CREATE TABLE prousers (
+id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+username VARCHAR(255) NOT NULL,
+amount INT(11) NOT NULL,
+buy_time TIMESTAMP
+)";
+
+if ($conn->query($sql) === TRUE) {
+    echo "Table prousers created successfully";
+} else {
+    echo "Error creating table: " . $conn->error;
+}
+
+
 
 
 /*'
