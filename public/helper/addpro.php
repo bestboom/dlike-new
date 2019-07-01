50a9f3dafc11cf54c0400f385ff9b33835fe2491
bestboom <certseek@gmail.com>
2019-07-01 08:14:54 +0500

    Update addpro.php

--- public/helper/addpro.php
+++ public/helper/addpro.php
@@ -10,6 +10,7 @@ if (isset($_POST["pro_user"]))
 	$amount = '10000';
 	$pro_status = '3';
 	$set_by = 'Paid';
+	$reason = 'PRO Membership';
 
 	$sqls = "SELECT amount FROM wallet where username='$user'"; 
 	$resultAmount = $conn->query($sqls);
@@ -29,27 +30,34 @@ if (isset($_POST["pro_user"]))
 		{
 
 			$updateWallet = "UPDATE wallet SET amount = '$user_bal' - '$amount' WHERE username = '$user'";
-						$updateWalletQuery = $conn->query($updateWallet);
+				$updateWalletQuery = $conn->query($updateWallet);
 									if ($updateWalletQuery === TRUE) 
 									{
-										$sqlu = "SELECT * FROM userstatus where username='$user'"; 
-										$resultu = $conn->query($sqlu);
+										$sqlj = "INSERT INTO transactions (username, amount, reason)
+											VALUES ('".$user."', '".$amount."', '".$reason."')";
 
-										if($resultu->num_rows > 0)
-										{
-											$updat_u = "UPDATE userstatus SET status = '$pro_status', set_by = '$set_by' WHERE username = '$user'";
-											$updateUserQuery = $conn->query($updat_u);
-											if ($updateUserQuery === TRUE) {}
-										} 
-										else 
-										{
-											$adduserstatus = "INSERT INTO userstatus (`username`, `status`  , `set_by` , `set_time` )
-												VALUES ('".$user."', '".$pro_status."', '".$set_by."', now())";
-											$adduserstatusQuery = $conn->query($adduserstatus);		
-										}
+											if (mysqli_query($conn, $sqlj)) 
+											{
 
-										echo '<div class="alert alert-success">PRO status Added</div>';
-										echo '<script>$(".pro-bt").attr("disabled","disabled"); document.getElementById("pro_sub").reset(); setTimeout(function(){location.reload();},2000);</script>';
+												$sqlu = "SELECT * FROM userstatus where username='$user'"; 
+												$resultu = $conn->query($sqlu);
+
+												if($resultu->num_rows > 0)
+												{
+													$updat_u = "UPDATE userstatus SET status = '$pro_status', set_by = '$set_by' WHERE username = '$user'";
+													$updateUserQuery = $conn->query($updat_u);
+													if ($updateUserQuery === TRUE) {}
+												} 
+												else 
+												{
+													$adduserstatus = "INSERT INTO userstatus (`username`, `status`  , `set_by` , `set_time` )
+														VALUES ('".$user."', '".$pro_status."', '".$set_by."', now())";
+													$adduserstatusQuery = $conn->query($adduserstatus);		
+												}
+
+												echo '<div class="alert alert-success">PRO status Added</div>';
+												echo '<script>$(".pro-bt").attr("disabled","disabled"); document.getElementById("pro_sub").reset(); setTimeout(function(){location.reload();},2000);</script>';
+											}
 									}
 
 		}
