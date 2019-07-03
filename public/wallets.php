1dfe8562a925c4c0d27388f496d0761bc38e33d1
bestboom <certseek@gmail.com>
2019-07-03 05:51:53 +0500

    up

--- public/wallets.php
+++ public/wallets.php
@@ -340,55 +340,7 @@ if ($resultp->num_rows > 0)
 	<br>
 	</div><!-- explorer-section -->
 
-	<?php
 
-/**
- * Checks if the given string is an address
- *
- * @method isAddress
- * @param {String} $address the given HEX adress
- * @return {Boolean}
-*/
-function isAddress($address) {
-    if (!preg_match('/^(0x)?[0-9a-f]{40}$/i',$address)) {
-        // check if it has the basic requirements of an address
-        return false;
-    } elseif (!preg_match('/^(0x)?[0-9a-f]{40}$/',$address) || preg_match('/^(0x)?[0-9A-F]{40}$/',$address)) {
-        // If it's all small caps or all all caps, return true
-        return true;
-    } else {
-        // Otherwise check each case
-        return isChecksumAddress($address);
-    }
-}
-
-/**
- * Checks if the given string is a checksummed address
- *
- * @method isChecksumAddress
- * @param {String} $address the given HEX adress
- * @return {Boolean}
-*/
-function isChecksumAddress($address) {
-    // Check each case
-    $address = str_replace('0x','',$address);
-    $addressHash = hash('sha3',strtolower($address));
-    $addressArray=str_split($address);
-    $addressHashArray=str_split($addressHash);
-
-    for($i = 0; $i < 40; $i++ ) {
-        // the nth letter should be uppercase if the nth digit of casemap is 1
-        if ((intval($addressHashArray[$i], 16) > 7 && strtoupper($addressArray[$i]) !== $addressArray[$i]) || (intval($addressHashArray[$i], 16) <= 7 && strtolower($addressArray[$i]) !== $addressArray[$i])) {
-            return false;
-        }
-    }
-    return true;
-}
-echo $address = 'aq';
-var_dump(isAddress("0x939523C1Be1547cd716EB5C7A70c2225c249f642"));
-$checkaddr = isAddress('0x939523C1Be1547cd716EB5C7A70c2225c249f642');
-if($checkaddr === true){ echo 'looks good';} else { echo 'looks bad';}
-?>
 
 
 <?php include('template/footer3.php'); ?>
