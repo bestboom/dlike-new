fbaaebc8015c69fb6573e62d05ec5d6ac7b145e8
bestboom <certseek@gmail.com>
2019-07-03 23:27:50 +0500

    Update tktransfer.php

--- public/template/modals/tktransfer.php
+++ public/template/modals/tktransfer.php
@@ -5,25 +5,34 @@
 			<div class="row line">
 				<div class="col-md-6">
 					<div class="form-group">
-						<div class="input-group-prepend">
-							<div class="input-group-text mb-deck"> To</div>
+						<div class="input-group mb-3">
+							<div class="input-group-prepend">
+								<div class="input-group-text mb-deck"> To</div>
+							</div>
+							<input type="text" class="form-control">
 						</div>
-						<input type="text" class="form-control">
 					</div>
 				</div>
 				<div class="col-md-6">
 					<div class="form-group">
-						<input type="number" class="form-control" min="0">
+						<div class="input-group mb-3">
+							<input type="number" class="form-control" min="0">
+							<div class="input-group-append">
+								<div class="input-group-text mb-deck"> Amount</div>
+							</div>
+						</div>
 					</div>
 				</div>
 			</div>
 			<div class="row line">
 				<div class="col-md-12">
 					<div class="form-group">
-						<div class="input-group-prepend">
-							<div class="input-group-text mb-deck"> Memo</div>
+						<div class="input-group mb-3">
+							<div class="input-group-prepend">
+								<div class="input-group-text mb-deck"> Memo</div>
+							</div>
+							<input type="text" class="form-control">
 						</div>
-						<input type="text" class="form-control">
 					</div>
 					<div class="form-group">
 						<div class="custom-control custom-checkbox">
