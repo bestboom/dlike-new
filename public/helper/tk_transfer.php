278551b63eea77ffc17392d9a5ed05fe0a5b4762
bestboom <certseek@gmail.com>
2019-07-04 12:06:56 +0500

    Update tk_transfer.php

--- public/helper/tk_transfer.php
+++ public/helper/tk_transfer.php
@@ -6,7 +6,12 @@ require '../includes/config.php';
 
 	if (isset($_POST["reciever"]) && isset($_POST["send_amt"]) && isset($_POST["user_bal"]) && isset($_POST["user_name"]))
 	{
+		echo $reciever = $_POST["reciever"];
 		echo '<div class="alert alert-success">Looks Good</div>';
+	}
+	else 
+	{
+		echo '<div class="alert alert-danger">There is some issue. Please Try Later!</div>';
 	}
 
 ?>
