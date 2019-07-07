f3959fd85be3fa45f89a644484b756be0320b592
bestboom <certseek@gmail.com>
2019-07-07 10:00:09 +0500

    Update tipswithdraw.php

--- public/template/modals/tipswithdraw.php
+++ public/template/modals/tipswithdraw.php
@@ -1,7 +1,9 @@
 <div class="modal-body ">
 	<div class="transfer-respond">
 		<h4 style="padding-bottom: 20px;font-weight: 600;text-align: center;">Withdraw</h4>
-		<p style="padding-bottom: 20px;font-weight: 600;text-align: center;">Balance: </p>
+		<p class="base-color" style="padding: 0px;margin-bottom:0px;font-weight: 600;text-align: center;">
+			Balance: <?php echo $tip_bal; ?>
+		</p>
 		<p>
 			<li>Minimum Withdraw is <?php echo $min_tip_withdraw; ?> USDT</li>
 			<li>You must add ETH address to withdraw</li>
