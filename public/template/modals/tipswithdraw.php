03a7246830eb2e349fca662034dd7e9d24835bcb
bestboom <certseek@gmail.com>
2019-07-07 09:50:05 +0500

    up

--- public/template/modals/tipswithdraw.php
+++ public/template/modals/tipswithdraw.php
@@ -2,8 +2,8 @@
 	<div class="transfer-respond">
 		<h4 style="padding-bottom: 20px;font-weight: 600;text-align: center;">Withdraw</h4>
 		<p>
-			<li>Minimum Withdraw is</li>
-			<li>Make sure you have added ETH address to withdraw</li>
+			<li>Minimum Withdraw is <?php echo $min_tip_withdraw; ?></li>
+			<li>You must add ETH address to withdraw</li>
 			<li>Withdrawls are processed daily</li>
 		</p>
 		
