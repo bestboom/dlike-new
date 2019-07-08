92e44c20e7eff4dfa1a33cda72f3eba94c31d535
bestboom <certseek@gmail.com>
2019-07-08 22:12:37 +0500

    Create tk_withdraw.php

---
+++ public/helper/tk_withdraw.php
@@ -0,0 +1,11 @@
+<?php 
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+require '../includes/config.php';
+
+	if (isset($_POST["reciever"]) && isset($_POST["send_amt"]) && isset($_POST["user_bal"]) && isset($_POST["user_name"]))
+	{
+
+	}
+?>
