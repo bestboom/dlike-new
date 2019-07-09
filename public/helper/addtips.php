562961cb202b6fda726682a7d4275a4fc603b29e
bestboom <certseek@gmail.com>
2019-07-09 12:17:48 +0500

    Update addtips.php

--- public/helper/addtips.php
+++ public/helper/addtips.php
@@ -32,10 +32,10 @@ if (isset($_POST["tipauthor"]) && isset($_POST["tippermlink"])){
 		$rowtime = $resulttime->fetch_assoc();
 		$lasttip = $rowtime['lasttime'];
 				
-				if($lasttip < 300) 
+				if(!$lasttip || $lasttip < 300) 
 					{
 						echo '<div class="alert alert-danger">There seems some issue</div>';
-						echo '<script>setTimeout(function(){location.reload();}, 1000);</script>'; 
+						echo '<script>setTimeout(function(){location.reload();}, 1000); return false;</script>'; 
 					} 
 			
 
