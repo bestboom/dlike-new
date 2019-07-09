5baa2fb3340200af86fd2d37b6ff6af790b3d743
bestboom <certseek@gmail.com>
2019-07-09 21:59:58 +0500

    Update post.php

--- public/post.php
+++ public/post.php
@@ -318,7 +318,7 @@ else
         }
 
         //image or youtube
-        let thumbnail = '<img src="' + img.src + '" alt="' + res.title + '" class="card-img-top img-fluid">';
+        let thumbnail = '<img src="' + img.src + '" alt="' + res.title + '" class="card-img-post img-fluid">';
 
             var getLocation = function(href) {
                     var l = document.createElement("a");
