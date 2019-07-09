e044987377760b332b3882cc9928be72c31d2025
bestboom <certseek@gmail.com>
2019-07-09 12:28:29 +0500

    Update addtips.php

--- public/helper/addtips.php
+++ public/helper/addtips.php
@@ -32,7 +32,7 @@ if (isset($_POST["tipauthor"]) && isset($_POST["tippermlink"])){
 		$rowtime = $resulttime->fetch_assoc();
 		$lasttip = $rowtime['lasttime'];
 				
-				if(!$lasttip || $lasttip < 300) 
+				if(!$lasttip) 
 				{
 					echo '<div class="alert alert-danger">There seems some issue</div>';
 					echo '<script>return false;</script>'; 
