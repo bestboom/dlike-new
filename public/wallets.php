5d1be3ef401c1daf3cd52280e85fc67427084b71
bestboom <certseek@gmail.com>
2019-07-10 10:36:36 +0500

    Update wallets.php

--- public/wallets.php
+++ public/wallets.php
@@ -614,7 +614,7 @@ if ($user_eth == '') {
                     return;
                 }
 
-                steem.api.claimRewardBalance(USERNAME, reward_steem, reward_sbd, reward_vests, function (err, res) {
+                window.broadcast.claimRewardBalance(USERNAME, reward_steem, reward_sbd, reward_vests, function (err, res) {
                     console.log(res);
 
                     if (err) {
