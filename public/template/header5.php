2330e2d49e81219575a762f6fe6846b6784c594e
bestboom <certseek@gmail.com>
2019-07-12 17:28:13 +0500

    up

--- public/template/header5.php
+++ public/template/header5.php
@@ -10,7 +10,7 @@
     <meta property="og:url" content="https://dlike.io" />
     <meta property="og:title" content="Dlike - Share To Get Rewarded" />
     <meta property="og:description" content="Dlike is a blockchain based dApp where you share links from your own blog articles or any useful link that is informative for community and get rewarded if community likes your links with steem upvotes." />
-    <meta property="og:image" content="<?php echo $og_image; ?>" />
+    <meta property="og:image" content="/images/dlike-main.jpg" />
 <!--End Facebook Meta Tags-->
     <link rel='favicon icon' type=image/x-icon href=images/favicon.ico />
     <link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
