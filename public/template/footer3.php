76c55710900a430413027427fca569dca1ceed8d
bestboom <certseek@gmail.com>
2019-07-12 18:12:04 +0500

    Update footer3.php

--- public/template/footer3.php
+++ public/template/footer3.php
@@ -117,6 +117,7 @@
         $("#btn_share").click(function () {
                     window.location.href="/share.php";
         })
+        function popup(e){var t=700;var n=400;var r=(screen.width-t)/2;var i=(screen.height-n)/2;var s="width="+t+", height="+n;s+=", top="+i+", left="+r;s+=", directories=no";s+=", location=no";s+=", menubar=no";s+=", resizable=no";s+=", scrollbars=no";s+=", status=no";s+=", toolbar=no";newwin=window.open(e,"windowname5",s);if(window.focus){newwin.focus()}return false}
     </script>
 </body>
 </html>
