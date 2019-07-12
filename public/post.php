dbf4b91385d48ffc26cd7caa094d3958b3ad4aaf
bestboom <certseek@gmail.com>
2019-07-12 16:34:23 +0500

    Update post.php

--- public/post.php
+++ public/post.php
@@ -246,7 +246,7 @@ else
                         <div class='row'>
                         <div class='col' style='padding: 0px !important;'>
                         <div style='background:#eee;border-bottom:40px solid #111;'>
-                        <a href='/post/@".$username."/".$permlink."'><img src='".$imgsrc."' class='img-fluid' style='width:100%;min-height:190px;'  onerror='this.src="'/images/post/8.png'"'></a>  
+                        <a href='/post/@".$username."/".$permlink."'><img src='".$imgsrc."' class='img-fluid' style='width:100%;min-height:190px;'  onerror='this.src='/images/post/8.png''></a>  
                         </div>
                         <h4 class='post-title' style='white-space: nowrap;overflow: hidden;text-overflow: ellipsis;margin-bottom: 20px;margin-top: -40px;color: #fff;padding: 10px;'><a href='/post/@".$username."/".$permlink."'>".$row1['title']."</a></h4> 
                         </div>
