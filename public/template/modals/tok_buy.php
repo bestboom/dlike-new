ae6685d315ccfb8285048bfdf0e270c5ce37ad74
bestboom <certseek@gmail.com>
2019-07-15 15:53:47 +0500

    Update tok_buy.php

--- public/template/modals/tok_buy.php
+++ public/template/modals/tok_buy.php
@@ -4,7 +4,7 @@
 		<p class="base-color respond_top">
 			DLIKE Tokens can be purchased in following ways
 		</p><br>
-		<li>Buy privately from other DLIKE users.Also available in #trade section of DLIKE official discord channel</li>
+		<li>Buy privately from other DLIKE users.Check #trade section of DLIKE official <a href="https://discord.gg/JYSkBFk" target="_blank">discord channel</a></li>
 		<li>Buy from DLIKE team contact @azmausa in discord or telegram channels of DLIKE</li>
 		<li>Buy from exchanges (coming soon)</li>
 	</div>
