85f80a2c62a17bb2d89fc33206e4b3c3bb575e7d
bestboom <certseek@gmail.com>
2019-07-16 11:51:38 +0500

    Update category.php

--- public/category.php
+++ public/category.php
@@ -1,5 +1,4 @@
 <?php include('template/header5.php');  ?>
-<input type="hidden" id="c_username" value="<?php echo $_COOKIE['username'];?>"/>
 </div><!-- sub-header -->
     
 <div class="latest-post-section">
@@ -26,87 +25,77 @@
 </div>
 
 <div class="modal fade" id="PostStatusModal" role="dialog">
-		    <div class="modal-dialog">
-		    
-		      <!-- Modal content-->
-		      <div class="modal-content">
-				<div class="modal-body text-center">
-					<input type="hidden" id="p_username" />
-					<input type="hidden" id="p_permlink" />
-					<input type="hidden" id="p_category" />
-					<p>What would you think about this post?</p>
-					<select class="form-control" id="status_select">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-body text-center">
+				<input type="hidden" id="p_username" />
+				<input type="hidden" id="p_permlink" />
+				<input type="hidden" id="p_category" />
+				<p>What would you think about this post?</p>
+				<select class="form-control" id="status_select">
 					<option value="">Please select</option>
 					<option value="Rejected">Rejected</option>
 					<option value="Low Level">Low Level</option>
 					<option value="High Level">High Level</option>
-					</select>
-					<br>
-					<p><input type="button" id="savepoststatus" class="btn btn-primary" value="Save it"/></p>
-						
-				</div>
-				<div class="modal-footer">
-				  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				</div>
-		      </div>
-		      
-		    </div>
-		  </div>
+				</select>
+				<br>
+				<p><input type="button" id="savepoststatus" class="btn btn-primary" value="Save it"/></p>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
 
 
-		  <div class="modal fade" id="userPostStatusModal" role="dialog">
-		    <div class="modal-dialog">
-		    
-		      <!-- Modal content-->
-		      <div class="modal-content">
-				<div class="modal-body text-center">
-					<input type="hidden" id="pu_username" />
-					<input type="hidden" id="pu_permlink" />
-					<input type="hidden" id="pu_category" />
-					<p>User Status</p>
-					<select class="form-control" id="userstatus_select">
-						<option value="">Please select</option>
-						<option value="0">Blacklisted</option>
-						<option value="1">Greenlisted</option>
-						<option value="2">Whitelisted</option>
-						<option value="3">Pro</option>
-					</select>
-					<br>
-					<p><input type="button" id="saveuserpoststatus" class="btn btn-primary" value="Save it"/></p>
-						
-				</div>
-				<div class="modal-footer">
-				  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				</div>
-		      </div>
-		      
-		    </div>
-		  </div>
+<div class="modal fade" id="userPostStatusModal" role="dialog">
+<div class="modal-dialog">
+	<div class="modal-content">
+		<div class="modal-body text-center">
+			<input type="hidden" id="pu_username" />
+			<input type="hidden" id="pu_permlink" />
+			<input type="hidden" id="pu_category" />
+			<p>User Status</p>
+			<select class="form-control" id="userstatus_select">
+				<option value="">Please select</option>
+				<option value="0">Blacklisted</option>
+				<option value="1">Greenlisted</option>
+				<option value="2">Whitelisted</option>
+				<option value="3">Pro</option>
+			</select>
+			<br>
+			<p><input type="button" id="saveuserpoststatus" class="btn btn-primary" value="Save it"/></p>
 
-		  <div class="modal fade" id="featuredPostStatusModal" role="dialog">
-		    <div class="modal-dialog">
-		    
-		      <!-- Modal content-->
-		      <div class="modal-content">
-				<div class="modal-body text-center">
-					<input type="hidden" id="pf_username" />
-					<input type="hidden" id="pf_permlink" />
-					<input type="hidden" id="pf_category" />
-					<input type="hidden" id="pf_imgurl" />
-					<input type="hidden" id="pf_title" />
-					
-					<p>What would you make this post featured?</p>
+		</div>
+		<div class="modal-footer">
+			<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+		</div>
+	</div>
+</div>
+</div>
 
-					<p><input type="button" id="savefeaturedpoststatus" class="btn btn-primary" value="Save it"/></p>
-						
-				</div>
-				<div class="modal-footer">
-				  <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-				</div>
-		      </div>
-		      
-		    </div>
-		  </div>
+<div class="modal fade" id="featuredPostStatusModal" role="dialog">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-body text-center">
+				<input type="hidden" id="pf_username" />
+				<input type="hidden" id="pf_permlink" />
+				<input type="hidden" id="pf_category" />
+				<input type="hidden" id="pf_imgurl" />
+				<input type="hidden" id="pf_title" />
+				
+				<p>What would you make this post featured?</p>
+
+				<p><input type="button" id="savefeaturedpoststatus" class="btn btn-primary" value="Save it"/></p>
+
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			</div>
+		</div>
+	</div>
+</div>
 
 		  
 <?php include('template/footer3.php'); ?>
