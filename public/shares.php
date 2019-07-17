7250e6428f583bdfda25bc9e6bdc4ea54203ae31
bestboom <certseek@gmail.com>
2019-07-17 14:05:29 +0500

    up

--- public/shares.php
+++ public/shares.php
@@ -11,23 +11,9 @@
                     <div class="user-connected-form-block">
                         <form class="user-connected-from user-login-form">
                             <div class="form-group">
-                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email address">
-                            </div>
-                            <div class="form-group">
                                 <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
                             </div>
-                            <div class="form-group pass-remember-block">
-                                <div class="custom-control custom-checkbox">
-                                    <div class="custom-checkbox-wrap">
-                                        <input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
-                                        <label class="custom-control-label" for="customCheck">Remember me</label>
-                                    </div>
-                                </div>
-                                <div>
-                                    <a href="#">Forget password?</a>
-                                </div>
-                            </div>
-                            <button type="submit" class="btn btn-default">Login</button>
+                            <center><button type="submit" class="btn btn-default">Login</button></center>
                         </form>
                         <p>Don't have an account?  <a href="#"> Register</a></p>
                     </div><!-- create-account-block -->
