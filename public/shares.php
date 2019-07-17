c4ff99fd6990d60df7ca0475596ed58827ca0489
bestboom <certseek@gmail.com>
2019-07-17 14:14:08 +0500

    Update shares.php

--- public/shares.php
+++ public/shares.php
@@ -6,16 +6,16 @@
             <div class="modal-content">
                 <div class="modal-body">
                     <div class="modal-info-block">
-                        <p style="font-size: 1.2rem;color: #c51d24;padding: 8px;font-weight: 600;text-align: center;margin-bottom: 2px;">Share To Get Rewarded</p>
+                        <p style="font-size: 1.2rem;color: #c51d24;padding-top: 18px;font-weight: 600;text-align: center;margin-bottom: 2px;">Share To Get Rewarded</p>
                     </div>
                     <div class="user-connected-form-block">
                         <form class="user-connected-from user-login-form">
                             <div class="form-group">
-                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                                <input type="text" class="form-control" id="url_field" required="true" placeholder="Enter URL">
                             </div>
-                            <center><button type="submit" class="btn btn-default">Login</button></center>
+                            <center><button type="submit" class="btn btn-default" style="width: 60%;padding-top: 10px;">Share</button></center>
                         </form>
-                        <p>Don't have an account?  <a href="#"> Register</a></p>
+                        <p>Must read  <a href="/help"> Terms &amp; Conditions</a> for sharing!</p>
                     </div><!-- create-account-block -->
                 </div>
             </div>
