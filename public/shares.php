b08006d4adbc9704bd373cc001db39561c13e3e9
bestboom <certseek@gmail.com>
2019-07-17 14:20:12 +0500

    up

--- public/shares.php
+++ public/shares.php
@@ -2,24 +2,24 @@
 </div>
 <div style="background: #1b1e63;">
     <div class="container">
-        <div class="user-login-signup-form-wrap"  style="padding: 7rem 0rem;">
+        <div class="user-login-signup-form-wrap" style="padding: 7rem 0rem;">
             <div class="modal-content">
                 <div class="modal-body">
-                    <div class="modal-info-block">
-                        <p style="font-size: 1.2rem;color: #c51d24;padding-top: 18px;font-weight: 600;text-align: center;margin-bottom: 2px;">Share To Get Rewarded</p>
+                    <div class="share-block">
+                        <p>Share To Get Rewarded</p>
                     </div>
                     <div class="user-connected-form-block">
                         <form class="user-connected-from user-login-form">
                             <div class="form-group">
                                 <input type="text" class="form-control" id="url_field" required="true" placeholder="Enter URL">
                             </div>
-                            <center><button type="submit" class="btn btn-default" style="width: 60%;padding-top: 10px;">Share</button></center>
+                            <center><button type="submit" class="btn btn-default" style="width: 40%;padding-top: 5px;">Share</button></center>
                         </form>
                         <p>Must read  <a href="/help"> Terms &amp; Conditions</a> for sharing!</p>
-                    </div><!-- create-account-block -->
+                    </div>
                 </div>
             </div>
-        </div><!-- user-login-signup-form-wrap -->
+        </div>
     </div>
 </div>   
 <?php include('template/footer3.php'); ?>
