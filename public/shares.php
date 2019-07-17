e9ef9244fd98023c382e02a3832d303671a585c8
bestboom <certseek@gmail.com>
2019-07-17 12:48:40 +0500

    Update shares.php

--- public/shares.php
+++ public/shares.php
@@ -1,50 +1,51 @@
 <?php include('template/header5.php'); ?>
 </div>
-<div class="container">
-    <div class="user-login-signup-form-wrap">
-        <div class="modal-content">
-            <h3>User Login</h3>
-            <div class="modal-body">
-                <div class="modal-info-block">
-                    <p>Always ensure you're on the correct website</p>
-                    <div class="block-inner">
-                        <p>
-                            <span>
-                                <i class="fas fa-lock"></i>
-                                https://
-                            </span>
-                            www.excoin.com
-                        </p>
+<div class="contact-form-section">
+    <div class="container">
+        <div class="user-login-signup-form-wrap">
+            <div class="modal-content">
+                <h3>User Login</h3>
+                <div class="modal-body">
+                    <div class="modal-info-block">
+                        <p>Always ensure you're on the correct website</p>
+                        <div class="block-inner">
+                            <p>
+                                <span>
+                                    <i class="fas fa-lock"></i>
+                                    https://
+                                </span>
+                                www.excoin.com
+                            </p>
+                        </div>
                     </div>
-                </div>
-                <div class="user-connected-form-block">
-                    <form class="user-connected-from user-login-form">
-                        <div class="form-group">
-                            <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email address">
-                        </div>
-                        <div class="form-group">
-                            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                        </div>
-                        <div class="form-group pass-remember-block">
-                            <div class="custom-control custom-checkbox">
-                                <div class="custom-checkbox-wrap">
-                                    <input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
-                                    <label class="custom-control-label" for="customCheck">Remember me</label>
+                    <div class="user-connected-form-block">
+                        <form class="user-connected-from user-login-form">
+                            <div class="form-group">
+                                <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email address">
+                            </div>
+                            <div class="form-group">
+                                <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                            </div>
+                            <div class="form-group pass-remember-block">
+                                <div class="custom-control custom-checkbox">
+                                    <div class="custom-checkbox-wrap">
+                                        <input type="checkbox" class="custom-control-input" id="customCheck" name="example1">
+                                        <label class="custom-control-label" for="customCheck">Remember me</label>
+                                    </div>
+                                </div>
+                                <div>
+                                    <a href="#">Forget password?</a>
                                 </div>
                             </div>
-                            <div>
-                                <a href="#">Forget password?</a>
-                            </div>
-                        </div>
-                        <button type="submit" class="btn btn-default">Login</button>
-                    </form>
-                    <p>Don't have an account?  <a href="#"> Register</a></p>
-                </div><!-- create-account-block -->
+                            <button type="submit" class="btn btn-default">Login</button>
+                        </form>
+                        <p>Don't have an account?  <a href="#"> Register</a></p>
+                    </div><!-- create-account-block -->
+                </div>
             </div>
-        </div>
-    </div><!-- user-login-signup-form-wrap -->
+        </div><!-- user-login-signup-form-wrap -->
+    </div>
 </div>
-
 <div class="contact-form-section">
     <div class="container">
         <div class="row">
@@ -70,7 +71,7 @@
                 </div>
             </div>
         </div>
-        
+
     </div>
 </div><!-- contact-section -->
 </div>    
