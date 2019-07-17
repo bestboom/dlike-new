45cbdb71986652ffb312cd69a04b8c63c4a90b8b
bestboom <certseek@gmail.com>
2019-07-17 15:07:56 +0500

    up

--- public/share.php
+++ public/share.php
@@ -1,31 +1,30 @@
-<?php include('template/header.php'); ?>
-    <div class="contact-form-section">
-        <div class="container">
-            <div class="row">
-                <div class="offset-lg-3 col-lg-6 offset-md-1 col-md-9">
-                    <div class="contact-form-wrap">
-                        <form class="contact-form share-form">
-                            <div class="form-row">
-                                <div class="form-group col-md-12">
-                                    <input type="text" class="form-control" id="url_field" required="true" placeholder="Enter URL">
-                                </div>
+<?php include('template/header5.php'); ?>
+</div>
+    <div class="container">
+        <div class="user-login-signup-form-wrap" style="padding: 7rem 0rem;">
+            <div class="modal-content" style="background: #1b1e63;border-radius: 14px;">
+                <div class="modal-body">
+                    <div class="share-block">
+                        <p>Share To Get Rewarded</p>
+                    </div>
+                    <div class="user-connected-form-block" style="background: #1b1e63;">
+                        <form class="user-connected-from share-form">
+                            <div class="form-group">
+                                <input type="text" class="form-control" id="url_field" required="true" placeholder="Enter URL" style="border-radius: 20px;">
                             </div>
-                            <div class="row justify-content-center">
-                                <button type="button" class="btn btn-default" id="share"><i class="fas fa-spinner fa-spin loader" style="display:none;"></i><span id="plus">Share</span></button>
-                            </div>
-                        </form><!-- contact-form -->
-                        <div class="row d-flex justify-content-center share-pack">
-                            <p>*Maximum 2 link shares allowed per user in 24 hours</p>
-                            <p>*Do not share same link shared by someone else</p>
-                            <p>*Do not share links which are not informative and useless</p>
-                            <p>*We prefer English Language based articles to be shared</p>
-                            <p>*Violation of rules will result in negative votes</p>
-                        </div>
+                            <center>
+                                <button type="button" class="btn btn-default" style="width: 40%;padding-top: 5px;"  id="share">
+                                    <i class="fas fa-spinner fa-spin loader" style="display:none;"></i>
+                                    <span id="plus">
+                                        Share
+                                    </span>
+                                </button>
+                            </center>
+                        </form>
+                        <p style="color: #fff;">Must read  <a href="/help"> Terms &amp; Conditions</a> for sharing!</p>
                     </div>
                 </div>
             </div>
-            
         </div>
-    </div><!-- contact-section -->
-</div>    
-<?php include('template/footer.php'); ?>
+    </div>  
+<?php include('template/footer3.php'); ?>
