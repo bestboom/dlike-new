ee01e38e805745e63936f1df18b5c98a1242b5c6
bestboom <certseek@gmail.com>
2019-07-18 21:17:39 +0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,22 +1,17 @@
-<?php include('template/header.php');?>
-    </div><!-- sub-header -->
-    <!-- <div class="row d-flex justify-content-center" style="background: #eeeeee94;width: 100%;"><a href="https://www.idcmkorea.io/coinsale/home" target="_blank"><img src="images/dlike-coin-sale-ban.png" style="margin-top: 10px;padding-bottom: 3px;margin-bottom:-95px;" class="img-responsive"></a></div> -->
-    <div class="latest-post-section">
-        <div class="container">
-            <!-- <div style="border: 1px dashed;border-radius: 7px;padding: 15px;margin-bottom: 25px;"><h4 style="font-weight: 600;line-height: 1.5;">Write an article about Dlike IEO promotion on (steemit, busy, medium, steempeak) to earn 1000 -3000 DLIKE tokens. Articles must include promotional banners and IDCM KOREA official ANN.</h4></div>
-             <article class="post-style-two post-full-width">
-                <?php //include('helper/top_post.php'); ?>
-            </article> --><!-- post-style-two -->
-            <div class="row  align-items-center h-100 post_select">
-                <div class="row col-md-3 justify-content-center">
-                        <h4 class="lab_post orderByLatest activeOrderBy">Latest</h4>
-                        <h4 class="lab_post orderByTopRated">Top Rated</h4>
-                </div>
-                <div class="col-md-9 lay">&nbsp;</div>
+<?php include('template/header5.php');?>
+</div><!-- sub-header -->
+<div class="latest-post-section">
+    <div class="container">
+        <div class="row  align-items-center h-100 post_select">
+            <div class="row col-md-3 justify-content-center">
+                <h4 class="lab_post orderByLatest activeOrderBy">Latest</h4>
+                <h4 class="lab_post orderByTopRated">Top Rated</h4>
             </div>
-            <div class="row" id="content">
-            </div>
+            <div class="col-md-9 lay">&nbsp;</div>
+        </div>
+        <div class="row" id="content">
         </div>
     </div>
+</div>
 <?php include('template/modals/modal.php'); ?>    
-<?php include('template/footer.php'); ?>
+<?php include('template/footer3.php'); ?>
