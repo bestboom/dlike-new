4f8ef40764ce2cd0bc729f85e1889e4d7932a7ce
bestboom <certseek@gmail.com>
2019-07-18 10:59:54 +0500

    Update welcome.php

--- public/welcome.php
+++ public/welcome.php
@@ -12,7 +12,7 @@
                                 <p style="color: #ffffffe3;">
                                     If you already have a steem account, login with your steem username through steemconnect.
                                 </p>
-                                <button class="btn btn-default" style="">Login</button>
+                                <button class="btn btn-default" style="padding: 5px 22px;background: transparent;border-color: #fff;    color: #fff;border-radius: 9px;">Login</button>
                             </div>
                         </div>
                     </div>
@@ -21,7 +21,7 @@
                     <div class="row">
                         <div class="contact-info-block">
                             <div class="contact-info-inner" style="text-align: center;">
-                                <h4>Create Account</h4>
+                                <h4 style="color: #1b1e63;">Create Account</h4>
                                 <ul class="contact-info-list">
                                     <li>
                                         <img src="./images/others/25.png" alt="img" class="img-responsive">
