41c0c346422a11f5d9a82b71014c6c3780397ddc
bestboom <certseek@gmail.com>
2019-07-18 19:35:19 +0500

    Update mint.js

--- public/js/mint.js
+++ public/js/mint.js
@@ -292,17 +292,17 @@
                         },
                     });
     });
-
-    //upvote
-    //if(rangeSlider != null){
-        var rangeSlider = document.getElementById("rs-range-line");
-        var rangeBullet = document.getElementById("rs-bullet");
-        rangeSlider.addEventListener("input", showSliderValue, false);
-    //}
-    function showSliderValue() {
-      rangeBullet.innerHTML = rangeSlider.value;
-    }
-
+//logout
+    $('.signup_btn').click(function () {
+        alert('look');
+        api.revokeToken(function(err_log, result_log) {
+            if (result_log && result_log.success) {   
+                $.removeCookie('username', { path: '/' });
+                $.removeCookie('access_token', { path: '/' });
+                document.location.href = '/';
+            }
+        });
+    });
 
 
     // here starts dlike-steem-upvote
@@ -373,17 +373,15 @@
             return false;
         };
     });
-//logout
-    $('.signin_btn').click(function () {
-        alert('look');
-        api.revokeToken(function(err_log, result_log) {
-            if (result_log && result_log.success) {   
-                $.removeCookie('username', { path: '/' });
-                $.removeCookie('access_token', { path: '/' });
-                document.location.href = '/';
-            }
-        });
-    });
+    //upvote
+    //if(rangeSlider != null){
+        var rangeSlider = document.getElementById("rs-range-line");
+        var rangeBullet = document.getElementById("rs-bullet");
+        rangeSlider.addEventListener("input", showSliderValue, false);
+    //}
+    function showSliderValue() {
+      rangeBullet.innerHTML = rangeSlider.value;
+    }
     //chat
     //function ChatbroLoader(chats,async){async=!1!==async;var params={embedChatsParameters:chats instanceof Array?chats:[chats],lang:navigator.language||navigator.userLanguage,needLoadCode:'undefined'==typeof Chatbro,embedParamsVersion:localStorage.embedParamsVersion,chatbroScriptVersion:localStorage.chatbroScriptVersion},xhr=new XMLHttpRequest;xhr.withCredentials=!0,xhr.onload=function(){eval(xhr.responseText)},xhr.onerror=function(){console.error('Chatbro loading error')},xhr.open('GET','//www.chatbro.com/embed.js?'+btoa(unescape(encodeURIComponent(JSON.stringify(params)))),async),xhr.send()}
     //ChatbroLoader({encodedChatId: '938nz'});
