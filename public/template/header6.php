9fe87c5e00296b09540b1a5d6901ff712be58a16
bestboom <certseek@gmail.com>
2019-07-19 09:36:19 +0500

    Update header6.php

--- public/template/header6.php
+++ public/template/header6.php
@@ -68,7 +68,7 @@
                             <div class="row" style="margin: 0px;">
                                 <div class="col-md-3 col-2">
                                     <?php if (!isset($_COOKIE['username']) || !$_COOKIE['username']) { 
-                                    echo '<button onclick="window.location.href='"https://steemconnect.com/oauth2/authorize?client_id=dlike.app&redirect_uri=https%3A%2F%2Fdlike.io%2Fsteemconnect&scope="';" type="button" class="btn btn-default btn-circle btn-lg custom_btn_icon search_btn_hover"><i class="fas fa-user"></i>
+                                    echo '<button onclick="window.location.href="https://steemconnect.com/oauth2/authorize?client_id=dlike.app&redirect_uri=https%3A%2F%2Fdlike.io%2Fsteemconnect&scope=";" type="button" class="btn btn-default btn-circle btn-lg custom_btn_icon search_btn_hover"><i class="fas fa-user"></i>
                                     </button>';
                                     } else {
                                     echo '<button onclick="window.location.href="#";" type="button" class="btn btn-default btn-circle btn-lg custom_btn_icon search_btn_hover"><span class="img_profile"><img src="" id="user_img" class="rounded-circle img-fluid my_img"></span>
