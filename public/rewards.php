34dc8af4c30079f94cd8acfb099d45f5aa964d43
bestboom <certseek@gmail.com>
2019-07-19 20:46:53 +0500

    Update rewards.php

--- public/rewards.php
+++ public/rewards.php
@@ -1,244 +1,163 @@
-<?php include('template/header5.php'); ?>
-    </div><!-- sub-header -->
-    <div class="support-category-section">
+<?php  include('template/header5.php');
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+if(isset($_COOKIE['username']) && !empty($_COOKIE['username'])) { $staker =  $_COOKIE['username']; }
+
+?>
+</div><!-- sub-header -->
+<div class="container" style="background: #191d5d;max-width: 100% !important;">
+    <div class="row" style="padding: 50px;">
+        <div class="col" style="text-align:center;color: #fff;">
+            <h3>DLIKE TOKENS STAKING</h3>
+        </div>
+    </div>
+</div>
+    <div class="working-process-section">
         <div class="container">
             <div class="row">
-                <div class="col-md-4">
-                    <div class="support-category-block">
-                        <img src="./images/others/21.png" alt="img" class="img-responsive">
-                        <h4>Ask a Question</h4>
+                <div class="col-lg-5  col-md-6">
+                    <div class="working-process">
+                        <ul class="working-process-list">
+                            <li>
+                                <div class="working-process-step">
+                                    <h4><span>#01.</span>  What is Staking?</h4>
+                                    <div class="process-details">
+                                        <p>
+                                            Staking means to "lock up" your DLIKE tokens for a certain period of time.For locking your tokens, you earn bonus rewards.
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="working-process-step">
+                                    <h4><span>#02.</span>  Staking Rewards</h4>
+                                    <div class="process-details">
+                                        <p>
+                                            We have 2 staking options.<br>
+                                            1. Stake for <b>90 days</b> to earn <b>9% bonus</b> tokens<br>
+                                            2. Stake for <b>180 days</b> to earn <b>25% bonus</b> tokens
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                            <li>
+                                <div class="working-process-step">
+                                    <h4><span>#03.</span>  Staking Terms</h4>
+                                    <div class="process-details">
+                                        <p>
+                                            Tokens staked for bonus will not be withdrawable for set period. You can continue staking after the maturity or can withdraw once staking period is over.
+                                        </p>
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
                     </div>
                 </div>
-                <div class="col-md-4">
-                    <div class="support-category-block">
-                        <img src="./images/others/22.png" alt="img" class="img-responsive">
-                        <h4>Contact Support</h4>
-                    </div>
-                </div>
-                <div class="col-md-4">
-                    <div class="support-category-block">
-                        <img src="./images/others/23.png" alt="img" class="img-responsive">
-                        <h4>Submit a request</h4>
-                    </div>
+                <div class="offset-lg-2 col-lg-5 col-md-6">
+                    <div class="user-connected-form-block">
+                        <h3>
+                            Stake DLIKE Tokens
+                        </h3>
+                        <div id="stak-msg"></div>
+                        <form action="" class="user-connected-from create-account-form" method="POST" id="stake_sub">   
+                        <input type="hidden" name="staker" id="staking_user" value="<? echo $staker; ?>" />   
+                            <div class="form-group">
+                                <input type="number" class="form-control" name="stakemaount" id="stakemaount" placeholder="Amount to Stake">
+                            </div>
+                            <div class="form-group">
+                                <select class="form-control form-control-lg period" name="stake_option" id="stake">
+                                    <option value="0">Staking Time</option>
+                                    <option value="1">90 Days</option>
+                                    <option value="2">180 Days</option>
+                                </select>
+                            </div>
+                            <button type="submit" class="btn btn-default" id="stake_me">STAKE NOW</button>
+                        </form>
+                        <p>By staking you agree to the Terms</a></p>
+                    </div><!-- create-account-block -->
                 </div>
             </div>
         </div>
-    </div><!-- support-category-section -->
-    <div class="container">
-        <div class="support-list-section">
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="support-list-block">
-                        <h5>ACCOUNT</h5>
-                        <ul class="support-list">
-                            <li><a href="#">How to Sign Up</a></li>
-                            <li><a href="#">Have Not Received Confirmation Email</a></li>
-                            <li><a href="#">Activation Link Invalid or Expired</a></li>
-                            <li><a href="#">How to Change  Email Address</a></li>
-                            <li><a href="#">How to Delete your Account?</a></li>
-                            <li class="colord"><a href="#">+5 Questions</a></li>
-                        </ul>
+    </div><!-- working-process-section -->
+<? if(isset($_COOKIE['username']) && !empty($_COOKIE['username'])) { $staker =  $_COOKIE['username']; ?>
+    <div class="latest-tranjections-area">
+        <div class="latest-tranjections-block">
+            <div class="container">
+                <div class="latest-tranjections-block-inner">
+                    <div class="panel-heading-block">
+                        <h5>My Stakings</h5>
                     </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="support-list-block">
-                        <h5>Your Wallet</h5>
-                        <ul class="support-list">
-                            <li><a href="#">Wallet confirmation</a></li>
-                            <li><a href="#">Wallet address</a></li>
-                            <li><a href="#">How to Delete your Account?</a></li>
-                            <li><a href="#">Buying with someone else's wallet</a></li>
-                            <li><a href="#">Sending bitcoins to another wallet</a></li>
-                            <li class="colord"><a href="#">+2 Questions</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="support-list-block">
-                        <h5>SECURITY</h5>
-                        <ul class="support-list">
-                            <li><a href="#">How to Sign Up</a></li>
-                            <li><a href="#">Buying with someone else's wallet</a></li>
-                            <li><a href="#">Common scams</a></li>
-                            <li><a href="#">How is my bank account information protected?</a></li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="others-support-list-section">
-            <div class="row">
-                <div class="col-lg-4 col-md-6">
-                    <div class="others-support-list-block">
-                        <h5>Wallet Service</h5>
-                        <ul class="others-support-list">
-                            <li><a href="#">Supported Digital Currencies</a></li>
-                            <li><a href="#">How do I send digital currency to another wallet?</a></li>
-                            <li><a href="#">How do I receive digital currency ?</a></li>
-                            <li><a href="#">How to Delete your Account?</a></li>
-                            <li><a href="#">Buying with someone else's wallet</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="others-support-list-block">
-                        <h5>Verification</h5>
-                        <ul class="others-support-list">
-                            <li><a href="#">Required quality for ID verification</a></li>
-                            <li><a href="#">Accepted identification</a></li>
-                            <li><a href="#">Payment Methods for UK Customers</a></li>
-                            <li><a href="#">Documents uploaded</a></li>
-                            <li class="colord"><a href="#">+4 Questions</a></li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-4 col-md-6">
-                    <div class="others-support-list-block">
-                        <h5>Mobile App</h5>
-                        <ul class="others-support-list">
-                            <li><a href="#">How to download app</a></li>
-                            <li><a href="#">Update your mobile app</a></li>
-                            <li><a href="#">Phone support scam</a></li>
-                            <li><a href="#">Documents uploaded</a></li>
-                            <li><a href="#">Accepted credit/debit cards</a></li>
-                        </ul>
-                    </div>
-                </div>
+                    <table class="table coin-list latest-tranjections-table">
+                        <thead>
+                            <tr>
+                                <th scope="col">Date Staked</th>
+                                <th scope="col">Amount</th>
+                                <th scope="col">Period</th>
+                                <th scope="col">Bonus</th>
+                                <th scope="col">Maturity Date</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php $sqlt = "SELECT * FROM staking where username ='$staker' ORDER BY start_time DESC";
+                            $result_t = $conn->query($sqlt);
+                                if ($result_t->num_rows > 0) {
+                                    while($row_t = $result_t->fetch_assoc()) { 
+                                    $period = $row_t["period"]; 
+                                    if($period == "2") {$period = '180'; $bonus = '25%'; $mature = '181';}
+                                    else if($period == "1") {$period = '90';$bonus = '9%'; $mature = '91';}
+                                    $entry_date = date('Y-m-d', strtotime($row_t["start_time"]));
+                            ?> 
+                            <tr>   
+                                <td><?php echo date('Y-m-d', strtotime($row_t["start_time"])); ?></td>
+                                <td><?php echo $row_t["amount"]; ?></td>
+                                <td><?php echo $period; ?> Days</td>
+                                <td><?php echo $bonus; ?></td>
+                                <td><?php echo date('Y-m-d', strtotime($entry_date. ' + '.$mature.' days')); ?></td>    
+                            </tr>
+                            <? } }?>
+                        </tbody>
+                    </table>
+                </div><!-- order-history-block-inner -->
             </div>
         </div>
     </div>
-    <div class="colto-section">
-        <div class="container">
-            <div class="colto-content-wrap">
-                <div class="colto-content">
-                    <h3>Getting started</h3>
-                    <p>We provide professional and secure trading services for investors</p>
-                </div>
-                <div class="colto-btn-group">
-                    <button class="btn callto-btn">Log In</button>
-                    <span>or</span>
-                    <button class="btn callto-btn">Create Account</button>
-                </div>
-            </div>
-        </div>
-    </div><!-- colto-section -->
-    <!-- Call to Action End -->
-<?php include('template/footer3.php'); ?>    
-    <footer class="footer">
-        <div class="footer-upper-section">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-md-4">
-                        <div class="footer-logo">
-                            <a href="#">
-                                <img src="./images/logo.png" alt="img" class="img-responsive"/>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4">
-                        <div class="footer-info-list">
-                            <h4>About Us</h4>
-                            <ul>
-                                <li><a href="#">Our Team</a></li>
-                                <li><a href="#">Our Company</a></li>
-                                <li><a href="#">Contact Us</a></li>
-                                <li><a href="#">Token Listing</a></li>
-                                <li><a href="#">Join Us</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4">
-                        <div class="footer-info-list">
-                            <h4>Learn</h4>
-                            <ul>
-                                <li><a href="#">Legal</a></li>
-                                <li><a href="#">Terms of Use</a></li>
-                                <li><a href="#">AML&CFT</a></li>
-                                <li><a href="#">Privacy Policy</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4">
-                        <div class="footer-info-list">
-                            <h4>Help</h4>
-                            <ul>
-                                <li><a href="#">Support</a></li>
-                                <li><a href="#">API Support</a></li>
-                                <li><a href="#">Coin/Token Listing</a></li>
-                                <li><a href="#">Partnership</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-4">
-                        <div class="footer-info-list">
-                            <h4>Contact Us</h4>
-                            <ul class="contact-info">
-                                <li>Email:  <span>info.excoin@gmail.com</span></li>
-                                <li>Phone:   <span>+99 5589 54789</span></li>
-                            </ul>
-                            <ul class="social-style-two">
-                                <li>
-                                    <a href="#">
-                                        <i class="fab fa-facebook-f"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fab fa-twitter"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fab fa-google-plus-g"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fab fa-github"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fab fa-linkedin-in"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-icon-wrap">
-                <a href="#">
-                    <img src="./images/others/31.png" alt="img" class="img-responsive">
-                </a>
-            </div>
-        </div>
-        <div class="footer-bottom">
-            <div class="container">
-                <div class="footer-bottom-wrap">
-                    <div class="trade-volume-block">
-                        <ul>
-                            <li>
-                                <span>39151</span> Active Traders
-                            </li>
-                            <li>
-                                <span>4191 BTC</span> 24h Volume
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="copyright-text">
-                        © 2019 <a href="#">Excoin</a>. All Rights Reserved
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <script src="./assets/js/jquery-1.12.4.min.js"></script>
-    <script src="./assets/js/popper.min.js"></script>
-    <script src="./assets/js/bootstrap.min.js"></script>
-    <script src="./assets/js/slick.min.js"></script>
-    <script src="./assets/js/jquery.peity.min.js"></script>
-    <script src="./assets/js/jquery.slimscroll.min.js"></script>
-    <script src="./js/custom.js"></script>
-</body>
-</html>
+<? } ?>     
+<?php include('template/footer3.php'); ?>
+<script type="text/javascript">
+    var optionstak = {
+        target: '#stak-msg',
+        url: 'helper/addstake.php',
+        success: function() {},
+    }
+$('#stake_sub').submit(function() {
+    if (username === null) {
+        toastr.error('hmm... You must be login!');
+        return false;
+    }
+
+    let stake_amt = parseInt($("#stakemaount").val());
+    let stake_period = parseInt($('.period').val());
+
+    if (!stake_amt) { 
+        $("#stakemaount").css("border-color", "RED"); 
+        toastr.error('phew... Enter Tokens Amount to Stake');
+        return false;
+    }
+
+    if (stake_amt < 500) { 
+        toastr.error('phew... Minimum Stake Amount is 500 DLIKE'); 
+        return false;
+    }
+
+    if (!stake_period) { 
+        toastr.error('phew... Select a staking period'); 
+        return false;
+    }
+
+    $(this).ajaxSubmit(optionstak);
+    return !1;
+});
+    
+</script>
