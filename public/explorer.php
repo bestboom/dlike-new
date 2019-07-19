560d053ba0680c4ceee8fc70f230c56b15f8c250
bestboom <certseek@gmail.com>
2019-07-19 11:51:12 +0500

    Update explorer.php

--- public/explorer.php
+++ public/explorer.php
@@ -1,22 +1,22 @@
-<?php include('template/header.php'); ?>
-        <div class="container explorer-top">
-            <div class="col-md-12">
-                <div class="banner-content explorer-form">
-                    <form action="/wallets.php" class="subs-form">
-                        <div class="input-box expl">
-                            <input type="text" value="" name="user" class="form-control" id="exp_search" placeholder="Search by steem username for token / transactions" required />
-                            <button type="button" class="wallet-search">Search</button>
-                        </div>
-                    </form>
+<?php include('template/header5.php'); ?>
+<div class="container explorer-top">
+    <div class="col-md-12">
+        <div class="banner-content explorer-form">
+            <form action="/wallets.php" class="subs-form">
+                <div class="input-box expl">
+                    <input type="text" value="" name="user" class="form-control" id="exp_search" placeholder="Search by steem username for token / transactions" required />
+                    <button type="button" class="wallet-search">Search</button>
                 </div>
-            </div>
+            </form>
         </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div><!-- banner-block -->
+    </div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div>
+</div><!-- banner-block -->
 <div class="explorer-section">
     <div class="support-category-section">
         <div class="container">
@@ -32,21 +32,21 @@
                 </div>
                 <div class="col-md-4">
                     <?php
-                        $sql = "SELECT sum(amount) as total FROM wallet";
-                        $result = $conn->query($sql);
-                            while($row = $result->fetch_assoc()) { ?>
-                    <div class="support-category-block">
-                        <h4>Total Distributed</h4>
-                        <h5><?php echo (number_format($row['total'])); ?></h5>
-                    <? }?>
+                    $sql = "SELECT sum(amount) as total FROM wallet";
+                    $result = $conn->query($sql);
+                    while($row = $result->fetch_assoc()) { ?>
+                        <div class="support-category-block">
+                            <h4>Total Distributed</h4>
+                            <h5><?php echo (number_format($row['total'])); ?></h5>
+                        <? }?>
                         <hr>
-                    <?php
+                        <?php
                         $sqlu = "SELECT COUNT(username) as users FROM wallet";
                         $resultIt = $conn->query($sqlu);
-                            while($row = $resultIt->fetch_assoc()) { ?>
-                        <h4>Total Token holders</h4>
-                        <h5><?php echo (number_format($row['users'])); ?></h5>
-                    <? } ?>
+                        while($row = $resultIt->fetch_assoc()) { ?>
+                            <h4>Total Token holders</h4>
+                            <h5><?php echo (number_format($row['users'])); ?></h5>
+                        <? } ?>
                     </div>
                 </div>
                 <div class="col-md-4">
@@ -61,9 +61,6 @@
             </div>
         </div>
     </div>
-
-
-    
     <div class="activity-section">
         <div class="container">
             <div class="row">
@@ -77,20 +74,20 @@
                         $resultWallet = $conn->query($sqlm);
                         if ($resultWallet->num_rows > 0) {
                             while($row = $resultWallet->fetch_assoc()) { ?>
-                        <div class="activity-block">
-                            <div class="row my-entry">
-                                <div class="col-sm-6">
-                                    <div class="row">
-                                        <div><img src="https://steemitimages.com/u/<?php echo $row["username"]; ?>/avatar" alt="img" class="img-responsive"></div>
-                                        <div class="exp-user"><?php echo $row["username"]; ?></div>
+                                <div class="activity-block">
+                                    <div class="row my-entry">
+                                        <div class="col-sm-6">
+                                            <div class="row">
+                                                <div><img src="https://steemitimages.com/u/<?php echo $row["username"]; ?>/avatar" alt="img" class="img-responsive"></div>
+                                                <div class="exp-user"><?php echo $row["username"]; ?></div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-6">
+                                            <div class="exp-amt"><span id="tk-amt"><?php echo (number_format($row["amount"])); ?></span> Dlikes</div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-sm-6">
-                                    <div class="exp-amt"><span id="tk-amt"><?php echo (number_format($row["amount"])); ?></span> Dlikes</div>
-                                </div>
-                            </div>
-                        </div>
-                        <? }
+                            <? }
                         } ?>
                     </div>
                 </div>
@@ -105,28 +102,26 @@
 
                         if ($result->num_rows > 0) {
                             while($row = $result->fetch_assoc()) { ?>
-                        <div class="activity-block">
-                            <div class="row my-entry">
-                                <div class="col-sm-8">
-                                    <div class="row">
-                                        <div><span class="btn btn-icon btn-exp"><span class="text-dark">Tx</span></span></div>
-                                        <div class="exp-user"><?php echo $row["username"]; ?></div>
-                                        <div class="exp-user">For <span><?php echo $row["reason"]; ?></span></div>
+                                <div class="activity-block">
+                                    <div class="row my-entry">
+                                        <div class="col-sm-8">
+                                            <div class="row">
+                                                <div><span class="btn btn-icon btn-exp"><span class="text-dark">Tx</span></span></div>
+                                                <div class="exp-user"><?php echo $row["username"]; ?></div>
+                                                <div class="exp-user">For <span><?php echo $row["reason"]; ?></span></div>
+                                            </div>
+                                        </div>
+                                        <div class="col-sm-4">
+                                            <div class="exp-amt"><span id="tk-amt"><?php echo (number_format($row["amount"])); ?></span> Dlikes</div>
+                                        </div>
                                     </div>
                                 </div>
-                                <div class="col-sm-4">
-                                    <div class="exp-amt"><span id="tk-amt"><?php echo (number_format($row["amount"])); ?></span> Dlikes</div>
-                                </div>
-                            </div>
-                        </div>
-                        <? }
+                            <? }
                         } $conn->close(); ?>
                     </div>
                 </div>
             </div>
         </div>
     </div><!-- activity-section -->
-
-                            
 </div><!-- explorer-section -->
-<?php include('template/footer.php'); ?>
+<?php include('template/footer3.php'); ?>
