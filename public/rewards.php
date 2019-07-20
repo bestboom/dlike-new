9ea2bc39a6d94685431fa1b3828f7121859f64e2
bestboom <certseek@gmail.com>
2019-07-20 11:34:10 +0500

    Update rewards.php

--- public/rewards.php
+++ public/rewards.php
@@ -14,7 +14,7 @@ error_reporting(E_ALL);
                         DLIKE REWARD POOL
                     </h4>
                     <p>
-                        You can earn tokens for your contributions to the Minds network. The more interactions on your content, the greater your share of the daily token reward pool to your OffChain address.
+                        You can earn tokens for your contributions to the DLIKE network. The more interactions on your content, the greater your share of the daily token reward pool to your OffChain wallet.
                     </p>
                     <div>
                         <img src="/images/post/dlike-reward.png" class="img-fluid" alt="">
@@ -84,7 +84,7 @@ error_reporting(E_ALL);
         <div class="offset-lg-1 col-lg-5 col-md-5">
             <div class="user-connected-form-block" style="box-shadow: 0px 0px 10px 1px #cccccc;">
                 <h3 style="text-align: center;">
-                    <span style="font-size: 1.1rem;">Total Reward Pool</span><br><span style="font-size: 1.4rem;color: #1a1a1a;">7,000 DLIKE</span>
+                    <span style="font-size: 1.1rem;">Total Reward Pool</span><br><span style="font-size: 1.4rem;color: #1652f0;">7,000 DLIKE</span>
                 </h3>
                 <form action="" class="user-connected-from create-account-form" /> 
                     <div class="form-group">
@@ -99,7 +99,7 @@ error_reporting(E_ALL);
                     <div class="form-group">
                         <input type="text" class="form-control" style="border: none;border-bottom: 1px solid #eee;background-color: #fff;" value=" | Estimated Reward" readonly><span class="fas fa-database inp_icon"></span>
                     </div>
-                    <button type="submit" class="btn btn-default" id="stake_me">STAKE NOW</button>
+                    <button class="btn btn-default" id="dividendCountDown"></button>
                 </form>
                 <p>By staking you agree to the Terms</a></p>
             </div><!-- create-account-block -->
@@ -108,3 +108,36 @@ error_reporting(E_ALL);
 </div>
 </div><!-- working-process-section  https://demo.w3layouts.com/demos_new/template_demo/18-05-2019/gadget_signup_form-demo_Free/1576182126/web/index.html-->
 <?php include('template/footer3.php'); ?>
+<script type="text/javascript">
+    $( document ).ready(function() { 
+    //var date = new Date();
+    //console.log(date.toLocaleString('en-GB')); 
+    var countDownDate = 0;
+    function counter() {
+        setInterval(() => {
+            var date = new Date().toLocaleString("en-US", { timeZone: "Europe/London"});
+            var countDownDate = new Date(date);
+            //console.log(date);
+            var i = 60;
+            var h = 24 - countDownDate.getHours();
+            if (h < 10) {
+                h = "0" + h;
+            }
+            var m = 59 - countDownDate.getMinutes();
+            if (m < 10) {
+                m = "0" + m;
+            }
+            var s = countDownDate.getSeconds();
+            s = i - s;
+            if (s < 10) {
+                s = "0" + s;
+            }
+            str = h + ":" + m + ":" + s;
+            i++;
+            //$("div.time").html(str);
+            $("#dividendCountDown").html(str);
+        }, 1000);
+    };
+    counter(); 
+});
+</script>
