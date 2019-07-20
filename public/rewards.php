b45fb2c93e1625c37c4888caef8deec8de4996ff
bestboom <certseek@gmail.com>
2019-07-20 17:59:29 +0500

    up

--- public/rewards.php
+++ public/rewards.php
@@ -10,9 +10,7 @@ error_reporting(E_ALL);
         <div class="row">
             <div class="col-lg-6  col-md-6">
                 <div class="working-process">
-                    <h4 style="text-align: center;font-weight: 800;font-size: 1.3rem;margin: 30px;">
-                        DLIKE REWARD POOL
-                    </h4>
+                    <h4 class="pool_head">DLIKE REWARD POOL</h4>
                     <p>
                         You can earn tokens for your contributions to the DLIKE network. The more interactions on your content, the greater your share of the daily token reward pool to your OffChain wallet.
                     </p>
@@ -44,20 +42,20 @@ error_reporting(E_ALL);
                         <input type="text" class="form-control reward_input" value=" | Estimated Reward" readonly><span class="fas fa-database inp_icon"></span>
                     </div>
                     <p>Time Remaining for Next Reward Pool</p>
-                    <button type="button" class="btn btn-default reward_btn" disabled><span class="fas fa-clock" style="font-size: 1.3rem;padding-right: 1rem;"></span><span class="dividendCountDown" style="font-size: 1.7rem;"></span></button>
+                    <button type="button" class="btn btn-default reward_btn" disabled><span class="far fa-clock" style="font-size: 1.3rem;padding-right: 1rem;"></span><span class="dividendCountDown" style="font-size: 1.7rem;"></span></button>
                 <p>By staking you agree to the Terms</p>
             </div><!-- create-account-block -->
         </div>
     </div>
                     <div class="features-section" style="padding-top: 40px;">
-                        <div class="container" style="box-shadow: 0px 0px 10px 1px #cccccc;padding: 40px;">
+                        <div class="container pool_box">
                             <div class="row">
                                 <div class="col-md-2 col-sm-3">
                                     <div class="features-block-icons">
                                         <span class="fab fa-gratipay reward_icon"></span>
                                         <p>Likes <br>
                                             <span style="font-size: 0.7rem;">Each like on your posts</span>
-                                            <br><span style="color:#c51d24;">20+</span>
+                                            <br><span class="head_color">20+</span>
                                         </p>
                                     </div><!-- features-block -->
                                 </div>
@@ -65,14 +63,14 @@ error_reporting(E_ALL);
                                     <div class="features-block-icons">
                                         <span class="fas fa-eye reward_icon"></span>
                                         <p>Views <br><span style="font-size: 0.7rem;">For 100 views on your posts</span><br>
-                                            <span style="color:#c51d24;">10+</span></p>
+                                            <span class="head_color">10+</span></p>
                                     </div><!-- features-block -->
                                 </div>
                                 <div class="col-md-2 col-sm-3">
                                     <div class="features-block-icons">
                                         <span class="fas fa-comment-alt reward_icon"></span>
                                         <p>comments <br><span style="font-size: 0.7rem;">Each one on your posts</span>
-                                            <br><span style="color:#c51d24;">10+</span>
+                                            <br><span class="head_color">10+</span>
                                         </p>
                                     </div><!-- features-block -->
                                 </div>
@@ -80,7 +78,7 @@ error_reporting(E_ALL);
                                     <div class="features-block-icons">
                                         <span class="fas fa-chevron-circle-up reward_icon"></span>
                                         <p>upvotes <br><span style="font-size: 0.7rem;">$1 steem upvote</span> 
-                                            <br><span style="color:#c51d24;">100+</span>
+                                            <br><span class="head_color">100+</span>
                                         </p>
                                     </div><!-- features-block -->
                                 </div>
@@ -88,14 +86,14 @@ error_reporting(E_ALL);
                                     <div class="features-block-icons">
                                         <span class="fas fa-users reward_icon"></span>
                                         <p>Referralss <br><span style="font-size: 0.7rem;">Each New Referral </span>
-                                            <br><span style="color:#c51d24;">50+</span>
+                                            <br><span class="head_color">50+</span>
                                         </p>
                                     </div><!-- features-block -->
                                 </div>
                                 <div class="col-md-2 col-sm-3">
                                     <div class="features-block-icons">
                                         <span class="fas fa-user-plus reward_icon"></span>
-                                        <p>Referral Income <br><span style="font-size: 0.7rem;">Each Post By Your Referrals</span><br/><span style="color:#c51d24;">5+</span></p>
+                                        <p>Referral Income <br><span style="font-size: 0.7rem;">Each Post By Your Referrals</span><br/><span class="head_color">5+</span></p>
                                     </div><!-- features-block -->
                                 </div>
                             </div>
