5d06e4ab37eac703236378d66965b4b1b9712650
bestboom <certseek@gmail.com>
2019-07-20 12:10:47 +0500

    Update rewards.php

--- public/rewards.php
+++ public/rewards.php
@@ -19,66 +19,7 @@ error_reporting(E_ALL);
                     <div>
                         <img src="/images/post/dlike-reward.png" class="img-fluid" alt="">
                     </div>
-                    <div class="features-section" style="box-shadow: 0px 0px 10px 1px #cccccc;">
-                        <div class="container">
-                            <div class="row">
-                                <div class="col-md-4 col-sm-6">
-                                    <div class="features-block">
-                                        <img src="./images/others/9.png" alt="img" class="img-responsive">
-                                        <h4>Votes</h4>
-                                        <p>
-                                            10+
-                                        </p>
-                                    </div><!-- features-block -->
-                                </div>
-                                <div class="col-md-4 col-sm-6">
-                                    <div class="features-block">
-                                        <img src="./images/others/10.png" alt="img" class="img-responsive">
-                                        <h4>Recomendations</h4>
-                                        <p>
-                                            10+
-                                        </p>
-                                    </div><!-- features-block -->
-                                </div>
-                                <div class="col-md-4 col-sm-6">
-                                    <div class="features-block">
-                                        <img src="./images/others/11.png" alt="img" class="img-responsive">
-                                        <h4>Fast and Reliable</h4>
-                                        <p>
-                                         10+
-                                     </p>
-                                 </div><!-- features-block -->
-                             </div>
-                             <div class="col-md-4 col-sm-6">
-                                <div class="features-block">
-                                    <img src="./images/others/12.png" alt="img" class="img-responsive">
-                                    <h4>Margin Trading</h4>
-                                    <p>
-                                        10+
-                                    </p>
-                                </div><!-- features-block -->
-                            </div>
-                            <div class="col-md-4 col-sm-6">
-                                <div class="features-block">
-                                    <img src="./images/others/13.png" alt="img" class="img-responsive">
-                                    <h4>24/7 Live Support</h4>
-                                    <p>
-                                        10+
-                                    </p>
-                                </div><!-- features-block -->
-                            </div>
-                            <div class="col-md-4 col-sm-6">
-                                <div class="features-block">
-                                    <img src="./images/others/14.png" alt="img" class="img-responsive">
-                                    <h4>Referrals</h4>
-                                    <p>
-                                        10+
-                                    </p>
-                                </div><!-- features-block -->
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                    
             </div>
         </div>
         <div class="offset-lg-1 col-lg-5 col-md-5">
