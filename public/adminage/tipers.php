a7a53a276c4681c4fbffc1ae55e510a9d86f2542
bestboom <certseek@gmail.com>
2019-07-21 14:25:40 +0500

    up

--- public/adminage/tipers.php
+++ public/adminage/tipers.php
@@ -5,7 +5,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-$sql_C = "SELECT sum(amount) as total_amount,count( DISTINCT(username) ) as total FROM staking";
+$sql_C = "SELECT sum(tip1) as total_tip,count(*) as total FROM TipsWallet";
 $result_C = $conn->query($sql_C);
 $row_C = $result_C->fetch_assoc();
 ?>
@@ -22,8 +22,8 @@ $row_C = $result_C->fetch_assoc();
 		<li>
 			<div class="btn apps-download-btn googleplay-btn">
 				<div class="btn-content" style="text-align: center;">
-					<span>Tokens Staked</span>
-					<p><? echo number_format($row_C['total_amount']); ?></p>
+					<span>Amount To Pay</span>
+					<p><? echo number_format($row_C['total_tip'],3); ?></p>
 				</div>
 			</div>
 		</li>
@@ -34,34 +34,28 @@ $row_C = $result_C->fetch_assoc();
 	<div class="row" style="margin: 30px;">
 		<table class="table coin-list table-hover" style="border: 1px solid #eee;">
 			<thead>
-				<tr>
-					<th scope="col" class="cent_me wid_2">User</th>
+				<tr style="text-align: center;">
+					<th scope="col" class="cent_me wid_2">Tip BY</th>
+					<th scope="col" class="cent_me wid_2">To</th>
+					<th scope="col" class="cent_me wid_2">For</th>
 					<th scope="col" class="cent_me wid_2">Amount</th>
-					<th scope="col" class="cent_me wid_2">Period</th>
 					<th scope="col" class="cent_me wid_2">Time</th>
 				</tr>
 			</thead>
 			<tbody>
 				<?php 
-				$sql_T = "SELECT * FROM staking ORDER BY start_time DESC";
+				$sql_T = "SELECT * FROM TipsWallet ORDER BY tip1 ASC LIMIT 100";
 				$result_T = $conn->query($sql_T);
 
 				if ($result_T && $result_T->num_rows > 0) {
-					while ($row_T = $result_T->fetch_assoc()) {
-						$start_time = strtotime($row_T["start_time"]); 
+					while ($row_T = $result_T->fetch_assoc()) { 
 						?>
 						<tr>
 							<td class="exp-user cent_me wid_2">
 								<span><?php echo $row_T["username"]; ?></span>
 							</td>
 							<td class="exp-amt cent_me wid_2">
-								<span><?php echo $row_T["amount"]; ?></span>
-							</td>
-							<td class="exp-amt cent_me wid_2">
-								<span><?php echo $row_T["period"]; ?></span>
-							</td>
-							<td class="exp-amt cent_me wid_2">
-								<?php echo time_ago($start_time); ?>
+								<span><?php echo $row_T["tip1"]; ?></span>
 							</td>
 						</tr>
 						<?php
