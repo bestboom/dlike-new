f25571a25cccd268c5c87d6d6fd9726f5453b797
bestboom <certseek@gmail.com>
2019-07-21 11:15:07 +0500

    up

---
+++ public/adminage/pro-users.php
@@ -0,0 +1,6 @@
+<?php include('head.php'); ?>
+
+
+
+
+<?php include('../template/footer2.php'); ?>
--- public/includes/config.php
+++ public/includes/config.php
@@ -56,5 +56,26 @@ if(!isset($_COOKIE['usertoken'])) {
 
 $min_tip_withdraw = '3';
 
-
+//time ago
+function time_ago($timestamp)
+{
+    $etime = time() - $timestamp;
+ 
+    if ($etime < 1)
+    {
+        return 'just now';
+    }
+ 
+    $a = array(12 * 30 * 24 * 60 * 60  =>  'year', 30 * 24 * 60 * 60 => 'month', 24 * 60 * 60 => 'day', 60 * 60 => 'hour', 60 => 'minute', 1 => 'second');
+ 
+    foreach ($a as $secs => $str)
+    {
+        $d = $etime / $secs;
+        if ($d >= 1)
+        {
+            $r = round($d);
+            return $r . ' ' . $str . ($r > 1 ? 's' : '') . ' ago';
+        }
+    }
+}
 ?>
