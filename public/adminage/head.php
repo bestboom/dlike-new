b0d31d4373cd461639c1ddf9eef04a93c991b663
bestboom <certseek@gmail.com>
2019-07-21 13:39:02 +0500

    up

--- public/adminage/head.php
+++ public/adminage/head.php
@@ -1,6 +1,7 @@
 <?php 
 if($_COOKIE['username'] != 'dlike'){die('<script>window.location.replace("https://dlike.io","_self")</script>');};
-include('../template/header2.php');  ?>
+include('../template/header2.php');  
+?>
 <style>
   .banner-block{background: transparent;}
   .admin-latest-post-section {
