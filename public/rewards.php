325469a6494437323f12f560b5d18b0819e4181c
bestboom <certseek@gmail.com>
2019-07-24 10:12:14 +0500

    Update rewards.php

--- public/rewards.php
+++ public/rewards.php
@@ -17,8 +17,8 @@ if (isset($_COOKIE['username']) || $_COOKIE['username']) {
         $result1 = $conn->query($sql1);
     
         if ($result1->num_rows > 0) {
-            while($row1 = $result1->fetch_assoc());
-            {$my_permlinks[] = $row1['permlink'];}
+            $row1 = $result1->fetch_assoc();
+            $my_permlinks = $row1['permlink'];
         } else {$my_permlinks = '';}
 
         $user_status = "";
@@ -61,7 +61,12 @@ if (isset($_COOKIE['username']) || $_COOKIE['username']) {
                         <div class="reward_amount">7,000 DLIKE</div>
                     </h3>
                     <p style="margin-top: -20px;font-weight: 600;color: red;"><?php echo $user_status; ?></p>
-                    <p><?php echo json_encode($my_permlinks); ?></p>
+
+
+                    <p><?php echo $my_permlinks; ?></p>
+
+
+
                     <form class="user-connected-from create-account-form reward_form" /> 
                     <div class="form-group reward_fileds">
                         <input type="text" class="form-control reward_input" value=" | Total Points" readonly><span class="fas fa-star inp_icon"></span>
