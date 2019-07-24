41ce22e8f0fdb2458303b7aa90708e204064d686
bestboom <certseek@gmail.com>
2019-07-24 13:12:03 +0500

    Update rewards.php

--- public/rewards.php
+++ public/rewards.php
@@ -23,10 +23,10 @@ if (isset($_COOKIE['username']) || $_COOKIE['username']) {
             }
             $permlinks_list = implode(',', $mydata);
 
-            $sql2 = "SELECT SUM(totalviews) FROM TotalPostViews where author = '$user_name' and permlink IN ('permlinks_list')";
+            $sql2 = "SELECT SUM(totalviews) AS views FROM TotalPostViews where author = '$user_name' and permlink IN ('permlinks_list')";
             $result2 = $conn->query($sql2);
             $row2 = $result2->fetch_assoc();
-            echo $row2['totalviews'];
+            echo $row2['views'];
 
         } else {$my_permlinks = '';}
 
