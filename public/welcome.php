1cbfb06d10e3d430ffb57a8f059bb8ad4763f3ce
bestboom <certseek@gmail.com>
2019-07-25 11:21:10 +0500

    Update welcome.php

--- public/welcome.php
+++ public/welcome.php
@@ -29,7 +29,7 @@ include('template/header6.php');
                                 <div class="form-group">
                                     <input type="text" class="form-control" required="true" placeholder="username">
                                 </div>
-                                <p style="margin: 0px;"><?php echo 'Referred By '.$referrer; ?></p>
+                                <p style="margin: 0px;"><?php if($referrer !=''){ echo 'Referred By '.$referrer; } ?></p>
                                 <button type="button" class="btn btn-default signup_btn">
                                     <i class="fas fa-spinner" style="display:none;"></i>
                                     <span>Continue</span>
