4421c4c7f393a7396f355e2651e4325fa2f9d96e
KishanChauhan8313 <kishanchauhan8313@gmail.com>
2019-07-27 16:25:13 +0530

    js 2

--- public/welcome.php
+++ public/welcome.php
@@ -351,7 +351,7 @@ input.addEventListener('blur', function(){
             console.log(number);
             /*verify number call*/
             $.ajax({
-                url: 'https://dlike.io/welcome.php/pulic/helper/signup_verify.php";',
+                url: 'helper/signup_verify.php";',
                 type: 'post',
                 cache : false,
                 dataType: 'json',
@@ -393,7 +393,7 @@ $(document).on('click',".signup-signup-phone .next.btn",function(){
         if(number != ''){
             /*verify number call*/
             $.ajax({
-                url: 'https://dlike.io/welcome.php/pulic/helper/signup_verify.php";',
+                url: 'helper/signup_verify.php";',
                 type: 'post',
                 cache : false,
                 dataType: 'json',
