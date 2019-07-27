cce9bafd5e73b491de94221a43623e82b112f96c
bestboom <certseek@gmail.com>
2019-07-28 00:17:01 +0500

    Update signup_verify.php

--- public/helper/signup_verify.php
+++ public/helper/signup_verify.php
@@ -6,13 +6,12 @@ require '../includes/config.php';
 require_once("../includes/Twilio/autoload.php");
 
 //$client = new Twilio\Rest\Client(TWILIO_SID, TWILIO_TOKEN);
-//$phone =  $_POST['number'];
 
-//use Twilio\Rest\Client;
+use Twilio\Rest\Client;
 
 $sid    = getenv('twilio_sid');
 $token  = getenv('twilio_token');
-//$twilio = new Client($sid, $token);
+$twilio = new Client($sid, $token);
 
 
 //print($verification->sid);
