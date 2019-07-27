2bd42ad3ab7c8e6054f77208f2f608aff60bf2ca
KishanChauhan8313 <kishanchauhan8313@gmail.com>
2019-07-27 17:06:55 +0530

    send sms

--- public/helper/signup_verify.php
+++ public/helper/signup_verify.php
@@ -3,7 +3,28 @@
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
 require '../includes/config.php';
-
+// Sandbox Twillio Constants
+define("TWILIO_FROM_NO", "+919586561149");
+define("TWILIO_SID", "ACe117d252bc601c7d773357dcbfa29f69");
+define("TWILIO_TOKEN", "30c7d02328017d4b76ddb7652ec32bc1"); 
+function sendSMS($country_code = '',$mobile_no, $message) {
+    require_once("../includes/Twilio/autoload.php");
+    $client = new Twilio\Rest\Client(TWILIO_SID, TWILIO_TOKEN);
+    $message = $client->messages->create($country_code.$mobile_no, array('from' => TWILIO_FROM_NO, 'body' => $message));
+    /*File writer code*/
+	  $txt = '========================='.date('Y-m-d H:i:s').'=====================';
+	  $myfile = fopen("sms-log.txt", "a");
+	  fwrite($myfile, "\n". $message);
+	  fclose($myfile);
+	  /*File writer code*/
+    if($message != ""){
+        $message = print_r($message, true);
+    	return true;
+    }
+    else{
+    	return false;
+    }
+}
 if (isset($_POST['action'])  && $_POST['action'] == 'check_number' && isset($_POST['number'])  && $_POST['number'] != ''){
 	$return = array();
 	$return['status'] = false;
@@ -14,6 +35,31 @@ if (isset($_POST['action'])  && $_POST['action'] == 'check_number' && isset($_PO
 	if ($result_phone->num_rows <= 0){
 		$return['status'] = true;
 		$return['message'] = 'Number is availabel';
+	}
+	else{
+		$return['message'] = 'Number already in use by other account';
+	}
+	echo json_encode($return);
+	exit;
+}
+else if (isset($_POST['action'])  && $_POST['action'] == 'send_sms' && isset($_POST['number'])  && $_POST['number'] != '' && isset($_POST['countryCode'])  && $_POST['countryCode'] != ''){
+	$return = array();
+	$return['status'] = false;
+	$return['message'] = '';
+	$countryCode =str_replace('+','',$_POST['countryCode']); 
+	$phone =  $countryCode.$_POST['number'];
+	$check_phone = "SELECT * FROM wallet where phone_number = '".$phone."' ";
+	$result_phone = $conn->query($check_phone);
+	if ($result_phone->num_rows <= 0){
+		$otp = mt_rand(1111,9999);
+		$sms = sendSMS($_POST['countryCode'],$_POST['number'],"You otp is : ".$otp);
+		if($sms){
+			$return['status'] = true;
+			$return['message'] = 'We have sent you otp on your number please verify it.';
+		}
+		else{
+			$return['message'] = 'We are unable to send otp, Please try again later.';
+		}
 	}
 	else{
 		$return['message'] = 'Number already in use by other account';
