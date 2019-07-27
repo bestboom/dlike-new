d6b6d82e70e928365e7348855804b0ac93071fb1
bestboom <certseek@gmail.com>
2019-07-27 12:01:37 +0500

    up

--- public/welcome.php
+++ public/welcome.php
@@ -275,3 +275,49 @@ include('template/header6.php');
 
 <!--https://github.com/jackocnr/intl-tel-input -->
 <?php include('template/footer4.php'); ?>
+<script type="text/javascript">
+    var input = document.querySelector("#phone"),
+    errorMsg = document.querySelector("#error-msg"),
+    validMsg = document.querySelector("#valid-msg");
+
+// Error messages based on the code returned from getValidationError
+var errorMap = [ "Invalid number", "Invalid country code", "Too short", "Too long", "Invalid number"];
+
+// Initialise plugin
+var intl = window.intlTelInput(input, {
+    initialCountry: "auto",
+    geoIpLookup: function(success, failure) {
+        $.get("https://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+            var countryCode = (resp && resp.country) ? resp.country : "";
+            success(countryCode);
+        });
+    },
+    utilsScript: "/js/phone_input.js"
+});
+
+var reset = function() {
+    input.classList.remove("error");
+    errorMsg.innerHTML = "";
+    errorMsg.classList.add("hide");
+    validMsg.classList.add("hide");
+};
+
+// Validate on blur event
+input.addEventListener('blur', function() {
+    reset();
+    if(input.value.trim()){
+        if(intl.isValidNumber()){
+            validMsg.classList.remove("hide");
+        }else{
+            input.classList.add("error");
+            var errorCode = intl.getValidationError();
+            errorMsg.innerHTML = errorMap[errorCode];
+            errorMsg.classList.remove("hide");
+        }
+    }
+});
+
+// Reset on keyup/change event
+input.addEventListener('change', reset);
+input.addEventListener('keyup', reset);
+</script>
