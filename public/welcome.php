97375e309cff1a41fa9aefb3436c639c80f73ed5
bestboom <certseek@gmail.com>
2019-07-27 11:39:29 +0500

    Update welcome.php

--- public/welcome.php
+++ public/welcome.php
@@ -279,5 +279,14 @@ include('template/header6.php');
 <script src="/js/intlTelInput.js"></script>
 <script>
 var input = document.querySelector("#phone");
-window.intlTelInput(input);
+intlTelInput(input, {
+    initialCountry: "auto",
+    geoIpLookup: function(success, failure) {
+        $.get("https://ipinfo.io", function() {}, "jsonp").always(function(resp) {
+            var countryCode = (resp && resp.country) ? resp.country : "";
+            success(countryCode);
+        });
+    },
+    utilsScript: "/js/phone_input.js"
+});
 </script>
