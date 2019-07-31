2e1224ab2a4c09cbe7d4a679fa1544947588bf78
bestboom <certseek@gmail.com>
2019-07-31 11:40:35 +0500

    Update sign_check.php

--- public/helper/sign_check.php
+++ public/helper/sign_check.php
@@ -12,7 +12,7 @@ if (isset($_POST['action'])  && $_POST['action'] == 'acc_create' && isset($_POST
 	//$active_key =  $keys["active"];
 	$ops = $_POST['ops'];
 	$opst = echo '<script>let ops = $ops;</script>';
-
+	//$password = echo '';
 		if($user != ''){
 
 
@@ -30,7 +30,53 @@ if (isset($_POST['action'])  && $_POST['action'] == 'acc_create' && isset($_POST
 }
 
 ?>
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+	<script src="https://unpkg.com/dsteem@^0.10.1/dist/dsteem.js"></script>
+<script type="text/javascript">
+		
+	    var Client = new dsteem.Client('https://api.steemit.com');
 
+    function suggestPassword() {
+        const array = new Uint32Array(10);
+        window.crypto.getRandomValues(array);
+        return 'P' + dsteem.PrivateKey.fromSeed(array).toString();
+    }
+
+    function getPrivateKeys(username, password, roles = ['owner', 'active', 'posting', 'memo']) {
+        const privKeys = {};
+        roles.forEach((role) => {
+            privKeys[role] = dsteem.PrivateKey.fromLogin(username, password, role).toString();
+            privKeys[`${role}Pubkey`] = dsteem.PrivateKey.from(privKeys[role]).createPublic().toString();
+        });
+
+        return privKeys;
+    };
+
+
+	        let password = suggestPassword();
+            console.log(password);
+            let created_by = 'dlike';
+
+            const ops = [];
+
+            let keys = getPrivateKeys(my_name, password);
+            console.log(keys);
+
+            const create_op = [
+              'create_claimed_account',
+              {
+                
+                creator: created_by,
+                new_account_name: my_name,
+                extensions: [],
+                json_metadata: '',
+                active: dsteem.Authority.from(keys.activePubkey),
+                memo_key: keys.memoPubkey,
+                owner: dsteem.Authority.from(keys.ownerPubkey),
+                posting: dsteem.Authority.from(keys.postingPubkey),
+              },
+            ];
+</script>
 
 /*
             
