d025a59444a7ea60b6d033e5adf972f58b3cff99
bestboom <certseek@gmail.com>
2019-07-31 12:40:26 +0500

    Update sign_check.php

--- public/helper/sign_check.php
+++ public/helper/sign_check.php
@@ -1,4 +1,58 @@
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<?php
+
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+
+if (isset($_POST['action'])  && $_POST['action'] == 'acc_create' && isset($_POST['user'])  && $_POST['user'] != ''){
+
+
+	$return = array();
+	$return['status'] = false;
+	$return['message'] = '';
+
+	$user =  $_POST['user'];
+	//$keys = $_POST['myKeys'];
+	//$keys   = json_decode("$keys", true);
+	//$active_key =  $keys["active"];
+	//$ops = $_POST['ops'];
+	//$opst = echo '<script>let ops = $ops;</script>';
+	//$password = echo '';
+	if($user != ''){ 	
+			echo '<script>
+
+				let ditp = (async function(){
+				toastr.error("phew... You forgot to enter URL");
+
+			})();</script>';
+			$return['status'] = true;
+			$return['message'] = 'Looks data done';
+		}
+		else{
+			$return['message'] = 'data not good.';
+		}
+	echo json_encode($return);
+	exit;
+}
+
+?>
+
+
+<!--
+            
+
+            steem.api.broadcast.sendOperations(ops, activekeyhere)
+            .then((r) => {
+            console.log(r);
+            })
+            .catch(e => {
+            console.log(e);
+            });
+
+
+
+            <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
 	<script src="https://unpkg.com/dsteem@^0.10.1/dist/dsteem.js"></script>
 <script type="text/javascript">
 		
@@ -48,58 +102,4 @@
             ops.push(create_op);
             console.log(ops);
 </script>
-<?php
-
-	ini_set('display_errors', 1);
-	ini_set('display_startup_errors', 1);
-	error_reporting(E_ALL);
-
-
-if (isset($_POST['action'])  && $_POST['action'] == 'acc_create' && isset($_POST['user'])  && $_POST['user'] != ''){
-
-
-	$return = array();
-	$return['status'] = false;
-	$return['message'] = '';
-
-	$user =  $_POST['user'];
-	//$keys = $_POST['myKeys'];
-	//$keys   = json_decode("$keys", true);
-	//$active_key =  $keys["active"];
-	//$ops = $_POST['ops'];
-	//$opst = echo '<script>let ops = $ops;</script>';
-	//$password = echo '';
-	if($user != ''){ 	
-			echo '<script>
-				steem.api.broadcast.sendOperations(ops, activekeyhere)
-            .then((r) => {
-            console.log(r);
-            })
-            .catch(e => {
-            console.log(e);
-            });
-			</script>';
-			$return['status'] = true;
-			$return['message'] = 'Looks data done';
-		}
-		else{
-			$return['message'] = 'data not good.';
-		}
-	echo json_encode($return);
-	exit;
-}
-
-?>
-
-
-/*
-            
-
-            steem.api.broadcast.sendOperations(ops, activekeyhere)
-            .then((r) => {
-            console.log(r);
-            })
-            .catch(e => {
-            console.log(e);
-            });
-*/
+-->
