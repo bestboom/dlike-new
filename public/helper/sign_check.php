f15147d086fe33b6d00f4ab93cf5018acc760836
bestboom <certseek@gmail.com>
2019-08-01 22:26:29 +0500

    Update sign_check.php

--- public/helper/sign_check.php
+++ public/helper/sign_check.php
@@ -1,5 +1,35 @@
 <?php
 
+broadcast($trx);
+    public function broadcast($trx)
+    {
+        $curl = curl_init();
+
+        curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.steemit.com/broadcast",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => "",
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 30,
+            CURLOPT_POSTFIELDS => $trx,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => "POST",
+        ));
+
+        $response = curl_exec($curl);
+        $err = curl_error($curl);
+
+        curl_close($curl);
+
+        if ($err) {
+            return json_decode('{"error":"server_comms","error_description":"Failed to connect to the server!"}');
+        } else {
+            return json_decode($response);
+        }
+    }
+
+?>
+
 if (isset($_POST['action'])  && $_POST['action'] == 'acc_create' && isset($_POST['user'])  && $_POST['user'] != ''){
 
 	$return = array();
