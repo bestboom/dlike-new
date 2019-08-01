9812fa42d6fb140103c10e4e87490833934d8323
bestboom <certseek@gmail.com>
2019-08-01 20:35:41 +0500

    Update create_account.php

--- public/helper/create_account.php
+++ public/helper/create_account.php
@@ -29,10 +29,8 @@ if (isset($_POST['action'])  && $_POST['action'] == 'acc_create' && isset($_POST
 
 	if (empty($errors)) {
     $publish = $accountGenerator->createAccount($created_by, $user, $owner_key, $active_key, $posting_key, $memo_key);
-    $state = $accountGenerator->broadcast($publish, $active_owner);
-	} else {
-        echo $errors;
-    }
+    $state = $accountGenerator->broadcast($publish);
+	} 
 
 	if ($state) { 
 			$return['status'] = true;
