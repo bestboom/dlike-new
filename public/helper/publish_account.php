ff8d4e6cd3bf30c24d083382be2cb28442b92980
darksepiol1 <53448658+darksepiol1@users.noreply.github.com>
2019-08-02 03:07:10 -0500

    Update publish_account.php

--- public/helper/publish_account.php
+++ public/helper/publish_account.php
@@ -28,23 +28,15 @@ class makeAccount
     
     public function broadcast($create)
     {
-        $curl = curl_init();
+        
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, "https://api.steemit.com/broadcast");
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, $create);
+        $response = curl_exec($ch);
+        $err = curl_error($ch);
+       
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => 'https://api.steemit.com/broadcast',
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_POSTFIELDS => $create,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST"
-        ));
-
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-
-        curl_close($curl);
 
         if ($err) {
             return json_decode('{"error":"server_comms","error_description":"Failed to connect to the server!"}');
