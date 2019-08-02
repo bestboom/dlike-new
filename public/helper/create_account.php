5b9237d4b14b05f1107410f46eb5e2ab83720e8b
bestboom <certseek@gmail.com>
2019-08-02 09:50:55 +0500

    Update create_account.php

--- public/helper/create_account.php
+++ public/helper/create_account.php
@@ -36,7 +36,7 @@ if (isset($_POST['action'])  && $_POST['action'] == 'acc_create' && isset($_POST
 			$return['status'] = true;
             $return['message'] = 'Account created'.$state;
 	} else {
-			$return['message'] = $publish;
+			$return['message'] = $state;
 
 	} 
 
