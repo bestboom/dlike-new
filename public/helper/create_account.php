369f97edeabc22162fa64c7ca7bbd365ebad05b9
bestboom <certseek@gmail.com>
2019-08-04 11:43:21 +0500

    Update create_account.php

--- public/helper/create_account.php
+++ public/helper/create_account.php
@@ -13,47 +13,6 @@ if (isset($_POST['action'])  && $_POST['action'] == 'acc_create' && isset($_POST
  
     $active_owner=getenv('active_account');
 
-	$user =  $_POST['user'];
-
-    $return = array();
-    $return['status'] = false;
-    $return['message'] = '';
-
-	if ($user !='') {
-    $state = exec("node app/public/js/newAccount.js", $output); 
-    echo $result = implode("\n", $output);
-	} 
-
-	if (isset($state)) { 
-			$return['status'] = true;
-            $return['message'] = 'Account Created'.$result;
-	} else {
-			$return['message'] = 'Failed'.$result;
-
-	} 
-
-    echo json_encode($return);
-    exit;
-
-} else {die('Some error');}
-?>
-
-
-<?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-function validator($data){
-    return htmlspecialchars(strip_tags(trim($data)));
-}
-
-if (isset($_POST['action'])  && $_POST['action'] == 'acc_create' && isset($_POST['user'])  && $_POST['user'] != ''){
- 
-    $active_owner=getenv('active_account');
-
     $user =  $_POST['user'];
 
     $return = array();
