d87ca57f6b5aded60d60bbd14d550e9c589283fb
Jochem Stoel <jochemstoel@gmail.com>
2019-08-04 13:03:32 +0200

    Update create_account.php

--- public/helper/create_account.php
+++ public/helper/create_account.php
@@ -21,9 +21,10 @@ if (isset($_POST['action'])  && $_POST['action'] == 'acc_create' && isset($_POST
 
     if ($_POST['user'] !='') {
   //  echo 12345;
-            $state = shell_exec('/usr/local/bin/node app/public/js/newAccount.js '.$user); 
-        $password = $state; // to check if trim() is causing error
-           // $password = trim($state); // do what you want with the password here
+        $here = dirname(__FILE__);.
+            $state = shell_exec("node {$here}/../js/newAccount.js \"{$user}\""); 
+        //$password = $state; // to check if trim() is causing error
+            $password = trim($state); // do what you want with the password here
 
             //echo json_encode($password); // this is the password
             $return['status'] = true;
