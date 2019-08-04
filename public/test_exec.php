e218447be5e237f52b379ef4f1390b92574201f3
Jochem Stoel <jochemstoel@gmail.com>
2019-08-04 12:24:22 +0200

    Update test_exec.php

--- public/test_exec.php
+++ public/test_exec.php
@@ -2,7 +2,7 @@
 $user = 'fanfeox';
 $here = dirname(__FILE__);
 echo "here: $here\n";
-$cmd = "/app/.heroku/node/bin/node {$here}/js/newAccount.js {$user}";
+$cmd = "node {$here}/js/newAccount.js \"{$user}\"";
 echo "cmd: $cmd\n";
 $output = shell_exec($cmd); 
 echo $output;
