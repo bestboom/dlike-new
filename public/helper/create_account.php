e8248d06498561656ac49caed6e35659da274621
Jochem Stoel <jochemstoel@gmail.com>
2019-08-04 11:44:06 +0200

    Update create_account.php

--- public/helper/create_account.php
+++ public/helper/create_account.php
@@ -3,20 +3,24 @@
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
-
+    
 //print_r($_POST);
 function validator($data){
     return htmlspecialchars(strip_tags(trim($data)));
 }
 
-if (isset($_POST['action'])  && $_POST['action'] == 'acc_create' && isset($_POST['user'])  && $_POST['user'] != ''){
- $user = $_POST['user'];
     $return = array();
     $return['status'] = false;
     $return['message'] = '';
+    
+    
+try {
+if (isset($_POST['action'])  && $_POST['action'] == 'acc_create' && isset($_POST['user'])  && $_POST['user'] != ''){
+ $user = $_POST['user'];
+
 
     if ($_POST['user'] !='') {
-        try {
+    
             $state = shell_exec('/usr/local/bin/node app/public/js/newAccount.js '.$user); 
         
             $password = trim($state); // do what you want with the password here
@@ -25,14 +29,16 @@ if (isset($_POST['action'])  && $_POST['action'] == 'acc_create' && isset($_POST
             $return['status'] = true;
             $return['message'] = 'account created'.$password;
         
-        } catch (Exception $e) {
+       
+    }
+} else {die('Some error');}
+    
+     } catch (Exception $e) {
 echo $e->getMessage();
         $return['status'] = false;
         $return['message'] = $e->getMessage();
-        echo json_encode($message);
+        echo json_encode(return);
 
 
         }
-    }
-} else {die('Some error');}
 ?>
