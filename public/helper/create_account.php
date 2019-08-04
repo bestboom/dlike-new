a739a8e39599580eca5a327ba6ad43840badeded
Jochem Stoel <jochemstoel@gmail.com>
2019-08-04 11:02:21 +0200

    Update create_account.php

--- public/helper/create_account.php
+++ public/helper/create_account.php
@@ -4,7 +4,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-print_r($_POST);
+//print_r($_POST);
 function validator($data){
     return htmlspecialchars(strip_tags(trim($data)));
 }
