f439d2f1b8eb9d86a852ea7fc223226d3e9b0ccd
bestboom <certseek@gmail.com>
2019-08-05 07:52:37 +0500

    Create password_modal.php

---
+++ public/template/modals/password_modal.php
@@ -0,0 +1,15 @@
+<div class="modal-body ">
+	<div class="transfer-respond">
+		<h4>Account Password</h4>
+		<p class="base-color respond_top">
+			Have you saved it securely?
+		</p><br>
+		<p>
+            Your private key is used to generate a signature for actions, such as signing-in and creating transactions in the Steem blockchain.
+            <b> We cannot recover your key if you lose it. </b>
+            Please securely store the key in a way that only you can access it.
+          </p>
+	</div>
+	<button type="button" class="btn btn-danger" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">No I ahve not saved!</span></button>
+	<button type="button" class="btn btn-success"><span aria-hidden="true">Yes I have saved!</span></button>
+</div>
