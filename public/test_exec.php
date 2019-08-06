1f69ae32acdf518a9a266bf3e71961f836039b1d
Jochem Stoel <jochemstoel@gmail.com>
2019-08-06 08:27:13 +0200

    Update test_exec.php

--- public/test_exec.php
+++ public/test_exec.php
@@ -3,3 +3,13 @@ Hi
 have you discord or telegram or some other direct way of communication
 
 can you directly accpet payment in skrill? 
+=====
+
+Skype: jochem.stoel
+Discord: jochemstoel#7529
+
+I prefer Skype but both is fine.
+
+I don't know about Skrill, I'll have to check it out.
+
+By the way, we are not supposed to talk outside of Freelancer.com it is against the rules.
