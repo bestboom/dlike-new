a3accf75ad9c8bd94c70b4b9220cae814dde1e4e
bestboom <certseek@gmail.com>
2019-08-06 12:07:32 +0500

    Update test_exec.php

--- public/test_exec.php
+++ public/test_exec.php
@@ -2,7 +2,7 @@
 $user = 'fanfeox';
 $here = dirname(__FILE__);
 echo "here: $here\n";
-$cmd = "node {$here}/js/newAccount.js \"{$user}\"";
+$cmd = "node {$here}/js/newAccounts.js \"{$user}\"";
 echo "cmd: $cmd\n";
 $output = shell_exec($cmd); 
 echo $output;
