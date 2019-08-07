6245e7702a61dd0e385c929da34d77b4d393593d
bestboom <certseek@gmail.com>
2019-08-07 14:41:13 +0500

    Update signup.js

--- public/js/signup.js
+++ public/js/signup.js
@@ -81,7 +81,7 @@ domReady(function () {
         var username = Input.value;
         //var letterNumber = /^[0-9a-zA-Z]+$/;
         //var letterNumber = /^[0-9a-zA-Z]*\.?[0-9a-zA-Z]+$/;
-        var letterNumber = /^[\0-9a-zA-Z]+.[\0-9a-zA-Z]{3,}$/;
+        var letterNumber = /^[\w]+.[\w]{3,}$/;
 
         if (username.length <= 2) {
             Message.innerHTML     = msg_notAllowed;
