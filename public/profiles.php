35d4f52fefab2ec82a89706792b2195adbabf3bb
bestboom <certseek@gmail.com>
2019-08-15 21:22:04 +0500

    Update profiles.php

--- public/profiles.php
+++ public/profiles.php
@@ -173,7 +173,7 @@ include('template/header5.php'); ?>
     		let activeDate = moment.utc($post.created + "Z", 'YYYY-MM-DD  h:mm:ss').fromNow();
 			let reputation = steem.formatter.reputation($post.author_reputation);
 
-    		$(cmt_content).append('<div>\n' +
+    		$(cmt_content).append('<div style="padding: 18px 10px 5px 5px;border-top:1px solid #ded4e4;width:100%;">\n' +
 				'\n' +
 				'<div style="padding-bottom:5px;"><span><a href="#"><img src="https://steemitimages.com/u/' + $post.author + '/avatar" alt="img" class="img-fluid rounded-circle" style="width:32px;margin-right:12px;"></a></span><span style="font-weight:600;padding-right:2px;"><a href="#">' + $post.author + '</a></span><span style="padding-right:4px;">('+reputation+')</span><span style="padding-right:5px;">in '+$post.category+'</span><span class="time"><i class="far fa-clock"></i> ' + activeDate + '</span></div>\n' +
 				'\n' +  
