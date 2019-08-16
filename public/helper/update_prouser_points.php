5fcfb2d9b29cd73854f1d57f1b4f2f187ea2d4b0
Oscar Lundberg <info@oscarlundberg.com>
2019-08-16 09:03:45 +0200

    Update update_prouser_points.php

--- public/helper/update_prouser_points.php
+++ public/helper/update_prouser_points.php
@@ -1,7 +1,7 @@
 <?php include('./../includes/config.php');
   $user = $_POST["user"];
   $sql = "SELECT total_points FROM prousers";
-  $result = $conn->query($sql2);
+  $result = $conn->query($sql);
   $row = $result->fetch_all();
   var_dump($row);
   $my_views = $row2['views'];
