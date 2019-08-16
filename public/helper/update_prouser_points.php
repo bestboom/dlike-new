99643c1c31db739b2f23cc50e272fd62bbc5931c
Oscar Lundberg <info@oscarlundberg.com>
2019-08-16 09:01:02 +0200

    Create update_prouser_points.php

---
+++ public/helper/update_prouser_points.php
@@ -0,0 +1,8 @@
+<?php include('./../includes/config.php');
+  $user = $_POST["user"];
+  $sql = "SELECT total_views FROM prousers";
+  $result = $conn->query($sql2);
+  $row = $result->fetch_all();
+  var_dump($row);
+  $my_views = $row2['views'];
+?>
