ba1701b95927bedbf948f602514b05dbd9b16c80
bestboom <certseek@gmail.com>
2019-08-16 23:36:12 +0500

    up

---
+++ public/helper/unfollow.php
@@ -0,0 +1,35 @@
+<?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+require_once "../helper/publish_unfollow.php";
+
+$follower = 'crypto.genius';
+echo $username = $_COOKIE['username'];
+$response = [];
+
+    $_json = ['follow',[
+        'follower'=> $username,
+        'following'=> $follower,
+        'what'=>['blog']
+    ]];
+
+$voteGenerator = new dlike\unfollowit\makeunFollow();
+    if (!empty($username)){
+
+    $publish = $voteGenerator->unfollowMe($username, $_json);
+    $state = $voteGenerator->broadcast($publish);
+    //var_dump($state);
+        if (isset($state->error)){
+            $response["success"] = false;
+            $response["message"] = $state->error_description;
+        }else{
+            $response["success"] = true;
+            $response["message"] = "You Unfollowed Successfully";
+        }
+
+    }
+print json_encode($response);
+?>
