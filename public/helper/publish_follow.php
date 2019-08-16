d0afe2a9d3243ff8d31eee69ac785ef0a14969e9
bestboom <certseek@gmail.com>
2019-08-16 20:17:01 +0500

    Update publish_follow.php

--- public/helper/publish_follow.php
+++ public/helper/publish_follow.php
@@ -18,7 +18,7 @@ class makeFollow
             ]
         ];
         //$fixed_str = json_encode($do_follow);
-        print($do_follow);
+        //print($do_follow);
         return $do_follow;
     }
 
