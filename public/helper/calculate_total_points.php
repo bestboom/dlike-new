4efa79526a80dc2ef87074da06104684701bfad5
Oscar Lundberg <info@oscarlundberg.com>
2019-08-16 15:31:42 +0200

    Update calculate_total_points.php

--- public/helper/calculate_total_points.php
+++ public/helper/calculate_total_points.php
@@ -79,6 +79,7 @@ for(input of users)
         value: totalPointValue
       },
     });
+    document.body.append("\nPOSTED " + totalPointValue + "points for user: " + input.username + "\n");
   });
 }
 
