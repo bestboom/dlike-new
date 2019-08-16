e98e44ff860c9305506b15134467ca49eeb1105c
bestboom <certseek@gmail.com>
2019-08-16 17:15:36 +0500

    up

--- public/helper/follow.php
+++ public/helper/follow.php
@@ -1,67 +1,10 @@
 <?php
 
-namespace dlike\follow;
-
-class makeFollow
-{
-
-    public function followMe($_json)
-    {
-        $do_follow = [
-            "operations"=> [
-                ["custom_json", [
-                    "required_auths"=> [],
-                    "required_posting_auths"=> $_COOKIE['access_token'],
-                    "id"=> 'follow',
-                    "json"=> $_json
-                ]]
-            ]
-        ];
-        $fixed_str = json_encode($do_follow);
-        //print($fixed_str);
-        return $fixed_str;
-    }
-
-
-    public function broadcast($post)
-    {
-
-        $curl = curl_init();
-
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://steemconnect.com/api/broadcast",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_POSTFIELDS => $post,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_HTTPHEADER => array(
-                "accept: application/json",
-                "authorization: " . $_COOKIE['access_token'],
-                "cache-control: no-cache",
-                "content-type: application/json",
-            ),
-        ));
-
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-
-        curl_close($curl);
-
-        if ($err) {
-            return json_decode('{"error":"server_comms","error_description":"Failed to connect to the server!"}');
-        } else {
-            return json_decode($response);
-        }
-    }
-}
-
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require_once "../helper/publish_follow.php";
 
 $follower = 'goldenwhale';
 echo $username = $_COOKIE['username'];
@@ -73,8 +16,8 @@ $response = [];
         "what"=>['blog']
     ]];
 
-$voteGenerator = dlike\follow\makeFollow();
-//$voteGenerator = new dlike\vote\makeVote();
+$voteGenerator = new dlike\follow\makeFollow();
+//$voteGenerator = new  dlike\vote\makeVote();
     if (!empty($username)){
 
     $publish = $voteGenerator->followMe($_json);
