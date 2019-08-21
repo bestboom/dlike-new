736f17fa0fd393877a2d474921c014d1b7bd9b85
bestboom <certseek@gmail.com>
2019-08-21 15:03:21 +0500

    Update header6.php

--- public/template/header6.php
+++ public/template/header6.php
@@ -21,7 +21,7 @@
         <link rel="canonical" href="<?php echo $og_url; ?>" />
     <? } ?>
     <?php if(basename($_SERVER['PHP_SELF']) == 'profile.php'){ ?>  
-        <title><?php echo ucfirst($prof_user); ?> Posts Shared on DLIKE</title>
+        <title><?php echo ucfirst($prof_user).'@('.$prof_user')'; ?> Posts Shared on DLIKE</title>
     <? } ?>    
         <title>Dlike :: Share To Get Rewarded</title>
         <meta name="description" content="Dlike is a blockchain based dApp where you share links from your own blog articles or any useful link that is informative for community and get rewarded if community likes your links with steem upvotes.">
