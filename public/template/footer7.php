3a3555638d8ce182f254753817b8136b7c1e6d18
bestboom <certseek@gmail.com>
2019-08-25 22:18:43 +0500

    up

---
+++ public/template/footer7.php
@@ -0,0 +1,155 @@
+    <div class="colto-section">
+        <div class="container">
+            <div class="colto-content-wrap">
+                <div class="colto-content">
+                    <h3>Getting started</h3>
+                    <p>We provide professional and secure trading services for investors</p>
+                </div>
+                <div class="colto-btn-group">
+                    <button class="btn callto-btn">Log In</button>
+                    <span>or</span>
+                    <button class="btn callto-btn">Create Account</button>
+                </div>
+            </div>
+        </div>
+    </div><!-- colto-section -->
+    <footer class="footer">
+        <div class="footer-upper-section">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-3 col-md-4">
+                        <div class="footer-logo">
+                            <a href="#">
+                                <img src="./images/logo.png" alt="img" class="img-responsive"/>
+                            </a>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-4">
+                        <div class="footer-info-list">
+                            <h4>About Us</h4>
+                            <ul>
+                                <li><a href="#">Our Team</a></li>
+                                <li><a href="#">Our Company</a></li>
+                                <li><a href="#">Contact Us</a></li>
+                                <li><a href="#">Token Listing</a></li>
+                                <li><a href="#">Join Us</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-4">
+                        <div class="footer-info-list">
+                            <h4>Learn</h4>
+                            <ul>
+                                <li><a href="#">Legal</a></li>
+                                <li><a href="#">Terms of Use</a></li>
+                                <li><a href="#">AML&CFT</a></li>
+                                <li><a href="#">Privacy Policy</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-2 col-md-4">
+                        <div class="footer-info-list">
+                            <h4>Help</h4>
+                            <ul>
+                                <li><a href="#">Support</a></li>
+                                <li><a href="#">API Support</a></li>
+                                <li><a href="#">Coin/Token Listing</a></li>
+                                <li><a href="#">Partnership</a></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-4">
+                        <div class="footer-info-list">
+                            <h4>Contact Us</h4>
+                            <ul class="contact-info">
+                                <li>Email:  <span>info.excoin@gmail.com</span></li>
+                                <li>Phone:   <span>+99 5589 54789</span></li>
+                            </ul>
+                            <ul class="social-style-two">
+                                <li>
+                                    <a href="#">
+                                        <i class="fab fa-facebook-f"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fab fa-twitter"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fab fa-google-plus-g"></i>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#">
+                                        <i class="fab fa-github"></i>
+                                    </a>
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="footer-icon-wrap">
+                <a href="#">
+                    <img src="./images/others/31.png" alt="img" class="img-responsive">
+                </a>
+            </div>
+        </div>
+        <div class="footer-bottom">
+            <div class="container">
+                <div class="footer-bottom-wrap">
+                    <div class="trade-volume-block">
+                        <ul>
+                            <li>
+                                <span>39151</span> Active Traders
+                            </li>
+                            <li>
+                                <span>4191 BTC</span> 24h Volume
+                            </li>
+                        </ul>
+                    </div>
+                    <div class="copyright-text">
+                        © 2019 <a href="#">Excoin</a>. All Rights Reserved
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script> -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
+<script src="/assets/js/popper.min.js"></script>
+<script src="/assets/js/bootstrap.min.js"></script>
+<script src="/assets/js/slick.min.js"></script>
+<script src="/assets/js/jquery.peity.min.js"></script>
+<script src="/assets/js/jquery.slimscroll.min.js"></script>
+<script src="/js/custom.js"></script>
+<script src="https://momentjs.com/downloads/moment.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/steemconnect@latest"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/4.2.2/jquery.form.js"></script>
+<script src="https://unpkg.com/dsteem@^0.10.1/dist/dsteem.js"></script>
+<script src="https://cdn.steemjs.com/lib/latest/steem.min.js"></script>
+<script src="/js/steemconnect.js"></script>
+<script src="/js/toaster.js"></script>
+<script src="/js/mint.js"></script>
+<script src="/js/posts.js"></script>
+<?php if(basename($_SERVER['PHP_SELF']) == 'welcome.php'){ ?> 
+<script src="/js/intlTelInput.js?1562189064761"></script>
+<script src="https://intl-tel-input.com/node_modules/intl-tel-input/build/js/intlTelInput.js?1562189064761"></script>
+<script src="/js/signup.js"></script>
+<? } ?> 
+<script async src="https://appsha1.cointraffic.io//js/?wkey=hLMQzDKQgG"></script>
+    <script>
+        function openNav() {
+            document.getElementById("mySidenav").style.width = "250px";
+        }
+
+        function closeNav() {
+            document.getElementById("mySidenav").style.width = "0";
+        }
+        function popup(e){var t=700;var n=400;var r=(screen.width-t)/2;var i=(screen.height-n)/2;var s="width="+t+", height="+n;s+=", top="+i+", left="+r;s+=", directories=no";s+=", location=no";s+=", menubar=no";s+=", resizable=no";s+=", scrollbars=no";s+=", status=no";s+=", toolbar=no";newwin=window.open(e,"windowname5",s);if(window.focus){newwin.focus()}return false}
+    </script>     
+</body>
+</html>
