76d6acbe5a148dcc6f173caaaaf8477bb8b239cc
bestboom <certseek@gmail.com>
2019-08-28 13:19:06 +0500

    up

--- public/privacy.php
+++ public/privacy.php
@@ -1,46 +1,49 @@
 <?php include('template/header5.php');?>
 </div>
-<div class="containe info-sec">
-	<h1>Privacy Policy</h1>
+<div class="containe">
+	<div class="info-sec">
+		<h1>Privacy Policy</h1>
 
-	<p>This Privacy Policy describes how DLIKE collectively referred to as "dlike.io" (or "we" or "us" or "our") collects, uses, and handles your data when you use our website, products and services ("Services"). Please take the time to carefully read through this policy. As you use our Services, we want to be clear how DLIKE uses your information and how your privacy is protected. By using the Services, you consent to the data practices detailed in this policy.</p>
-	<h2>1. Information You Provide to Us</h2>
-	<p>We collect information you provide directly to us when you use our Services. Depending on which Service you use, we may collect different information about you. <br>
-	This includes:</p>
-	<h3>1.1. Information Regarding Your Use of the Services</h3>
-	<p>We collect the content and other information you provide when you use our Services. This includes information used to create your account (e.g. username, phone number), account preferences and the content of information you post to the Services (e.g. text, photos, videos, links).</p>
-	<h3>1.2. Transactional Information</h3>
-	<p>If you purchase products or services from us (e.g. DLIKE Tokens), we will collect certain information from you, including your name, address, email address, and information about the product or service you are purchasing. Payments are processed by third-party payment processors (e.g. Stripe and PayPal), so please refer to the applicable processor's terms and privacy policy for more information about how payment information is processed and stored.</p>
-	<h3>1.3. Other Information</h3>
-	<p>You may choose to provide other information directly to us. For example, we may collect information when you fill out a form, participate in contests, sweepstakes or promotions, apply for a job, communicate with us via third-party sites and services, request customer support or otherwise communicate with us.</p>
-	<h2>2. Information We Collect Automatically</h2>
-	<p>When you access or use our Services, we may also automatically collect information about you. <br>
-	This includes:</p>
-	<h3>2.1. Log and Usage Data</h3>
-	<p>We may log information when you access and use the Services. This may include your IP address, user-agent string, browser type, operating system, referral URLs, device information (e.g., device IDs), pages visited, links clicked, user interactions (e.g., voting data), the requested URL, hardware settings, and search terms.</p>
-	<h3>2.2. Information Collected from Cookies</h3>
-	<p>We may receive information from cookies, which are pieces of data your browser stores and sends back to us when making requests. We use this information to improve your experience, understand user activity, and improve the quality of our Services. For example, we store and retrieve information about your preferred language and other settings. On how you can disable cookies, please see "Your Choices" below. For more information about cookies read our cookie policy section.</p>
-	<h3>2.3. Location Information</h3>
-	<p>With your consent, we may collect information about the specific location of your mobile device (for example, by using GPS or Bluetooth). You can revoke this consent at any time by changing the preferences on your device, but doing so may affect your ability to use all of the features and functionality of our Services.</p>
-	<h3>2.4. Social Sharing</h3>
-	<p>We may offer social sharing features or other integrated tools that let you share content or actions you take on our Services with other media. Your use of these features enables the sharing of certain information with your friends or the public, depending on the settings you establish with the third party that provides the social sharing feature. For more information about the purpose and scope of data collection and processing in connection with social sharing features, please visit the privacy policies of the third parties that provide these social sharing features (e.g., Tumblr, Facebook, Reddit, Pinterest, and Twitter).</p>
-	<h2>3. How We Use Information About You</h2>
-	<p>We use information about you to:</p>
-	<l1>	Provide, maintain, and improve the Services </l1>
-	<li>	Help protect the safety of DLIKEand our users, which includes blocking suspected spammers, addressing abuse, and enforcing the DLIKE user agreement and the Terms of Service</li>
-	<li>	Send you technical notices, updates, security alerts, invoices and other support and administrative messages</li>
-	<li>	Provide customer service</li>
-	<li>	Communicate with you about products, services, offers, promotions, and events, and provide other news and information we think will be of interest to you (for information about how to opt out of these communications, see "Your Choices" below)</li>
-	<li>	Monitor and analyze trends, usage, and activities in connection with our Services</li>
-	<li>	Personalize the Services and provide advertisements, content and features that match user profiles or interests</li>
+		<p>This Privacy Policy describes how DLIKE collectively referred to as "dlike.io" (or "we" or "us" or "our") collects, uses, and handles your data when you use our website, products and services ("Services"). Please take the time to carefully read through this policy. As you use our Services, we want to be clear how DLIKE uses your information and how your privacy is protected. By using the Services, you consent to the data practices detailed in this policy.</p>
+		<h2>1. Information You Provide to Us</h2>
+		<p>We collect information you provide directly to us when you use our Services. Depending on which Service you use, we may collect different information about you. <br>
+		This includes:</p>
+		<h3>1.1. Information Regarding Your Use of the Services</h3>
+		<p>We collect the content and other information you provide when you use our Services. This includes information used to create your account (e.g. username, phone number), account preferences and the content of information you post to the Services (e.g. text, photos, videos, links).</p>
+		<h3>1.2. Transactional Information</h3>
+		<p>If you purchase products or services from us (e.g. DLIKE Tokens), we will collect certain information from you, including your name, address, email address, and information about the product or service you are purchasing. Payments are processed by third-party payment processors (e.g. Stripe and PayPal), so please refer to the applicable processor's terms and privacy policy for more information about how payment information is processed and stored.</p>
+		<h3>1.3. Other Information</h3>
+		<p>You may choose to provide other information directly to us. For example, we may collect information when you fill out a form, participate in contests, sweepstakes or promotions, apply for a job, communicate with us via third-party sites and services, request customer support or otherwise communicate with us.</p>
+		<h2>2. Information We Collect Automatically</h2>
+		<p>When you access or use our Services, we may also automatically collect information about you. <br>
+		This includes:</p>
+		<h3>2.1. Log and Usage Data</h3>
+		<p>We may log information when you access and use the Services. This may include your IP address, user-agent string, browser type, operating system, referral URLs, device information (e.g., device IDs), pages visited, links clicked, user interactions (e.g., voting data), the requested URL, hardware settings, and search terms.</p>
+		<h3>2.2. Information Collected from Cookies</h3>
+		<p>We may receive information from cookies, which are pieces of data your browser stores and sends back to us when making requests. We use this information to improve your experience, understand user activity, and improve the quality of our Services. For example, we store and retrieve information about your preferred language and other settings. On how you can disable cookies, please see "Your Choices" below. For more information about cookies read our cookie policy section.</p>
+		<h3>2.3. Location Information</h3>
+		<p>With your consent, we may collect information about the specific location of your mobile device (for example, by using GPS or Bluetooth). You can revoke this consent at any time by changing the preferences on your device, but doing so may affect your ability to use all of the features and functionality of our Services.</p>
+		<h3>2.4. Social Sharing</h3>
+		<p>We may offer social sharing features or other integrated tools that let you share content or actions you take on our Services with other media. Your use of these features enables the sharing of certain information with your friends or the public, depending on the settings you establish with the third party that provides the social sharing feature. For more information about the purpose and scope of data collection and processing in connection with social sharing features, please visit the privacy policies of the third parties that provide these social sharing features (e.g., Tumblr, Facebook, Reddit, Pinterest, and Twitter).</p>
+		<h2>3. How We Use Information About You</h2>
+		<p>We use information about you to:</p>
+		<l1>	Provide, maintain, and improve the Services </l1>
+		<li>	Help protect the safety of DLIKEand our users, which includes blocking suspected spammers, addressing abuse, and enforcing the DLIKE user agreement and the Terms of Service</li>
+		<li>	Send you technical notices, updates, security alerts, invoices and other support and administrative messages</li>
+		<li>	Provide customer service</li>
+		<li>	Communicate with you about products, services, offers, promotions, and events, and provide other news and information we think will be of interest to you (for information about how to opt out of these communications, see "Your Choices" below)</li>
+		<li>	Monitor and analyze trends, usage, and activities in connection with our Services</li>
+		<li>	Personalize the Services and provide advertisements, content and features that match user profiles or interests</li>
 
-	<h2>4. How We Share Information</h2>
-	<p>When you use the Services, certain information may be shared with other users and the public. For example:</p>
-	<p>When you submit a post or comment to the Services, visitors and users of our Services will be able to see the content of your posts and comments, the username associated with your posts or comments and the date and time you originally submitted the post or comment. Although some parts of the Services may be private or quarantined, they may become public and you should take that into consideration before posting to the Services.</p>
-	<p>When other users view your profile, they will be able to see information about your activities on the Services such as your username, prior posts and comments, token balances and how long you have been a member of the Services.</p>
-	<p>Please note that, even when you delete your account, the posts, comments and messages you submit through the Services may still be viewable or available on our servers. For more information, see "Your Choices" below.</p>
-	<p>We will not share, sell or give away any of our user's personal information to third parties, unless one of the following circumstances applies:</p>
+		<h2>4. How We Share Information</h2>
+		<p>When you use the Services, certain information may be shared with other users and the public. For example:</p>
+		<p>When you submit a post or comment to the Services, visitors and users of our Services will be able to see the content of your posts and comments, the username associated with your posts or comments and the date and time you originally submitted the post or comment. Although some parts of the Services may be private or quarantined, they may become public and you should take that into consideration before posting to the Services.<br>
+		When other users view your profile, they will be able to see information about your activities on the Services such as your username, prior posts and comments, token balances and how long you have been a member of the Services.<br>
+		Please note that, even when you delete your account, the posts, comments and messages you submit through the Services may still be viewable or available on our servers. For more information, see "Your Choices" below.<br>
+		We will not share, sell or give away any of our user's personal information to third parties, unless one of the following circumstances applies:</p>
 
 
+
+	</div>	
 </div>
 <?php include('template/footer7.php'); ?> 
