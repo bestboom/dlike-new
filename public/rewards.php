c095559e74f5edbf9f98aa843c38bff3ca6fd78f
bestboom <certseek@gmail.com>
2019-08-31 09:38:18 +0500

    Update rewards.php

--- public/rewards.php
+++ public/rewards.php
@@ -32,7 +32,7 @@ $my_earnings = 0;
 
 $today_date = gmdate("d-m-Y");
 
-$sql1 = "SELECT SUM(total_points) as pts FROM prousers where DATE_FORMAT(last_points_update_time,'%y-%m-%d')='$today_date'";
+$sql1 = "SELECT SUM(total_points) as pts FROM prousers where DATE_FORMAT(last_points_update_time,'%d-%m-%y')='$today_date'";
 $result1 = $conn->query($sql1);
 
 if ($result1 && $result1->num_rows > 0) 
