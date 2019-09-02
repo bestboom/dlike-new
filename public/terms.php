b8eca96f380d75fbd5b6065bd1177f6fd8a16c65
bestboom <certseek@gmail.com>
2019-09-02 17:23:44 +0500

    up

---
+++ public/terms.php
@@ -0,0 +1,87 @@
+<?php include('template/header5.php');?>
+</div>
+<div class="container">
+	<div class="info-sec">
+		<h1>Terms and Conditions</h1>
+
+		<p>These Terms of Service ("Terms") apply to your access to and use of dlike.io and any other products or services that link to these Terms ("DLIKE"). DLIKE is a dapp on steem blockchain developed by BlockTalk Solutions Company (“DLIKE, “we” or “us”. By accessing or using DLIKE, you agree to be bound by these Terms. If you do not agree to these Terms, do not access or use DLIKE. If we make changes to these Terms, we will provide notice of those changes by updating the "Last Updated" date above or posting notice on DLIKE (website, mobile apps). Your continued use of DLIKE services will confirm your acceptance of the changes.</p>
+		<p>You understand and agree that these Terms apply solely to your access to and use of DLIKE services and that, when you use other DLIKE services such as mobile apps, browser extensions and website, the terms and policies particular to those services apply.</p>
+		<h2>1. Privacy Policy</h2>
+		<p>Please refer to our <a href="/privacy" target="_blank">Privacy Policy</a> for information about how we collect, use and disclose information about you.</p>
+		<h2>2. Eligibility</h2>
+		<p>DLIKE is not targeted towards, nor intended for use by, anyone under the age of 18. You must be at least 18 years of age to access or use DLIKE.</p>
+		<h2>2. Copyright and Limited License</h2>
+		<p>DLIKE contains data, text, photographs, images, video, audio, graphics, articles, comments, software, code, scripts and other content supplied by us, the Steem blockchain or our licensors, which we call "DLIKE Content." DLIKE Content is protected by intellectual property laws, including copyright and other proprietary rights of applicable countries. Except as explicitly stated in these Terms, DLIKE does not grant any express or implied rights to use DLIKE Content.</p>
+		<p>You are granted a limited, nonexclusive, non-transferable, and non-sublicensable license to access and use DLIKE and DLIKE Content for your personal, non-commercial use. This license is subject to these Terms and does not include any right to: (a) distribute, publicly perform or publicly display any DLIKE Content; (b) modify or otherwise make any derivative uses of DLIKE or DLIKE Content, or any portion thereof; (c) use any data mining, robots or similar data gathering or extraction methods; and (d) use DLIKE or DLIKE Content other than for their intended purposes. Any use of DLIKE or DLIKE Content other than as authorized in these Terms is strictly prohibited and will terminate the license granted herein. This license is revocable at any time.</p>
+
+
+
+		<h3>2.1. Log and Usage Data</h3>
+		<p>We may log information when you access and use the Services. This may include your IP address, user-agent string, browser type, operating system, referral URLs, device information (e.g., device IDs), pages visited, links clicked, user interactions (e.g., voting data), the requested URL, hardware settings, and search terms.</p>
+		<h3>2.2. Information Collected from Cookies</h3>
+		<p>We may receive information from cookies, which are pieces of data your browser stores and sends back to us when making requests. We use this information to improve your experience, understand user activity, and improve the quality of our Services. For example, we store and retrieve information about your preferred language and other settings. On how you can disable cookies, please see "Your Choices" below. For more information about cookies read our cookie policy section.</p>
+		<h3>2.3. Location Information</h3>
+		<p>With your consent, we may collect information about the specific location of your mobile device (for example, by using GPS or Bluetooth). You can revoke this consent at any time by changing the preferences on your device, but doing so may affect your ability to use all of the features and functionality of our Services.</p>
+		<h3>2.4. Social Sharing</h3>
+		<p>We may offer social sharing features or other integrated tools that let you share content or actions you take on our Services with other media. Your use of these features enables the sharing of certain information with your friends or the public, depending on the settings you establish with the third party that provides the social sharing feature. For more information about the purpose and scope of data collection and processing in connection with social sharing features, please visit the privacy policies of the third parties that provide these social sharing features (e.g., Tumblr, Facebook, Reddit, Pinterest, and Twitter).</p>
+		<h2>3. How We Use Information About You</h2>
+		<p>We use information about you to:</p>
+		<l1>	Provide, maintain, and improve the Services </l1>
+		<li>	Help protect the safety of DLIKEand our users, which includes blocking suspected spammers, addressing abuse, and enforcing the DLIKE user agreement and the Terms of Service</li>
+		<li>	Send you technical notices, updates, security alerts, invoices and other support and administrative messages</li>
+		<li>	Provide customer service</li>
+		<li>	Communicate with you about products, services, offers, promotions, and events, and provide other news and information we think will be of interest to you (for information about how to opt out of these communications, see "Your Choices" below)</li>
+		<li>	Monitor and analyze trends, usage, and activities in connection with our Services</li>
+		<li>	Personalize the Services and provide advertisements, content and features that match user profiles or interests</li>
+
+		<h2>4. How We Share Information</h2>
+		<p>When you use the Services, certain information may be shared with other users and the public. For example:</p>
+		<p>When you submit a post or comment to the Services, visitors and users of our Services will be able to see the content of your posts and comments, the username associated with your posts or comments and the date and time you originally submitted the post or comment. Although some parts of the Services may be private or quarantined, they may become public and you should take that into consideration before posting to the Services.<br>
+		When other users view your profile, they will be able to see information about your activities on the Services such as your username, prior posts and comments, token balances and how long you have been a member of the Services.<br>
+		Please note that, even when you delete your account, the posts, comments and messages you submit through the Services may still be viewable or available on our servers. For more information, see "Your Choices" below.<br>
+		We will not share, sell or give away any of our user's personal information to third parties, unless one of the following circumstances applies:</p>
+		<li>	Except as it relates to advertisers and our ad partners, we may share information with vendors, consultants, and other service providers who need access to such information to carry out work for us</li>
+		<li>	If you participate in contests, sweepstakes, promotions, special offers or other events or activities in connection with our Services, we may share information with entities that partner with us to provide these offerings</li>
+		<li>	We may share information (and will attempt to provide you with prior notice, to the extent legally permissible) in response to a request for information if we believe disclosure is in accordance with or required by, any applicable law, regulation, legal process or governmental request</li>
+		<li>	We may share information in response to an emergency if we believe it's necessary to prevent imminent and serious bodily harm to a person</li>
+		<li>	We may share information if we believe your actions are inconsistent with our user agreements or other DLIKE policies or to protect the rights, property, and safety of ourselves and others</li>
+		<li>	We may share information between and among DLIKE, and its current and future parents, affiliates, subsidiaries and other companies under common control and ownership</li>
+		<li>	We may share information with your consent or at your direction</li>
+		<p>We may share aggregated or de-identified information, which cannot reasonably be used to identify you.</p>
+		<h2>5. Ads and Analytics Partners</h2>
+		<p>We may partner with third-party advertisers, ad networks and analytics providers to deliver advertising and content targeted to your interests and to better understand your use of the Services. These third parties may collect information sent by your computer, browser or mobile device in response to a request for content, such as unique identifiers, your IP address, or other information about your computer or device. For example:</p>
+		<h3>5.1. Advertisers and Ad Networks</h3>
+		<p>Our ad partners and network may use cookies and use related technologies to collect information when ads are delivered to you on our Services, but DLIKE does not link to or provide your actual DLIKE account details to these advertising partners. This means that DLIKE does not share your individual account browsing habits with advertisers. DLIKE cannot see advertisers" cookies and advertisers will not see DLIKE cookies.</p>
+		<h3>5.2. Analytics Partners</h3>
+		<p>We use analytics partners (such as Google Analytics) to help analyze usage and traffic for our Services. As an example, we may use analytics partners to analyze and measure, in the aggregate, the number of unique visitors to our Services.
+		For more information about how you may control the collection or use of information for advertising and analytics purposes, please see "Your Choices."</p>
+		<h2>6. Security</h2>
+		<p>We take reasonable measures to help protect information about you from loss, theft, misuse and unauthorized access, disclosure, alteration, and destruction.</p>
+		<h2>7. Children under 18</h2>
+		<p>Although we welcome users from all walks of life, DLIKE is not intended or directed at individuals under the age of 18. Therefore, individuals under the age of 18 may not create an account or otherwise access or use the Services.</p>
+		<h2>8. Your Choices</h2>
+		<p>As a DLIKE user, you have choices about how to protect and limit the collection, us, and disclosure of, information about you. This includes:</p>
+		<h3>8.1. Preferences</h3>
+		<p>We may provide you with tools and preference settings that allow you to access, correct, delete, and modify information associated with your account.</p>
+		<h3>8.2. Delete Account</h3>
+		<p>DLIKE does not receive or store your Account password or the unencrypted keys and addresses. DLIKE uses SteemConnect authentication. In order to delete your connection with DLIKE, please visit your SteemConnect dashboard and revoke the authentication of DLIKE. Please note, however, that the posts, comments and messages you submitted prior to deleting your account will still be visible to others, unless you delete such content. We may also retain certain information about you as required by law or for legitimate business purposes after you delete your account. Any information that has been published to the Steem blockchain will also remain indefinitely.</p>
+		<h3>8.3. Cookies</h3>
+		<p>Most web browsers are set to accept cookies by default. If you prefer, you can usually choose to set your browser to remove or reject first- and third-party cookies. Please note that if you choose to remove or reject cookies, this could affect the availability and functionality of our Services. For more information about cookies read our cookie policy section.</p>
+		<h3>8.4. Third-Party Advertising and Analytics</h3>
+		<p>Some analytics providers may provide specific opt-out mechanisms (e.g. Google Analytics Opt-out), and we will provide, as needed, additional tools and third-party services that allow you to better understand cookies and how you can opt-out. You have the ability to opt out of having your web browsing information used for behavioral advertising purposes. For more information about behavioral advertising, or to opt out, please visit <a href="https://www.aboutads.info/choices" target="_blank">www.aboutads.info/choices</a>.</p>
+		<h3>8.5. Do Not Track</h3>
+		<p>Do Not Track ("DNT") is a privacy preference that you can set in most web browsers. We support DNT in our Services. There is no accepted standard on how to respond to web browsers" DNT signals. When you have DNT enabled, we may still use information collected for analytics and measurement purposes or to otherwise provide our Services (e.g DLIKE buttons) but we will not load any third-party trackers. You may however, opt out of having information about you collected and used for behavioral advertising purposes, as described above.</p>
+		<h3>8.6. Promotional Communications</h3>
+		<p>You may opt out of receiving promotional communications from us by following the instructions in those communications. If you opt out, we may still send you non-promotional communications, such as information about your account or your use of our Services.</p>
+		<h3>8.7. Mobile Notifications</h3>
+		<p>With your consent, we may send promotional and non-promotional push notifications or alerts to your mobile device. You can deactivate these messages at any time by changing the notification settings on your mobile device.</p>
+		<h3>8.8. International Data Transfers</h3>
+		<p>By accessing or using the Services or otherwise providing information to us, you consent to the processing, transfer and storage of information in and to applicable countries, where you may not have the same rights as you do under local law.</p>
+		<h2>9. Changes</h2>
+		<p>We may change this Privacy Policy from time to time. If we do, we will let you know by revising the date at the top of the policy. If we make a change to this policy that, in our sole discretion, is material, we will provide you with additional notice (such as adding a statement to announcements on our social channels, the front page of the Services or sending you a notification). We encourage you to review the Privacy Policy whenever you access or use our Services or otherwise interact with us to stay informed about our information practices and the ways you can help protect your privacy. If you continue to use our Services after Privacy Policy changes go into effect, you consent to the revised policy.</p>
+		<h2>10. Contact Us</h2>
+		<p>If you have any questions about this Privacy Policy, please email admin&#64;dlik.io.</p>
+
+	</div>	
+</div>
+<?php include('template/footer7.php'); ?> 
