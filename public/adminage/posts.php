013f9a5f95f6f16e448637358a51f449004152bf
bestboom <certseek@gmail.com>
2019-09-03 11:42:36 +0500

    Update posts.php

--- public/adminage/posts.php
+++ public/adminage/posts.php
@@ -63,7 +63,7 @@
       </div>
       
 </div>
-<?php include('../template/footer2.php'); ?>
+<?php include('../template/footer5.php'); ?>
 
 <link rel="stylesheet" href="//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
