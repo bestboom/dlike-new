d64586df72f2fafdb8c2df7dfde05e7361c226e9
bestboom <certseek@gmail.com>
2019-09-03 23:43:46 +0500

    Update tags.php

--- public/tags.php
+++ public/tags.php
@@ -1,53 +1,6 @@
 <?php include('template/header5.php');  ?>
 <input type="hidden" id="c_username" value="<?php echo $_COOKIE['username'];?>"/>
-    <?php if($_COOKIE['username'] == ''){ ?>
-        <div class="container">
-            <div class="row home-banner">
-                <div class="col-md-3 main main_offer">
-                    <div class="daily">
-                        <div class="daily_box"><p class="daily_start">10</p><p class="daily_mid">STEEM</p><p class="daily_end">Daily</p></div>
-                    </div>
-                </div>
-                <div class="col-md-6">
-                    <div class="banner-content">
-                        <h2>Dlike - Where You Get Liked</h2>
-                        <p>Share What you like with community <br>Get rewarded if community likes your shares</p>
-                        <h5>Daily Top Recommended Post (most dlikes) is Rewarded By Dlike</h5>
-                        <h5>Anyone can recommend a post for which steem account is not compulsory</h5>
-                        <!-- <h2 class="annc">Starting 11th March</h2> -->
-                    </div>
-                </div>
-                <div class="col-md-3 main_offer">
-                    <div class="daily">
-                        <div class="daily_box"><p class="daily_start">2500</p><p class="daily_mid">DLike Tokens</p><p class="daily_end">Daily</p></div>
-                    </div>
-                </div>
-             </div>
-         </div>
-            <? } else { ?>
-                <div class="banner-content home-connect">
-                    <div class="news-headline-block">
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-1 col-md-2">
-                                <i class="fas fa-volume-up vol"></i>
-                            </div>
-                            <div class="col-lg-11 col-md-10">
-                                <div class="news-headlines-block">
-                                    <div class="news-headlines-slider ticker">
-                                        <ul>
-                                            <li>Dlike will soon start token sale for steemians with huge bonus.</li>
-                                        </ul>
-                                    </div><!-- news-headlines-slider -->
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-            <? } ?>
-            </div>
-        </div>
-    </div><!-- sub-header -->
-    
+</div><!-- sub-header -->
     
     <div class="latest-post-section">
 	<div class="container">
