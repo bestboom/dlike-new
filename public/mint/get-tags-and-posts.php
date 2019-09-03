e7cf2e6488eac537d25037441db538542ce7eae9
bestboom <certseek@gmail.com>
2019-09-04 00:28:26 +0500

    up

--- public/mint/get-tags-and-posts.php
+++ public/mint/get-tags-and-posts.php
@@ -26,34 +26,35 @@ function timeago($date) {
    }
 }
 	
-if(isset($_REQUEST['tagname']) && $_REQUEST['tagname'] != "") {
+if(isset($_POST['tagname']) && $_POST['tagname'] != "") {
 	
 	  
-    	$sql = "SELECT postid FROM posttags where tagname = '".$_REQUEST['tagname']."'";
+    	$sql = "SELECT postid FROM posttags where tagname = '".$_POST['tagname']."'";
 		$result = $conn->query($sql);
 		if ($result->num_rows > 0) {
-			while($row = $result->fetch_assoc()) 
-			{
-				$postid = $row['postid'];
-			}
-			$sql1 = "SELECT * FROM steemposts where id IN (".$postid.") order by created_at DESC";
-			$result1 = $conn->query($sql1);
-			if ($result1->num_rows > 0) {
-				while($row1 = $result1->fetch_assoc()) {
-				    
-					$json_metadata = json_decode($row1['json_metadata'],true);
-					$data['username'] = $row1['username'];
-					$data['permlink'] = $row1['permlink'];
-					$data['title'] = $row1['title'];
-					$data['category'] = $json_metadata['category'];
-					$data['created_at'] = timeago($row1['created_at']);
-					$data['imgsrc'] = $json_metadata['image'];
+		while($row = $result->fetch_assoc()) 
+		{
+			$postid = $row['postid'];
+		}
+		$sql1 = "SELECT * FROM steemposts where id IN (".$postid.") order by created_at DESC";
+		$result1 = $conn->query($sql1);
+		if ($result1->num_rows > 0) {
+			while($row1 = $result1->fetch_assoc()) {
+			    
+				$json_metadata = json_decode($row1['json_metadata'],true);
+				$data['username'] = $row1['username'];
+				$data['permlink'] = $row1['permlink'];
+				$data['title'] = $row1['title'];
+				$data['category'] = $json_metadata['category'];
+				$data['created_at'] = timeago($row1['created_at']);
+				$data['imgsrc'] = $json_metadata['image'];
 
-					$strReturn['data_row'][] = $data;
-				}
-				$strReturn['status'] = 'OK';
+				$strReturn['data_row'][] = $data;
 			}
-		} else {
+			$strReturn['status'] = 'OK';
+		}
+		
+	} else {
 		$strReturn['status'] = 'error';
 	}
   	echo json_encode($strReturn);die;
@@ -69,6 +70,27 @@ if(isset($_REQUEST['catname']) && $_REQUEST['catname'] != "") {
 			
 			$json_metadata = json_decode($row1['json_metadata'],true);
 			if(strtolower($json_metadata['category']) == strtolower($_REQUEST['catname'])) {
+				
+				$userstatus = "SELECT * FROM userstatus where username = '".$row1['username']."'";
+				$userstatusresult = $conn->query($userstatus);
+				if ($userstatusresult->num_rows > 0) {
+					while($row = $userstatusresult->fetch_assoc()) {
+						$data['userstatus'] = $row['status'];
+					}
+				} else {
+					$data['userstatus'] = '';
+				}
+				
+				$poststatus = "SELECT * FROM poststatus where permlink = '".$row1['permlink']."'";
+				$resultpoststatus = $conn->query($poststatus);
+				if ($resultpoststatus->num_rows > 0) {
+					while($row = $resultpoststatus->fetch_assoc()) {
+						$data['poststatus'] = $row['status'];
+					}
+				} else {
+					$data['poststatus'] = 'error';
+				}
+				
 				
 				$data['username'] = $row1['username'];
 				$data['permlink'] = $row1['permlink'];
