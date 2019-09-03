792cdc015d7901e2ed0ce02961bf349d807548ef
bestboom <certseek@gmail.com>
2019-09-03 14:08:23 +0500

    Update tags.php

--- public/tags.php
+++ public/tags.php
@@ -1,4 +1,4 @@
-<?php include('template/header2.php');  ?>
+<?php include('template/header5.php');  ?>
 <input type="hidden" id="c_username" value="<?php echo $_COOKIE['username'];?>"/>
     <?php if($_COOKIE['username'] == ''){ ?>
         <div class="container">
@@ -155,7 +155,7 @@
 		  </div>
 
 		  
-<?php include('template/footer2.php'); ?>
+<?php include('template/footer3.php'); ?>
 <style>
 .showcursor{cursor:pointer;}
 .defaultcoloruser{color:gray;}
