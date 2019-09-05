af09d7ac93386e66bb64758825cde5f9b8b8d8a7
Oscar Lundberg <info@oscarlundberg.com>
2019-09-05 13:38:38 +0200

    Update gettposts.php

--- public/helper/gettposts.php
+++ public/helper/gettposts.php
@@ -32,44 +32,44 @@ if(isset($_POST['mytag']) && $_POST['mytag'] != "") {
 
 		if(!empty($json)) { $strReturn['status'] = 'OK'; $strReturn['tagr'] = $tag; } else {$strReturn['status'] = 'error'; $strReturn['tagr'] = $tag;}
 	*/
-    	$sql = "SELECT postid FROM posttags where tagname = '$tag'";
+  	$sql = "SELECT postid FROM posttags where tagname = '$tag'";
 		$result = $conn->query($sql);
-		if ($result->num_rows > 0) {
+		if ($result->num_rows > 0)
+    {
+			$row = $result->fetch_all();
+			$postid = $row['postid'];
+			$strReturn['tagrs'] = $postid;
+			$posts = explode(',', $postid);
+			$tag_posts = array_map(function(){ return '?'; }, $posts);
+      $idStr = implode(',', $postid);
+			$sqlz = "SELECT * FROM steemposts where id IN ('$idStr') order by created_at DESC";
+			//$sqlz = "SELECT * FROM steemposts where `id` IN ('". implode(',', array_map('intval', $postid)) ."') order by created_at DESC";
+			$resultz = $conn->query($sqlz);
 
-			while($row = $result->fetch_assoc())
-			{
-				$postid = $row['postid'];
-				$strReturn['tagrs'] = $postid;
-				$posts = explode(',', $postid);
-				$tag_posts = array_map(function(){ return '?'; }, $posts);
-                $idStr = implode(',', $postid);
-				$sqlz = "SELECT * FROM steemposts where id IN ('$idStr') order by created_at DESC";
-				//$sqlz = "SELECT * FROM steemposts where `id` IN ('". implode(',', array_map('intval', $postid)) ."') order by created_at DESC";
-				$resultz = $conn->query($sqlz);
+			if ($resultz->num_rows > 0)
+      {
+				while($rowz = $resultz->fetch_assoc())
+        {
+					$json_metadata = json_decode($rowz['json_metadata'],true);
+					$data['username'] = $rowz['username'];
+					$data['permlink'] = $rowz['permlink'];
+					$data['title'] = $rowz['title'];
+					$data['category'] = $json_metadata['category'];
+					$data['created_at'] = timeago($rowz['created_at']);
+					$data['imgsrc'] = $json_metadata['image'];
 
-				if ($resultz->num_rows > 0) {
-
-					while($rowz = $resultz->fetch_assoc()) {
-
-						$json_metadata = json_decode($rowz['json_metadata'],true);
-						$data['username'] = $rowz['username'];
-						$data['permlink'] = $rowz['permlink'];
-						$data['title'] = $rowz['title'];
-						$data['category'] = $json_metadata['category'];
-						$data['created_at'] = timeago($rowz['created_at']);
-						$data['imgsrc'] = $json_metadata['image'];
-
-						$strReturn['data_row'][] = $data;
-					}
-					$strReturn['status'] = 'OK';
+					$strReturn['data_row'][] = $data;
 				}
-				else {
-					$strReturn['status'] = 'posts not coming';
-					$strReturn['tagrs'] = $conn->error;
-				}
+				$strReturn['status'] = 'OK';
 			}
-		} else {
-		$strReturn['status'] = 'error';
+			else
+      {
+				$strReturn['status'] = 'posts not coming';
+				$strReturn['tagrs'] = $conn->error;
+			}
+		} else
+    {
+		    $strReturn['status'] = 'error';
 		}
   	echo json_encode($strReturn);die;
 }
