17c7a8c05a9df376c8c557cb9b981691fbd71497
bestboom <certseek@gmail.com>
2019-09-06 00:21:39 +0500

    Update gettposts.php

--- public/helper/gettposts.php
+++ public/helper/gettposts.php
@@ -27,34 +27,32 @@ function timeago($date) {
 }
 
 if(isset($_POST['mytag']) && $_POST['mytag'] != "") {
-	
 	  
     	$sql = "SELECT postid FROM posttags where tagname = '".$_POST['mytag']."'";
-	$result = $conn->query($sql);
-	if ($result->num_rows > 0) {
-		while($row = $result->fetch_assoc()) {
-			$postid = $row['postid'];
-		}
-		$sql1 = "SELECT * FROM steemposts where id IN (".$postid.")";
-		$result1 = $conn->query($sql1);
-		if ($result1->num_rows > 0) {
-			while($row1 = $result1->fetch_assoc()) {
-			    
-				$json_metadata = json_decode($row1['json_metadata'],true);
-				$data['username'] = $row1['username'];
-				$data['permlink'] = $row1['permlink'];
-				$data['metatags'] = $meta_array;
-				$strReturn['data_row'][] = $data;
+			$result = $conn->query($sql);
+			if ($result->num_rows > 0) {
+				while($row = $result->fetch_assoc()) {
+					$postid = $row['postid'];
+				
+					$sql1 = "SELECT * FROM steemposts where id IN (".$postid.")";
+					$result1 = $conn->query($sql1);
+					if ($result1->num_rows > 0) {
+						while($row1 = $result1->fetch_assoc()) {
+						    
+							$json_metadata = json_decode($row1['json_metadata'],true);
+							$data['username'] = $row1['username'];
+							$data['permlink'] = $row1['permlink'];
+							$data['metatags'] = $meta_array;
+							$strReturn['data_row'][] = $data;
+						}
+						$strReturn['status'] = 'OK';
+					}
+				}				
+			} else {
+				$strReturn['status'] = $conn->error;
 			}
-			$strReturn['status'] = 'OK';
-		}
-		
-		
-	} else {
-		$strReturn['status'] = 'error';
-	}
-  	echo json_encode($strReturn);die;
-}
+  			echo json_encode($strReturn);die;
+}			
 
 /*
 if(isset($_POST['mytag']) && $_POST['mytag'] != "")
