566d0275b67a9e5c86029d26c0bb34e9a6c889d7
bestboom <certseek@gmail.com>
2019-09-06 22:01:08 +0500

    Update footer7.php

--- public/template/footer7.php
+++ public/template/footer7.php
@@ -72,10 +72,11 @@
 <script src="/assets/js/popper.min.js"></script>
 <script src="/assets/js/jquery.peity.min.js"></script>
 <script src="/assets/js/jquery.slimscroll.min.js"></script>
+<script src="/js/custom.js"></script>
 -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js"></script>
 <script src="/assets/js/bootstrap.min.js"></script>
-<script src="/js/custom.js"></script>
+
 <script src="https://momentjs.com/downloads/moment.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/steemconnect@latest"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js"></script>
