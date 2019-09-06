72b7b94e7fc3ee1cbf81f2e31148fd39170876f1
bestboom <certseek@gmail.com>
2019-09-06 23:29:26 +0500

    Update check_share.php

--- public/helper/check_share.php
+++ public/helper/check_share.php
@@ -12,7 +12,7 @@ function validator($data){
 
 //if (isset($_POST["url"])){
     $url = $_GET['url'];
-    $sqls = "SELECT ext_url FROM steemposts WHERE ext_url = '$url' and created_at > now() - INTERVAL 48 HOUR"; 
+    $sqls = "SELECT ext_url FROM steemposts WHERE ext_url = '$url' and created_at > now() - INTERVAL 96 HOUR"; 
     $resultAmount = $conn->query($sqls);
         if ($resultAmount->num_rows > 0) {
 
