12075b775a92a12d2d38983ad1cc69f4dfd3aad3
bestboom <certseek@gmail.com>
2019-09-07 12:49:08 +0500

    Update submit_post.php

--- public/helper/submit_post.php
+++ public/helper/submit_post.php
@@ -55,10 +55,9 @@ $postGenerator = new dlike\post\makePost();
 	if (isset($state->result)) { 
 	// insert into DB	
 		$tags = $_POST['tags'];
-		//$title= mysql_real_escape_string($title);
-		$addposts = "INSERT INTO steemposts (`username`,`title`, `permlink`, `post_tags`, `ext_url`, `img_url`, `parent_ctegory`,`created_at`) VALUES ('".$_COOKIE['username']."','".$title."', '".$permlink."', '".$tags."', '".$url."', '".$urlImage."', '".$category."','".date("Y-m-d H:i:s")."')";
-
-			$addpostsquery = $conn->query($addposts);
+		$post_title= mysqli_real_escape_string($conn, $_POST['title']);
+		$addposts = "INSERT INTO steemposts (`username`,`title`, `permlink`, `post_tags`, `ext_url`, `img_url`, `parent_ctegory`,`created_at`) VALUES ('".$_COOKIE['username']."','".$post_title."', '".$permlink."', '".$tags."', '".$url."', '".$urlImage."', '".$category."','".date("Y-m-d H:i:s")."')";
+		$addpostsquery = $conn->query($addposts);
 			
 		$post_id = mysqli_insert_id($conn);
 
