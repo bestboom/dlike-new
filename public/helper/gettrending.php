a9c3a03c540fde00b0e7871164f422b197eeb71c
bestboom <certseek@gmail.com>
2019-09-09 21:40:56 +0500

    Update gettrending.php

--- public/helper/gettrending.php
+++ public/helper/gettrending.php
@@ -14,7 +14,7 @@
 			if (strpos($row['tagname'], 'dlike') === false && strpos($row['tagname'], ' ') === false) {
 				//$html .= '<div class="colxs-1"><a href="/tags/'.$row['tagname'].'">'.$row['tagname'].'</a></div>';
 				
-				$trending_html .= '<a class="nav-item nav-link" href="/tags/'.$row['tagname'].'" role="tab" data-toggle="tab">'.strtoupper($row['tagname']).'&nbsp;<button type="button" class="close closeBtn" aria-label="Close"><span aria-hidden="true"></span></button></a>';
+				$trending_html .= '<div class="wrapper"><nav class="nav nav-tabs list-2 mt-2" id="myTab" role="tablist"><a class="nav-item nav-link" href="/tags/'.$row['tagname'].'" role="tab" data-toggle="tab">'.strtoupper($row['tagname']).'&nbsp;<button type="button" class="close closeBtn" aria-label="Close"><span aria-hidden="true"></span></button></a></nav></div>';
 				++$counter;
 			}
 			
