962ab7cc45d1ff8700b30d62d796422118bcb5b5
bestboom <certseek@gmail.com>
2019-09-09 12:26:47 +0500

    Update gettrending.php

--- public/helper/gettrending.php
+++ public/helper/gettrending.php
@@ -4,16 +4,16 @@
 	error_reporting(0);
     	require '../includes/config.php';
 	  
-    	$sql = "SELECT tagname, COUNT(1) AS tag_count FROM posttags WHERE updated_at > DATE_SUB( NOW(), INTERVAL 24 HOUR) Group by tagname DESC Limit 10";
+    	$sql = "SELECT tagname, count(*) FROM posttags WHERE updated_at > DATE_SUB( NOW(), INTERVAL 24 HOUR) Group by tagname order by count(*) DESC Limit 5";
 	$result = $conn->query($sql);
 	if ($result->num_rows > 0) {
 		$html = '<div class="colxs-1 trendingword">Trending Now ></div>';
 		$counter = 0; 
 		while($row = $result->fetch_assoc()) {
-			if (strpos($row['tagname'], 'dlike') === false && $counter < 10) {
+			//if (strpos($row['tagname'], 'dlike') === false && $counter < 10) {
 				$html .= '<div class="colxs-1"><a href="/tags/'.$row['tagname'].'">'.$row['tagname'].'</a></div>';
 				++$counter;
-			}
+			//}
 			
 		}
 		$strReturn['status'] = 'OK';
