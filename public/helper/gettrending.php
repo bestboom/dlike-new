00ddf58626b1fcfd8cce2956f64f032d5544cc27
bestboom <certseek@gmail.com>
2019-09-09 13:26:01 +0500

    up

--- public/helper/gettrending.php
+++ public/helper/gettrending.php
@@ -10,8 +10,10 @@
 		$html = '<div class="colxs-1 trendingword">Trending Now ></div>';
 		$counter = 0; 
 		while($row = $result->fetch_assoc()) {
-			if (strpos($row['tagname'], 'dlike') === false) {
-				$html .= '<div class="colxs-1"><a href="/tags/'.$row['tagname'].'">'.$row['tagname'].'</a></div>';
+			if (strpos($row['tagname'], 'dlike') === false && strpos($row['tagname'], ' ') === false) {
+				//$html .= '<div class="colxs-1"><a href="/tags/'.$row['tagname'].'">'.$row['tagname'].'</a></div>';
+				
+				$html .= '<a class="nav-item nav-link" href="/tags/'.$row['tagname'].'" role="tab" data-toggle="tab">'.strtoupper($row['tagname']).'&nbsp;<button type="button" class="close closeBtn" aria-label="Close"><span aria-hidden="true"></span></button></a>';
 				++$counter;
 			}
 			
