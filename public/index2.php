2e4038ea142dbfa953db99f5b67af8d571295a8c
bestboom <certseek@gmail.com>
2019-09-12 00:33:32 +0500

    Update index2.php

--- public/index2.php
+++ public/index2.php
@@ -585,7 +585,7 @@ $posttags = "SELECT tagname, count(*) FROM posttags WHERE updated_at > DATE_SUB(
 					'\n' +
 					'<div class="post-footer">\n' +
 					'<div class="post-author-block">\n' +
-					'<div class="author-info"><i class="fas fa-dollar-sign"></i><span>&nbsp;' + $post.pending_payout_value.substr(0, 4) + '</span> + <span id="se_token">0</span> DLIKER | <i class="fas fa-comments"></i>&nbsp;<span id="DlikeComments'+$post.permlink +$post.author +'">0</span></div>\n' +
+					'<div class="author-info"><i class="fas fa-dollar-sign"></i><span>&nbsp;' + $post.pending_payout_value.substr(0, 4) + '</span> + <span id="se_token'+$post.permlink +$post.author +'">0</span> DLIKER | <i class="fas fa-comments"></i>&nbsp;<span id="DlikeComments'+$post.permlink +$post.author +'">0</span></div>\n' +
 					'</div>\n' +
 					'<div class="post-comments">'+addfeaturedhtml+addposthtml+'| &nbsp;<a class="upvoting" data-toggle="modal" data-target="#upvoteModal" data-permlink="' + $post.permlink + '" data-author="' + $post.author + '"><i class="fas fa-chevron-circle-up" id="vote_icon'+$post.permlink +$post.author +'"></i></a><span>&nbsp; | ' + $post.active_votes.length + ' Votes</span></div>\n' +
 					'</div>\n' +
@@ -599,10 +599,10 @@ $posttags = "SELECT tagname, count(*) FROM posttags WHERE updated_at > DATE_SUB(
         		let author = $post.author;
         		let permlink = $post.permlink;	
 
-        		$.getJSON('https://scot-api.steem-engine.com/@'+author+'/'+permlink+'', function(data) {
+        		$.getJSON('https://scot-api.steem-engine.com/@'+$post.author+'/'+$post.permlink+'', function(data) {
     						console.log(data.DLIKER.pending_token);
     						let pending_token = (data.DLIKER.pending_token)/1000;
-    						$('#se_token').html(pending_token);
+    						$('#se_token' + $post.permlink + $post.author).html(pending_token);
 						});			
 
     		//check if voted
