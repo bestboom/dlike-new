d547dff20a63c2d76a92dd14a4f88d9121157dc7
bestboom <certseek@gmail.com>
2019-09-11 14:32:38 +0500

    Update scot.php

--- public/scot.php
+++ public/scot.php
@@ -8,7 +8,7 @@ $sct_result = json_decode($sct_response);
 echo '<br/>';
 echo $meta_data = $sct_result->DLIKER->json_metadata;
 echo '<br/>';
-echo $body = $meta_data[0]->image;
+echo $body = $meta_data[0]->community;
 echo '<br/>';
 $og_description = explode("\n\n#####\n\n",$sct_result->DLIKER->json_metadata[body]);
 var_dump($og_description);
