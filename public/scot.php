898a3b40f8469af0096093f68e272e89d37d5eaa
bestboom <certseek@gmail.com>
2019-09-11 12:15:08 +0500

    Update scot.php

--- public/scot.php
+++ public/scot.php
@@ -4,7 +4,11 @@ $post_url = "http://scot-api.steem-engine.com/@tophash/alibaba-set-for-big-chall
 $response = file_get_contents($post_url);
 $result = json_decode($response);
 
-echo $result->DLIKER->pending_token;
+echo $pending_amount = $result->DLIKER->pending_token;
+echo '<br/>';
+echo $precision = $result->DLIKER->precision;
+echo '<br/>';
+echo $pending_payout = number_format((float) $pending_amount, $precision, '.', ''); ;
 echo '<br/>';
 echo $cashout_time = $result->DLIKER->cashout_time;
 
@@ -12,7 +16,7 @@ include('template/footer.php');
 ?>
 
 <script type="text/javascript">
-	let cashout_time = <?=($cashout_time)?>;
+	let cashout_time = '<?=($cashout_time)?>';
 	console.log(cashout_time);
 	let time_remian = moment.utc(cashout_time + "Z", 'YYYY-MM-DD  h:mm:ss').fromNow();
 	console.log(time_remian);
