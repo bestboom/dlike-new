245e213243939d1bae9cae996ed0de1050d92fb4
bestboom <certseek@gmail.com>
2019-09-11 11:07:23 +0500

    Update scot.php

--- public/scot.php
+++ public/scot.php
@@ -1,9 +1,10 @@
 <?php
 
-$post_url = "http://scot-api.steem-engine.com/@tophash/alibaba-set-for-big-challenge-as-flamboyant-chairman-ma-departs";
+$post_url = "http://scot-api.steem-engine.com/@tophash/alibaba-set-for-big-challenge-as-flamboyant-chairman-ma-departs
+";
 $response = file_get_contents($post_url);
 $result = json_decode($response);
 
-var_dump($result);
+var_dump($result->pending_token);
 
 ?>
