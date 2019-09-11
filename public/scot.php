baba1b5a424d9236870d65d579ba744bbc02759f
bestboom <certseek@gmail.com>
2019-09-11 12:52:41 +0500

    Update scot.php

--- public/scot.php
+++ public/scot.php
@@ -1,10 +1,12 @@
 <?php
+$link = 'alibaba-set-for-big-challenge-as-flamboyant-chairman-ma-departs';
+$user = '@tophash';
 
-$post_url = "http://scot-api.steem-engine.com/@tophash/alibaba-set-for-big-challenge-as-flamboyant-chairman-ma-departs";
-$response = file_get_contents($post_url);
-$result = json_decode($response);
-
-echo $pending_amount = ($result->DLIKER->pending_token)/1000;
+echo $scot_url = "http://scot-api.steem-engine.com/$user/$link";
+$sct_response = file_get_contents($scot_url);
+$sct_result = json_decode($sct_response);
+echo '<br/>';
+echo $pending_amount = ($sct_result->DLIKER->pending_token)/1000;
 echo '<br/>';
 echo $precision = $result->DLIKER->precision;
 echo '<br/>';
