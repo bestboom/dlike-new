bc4e5d9220690c0bc2440453ae1b37fe8565c3e5
bestboom <certseek@gmail.com>
2019-09-12 22:01:38 +0500

    Update posts.js

--- public/js/posts.js
+++ public/js/posts.js
@@ -156,7 +156,7 @@ $(document).ready(function(){
 					'\n' +
 					'<div class="post-footer">\n' +
 					'<div class="post-author-block">\n' +
-					'<div class="author-info"><i class="fas fa-dollar-sign"></i><span>&nbsp;' + $post.pending_payout_value.substr(0, 4) + '</span></div>\n' +
+					'<div class="author-info"><i class="fas fa-dollar-sign"></i><span>&nbsp;' + $post.pending_payout_value.substr(0, 4) + '</span> <b>+</b> <span id="se_token'+$post.permlink +$post.author +'">0</span> <b>DLIKER</b></div>\n' +
 					'</div>\n' +
 					'<div class="post-comments"><a class="upvoting" data-toggle="modal" data-target="#upvoteModal" data-permlink="' + $post.permlink + '" data-author="' + $post.author + '"><i class="fas fa-chevron-circle-up" id="vote_icon'+$post.permlink +$post.author +'"></i></a><span>&nbsp;' + $post.active_votes.length + '</span>&nbsp; | &nbsp;<i class="fas fa-comments"></i>&nbsp;<span id="DlikeComments'+$post.permlink +$post.author +'">0</span></div>\n' +
 					'</div>\n' +
@@ -167,6 +167,12 @@ $(document).ready(function(){
         		let author = $post.author;
         		let permlink = $post.permlink;				
 
+        		$.getJSON('https://scot-api.steem-engine.com/@'+$post.author+'/'+$post.permlink+'', function(data) {
+    				console.log(data.DLIKER.pending_token);
+    				let pending_token = (data.DLIKER.pending_token)/1000;
+    				$('#se_token' + $post.permlink + $post.author).html(pending_token);
+				});	
+				
     		//check if voted
     		steem.api.getActiveVotes($post.author, $post.permlink, function(err, result) {
                 //console.log(result);
