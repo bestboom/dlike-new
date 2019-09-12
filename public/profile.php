f75b180b08dcc0864b9325a96af0f90fd718ff9e
bestboom <certseek@gmail.com>
2019-09-12 21:19:42 +0500

    Update profile.php

--- public/profile.php
+++ public/profile.php
@@ -444,16 +444,22 @@ include('template/header5.php');
 					'\n' +
 					'<div class="post-footer">\n' +
 					'<div class="post-author-block">\n' +
-					'<div class="author-info"><i class="fas fa-dollar-sign"></i><span>&nbsp;' + $post.pending_payout_value.substr(0, 4) + '</span> | <i class="fas fa-comments"></i>&nbsp;<span id="DlikeComments'+$post.permlink +$post.author +'">0</span></div>\n' +
+					'<div class="author-info"><i class="fas fa-dollar-sign"></i><span>&nbsp;' + $post.pending_payout_value.substr(0, 4) + '</span> <b>+</b> <span id="se_token'+$post.permlink +$post.author +'">0</span> <b>DLIKER</b></div>\n' +
 					'</div>\n' +
-					'<div class="post-comments"><a class="upvoting" data-toggle="modal" data-target="#upvoteModal" data-permlink="' + $post.permlink + '" data-author="' + $post.author + '"><i class="fas fa-chevron-circle-up" id="vote_icon'+$post.permlink +$post.author +'"></i></a><span>&nbsp; | ' + $post.active_votes.length + ' Votes</span></div>\n' +
+					'<div class="post-comments"><a class="upvoting" data-toggle="modal" data-target="#upvoteModal" data-permlink="' + $post.permlink + '" data-author="' + $post.author + '"><i class="fas fa-chevron-circle-up" id="vote_icon'+$post.permlink +$post.author +'"></i></a><span>' + $post.active_votes.length + '</span>&nbsp; | &nbsp;<i class="fas fa-comments"></i>&nbsp;<span id="DlikeComments'+$post.permlink +$post.author +'">0</span></div>\n' +
 					'</div>\n' +
 					'</div>\n' +
 				'</article></div>');
 				//getTotalLikes($post.author,$post.permlink, currentLikesDivElement);
 
         		let author = $post.author;
-        		let permlink = $post.permlink;				
+        		let permlink = $post.permlink;	
+
+            $.getJSON('https://scot-api.steem-engine.com/@'+$post.author+'/'+$post.permlink+'', function(data) {
+                console.log(data.DLIKER.pending_token);
+                let pending_token = (data.DLIKER.pending_token)/1000;
+                $('#se_token' + $post.author + $post.permlink).html(pending_token);
+            }); 
 
     		//check if voted
     		steem.api.getActiveVotes($post.author, $post.permlink, function(err, result) {
