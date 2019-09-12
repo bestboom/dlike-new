f28b259862bb3c2520af66acd168961235a2bcb3
bestboom <certseek@gmail.com>
2019-09-12 21:06:13 +0500

    Update tags.php

--- public/tags.php
+++ public/tags.php
@@ -81,9 +81,9 @@
 					'\n' +
 					'<div class="post-footer">\n' +
 					'<div class="post-author-block">\n' +
-					'<div class="author-info"><i class="fas fa-dollar-sign"></i><span class="pending_payout_value"></span> + <span id="se_token">0</span> DLIKER | <i class="fas fa-comments"></i>&nbsp;<span id="DlikeComments'+permlink +author +'">0</span></div>\n' +
+					'<div class="author-info"><i class="fas fa-dollar-sign"></i><span class="pending_payout_value"></span> <b>+</b> <span id="se_token">0</span> <b>DLIKER</b></div>\n' +
 					'</div>\n' +
-					'<div class="post-comments">&nbsp;<a class="upvoting" data-toggle="modal" data-target="#upvoteModal" data-permlink="' + permlink + '" data-author="' + author + '"><i class="fas fa-chevron-circle-up" id="vote_icon'+permlink +author +'"></i></a><span class="active_votes">&nbsp; |  Votes</span></div>\n' +
+					'<div class="post-comments">&nbsp;<a class="upvoting" data-toggle="modal" data-target="#upvoteModal" data-permlink="' + permlink + '" data-author="' + author + '"><i class="fas fa-chevron-circle-up" id="vote_icon'+permlink +author +'"></i></a><span class="active_votes"></span>&nbsp; |<&nbsp;i class="fas fa-comments"></i>&nbsp;<span id="DlikeComments'+permlink +author +'">0</span></div>\n' +
 					'</div>\n' +
 					'</div>\n' +
 					'</article></div>';
@@ -122,7 +122,7 @@
 						$('#article_'+permlink+' h4.post-title a').html(title);
 						$('#article_'+permlink+' p.post-tags').html(metatags);
 						$('#article_'+permlink+' span.pending_payout_value').html(res.pending_payout_value.substr(0, 4));
-						$('#article_'+permlink+' span.active_votes').html("&nbsp; | "+res.active_votes.length+" Votes");
+						$('#article_'+permlink+' span.active_votes').html("&nbsp;"+res.active_votes.length);
 						$('#article_'+permlink+' #post_time').html(created_time);
 
 						$.getJSON('https://scot-api.steem-engine.com/@'+author+'/'+permlink+'', function(data) {
