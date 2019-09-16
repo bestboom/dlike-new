2682398d536c93a117f6b2d5687371164f1fd963
bestboom <certseek@gmail.com>
2019-09-16 17:18:31 +0500

    Update footer.php

--- public/template/footer.php
+++ public/template/footer.php
@@ -85,7 +85,9 @@
 <script src="/js/steemconnect.js"></script>
 <script src="/js/toaster.js"></script>
 <script src="/js/mint.js"></script>
+<?php if(basename($_SERVER['PHP_SELF']) != 'post.php'){ ?> 
 <script src="/js/posts.js"></script>
+<? } ?>
 <?php if(basename($_SERVER['PHP_SELF']) == 'welcome.php'){ ?> 
 <script src="/js/intlTelInput.js?1562189064761"></script>
 <script src="https://intl-tel-input.com/node_modules/intl-tel-input/build/js/intlTelInput.js?1562189064761"></script>
