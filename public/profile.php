8d1ba4f033e90406dc3eb19eaac7ec551bd1c10f
bestboom <certseek@gmail.com>
2019-09-18 02:36:16 +0500

    Update profile.php

--- public/profile.php
+++ public/profile.php
@@ -649,6 +649,28 @@ include('template/header5.php');
 
 	    }
 	// steem upvotes  
-	$('body').popover({ selector: '[data-popover]', trigger: 'click hover', placement: 'auto', delay: { show: 50, hide: 400 } }); 
+	var originalLeave = $.fn.popover.Constructor.prototype.leave;
+    $.fn.popover.Constructor.prototype.leave = function (obj) {
+        var self = obj instanceof this.constructor ?
+            obj : $(obj.currentTarget)[this.type](this.getDelegateOptions()).data('bs.' + this.type)
+        var voters, timeout;
+
+        originalLeave.call(this, obj);
+
+        if (obj.currentTarget) {
+            voters = $(obj.currentTarget).siblings('.popover')
+            timeout = self.timeout;
+            voters.one('mouseenter', function () {
+
+                clearTimeout(timeout);
+
+                voters.one('mouseleave', function () {
+                    $.fn.popover.Constructor.prototype.leave.call(self, self);
+                });
+            })
+        }
+    };
+    $('body').popover({ selector: '[data-popover]', trigger: 'click hover', placement: 'auto', delay: { show: 50, hide: 400 } });
+
 });
 </script>
