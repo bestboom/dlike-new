304db80540c337ab118269879d553c4aef07c321
bestboom <certseek@gmail.com>
2019-09-20 22:47:28 +0500

    Update scot.php

--- public/scot.php
+++ public/scot.php
@@ -10,7 +10,7 @@ include('template/header5.php');
 echo $para = $_GET['data'];
 echo '<br>';
 $check_data = stripslashes(Trim($para));
-/*
+
 $search_keyword = str_replace("�", "'", $check_data);
 $search_keyword = '"'.$search_keyword.'"';
 
@@ -31,8 +31,41 @@ echo $pageData = curlGET_Text($googleUrl);
         die('duplicate');   
     }
 
-*/
+function curlGET_Text( $url )
+    {
+        $user_agent='Mozilla/5.0 (Windows NT 6.1; rv:8.0) Gecko/20100101 Firefox/8.0';
 
+        $options = array(
+
+            CURLOPT_CUSTOMREQUEST  =>"GET",        //set request type post or get
+            CURLOPT_POST           =>false,        //set to GET
+            CURLOPT_USERAGENT      => $user_agent, //set user agent
+            CURLOPT_COOKIEFILE     =>"cookie.txt", //set cookie file
+            CURLOPT_COOKIEJAR      =>"cookie.txt", //set cookie jar
+            CURLOPT_RETURNTRANSFER => true,     // return web page
+            CURLOPT_HEADER         => false,    // don't return headers
+            CURLOPT_FOLLOWLOCATION => true,     // follow redirects
+            CURLOPT_ENCODING       => "",       // handle all encodings
+            CURLOPT_AUTOREFERER    => true,     // set referer on redirect
+            CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect
+            CURLOPT_TIMEOUT        => 120,      // timeout on response
+            CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects
+        );
+
+        $ch      = curl_init( $url );
+        curl_setopt_array( $ch, $options );
+        $content = curl_exec( $ch );
+        $err     = curl_errno( $ch );
+        $errmsg  = curl_error( $ch );
+        $header  = curl_getinfo( $ch );
+        curl_close( $ch );
+
+        $header['errno']   = $err;
+        $header['errmsg']  = $errmsg;
+        $header['content'] = $content;
+        return $header;
+    }
+/*
 function googleCSEQueryCheck($searchQuery,$domain='com') {
     
     $apiKey = 'AIzaSyCVAXiUzRYsML1Pv6RwSG1gunmMikTzQqY';
@@ -72,7 +105,7 @@ function googleCSEQueryCheck($searchQuery,$domain='com') {
 
 
 
-/*
+
 $link = 'alibaba-set-for-big-challenge-as-flamboyant-chairman-ma-departs';
 $user = '@tophash';
 
