87ecb71ecdfbd6ada377bd1a26c3d92fa550c5c4
bestboom <certseek@gmail.com>
2019-09-23 00:19:27 +0500

    Update profile.php

--- public/profile.php
+++ public/profile.php
@@ -463,7 +463,7 @@ include('template/header5.php');
 	            $.getJSON('https://scot-api.steem-engine.com/@'+author+'/'+permlink+'', function(data) {
 	                console.log(data.DLIKER.pending_token);
 	                let my_pending_token = (data.DLIKER.pending_token)/1000;
-	                $('#se_profile_token'+$post.permlink +$post.author).html(my_pending_token);
+	                $('#se_profile_token'+permlink +author).html(my_pending_token);
 
 	                /*
 	                let voters = data.DLIKER.active_votes;
