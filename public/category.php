ee875edc0e5f16cccb12e3c08eb6f2e0b3453555
bestboom <certseek@gmail.com>
2019-09-23 16:09:50 +0500

    Update category.php

--- public/category.php
+++ public/category.php
@@ -88,7 +88,7 @@ include('template/header5.php');
 					'<div class="post-author-block">\n' +
 					'<div class="author-info"><i class="fas fa-dollar-sign"></i><span class="pending_payout_value"></span> <b>+</b> <span id="se_token" data-popover="true" data-html="true" data-content="">0</span> <b>DLIKER</b></div>\n' +
 					'</div>\n' +
-					'<div class="post-comments">&nbsp;<a class="upvoting" data-toggle="modal" data-target="#upvoteModal" data-permlink="' + permlink + '" data-author="' + author + '"><i class="fas fa-chevron-circle-up" id="vote_icon'+ newValue +'"></i></a><span class="active_votes"></span>&nbsp; | &nbsp;<i class="fas fa-comments"></i>&nbsp;<span id="DlikeComments'+permlink +author +'">0</span></div>\n' +
+					'<div class="post-comments">&nbsp;<a class="upvoting" data-toggle="modal" data-target="#upvoteModal" data-permlink="' + permlink + '" data-author="' + author + '"><i class="fas fa-chevron-circle-up" id="vote_icon"></i></a><span class="active_votes"></span>&nbsp; | &nbsp;<i class="fas fa-comments"></i>&nbsp;<span id="DlikeComments'+permlink +author +'">0</span></div>\n' +
 					'</div>\n' +
 					'</div>\n' +
 					'</article></div>';
@@ -152,9 +152,9 @@ include('template/header5.php');
 							var voterList = result;
 							for (let j = 0; j < voterList.length; j++) {
 								if (voterList[j].voter == c_username) { 
-									$('#vote_icon' +newValue).css("color", "RED"); 
-									$('#vote_icon' +newValue).click(function(){return false;});
-									$('#vote_icon' +newValue).hover(function() {toastr.error('hmm... Already Upvoted');})
+									$('#article_'+permlink+' #vote_icon').css("color", "RED"); 
+									$('#article_'+permlink+' #vote_icon').click(function(){return false;});
+									$('#article_'+permlink+' #vote_icon').hover(function() {toastr.error('hmm... Already Upvoted');})
 								}
 							}                        
 						});
