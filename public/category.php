6c10375944fbb5e5577555ee9fd136223120552a
bestboom <certseek@gmail.com>
2019-09-23 15:12:50 +0500

    up

--- public/category.php
+++ public/category.php
@@ -47,6 +47,8 @@ include('template/header5.php');
 					var currentPostNumber = i;
 					var permlink = resulthtml[i]['permlink'];
 					var author = resulthtml[i]['username'];
+					var mylabel = permlink +author;
+					var newValue = mylabel.replace('.', '');
 
 					responsehtml = '<div class="col-lg-4 col-md-6 postsMainDiv mainDiv" postLikes="0" postNumber="'+currentPostNumber+'" id="article_'+permlink+'">\n' +
 					'\n' +
@@ -86,7 +88,7 @@ include('template/header5.php');
 					'<div class="post-author-block">\n' +
 					'<div class="author-info"><i class="fas fa-dollar-sign"></i><span class="pending_payout_value"></span> <b>+</b> <span id="se_token">0</span> <b>DLIKER</b></div>\n' +
 					'</div>\n' +
-					'<div class="post-comments">&nbsp;<a class="upvoting" data-toggle="modal" data-target="#upvoteModal" data-permlink="' + permlink + '" data-author="' + author + '"><i class="fas fa-chevron-circle-up" id="vote_icon'+permlink +author +'"></i></a><span class="active_votes"></span>&nbsp; | &nbsp;<i class="fas fa-comments"></i>&nbsp;<span id="DlikeComments'+permlink +author +'">0</span></div>\n' +
+					'<div class="post-comments">&nbsp;<a class="upvoting" data-toggle="modal" data-target="#upvoteModal" data-permlink="' + permlink + '" data-author="' + author + '"><i class="fas fa-chevron-circle-up" id="vote_icon'+newValue +'"></i></a><span class="active_votes"></span>&nbsp; | &nbsp;<i class="fas fa-comments"></i>&nbsp;<span id="DlikeComments'+permlink +author +'">0</span></div>\n' +
 					'</div>\n' +
 					'</div>\n' +
 					'</article></div>';
@@ -149,9 +151,9 @@ include('template/header5.php');
 							var voterList = result;
 							for (let j = 0; j < voterList.length; j++) {
 								if (voterList[j].voter == c_username) { 
-									$("#vote_icon" + permlink + author).css("color", "RED"); 
-									$('#vote_icon' + permlink + author).click(function(){return false;});
-									$('#vote_icon' + permlink + author).hover(function() {toastr.error('hmm... Already Upvoted');})
+									$("#vote_icon" + newValue).css("color", "RED"); 
+									$('#vote_icon' + newValue).click(function(){return false;});
+									$('#vote_icon' + newValue).hover(function() {toastr.error('hmm... Already Upvoted');})
 								}
 							}                        
 						});
