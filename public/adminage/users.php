34b65e55ca004b6c2fe378c66727c123eed20597
bestboom <certseek@gmail.com>
2019-09-24 22:07:20 +0500

    Update users.php

--- public/adminage/users.php
+++ public/adminage/users.php
@@ -63,7 +63,7 @@
 
   
 </div>
-<?php include('../template/footer2.php'); ?>
+<?php include('../template/footer.php'); ?>
 
 <link rel="stylesheet" href="//cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css">
 <script src="//cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
