e57b1c19be19bbf58047fc40bc6779dee1782a4f
bestboom <certseek@gmail.com>
2019-09-26 12:13:17 +0500

    Update check_pro.php

--- public/helper/check_pro.php
+++ public/helper/check_pro.php
@@ -35,7 +35,7 @@ if (isset($_POST["user"])){
             $sql2 = "SELECT * FROM steemposts WHERE username = '$user_name' and created_at > now() - INTERVAL 24 HOUR"; 
                 $result2 = $conn->query($sql2);
 
-                if ($result2->num_rows >= 1) {
+                if ($result2->num_rows > 0) {
 
                     $return['status'] = false;
                     $return['message'] = 'Only 1 share allowed every 24 hours. To share more become PRO!';
