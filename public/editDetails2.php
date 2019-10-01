5ef730df6333c42062bfdc5ffdfeca96e534cfe0
bestboom <certseek@gmail.com>
2019-10-01 18:50:59 +0500

    Update editDetails2.php

--- public/editDetails2.php
+++ public/editDetails2.php
@@ -133,7 +133,15 @@ $categories  = array("News", "Cryptocurrency", "Food", "Sports", "Technology", "
 let editor;
 ClassicEditor
     .create( document.querySelector( '#editor' ), {
-        placeholder: 'Write minimum 50 words to explain this link share!'
+        toolbar: [ 'heading', '|', 'bold', 'italic', 'link', 'bulletedList', 'numberedList', 'blockQuote' ],
+        heading: {
+            options: [
+                { model: 'paragraph', title: 'Paragraph', class: 'ck-heading_paragraph' },
+                { model: 'heading1', view: 'h1', title: 'Heading 1', class: 'ck-heading_heading1' },
+                { model: 'heading2', view: 'h2', title: 'Heading 2', class: 'ck-heading_heading2' }
+                { model: 'heading3', view: 'h3', title: 'Heading 3', class: 'ck-heading_heading3' }
+            ]
+        }
     } )
     .then( newEditor => {
         editor = newEditor;
