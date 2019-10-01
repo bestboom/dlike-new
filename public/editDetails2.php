366117e316076c6e606b99a0e9443beba5bd65dc
Jochem Stoel <jochemstoel@gmail.com>
2019-10-01 18:53:26 +0200

    fixed another typo :P

--- public/editDetails2.php
+++ public/editDetails2.php
@@ -178,7 +178,7 @@ document.querySelector( '#com-sbmt' ).addEventListener( 'click', (clickEvent) =>
       'Accept': 'application/json',
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({editorData})
+    body: JSON.stringify({text: editorData})
   }).then(response => response.json()).then(object => {
       
       console.log(object)
