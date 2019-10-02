edfd8f9c9c6a977f76dea589888f61f5c0642221
bestboom <certseek@gmail.com>
2019-10-02 14:52:25 +0500

    Update post.php

--- public/post.php
+++ public/post.php
@@ -407,9 +407,9 @@ else
         let profile_url = "/@" + author;
         let auth_img = "https://steemitimages.com/u/" + author + "/avatar";
         let resBody = res.body;
-        let resBody = resBody.split(/\n\n#####\n\n/);
-        let resBody = resBody[1];
-        console.log(resBody);
+        let restBody = resBody.split(/\n\n#####\n\n/);
+        let resyBody = restBody[1];
+        console.log(resyBody);
         let post_description = metadata.body.replace(/<[\/]{0,1}(p)[^><]*>/ig,"");
         let ext_url = metadata.url;
         let pending_steem = res.pending_payout_value.substr(0, 4);
