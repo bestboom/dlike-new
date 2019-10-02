10b5c35c1a6b1af7b888e8f0034fa8175a1f669e
bestboom <certseek@gmail.com>
2019-10-02 23:03:26 +0500

    Update post.php

--- public/post.php
+++ public/post.php
@@ -423,7 +423,7 @@ else
         postbody = postbody.split(/\n\n#####\n\n/);
         postbody = postbody[1];
         //postbody = postbody.replace(/#([^\s]*)/g,'');
-        postbody = postbody.replace(/<(.|\n)*?>/g, '');
+        //postbody = postbody.replace(/<(.|\n)*?>/g, '');
         }catch(err) {
         postbody = "";
         }
@@ -438,7 +438,7 @@ else
         $('.author-thumb-link').attr("href", profile_url);
         $('.mod-auth').attr("href", profile_url);
         $('.mod-tags').html(posttags);
-        $('.mod-post').html(post_description);
+        $('.mod-post').html(postbody);
         //$('.post_link').html('<a href="' + ext_url + '" target="_blank">Source of shared link</a>');
         $('.pending_payout').html(pending_steem);
 
