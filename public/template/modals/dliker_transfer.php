a86e339706e9d2a7b067aa25ec8be1e0be5eb6ee
bestboom <certseek@gmail.com>
2019-10-07 15:54:55 +0500

    Update dliker_transfer.php

--- public/template/modals/dliker_transfer.php
+++ public/template/modals/dliker_transfer.php
@@ -1,6 +1,6 @@
 <div class="modal-body ">
 	<div class="transfer-respond">
-		<h4>Transfer DLIKE Tokens</h4>
+		<h4>Transfer DLIKER Tokens</h4>
 		<div id="tsf-msg"></div>
 		<form action="" id="tsf_sub" method="POST">
 			<div class="row line">
@@ -8,7 +8,7 @@
 					<div class="form-group">
 						<div class="input-group mb-3">
 							<div class="input-group-prepend">
-								<div class="input-group-text mb-deck"> To</div>
+								<div class="input-group-text mb-deck"> @</div>
 							</div>
 							<input type="text" class="form-control reciever" name="reciever" value="" />
 						</div>
@@ -19,7 +19,7 @@
 						<div class="input-group mb-3">
 							<input type="number" class="form-control send_amt" name="send_amt" min="10" value="" />
 							<div class="input-group-append">
-								<div class="input-group-text mb-deck"> DLIKE</div>
+								<div class="input-group-text mb-deck"> DLIKER</div>
 							</div>
 						</div>
 					</div>
@@ -36,9 +36,7 @@
 						</div>
 					</div>
 					<div class="form-group">
-						<input type="hidden" name="user_bal" id="user_bal" value="<? echo $dlike_bal; ?>" />
-						<input type="hidden" name="user_name" id="user_name" value="<? echo $user_wallet; ?>" />
-						<label><b>Balance: </b><?php echo (number_format($rowIt['amount'])); ?> DLIKE</label>
+						<label><b>Balance: </b><?php echo $my_balance; ?> DLIKER</label>
 					</div>
 				</div>
 			</div>
