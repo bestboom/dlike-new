ec46db5c1bacff5d8bb5165ca40be292bb3ee3af
bestboom <certseek@gmail.com>
2019-10-07 16:56:12 +0500

    Update dliker_delegate_in.php

--- public/template/modals/dliker_delegate_in.php
+++ public/template/modals/dliker_delegate_in.php
@@ -1,17 +1,19 @@
-<div class="row">
-	<h3 class="column">Delegations</h3>
+<div class="modal-body ">
+	<div class="transfer-respond">
+		<h4>UNSTAKE DLIKER Tokens</h4>
+		<table>
+			<thead>
+				<tr>
+					<th>From</th>
+					<th>To</th>
+					<th class="amount">Amount</th>
+					<th>Symbol</th><th>Created</th>
+					<th>Updated</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr></tr>
+			</tbody>
+		</table>
+	</div>
 </div>
-<table>
-	<thead>
-		<tr>
-			<th>From</th>
-			<th>To</th>
-			<th class="amount">Amount</th>
-			<th>Symbol</th><th>Created</th>
-			<th>Updated</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr></tr>
-	</tbody>
-</table>
