ca2e574be1dec68493d09c6aecbd3edaf8ea6cd7
bestboom <certseek@gmail.com>
2019-10-20 20:55:40 +0500

    Update tk_transfer.php

--- public/helper/tk_transfer.php
+++ public/helper/tk_transfer.php
@@ -17,47 +17,50 @@ require '../includes/config.php';
 		$rowR = $resultR->fetch_assoc();
 		$reciever_bal = $rowR['amount'];
 
-		$sqls = "SELECT amount FROM wallet where username='$user'"; 
-		$resultAmount = $conn->query($sqls);
+		if($amount < 10) { echo '<div class="alert alert-danger">Minimum amount to transfer is 10 DLIKE!</div>'; die();}
+		else{
+			$sqls = "SELECT amount FROM wallet where username='$user'"; 
+			$resultAmount = $conn->query($sqls);
 
-		if ($resultAmount->num_rows > 0) {
-				$rowIt = $resultAmount->fetch_assoc();	
-				$user_bal = $rowIt['amount'];
+			if ($resultAmount->num_rows > 0) {
+					$rowIt = $resultAmount->fetch_assoc();	
+					$user_bal = $rowIt['amount'];
 
-			if ($amount > $user_bal){ echo '<div class="alert alert-danger">You do not have enough tokens!</div>'; die();}
-			else 
-			{
-				$updateWallet = "UPDATE wallet SET amount = '$user_bal' - '$amount' WHERE username = '$user'";
-				$updateWalletQuery = $conn->query($updateWallet);
+				if ($amount > $user_bal){ echo '<div class="alert alert-danger">You do not have enough tokens!</div>'; die();}
+				else 
+				{
+					$updateWallet = "UPDATE wallet SET amount = '$user_bal' - '$amount' WHERE username = '$user'";
+					$updateWalletQuery = $conn->query($updateWallet);
 
-					if ($updateWalletQuery === TRUE) 
-					{
-						$sqlD = "SELECT amount FROM wallet where username='$reciever'"; 
-						$resultD = $conn->query($sqlD);
+						if ($updateWalletQuery === TRUE) 
+						{
+							$sqlD = "SELECT amount FROM wallet where username='$reciever'"; 
+							$resultD = $conn->query($sqlD);
 
-							if ($resultD->num_rows > 0) {
+								if ($resultD->num_rows > 0) {
 
-								$updateRec = "UPDATE wallet SET amount = '$reciever_bal' + '$amount' WHERE username = '$reciever'";
-								$updateRecQuery = $conn->query($updateRec);
-		
-							}
-							else
-							{
-								$sqln = "INSERT INTO WALLET (username, amount)
-										VALUES ('".$reciever."', '".$amount."')";
-								$addNewWallet = $conn->query($sqln);
-							}
+									$updateRec = "UPDATE wallet SET amount = '$reciever_bal' + '$amount' WHERE username = '$reciever'";
+									$updateRecQuery = $conn->query($updateRec);
+			
+								}
+								else
+								{
+									$sqln = "INSERT INTO WALLET (username, amount)
+											VALUES ('".$reciever."', '".$amount."')";
+									$addNewWallet = $conn->query($sqln);
+								}
 
 
-						$sqlj = "INSERT INTO transactions (username, amount, reason, receiver)
-									VALUES ('".$user."', '".$amount."', '".$reason."', '".$reciever."')";
+							$sqlj = "INSERT INTO transactions (username, amount, reason, receiver)
+										VALUES ('".$user."', '".$amount."', '".$reason."', '".$reciever."')";
 
-							if (mysqli_query($conn, $sqlj)) {
+								if (mysqli_query($conn, $sqlj)) {
 
-								echo '<div class="alert alert-success">Transfer Successful</div>';
-								echo '<script>$(".tsf_btn").attr("disabled","disabled"); document.getElementById("tsf_sub").reset(); setTimeout(function(){location.reload();},2000);</script>';
-							}
-					}
+									echo '<div class="alert alert-success">Transfer Successful</div>';
+									echo '<script>$(".tsf_btn").attr("disabled","disabled"); document.getElementById("tsf_sub").reset(); setTimeout(function(){location.reload();},2000);</script>';
+								}
+						}
+				}
 			}
 		}
 	}
