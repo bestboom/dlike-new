acc1933ac2415ed241c20836b935741612175529
bestboom <certseek@gmail.com>
2019-11-25 15:13:50 +0500

    Update tips.php

--- public/tips.php
+++ public/tips.php
@@ -1,3 +1,9 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+include('template/header5.php');
+?>
 <div class="details-post-meta-tip" style="background: #080e70;">
     <div class="container">
         <div class="row">
@@ -13,7 +19,7 @@
                 <form action="/helper/addtips.php" method="post" id="tipsubmit">
                     <input type="hidden" name="tipauthor" value="livaria">
                     <input type="hidden" name="tippermlink" value="astronomers-find-three-supermassive-black-holes-in-ngc-6240-astronomy-sci-newscom">
-                    <center><button class="btn btn-default btn-tip" style="background: #fff;color: #090e68;">TIP</button></center>
+                    <center><button class="btn btn-default up_tip" style="background: #fff;color: #090e68;">TIP</button></center>
                 </form>            
 
 
@@ -26,3 +32,19 @@
         </div>
     </div>
 </div>
+
+        <div class="modal fade" id="recomendModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog modal-sm" role="document">
+                <div class="modal-content mybody">
+                    <?php include('template/modals/recomend.php'); ?>
+                </div>
+            </div>
+        </div>
+
+<?php include('template/footer.php'); $conn->close(); ?>
+<script type="text/javascript">
+    $('.up_tip').click(function () {
+        $("#recomendModal").modal("show");
+    });   
+
+</script>
