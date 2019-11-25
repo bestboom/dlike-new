86830f186d8613053f6fb3aeb4fc78deaea06e94
bestboom <certseek@gmail.com>
2019-11-25 15:06:22 +0500

    Create tips.php

---
+++ public/tips.php
@@ -0,0 +1,28 @@
+<div class="details-post-meta-tip" style="background: #080e70;">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-3">
+                <center><img src="/images/tips-dlike.png" alt="img" class="img-responsive"></center>
+            </div>
+            <div class="col-sm-5 mid-tip">
+                <h2 class="tipratio" style="font-size: 18px;line-height: 2;font-weight: 600;color: #fffffffc;">DLIKE pays to share Links <br>Start sharing to earn money Now!</h2>
+                <p class="tipthnk" style="display: none;">You need to wait before you can TIP again.</p>
+            </div>
+            <div class="col-sm-4">
+
+                <form action="/helper/addtips.php" method="post" id="tipsubmit">
+                    <input type="hidden" name="tipauthor" value="livaria">
+                    <input type="hidden" name="tippermlink" value="astronomers-find-three-supermassive-black-holes-in-ngc-6240-astronomy-sci-newscom">
+                    <center><button class="btn btn-default btn-tip" style="background: #fff;color: #090e68;">TIP</button></center>
+                </form>            
+
+
+            </div>
+        </div>
+    </div>
+    <div class="container tip-sponsor">
+        <div class="row">
+            <div class="col tip-foot" style="color: #fff;">Tip this post for free - Author (40%) - You (60%)</div>
+        </div>
+    </div>
+</div>
