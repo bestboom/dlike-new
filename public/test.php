08cd0722499954c546640abf4424eb29ee084977
bestboom <certseek@gmail.com>
2020-01-09 21:50:03 +0500

    Create test.php

---
+++ public/test.php
@@ -0,0 +1,9 @@
+<?php
+
+$post_url = "https://api.steemjs.com/get_content?author=dlike&permlink=dlike-weekly-report-72-dliker-token-details";
+$response = file_get_contents($post_url);
+$result = json_decode($response);
+$og_description = explode("\n\n#####\n\n",$result->body);
+$og_description = $og_description[1];
+echo $og_title = $result->title;
+?>
