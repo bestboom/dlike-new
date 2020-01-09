06e9de0abd25a01627dce9a776d78a9c6303a595
bestboom <certseek@gmail.com>
2020-01-09 22:02:17 +0500

    Update test.php

--- public/test.php
+++ public/test.php
@@ -6,4 +6,16 @@ $result = json_decode($response);
 $og_description = explode("\n\n#####\n\n",$result->body);
 $og_description = $og_description[1];
 echo $og_title = $result->title;
+function removeTags($str) {  
+	$str = preg_replace("#<(.*)/(.*)>#iUs", "", $str);
+	return $str;
+}
+$og_description = removeTags($og_description);
+$meta_data = json_decode($result->json_metadata);
+echo $og_image = $meta_data->image;
+$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+$uri = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+echo $og_url = $uri;
+
+echo "aq";
 ?>
