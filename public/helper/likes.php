7c651e6bf264505c9753648fe37ea1480cf02dfb
bestboom <certseek@gmail.com>
2020-01-15 14:58:59 +0500

    Update likes.php

--- public/helper/likes.php
+++ public/helper/likes.php
@@ -1,5 +1,5 @@
 <?php
-if($_COOKIE['username'] != 'dlike'){die('<script>window.location.replace("https://dlike.io","_self")</script>');};
+#if($_COOKIE['username'] != 'dlike'){die('<script>window.location.replace("https://dlike.io","_self")</script>');};
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
