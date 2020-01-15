612c506fc59225d59be520b41136eaf90615dbe1
bestboom <certseek@gmail.com>
2020-01-15 14:23:57 +0500

    Update tkad.php

--- public/helper/tkad.php
+++ public/helper/tkad.php
@@ -1,8 +1,9 @@
 <?php 
+require '../includes/config.php';
 	ini_set('display_errors', 1);
 	ini_set('display_startup_errors', 1);
 	error_reporting(E_ALL);
-require '../includes/config.php';
+
 
 if (isset($_POST["user"]) && isset($_POST["amount"]) && isset($_POST["reason"])){
 
