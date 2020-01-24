9ce2eaf9a2773696987a47c02f3c7465262e9cb5
bestboom <certseek@gmail.com>
2020-01-24 17:44:57 +0500

    Update test.php

--- public/test.php
+++ public/test.php
@@ -40,7 +40,7 @@ echo '<br>';
 echo 'this is alst line of description check';
 echo '<br>';
 //$og_description = preg_replace('/[ \t]+/', ' ', preg_replace('/[\r\n]+/', "\n", $og_description));
-$og_description = html_entity_decode(nl2br($og_description));
+//$og_description = html_entity_decode(nl2br($og_description));
 //$og_description = removeTags($og_description);
 echo $og_description = implode(' ', array_slice(explode(' ', $og_description), 0, 23));
 
@@ -59,14 +59,12 @@ echo $og_description = implode(' ', array_slice(explode(' ', $og_description), 0
 
 echo '<br>';
 echo 'image here';
+echo '<br>';
 $meta_data = $result['results'][0]['json'];
 $metadata = json_decode($meta_data, TRUE);
 print_r($metadata);
 echo '<br>';
 echo '<br>';
-$og_image = $meta_data['image'];
-echo $og_image;
-echo 'or this works';
 echo $og_image = $metadata['image'][0];
 echo '<br>';
 $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
