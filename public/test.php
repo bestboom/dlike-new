f7ca5b34ec7ed192ce9d461860d524195dbf8079
bestboom <certseek@gmail.com>
2020-01-24 12:36:17 +0500

    Update test.php

--- public/test.php
+++ public/test.php
@@ -9,18 +9,41 @@ $link = "dlike-weekly-report-72-dliker-token-details";
 $user = "dlike";
 $post_url = "https://tower.emrebeyler.me/api/v1/posts/?$user/$link";
 
+
 $response = file_get_contents($post_url);
-$result = json_decode($response);
-$og_description = explode("\n\n#####\n\n",$result->body);
-$og_description = $og_description[1];
-echo $og_title = $result->title;
+$result = json_decode($response, TRUE);
+
 function removeTags($str) {  
 	$str = preg_replace("#<(.*)/(.*)>#iUs", "", $str);
 	return $str;
 }
-echo $og_description = removeTags($og_description);
-$meta_data = json_decode($result->json);
+
+echo $og_title = $result['results']['title'];
+    
+$og_description = explode("\n\n#####\n\n",$result['results']['body']);
+$og_description = $og_description[1];
+$og_description = removeTags($og_description);
+echo $og_description = implode(' ', array_slice(explode(' ', $og_description), 0, 23));
+
+
+
+//$pending_amount = ($result['DLIKER']['pending_token'])/1000;
+
+
+//$response = file_get_contents($post_url);
+//$result = json_decode($response);
+//$og_description = explode("\n\n#####\n\n",$result->body);
+//$og_description = $og_description[1];
+//echo $og_title = $result->title;
+
+//echo $og_description = removeTags($og_description);
+
+
+$meta_data = json_decode($result['results']['json']);
 echo $og_image = $meta_data->image;
+echo 'or this works';
+echo $og_image = $meta_data['image'];
+
 $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 $uri = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 echo $og_url = $uri;
