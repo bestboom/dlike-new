8d47ab8a557be9c68c6f9d3ffff2b1ae829ae4f9
bestboom <certseek@gmail.com>
2020-01-24 12:20:04 +0500

    Update test.php

--- public/test.php
+++ public/test.php
@@ -1,7 +1,13 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+
 $link = "dlike-weekly-report-72-dliker-token-details";
 $user = "dlike";
-$post_url = "https://api.steemit.com/$user/$link";
+$post_url = "https://tower.emrebeyler.me/api/v1/posts/?$user/$link";
 
 $response = file_get_contents($post_url);
 $result = json_decode($response);
@@ -12,12 +18,13 @@ function removeTags($str) {
 	$str = preg_replace("#<(.*)/(.*)>#iUs", "", $str);
 	return $str;
 }
-$og_description = removeTags($og_description);
-$meta_data = json_decode($result->json_metadata);
+echo $og_description = removeTags($og_description);
+$meta_data = json_decode($result->json);
 echo $og_image = $meta_data->image;
 $protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 $uri = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 echo $og_url = $uri;
 
 echo "aq";
+
 ?>
