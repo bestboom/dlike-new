a67fad9393d2f1f79fbef1aee28cdc2c1f691b16
bestboom <certseek@gmail.com>
2020-03-05 23:40:20 +0500

    up

--- public/helper/image_upload/ck_upload.php
+++ public/helper/image_upload/ck_upload.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 
 require __DIR__ . '/../../../vendor/autoload.php';
 //require __DIR__ . '/../../helper/image_upload/B2.php';
-require_once "../../helper/image_upload/B2.php";
+require_once "../../../helper/image_upload/B2.php";
 
 $appKeyId = "00063e4beabebed0000000003"; 
 $appKey = "K000SsnlbgCLwONCLR5SH7GByCBHGy4"; 
