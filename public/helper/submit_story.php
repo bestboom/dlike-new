1a3286fc00c09de80bcc7a8f0637fee793bb6465
bestboom <certseek@gmail.com>
2020-03-09 15:44:37 +0500

    Update submit_story.php

--- public/helper/submit_story.php
+++ public/helper/submit_story.php
@@ -61,11 +61,11 @@ if (isset($_POST["story_title"]) && isset($_POST["story_tags"]) && isset($_POST[
 
     	if (isset($state->result)) { 
 	    	// insert into DB
-			$post_title= mysqli_real_escape_string($conn, $_POST['title']);
+			$post_title= mysqli_real_escape_string($conn, $_POST['story_title']);
 			$addposts = "INSERT INTO steemposts (`username`,`title`, `permlink`, `ext_url`, `img_url`, `parent_ctegory`,`created_at`) VALUES ('".$posting_user."','".$post_title."', '".$permlink."', '".$url."', '".$urlImage."', '".$category."','".date("Y-m-d H:i:s")."')";
 			$addpostsquery = $conn->query($addposts);
 
-			$posts_tags = array_unique(explode(",",$_POST['tags']));
+			$posts_tags = array_unique(explode(",",$_POST['story_tags']));
 			if(count($posts_tags)>0)  {
 				foreach($posts_tags as $p_tag) {
 					$add_tags = $conn->query("INSERT INTO posttags (`tagname`,`updated_at`) VALUES ('".$p_tag."','".date("Y-m-d H:i:s")."')");
