9042185a8b721d27a6529bbe75d781e7795523ca
bestboom <certseek@gmail.com>
2020-03-14 09:12:52 +0500

    Update editDetails2.php

--- public/editDetails2.php
+++ public/editDetails2.php
@@ -169,8 +169,18 @@ ClassicEditor
         return domain;
     }
 
+    function getHostName(url) {
+        var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+        if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
+            return match[2];
+        } else {
+            return null;
+        }
+    }
+
+    
     var urlInput = '<?php echo $url; ?>';
-    console.log(urlInput)
+    console.log(urlInput);
 
     let verifyUrl = getDomain(urlInput);
 
