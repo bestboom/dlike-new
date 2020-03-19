0ac81553a2dc62fed3fa1b39a0e929c1e99b8d52
bestboom <certseek@gmail.com>
2020-03-19 14:23:00 +0500

    up

--- public/post.php
+++ public/post.php
@@ -171,16 +171,10 @@ else
 </div></div></div>
 <div id="tip-msg"></div>
 <div class="details-post-meta-tip" style="background: #080e70;">
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-3">
-                <center><img src="/images/tips-dlike.png" alt="img" class="img-responsive"></center>
-            </div>
-            <div class="col-sm-5 mid-tip">
-                <h2 class="tipratio" style="font-size: 18px;line-height: 2;font-weight: 600;color: #fffffffc;">DLIKE pays to share Links <br>Start sharing to earn money Now!</h2>
-                <p class="tipthnk">You need to wait before you can TIP again.</p>
-            </div>
-            <div class="col-sm-4">
+    <div class="container"><div class="row">
+        <div class="col-sm-3"><center><img src="/images/tips-dlike.png" alt="img" class="img-responsive"></center></div>
+        <div class="col-sm-5 mid-tip"><h2 class="tipratio" style="font-size: 18px;line-height: 2;font-weight: 600;color: #fffffffc;">DLIKE pays to share Links <br>Start sharing to earn money Now!</h2><p class="tipthnk">You need to wait before you can TIP again.</p></div>
+        <div class="col-sm-4">
                 <?php
                 if(empty($_COOKIE['username']) && !isset($_COOKIE['username'])) { echo '<center><a href="/welcome"><button class="btn btn-danger">Login To Tip</button></a></center>'; $tiptime = '601';} else {
                     $verifysender = "SELECT * FROM tiptop where sender = '$sender'";
