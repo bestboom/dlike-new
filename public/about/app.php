ff64642370a624c4f307806d4d9af66af01e6c5e
bestboom <certseek@gmail.com>
2020-03-25 01:18:04 +0500

    up

---
+++ public/about/app.php
@@ -0,0 +1,47 @@
+<?php
+include('../template/header.php');
+
+    <div class="main-container fixed-top-padding cont-sec" style="background:#e4e4e4;">
+	<div class="container" style="margin-top: 0.1%;position:relative;">
+        <div id="man-cont">
+		<div class="agile_info">
+            <div class="w3_info" style="flex-basis: 100%; -webkit-flex-basis: 100%; padding-bottom: 0px;">
+                <center>
+                    <h2 style="color:#ad1f23;padding-top:60px;text-transform: uppercase;font-weight: 600;font-size: 1.5rem;">Dlike App</h2>
+                    <h4 id="er" style="color: #000000c7;font-weight: 500;font-size: 20px;
+    margin-top: 12px;">Beta version for Android App</h4>
+                </center>
+            </div>
+        </div>
+            <div style="padding:1.5rem;">
+					<h4 style="color:#111;padding-top:10px;padding-bottom:5px;"></h4>
+					<p style="padding:10px;font-size:14px;"> Dlike android version is now availble to download for all users. At present you can only install it directly on your phones as after testing period it will soon be available on Google Play store.</p>
+					<br/>
+					<center><a download="dlike.apk" href="https://dlike.io/about/dlike.apk" title="Dlike Apk">
+                        <img alt="Dlike APK" src="https://dlike.io/images/apk-download.jpg"></a></center>
+					<center><h4 style="color:#111;padding-top:10px;padding-bottom:5px;">Instalation Guidelines</h4></center>
+                        
+                        <p style="padding:10px;font-size:14px;"><br/>
+                        If you are not sure how to install apk file directly on your phone, then follow the tutorials given below.</p>
+                     <br/>
+                         <h4 style="color:#111;padding-top:10px;padding-bottom:5px;">How to Install APK?</h4>
+					<p><strong>Wiki Article: </strong> <a href="https://www.wikihow.tech/Install-APK-Files-on-Android">https://www.wikihow.tech/Install-APK-Files-on-Android</a></p>
+                    <br>
+                    <center><iframe width="560" height="315" src="https://www.youtube.com/embed/bEX4UHV6Mms" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe></center>
+                        
+                      
+                        <h4 style="color:#111;padding-top:10px;padding-bottom:5px;">Need Help?</h4>
+                        <p>If you need help regarding any issue for app installation. Please check android-app channel on official discord channel</p> <br><br>
+                        <center><h4 style="color:#111;padding-top:10px;padding-bottom:5px;"><a href="https://discord.gg/JYSkBFk">Dlike Official Discord Server</a></h4></center>
+                        <br><br>
+                        
+    
+            </div>
+            
+
+        <div class="clear"></div>
+	   </div>
+	</div>
+</div>
+
+include('../template/footer.php');
