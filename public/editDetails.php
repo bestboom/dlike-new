530233fb0c90a963c2644f6e97bd253b5cba01b6
bestboom <certseek@gmail.com>
2020-04-03 13:30:58 +0500

    Update editDetails.php

--- public/editDetails.php
+++ public/editDetails.php
@@ -233,11 +233,11 @@ ClassicEditor
                                     toastr['error'](response.data);
                                     return false;
                                 } else { 
-                                    var tags: $('.tags').val();
+                                    var tags = $('.tags').val();
                                     console.log(tags);
-                                    var rewards: $('.rewards').val();
+                                    var rewards =  $('.rewards').val();
                                     console.log(rewards);
-                                    var description: editor.getData();
+                                    var description = editor.getData();
                                     console.log(description);
                                     var datam = {
                                         title: $('.title_field').val(),
