92b5cc683bf8289f7308008a6365903940e15426
bestboom <certseek@gmail.com>
2020-04-03 21:29:10 +0500

    Update made.php

--- public/made.php
+++ public/made.php
@@ -11,7 +11,8 @@ include('template/footer.php'); ?>
 	var tags = "new-test blockchain";
 	var tags = tags.replace(/([a-zA-Z0-9-]+)/g, "\"$1\"");
 	var tags = tags.replace(/\s+/g, ', ').toLowerCase();
-	var post_tags = '["hive-116221", "dlike", ' + tags+ ']';
+	var post_tags = '["hive-116221", "dlike", '+ tags +']';
+	var meta_tags = JSON.parse(post_tags);
 
 	var rewards = "2";
 	if(rewards == '1'){
@@ -34,14 +35,14 @@ include('template/footer.php'); ?>
 	var parentPermlink = 'test';
 	var author = username;
 	//var permlink = 'a-new-steem-test-6';
-	var title = "posting test no 9";
+	var title = "posting test no 10";
 	var permlink = title.replace(/[^a-z0-9\s]/gi, '').replace(/[_\s]/g, '-').toLowerCase();
 	var body = '<center><img src="https://i.postimg.cc/d0Vf2w92/hummingbird.jpg" alt="New Share" /></center><br><p>This is an other test post with rewards check</p><p>This is (check <a href="https://www.steemit.com">steemit</a>) , Now content can also be published.&nbsp;</p><br><center><br><a href="https://steemit.com/">Posting Test</a><hr><br>';
 	var post_body = body.replace(/[\u2018\u2019]/g, "'").replace(/[\u201C\u201D]/g, '"');
 	//var max_accepted_payout = max_accepted_payout;
     //var percent_steem_dollars = percent_steem_dollars;
 	var jsonMetadata = {
-	    "tags": post_tags,
+	    "tags": meta_tags,
 	    "app": "steemit/0.1",
 	    "community": "steem",
     	"format": "html",
