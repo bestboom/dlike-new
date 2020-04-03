87769f5bd473f1d941a16645daed488e38c4ff71
bestboom <certseek@gmail.com>
2020-04-03 09:39:21 +0500

    up

--- public/made.php
+++ public/made.php
@@ -4,8 +4,8 @@
 <script type="text/javascript">
 	console.log(username);
 	voter = username;
-	author = 'indegassi';
-	permlink = 'spain-sheds-nearly-900000-jobs-since-coronavirus-lockdown--reuters';
+	author = 'myinns';
+	permlink = 'what-are-the-challenges-of-battling-coronavirus-in-india-india-al-jazeera';
 	weight = 1000;
 
 	api.vote(voter, author, permlink, weight, function (err, res) {
