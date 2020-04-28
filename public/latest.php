1f26245848deef711430111afd7f4133dbd261de
bestboom <certseek@gmail.com>
2020-04-28 15:43:52 +0500

    Update latest.php

--- public/latest.php
+++ public/latest.php
@@ -4,25 +4,26 @@ include('template/header5.php');
 </div><!-- sub-header -->
 <style>
   .faq-section{background: #e6e6e6;}
-  .container2 {max-width: 1366px;background: #fff;padding: 100px 50px 50px;}
+  .news-set {max-width: 1366px;background: #fff;padding: 100px 50px 50px;}
+  .news-detail{font-weight: 500;color: #202020;font-family: Verdana,Arial,Helvetica,sans-serif;font-size: 0.9rem;font-weight: 500;}
 </style>
 <div class="container" style="background: #191d5d;max-width: 100% !important;">
-    <div class="row" style="padding: 50px;">
+    <div class="row" style="padding: 25px;">
         <div class="col" style="text-align:center;color: #fff;">
             <h3>Latest News</h3>
         </div>
     </div>
 </div>
-    <div class="faq-section">
-        <div class="container2">
-            <div class="row">
+    <div class="faq-section" style="padding-top:1px;">
+        <div class="container news-set">
+            <div class="row" style="margin:0px;padding-left:10px;">
           <?php $sql1 = "SELECT * FROM latestnews ORDER BY id DESC LIMIT 48";
                 $result1 = $conn->query($sql1);
                 if ($result1->num_rows > 0) 
                 {
                   while($row1 = $result1->fetch_assoc()) 
                   {
-                      echo $title = $row1['title']."<br>";  
+                      echo $title = "<span class='news-detail'>".$row1['title']."</span><br>";  
                   }
                 } ?>
             </div>
