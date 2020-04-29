0e8f52dab7c7151d7e5ecd56761a3c8f86fded05
bestboom <certseek@gmail.com>
2020-04-29 13:23:15 +0500

    Update latest.php

--- public/latest.php
+++ public/latest.php
@@ -5,8 +5,8 @@ include('template/header5.php');
 <style>
   .faq-section{background: #e6e6e6;}
   .news-set {max-width: 1366px;background: #fff;padding: 80px 50px 50px;}
-  .news-detail{color: #202020;font-family: Verdana,Arial,Helvetica,sans-serif;font-size: 0.9rem;font-weight: 500;}
-  .news-detail a:hover{color: #86828C;text-decoration: underline;}
+  .news-detail{color: #202020;font-family: Verdana,Arial,Helvetica,sans-serif;font-size: 1rem;font-weight: 500;line-height: 2.2;text-decoration: underline;}
+  .news-detail a:hover {color: #86828C;}
 </style>
 <div class="container" style="background: #191d5d;max-width: 100% !important;">
     <div class="row" style="padding: 25px;">
@@ -32,3 +32,5 @@ include('template/header5.php');
         </div>
     </div>
 <?php include('template/footer.php'); ?>
+
+var aq = moment.unix(1588100604).format('HH:mm')
