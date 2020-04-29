b5b0789e0782b9432f9529c31ab6152e164cf292
bestboom <certseek@gmail.com>
2020-04-29 15:24:49 +0500

    up

--- public/latest/post.php
+++ public/latest/post.php
@@ -1,8 +1,16 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 include('../template/header5.php'); 
 $ipInfo = file_get_contents('http://ip-api.com/json/' . $thisip);
 $ipInfo = json_decode($ipInfo);
 $timezone = $ipInfo->timezone;
 date_default_timezone_set($timezone);
 echo $mytimezone =  date_default_timezone_get();
+
+echo $link = $_GET['link'];
 ?>
+
