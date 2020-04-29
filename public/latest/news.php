29c425860f4cb14d8334107bb16743710e530c38
bestboom <certseek@gmail.com>
2020-04-30 00:33:06 +0500

    Update news.php

--- public/latest/news.php
+++ public/latest/news.php
@@ -13,9 +13,10 @@ echo $news_id = $_GET['id'];
 </div><!-- sub-header -->
 <style>
   .faq-section{background: #e6e6e6;}
-  .news-set {max-width: 1366px;background: #fff;padding: 80px 50px 50px;}
+  .news-set {max-width: 1366px;background: #fff;padding: 40px 15px 15px 15px;}
   .news-detail{color: #202020;font-family: Verdana,Arial,Helvetica,sans-serif;font-size: 1rem;font-weight: 500;line-height: 2.2;text-decoration: underline;}
   .news-detail a:hover {color: #86828C;}
+  .title {font-size: 1.5rem;font-weight: 600;letter-spacing: -0.5px;line-height: 1.3;}
 </style>
 <div class="container" style="background: #191d5d;max-width: 100% !important;">
     <div class="row" style="padding: 25px;">
@@ -26,7 +27,7 @@ echo $news_id = $_GET['id'];
 </div>
     <div class="faq-section" style="padding-top:1px;">
         <div class="container news-set">
-            <div style="padding-left:10px;">
+            <div class="row">
             <?php $sql1 = "SELECT * FROM latestnews where id='$news_id'";
                 $result1 = $conn->query($sql1);
                 if ($result1->num_rows > 0) 
@@ -42,13 +43,17 @@ echo $news_id = $_GET['id'];
         			$description = $grab->getDescription();
                 }
                 ?>
-                <h2><?php echo $post_title;?></h2>
-                <br>
-                <img src="<?php echo $image;?>" style="width:100%">
-                <br>
-                <p><?php echo $description; ?></p>
-                <br>
-                <p><a href="<?php echo $post_ext_link; ?>">Read This News Here</a></p>
+                <div class="col-md-8">
+	                <h2 class="title"><?php echo $post_title;?></h2>
+	                <br>
+	                <img src="<?php echo $image;?>" style="width:100%">
+	                <br>
+	                <p style="padding-top: 15px;"><?php echo $description; ?></p>
+	                <p style="font-weight: bold">
+	                	<a href="<?php echo $post_ext_link; ?>">continue reading <?php echo $post_title;?></a>
+	                </p>
+	            </div>
+	            <div class="col-md-4"></div>
             </div>
         </div>
     </div>
