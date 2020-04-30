e6a060c05bcffd16f179b039560402db528c3604
bestboom <certseek@gmail.com>
2020-04-30 15:15:59 +0500

    Update news.php

--- public/latest/news.php
+++ public/latest/news.php
@@ -24,10 +24,11 @@ $sql1 = "SELECT * FROM latestnews where id='$news_id'";
 $og_title = $post_title;
 $og_description = $description;
 $og_image = $image;
+$protocol = ((!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != 'off') || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
 $uri = $protocol . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
 $og_url = $uri;
 include('../template/news-header.php');
-  
+include('../functions/main.php');
 ?>
 </div><!-- sub-header -->
 <style>
@@ -40,7 +41,7 @@ include('../template/news-header.php');
 <div class="container" style="background: #191d5d;max-width: 100% !important;">
     <div class="row" style="padding: 25px;">
         <div class="col" style="text-align:center;color: #fff;">
-            <h3>Latest News</h3>
+            <h3>Latest <?php echo $news_category; ?> News</h3>
         </div>
     </div>
 </div>
@@ -60,8 +61,29 @@ include('../template/news-header.php');
 	                </p>
 	            </div>
 	            <div class="col-md-4">
-	            	<div style="width:100%;background: #c51d24;padding: 15px;color: #fff;font-size: 1.3rem;font-weight: 600;text-align: center;">Share on DLIKE to Get Rewarded<br><span>STEEM + DLIKER Upvotes</span><br><span>Daily Reward Pool</span><br><button style="background-color: #fff;color: #373f59;border: 2px solid transparent;border-radius: 32px;font-size: 18px;">Share Now</button></div>
-	            	<div style="margin-top: 25px;"><span style="border-bottom: 1px solid #111;">Latest <?php echo $news_category; ?> News</span></div>
+	            	<div style="width:100%;background: #c51d24;padding: 15px;color: #fff;font-size: 1.3rem;font-weight: 600;text-align: center;">
+	            		<span style="padding-bottom: 20px;">Share on DLIKE to Get Rewarded</span><br>
+	            		<span style="font-size: 1.1rem;padding: 10px;">STEEM + DLIKER Upvotes</span><br>
+	            		<span style="font-size: 1.1rem;padding: 10px;">Daily Reward Pool</span>
+	            		<button style="background-color: #fff;color: #373f59;border: 2px solid transparent;border-radius: 22px;margin-bottom: 10px;margin-top: 20px;padding: 5px 25px;font-weight: 600;font-size: 18px;">Share Now</button>
+	            	</div>
+	            	<div style="margin-top: 25px;"><span style="color: #202020;font-size: 1.1rem;">Latest <?php echo $news_category; ?> News</span><hr style="margin: 0;background-color: #5f5f5f;">
+	            		<span style="margin-top: 10px;">
+	            			<?php $sql2 = "SELECT * FROM latestnews ORDER BY id DESC LIMIT 48";
+				                $result2 = $conn->query($sql2);
+				                if ($result2->num_rows > 0) 
+				                {
+				                  while($row2 = $result2->fetch_assoc()) 
+				                  { 
+				                    $side_title = $row2['title'];
+				                    $side_category = $row2['category'];
+				                    $side_id = $row2['id'];
+				                    $side_permlink = validationData(clean($side_title));
+				                    echo "<i class='fas fa-step-forward' style='color: #c51d24;'></i>&nbsp;&nbsp;<a href='https://dlike.io/latest/news/".$side_category."/".$side_id."/".$side_permlink."' class='news-detail'>".$side_title."</a><br>";
+				                  }
+				                } ?>
+	            		</span>
+	            	</div>
 	            </div>
             </div>
         </div>
