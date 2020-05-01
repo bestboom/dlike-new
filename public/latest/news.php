9d28b53f13d11e0f2b16dfbff03ff79ef4a97133
bestboom <certseek@gmail.com>
2020-05-01 17:08:57 +0500

    Update news.php

--- public/latest/news.php
+++ public/latest/news.php
@@ -36,9 +36,9 @@ include('../functions/main.php');
 </div>
 <div class="faq-section" style="padding-top:1px;padding-bottom: 0px;"><div class="container news-set"><div class="row" style="margin: 0px">
     <div class="col-md-8">
-        <div class="row" style="margin: 2px;font-size: 0.85rem;color: #c51d24;"><a href="/latest">Latest News </a> &nbsp; > &nbsp; <a href="/latest/<?php echo ucfirst($news_category); ?>"> <?php echo $news_category; ?></a></div>
+        <div class="row" style="margin: 2px;font-size: 0.85rem;color: #c51d24;"><a href="/latest">Latest News </a> &nbsp; > &nbsp; <a href="/latest/<?php echo $news_category; ?>"> <?php echo ucfirst($news_category); ?></a></div>
         <h2 class="title"><?php echo $post_title;?></h2><hr style="margin-top: 1px; margin-bottom: 5px;background-color: #202020;">
-        <span class="row" style="margin:0px;margin-bottom: 10px;">By <?php echo $post_source; ?> | Time: </span>
+        <span class="row" style="margin:0px;margin-bottom: 10px;font-size: 0.8rem;font-weight: 600;">By <?php echo $post_source; ?> | <?php echo ucfirst($news_category); ?> </span>
         <img src="<?php echo $image;?>" style="width:100%"><br>
         <p style="padding-top: 15px;"><?php echo $description; ?></p>
         <p style="font-weight: bold"><a href="<?php echo $post_ext_link; ?>" target="_blank">Continue Reading <?php echo $post_title;?></a></p>
