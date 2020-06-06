1fdc2b8031f314bd2ab030cfc32bafb502fb15a3
bestboom <certseek@gmail.com>
2020-06-06 23:31:41 +0500

    Update welcome2.php

--- public/welcome2.php
+++ public/welcome2.php
@@ -83,8 +83,8 @@ if (isset($_GET["ref"])){ $referrer = $_GET['ref'];} else { $referrer = 'dlike';
                     <div class="map-block signup-signup-email" style="display: none;">
                         <div class="map-block signup_email_block">
                             <div class="contact-info-inner" style="margin-top: 15%;">
-                                <h4 class="sign_head">Signup</h4>
-                                <form name="email_signup" style="margin-top: 15px;margin-left: 10%;margin-right: 10%;width: unset;float: none;">
+                                <h4  style="color: #0b132d;font-weight: 700;font-size: 24px;">Signup</h4>
+                                <form name="email_signup" style="margin-top: 15px;margin-left: 16%;margin-right: 16%;width: unset;float: none;">
                                     <div class="form-group input-username">
                                         <input type="hidden" id="refer_by_email" value="<?php echo $referrer; ?>" />
                                         <input type="hidden" id="user_loc_email" value="<?php echo $thisip; ?>" />
@@ -108,7 +108,7 @@ if (isset($_GET["ref"])){ $referrer = $_GET['ref'];} else { $referrer = 'dlike';
                                         <input type="password" name="signup_pass" id="signup_pass" placeholder="Password" class="form-control" />
                                     </div>
                                     <p style="margin: 0px;"><?php if($referrer !='dlike'){ echo 'Referred By <span style="font-weight:600;color:#1b1e63;">' .$referrer.'</span>';} ?></p>
-                                    <button class="next btn btn-lime" style="background-color: #1b1e63;border-color: #1b1e63;">SIGNUP</button>
+                                    <button class="btn btn-lime email_signup_btn" style="background-color: #1b1e63;border-color: #1b1e63;">SIGNUP</button>
                                 </form>
                             </div>
                         </div>
@@ -336,5 +336,8 @@ $('.signup_steem_btn').click(function() {
 });
 $('.signup_email_btn').click(function() {
     signupwithemail();
+});
+$('.email_signup_btn').click(function() {
+    toastr.error('phew... Ok Move on!');
 });
 </script>
