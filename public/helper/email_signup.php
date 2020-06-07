1fd4ff3a25485172ccc56c5a85541f3f3a165966
bestboom <certseek@gmail.com>
2020-06-07 23:43:45 +0500

    Update email_signup.php

--- public/helper/email_signup.php
+++ public/helper/email_signup.php
@@ -5,6 +5,19 @@
 
 if (isset($_POST['signup_email'])  && $_POST['signup_email'] != '' && isset($_POST['signup_username'])  && $_POST['signup_username'] != '' && isset($_POST['signup_pass'])  && $_POST['signup_pass'] != '')
 {
-	
+	if (isset($state->result)) { 
+	    die(json_encode([
+	    	'error' => false,
+    		'message' => 'Thankk You', 
+    		'data' => 'Upvoting'
+    		
+		]));
+	} else {
+	    die(json_encode([
+    		'error' => true,
+    		'message' => 'Sorry', 
+    		'data' => 'Already Upvoted'
+		]));
+	} 
 } else {die('Some error');}
 ?>
