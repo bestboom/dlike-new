486857b4984f0deb50827d6f9c9b811d61f26995
bestboom <certseek@gmail.com>
2020-06-07 23:17:13 +0500

    up

---
+++ public/helper/email_signup.php
@@ -0,0 +1,10 @@
+<?php 
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+if (isset($_POST['email'])  && $_POST['email'] != '' && isset($_POST['signup_username'])  && $_POST['signup_username'] != '' && isset($_POST['email'])  && $_POST['email'] != '')
+{
+	
+} else {die('Some error');}
+?>
