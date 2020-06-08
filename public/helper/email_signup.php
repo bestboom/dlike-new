8735f096e0cdf716f119b66b5dc9f6e93961ae4b
bestboom <certseek@gmail.com>
2020-06-08 15:41:19 +0500

    Update email_signup.php

--- public/helper/email_signup.php
+++ public/helper/email_signup.php
@@ -6,19 +6,18 @@
 if (isset($_POST['signup_email'])  && $_POST['signup_email'] != '' && isset($_POST['signup_username'])  && $_POST['signup_username'] != '' && isset($_POST['signup_pass'])  && $_POST['signup_pass'] != '')
 {
 	$signup_username = $_POST['signup_username'];
+
+	if(empty(trim($_POST["signup_username"]))){
+        $username_err = "Username Shoould not be empty.";
+    }
 	if (strlen($_POST['signup_pass']) > 20 || strlen($_POST['signup_pass']) < 5) {
 		$errors = 'Password must be between 5 and 20 characters long!';
 	}
 	if (!filter_var($_POST['signup_email'], FILTER_VALIDATE_EMAIL)) {
 		$errors = 'Email is not valid!';
 	}
-	//if (preg_match('/[A-Za-z0-9]+/', $_POST['signup_username'])) {
-		//$errors = 'Username is not valid!';
-	//}
 	if(!preg_match('/^[\w-]+$/', $signup_username)) {
 		$errors = 'Username is not valid!';
-   		//echo "String 1 not acceptable acceptable";
-   		// String2 acceptable
 	}
 	if (empty($errors)) { 
 	    die(json_encode([
