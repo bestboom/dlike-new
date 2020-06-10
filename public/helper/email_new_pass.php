983d2c14f2ab05c1da204e90faee55f085aee3ae
bestboom <certseek@gmail.com>
2020-06-10 17:50:22 +0500

    up

---
+++ public/helper/email_new_pass.php
@@ -0,0 +1,59 @@
+<?php 
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
+
+	require '../includes/config.php';
+
+if (isset($_POST['reset_pass'])  && $_POST['reset_pass'] != '' && isset($_POST['confirm_reset_pass'])  && $_POST['confirm_reset_pass'] != '' && isset($_POST['reset_email'])  && $_POST['reset_email'] != '') { 
+
+	$reset_pass = trim($_POST["reset_pass"]);
+	$confirm_reset_pass = trim($_POST["confirm_reset_pass"]);
+	$reset_email = trim($_POST["reset_email"]);
+
+	if(empty($reset_pass)){
+        $errors = "Password Shoould not be empty";
+    }
+    if(empty($confirm_reset_pass)){
+        $errors = "Confirm Password Shoould not be empty";
+    }
+    if($confirm_reset_pass != reset_pass){
+        $errors = "Both Passwords do nto match";
+    }
+    if (empty($errors)) {
+    	$email = mysqli_real_escape_string($conn, $reset_email);
+    	$newPW = mysqli_real_escape_string($conn, $reset_pass);
+		$escapedPWN = md5($newPW);
+		$hashedPW = hash('sha256', $escapedPWN);
+
+		$update_pass = "UPDATE dlikeaccounts SET password = '$hashedPW' WHERE email = '$email'";
+		$result_update_pass = $conn->query($update_pass);
+		if ($result_update_pass === TRUE) {
+
+			$dlike_user_login_url = 'https://dlike.io';
+
+			$deleteuser = "DELETE FROM dlikepasswordreset where `email` = '".$email."'";
+			$deleteuser_q = $conn->query($deleteuser);
+
+			die(json_encode([
+	    	'error' => false,
+    		'message' => 'Password Updated Successful!',
+    		'redirect' => $dlike_user_login_url
+			]));
+
+
+		} else {
+	    die(json_encode([
+    		'error' => true,
+    		'message' => 'Some issue in password reset. Please try later!'
+		])); }
+
+    } else {
+	    die(json_encode([
+    		'error' => true,
+    		'message' => $errors
+		]));
+	}
+} else {die('Some error');}
+
+?>
--- public/password_reset.php
+++ public/password_reset.php
@@ -13,6 +13,7 @@
 	}
 	$row = mysqli_fetch_assoc($result_link);
 	$reset_date = $row['reset_time'];
+	$reset_email = $row['email'];
   	if ($reset_date >= $curDate){
   		$errors = 'The link to reset password has expired';
   	}
@@ -23,13 +24,12 @@
             <div class="modal-content" style="background: #1b1e63;border-radius: 14px;">
                 <div class="modal-body">
                     <div class="share-block">
-                    	<?php echo $curDate; ?>
                         <p>Reset Password</p>
-                        <?php echo $reset_date; ?>
                     </div>
                     <div class="user-connected-form-block" style="background: #1b1e63;">
                     <?php if (empty($errors)) { ?>
-                        <form class="user-connected-from share-form">
+                        <form class="user-connected-from password-reset-form">
+                        	<input type="hidden" id="reset_email_id" value="<?php echo $reset_email; ?>" />
                             <div class="input-group mb-3" style="padding: 3px;">
                                 <div class="input-group-prepend">
                                     <div class="input-group-text mb-deck" style="background: #b6c9fb;"> <span class="fa fas fa-lock"></span></div>
@@ -46,10 +46,56 @@
                                 <button type="button" class="btn btn-default" style="width: 40%;padding-top: 15px;" id="reset_pass_btn">Reset Password</button>
                             </center>
                         </form>
-                    <? } else { echo '<h3>'.$errors.'</h3>'; } ?>
+                    <? } else { echo '<h3 style="color: #e1ec31;">'.$errors.'</h3>'; } ?>
                     </div>
                 </div>
             </div>
         </div>
     </div>  
 <?php include('template/footer.php'); ?>
+<script type="text/javascript">
+	$('.reset_pass_btn').click(function() {
+		let reset_email = $('#reset_email_id').val();
+		let reset_pass = $('#reset_pass').val();
+    	let confirm_reset_pass = $('#confirm_reset_pass').val();
+    	let email_verify_url = 'helper/email_new_pass.php';
+
+	    if (reset_pass == "") {
+	        toastr.error('phew... Password should not be empty');
+	        return false;
+	    }
+	    if (confirm_reset_pass == "") {
+	        toastr.error('phew... Confirm Password should not be empty');
+	        return false;
+	    }
+	    if (confirm_reset_pass !== reset_pass) {
+	        toastr.error('phew... Passwords do not match!');
+	        return false;
+	    }
+	    var data_new_pass = {
+	    	reset_email: reset_email,
+	        reset_pass: reset_pass,
+	        confirm_reset_pass: confirm_reset_pass
+	    };
+	    $.ajax({
+	        type: "POST",
+	        url: email_verify_url,
+	        data: data_new_pass,
+	        success: function(data) {
+	            try {
+	                var response = JSON.parse(data)
+	                if (response.error == true) {
+	                    toastr['error'](response.message);
+	                } else {
+	                    toastr['success'](response.message);
+	                    setTimeout(function(){
+	                        window.location.href = response.redirect;
+	                    }, 1000);
+	                }
+	            } catch (err) {
+	                toastr.error('Sorry. Server response is malformed');
+	            }
+	        }
+	    });
+	});
+</script>
