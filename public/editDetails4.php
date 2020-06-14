d835b7928d1c179d1c63bda5d9813b9f462bf730
bestboom <certseek@gmail.com>
2020-06-14 19:11:33 +0500

    Update editDetails4.php

--- public/editDetails4.php
+++ public/editDetails4.php
@@ -49,6 +49,7 @@ if (isset($_GET["url"])) {
         </div>
   
 <?php include('template/dlike_footer.php'); ?>
+<script src="https://unpkg.com/xregexp@3.2.0/xregexp-all.js"></script>
 <script type="text/javascript">
     $(document).ready(function() {
    window.mobilecheck = function() {
