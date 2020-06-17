706c307752bc184969f7f30a9817be0457d978db
bestboom <certseek@gmail.com>
2020-06-18 00:58:36 +0500

    Update index2.php

--- public/index2.php
+++ public/index2.php
@@ -15,7 +15,39 @@ $posttags = "SELECT tagname, count(*) FROM posttags WHERE updated_at > DATE_SUB(
     } else {
         $trending_html = '';
     }
+
+?>
+<div class="row" style="margin: 30px;">
+    <table class="table coin-list table-hover" style="border: 1px solid #eee;">
+        <tbody>
+<?php
+$sql_T = "SELECT * FROM dlikeposts ORDER BY created_at DESC";
+    $result_T = $conn->query($sql_T);
+
+    if ($result_T && $result_T->num_rows > 0) { 
+        while ($row_T = $result_T->fetch_assoc()) { 
+            ?>
+            <tr>
+                <td class="exp-user cent_me wid_2">
+                    <span><?php echo $row_T["username"]; ?></span>
+                </td>
+                <td class="exp-user cent_me wid_2">
+                    <span><?php echo $row_T["title"]; ?></span>
+                </td>
+                <td class="exp-amt cent_me wid_2">
+                    <span><?php echo $row_T["description"]; ?></span>
+                </td>
+                <td class="exp-amt cent_me wid_2">
+                    <span><?php echo $row_T["tags"]; ?></span>
+                </td>
+            </tr>
+            <?php
+        }
+    }
 ?> 
+        </tbody>
+    </table>
+</div>
 <div class="latest-post-section" style="min-height:80vh;padding: 70px 0px 60px 0px;">
     <div class="container">
         <div class="row  align-items-center h-100 post_select">
