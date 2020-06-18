b73e47970366abe4ddaa2e6dedf539fb2131b46d
bestboom <certseek@gmail.com>
2020-06-18 12:26:01 +0500

    Update index2.php

--- public/index2.php
+++ public/index2.php
@@ -7,25 +7,10 @@
         $sql_T = "SELECT * FROM dlikeposts ORDER BY created_at DESC";
             $result_T = $conn->query($sql_T);
 
-            if ($result_T && $result_T->num_rows > 0) { 
-                while ($row_T = $result_T->fetch_assoc()) { 
-                    ?>
-                    <tr>
-                        <td class="exp-user cent_me wid_2">
-                            <span></span>
-                        </td>
-                        <td class="exp-user cent_me wid_2">
-                            <span><?php echo $row_T["title"]; ?></span>
-                        </td>
-                        <td class="exp-amt cent_me wid_2">
-                            <span><?php echo $row_T["description"]; ?></span>
-                        </td>
-                        <td class="exp-amt cent_me wid_2">
-                            <span><?php echo $row_T["tags"]; ?></span>
-                        </td>
-                    </tr>
-
-
+        if ($result_T && $result_T->num_rows > 0) { 
+            while ($row_T = $result_T->fetch_assoc()) { 
+                $imgUrl = $row_T["img_url"];
+        ?>
             <div class="col-lg-4 col-md-6">
                 <article class="post-style-two">
                     <div class="post-contnet-wrap-top">
@@ -39,7 +24,7 @@
                     <div class="post-comments post-catg"><a href="/category/"><span class="post-meta"><?php echo $row_T["ctegory"]; ?></span></a></div>
                     </div>
                     </div>
-                    <div class="post-thumb img-fluid"><a href="/post/@"><?php echo $row_T["img_url"]; ?></a></div>
+                    <div class="post-thumb img-fluid"><a href="/post/@"><?php echo '<img src='.$imgUrl.'>'; ?></a></div>
                     <div class="post-contnet-wrap">
                     <div class="row d-flex justify-content-center hov-it"><div class="hov-item"><img src="./images/post/dlike-hover.png" alt="img" class="img-responsive"><span class="hov_me" data-toggle="modal" data-target="" data-likes="" data-permlink="' + $post.permlink + '" data-author="' + $post.author + '"><div class="hov-txt"><h5><span id="hov-num" class="commentsDiv' + currentLikesDivElement + '"></span></h5></div></span></div></div>
                     <h4 class="post-title"><a href="/post/@' + $post.author + '/' + $post.permlink + '"><?php echo $row_T["title"]; ?></a></h4>
