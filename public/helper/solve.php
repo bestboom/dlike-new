79a042d07590aa6e792b78fae4483bf902636f2a
bestboom <certseek@gmail.com>
2020-06-21 07:15:54 +0500

    Update solve.php

--- public/helper/solve.php
+++ public/helper/solve.php
@@ -12,19 +12,19 @@ if (isset($_POST["rec_author"]) && isset($_POST["rec_permlink"])){
 
 		$saved_ip = $_COOKIE['usertoken'];	
 		$rating = '5';
-		$userval = 'dlike';
+		$userval = $_COOKIE['dlike_username'];
 		$author =  $_POST['rec_author'];
 		$permlink =  $_POST['rec_permlink'];
 		$newLike = '1';
 
-		$verifyPost = "SELECT * FROM mylikes where userip = '$ip' and permlink = '$permlink' and author = '$author'";
+		$verifyPost = "SELECT * FROM mylikes where username = '$userval' and permlink = '$permlink' and author = '$author'";
 			$resultverify = $conn->query($verifyPost);
 			if ($resultverify->num_rows > 0) { 
-										die(json_encode([
-                                        'error' => true,
-                                        'message' => 'Sorry', 
-                                        'data' => 'Already Recomended'
-                                         ]));
+				die(json_encode([
+                'error' => true,
+                'message' => 'Sorry', 
+                'data' => 'Already Recomended'
+                 ]));
 				} else {
 				$sqlm = "INSERT INTO mylikes (username, stars, userip, author, permlink)
 						VALUES ('".$userval."', '".$rating."', '".$ip."', '".$author."', '".$permlink."')";
@@ -44,7 +44,7 @@ if (isset($_POST["rec_author"]) && isset($_POST["rec_permlink"])){
     								} else {
     									/*echo "post not exists";*/
     									$addPost = "INSERT INTO postslikes (author, permlink, likes, rating, lastUpdatedDate)
-													VALUES ('".$author."', '".$permlink."', '".$newLike ."', '".$rating."', '".date("Y-m-d h:m:s")."')";
+													VALUES ('".$author."', '".$permlink."', '".$newLike ."', '".rating."', '".date("Y-m-d h:m:s")."')";
 										$addPostQuery = $conn->query($addPost);
 													/*if ($addPostQuery === TRUE) {
    													echo "new Record added successfully"; } else { echo "new Record could not added"; }*/
