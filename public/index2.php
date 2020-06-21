1575384ceaf774c34876ccc15ec38bdb304c0b15
bestboom <certseek@gmail.com>
2020-06-21 12:19:17 +0500

    Update index2.php

--- public/index2.php
+++ public/index2.php
@@ -46,10 +46,8 @@ if ($result_T && $result_T->num_rows > 0)
         <div class="post-contnet-wrap-top">
         <div class="post-footer">
         <div class="post-author-block">
-        <div class="author-thumb"><a href="/@pillsjee"><img src="<?php echo $user_profile_pic; ?>" alt="<?php echo $row_T['username']; ?>" class="img-responsive"></a></div>
-        <div class="author-info">
-        <h5><a href="/@"><?php echo $author; ?></a><div class="time"><?php echo time_ago($post_time); ?></div></h5> 
-        </div>
+            <div class="author-thumb"><a href="/@pillsjee"><img src="<?php echo $user_profile_pic; ?>" alt="<?php echo $row_T['username']; ?>" class="img-responsive"></a></div>
+            <div class="author-info"><h5><a href="/@"><?php echo $author; ?></a><div class="time"><?php echo time_ago($post_time); ?></div></h5> </div>
         </div>
         <div class="post-comments post-catg"><a href="/category/"><span class="post-meta"><?php echo ucfirst($row_T["ctegory"]); ?></span></a></div>
         </div>
@@ -60,9 +58,7 @@ if ($result_T && $result_T->num_rows > 0)
         <p class="post-entry post-tags"><?php echo $row_T["tags"]; ?></p>
         <div class="post-footer">
         <div class="post-author-block" style="width:100%">
-        
         <div class="post-comments"><a  class="hov_me" data-toggle="modal" data-target="" data-permlink="<?php echo $permlink; ?>" data-author="<?php echo $author; ?>"><img src="./images/post/dlike-hover.png" style="cursor:pointer;width: 21px;height: 21px;"></a><span>| &nbsp;<?php echo $postLikes; ?> LIKES</span></div>
-
         <div class="author-info"><span id="dlike_tokens" data-popover="true" data-html="true" data-content="">0</span> <b>DLIKE</b></div>
         </div>
         </div>
