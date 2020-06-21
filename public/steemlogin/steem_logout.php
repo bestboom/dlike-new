cf3ad485531bd4b777a1b59dbe9411392344596d
bestboom <certseek@gmail.com>
2020-06-21 23:03:01 +0500

    Update steem_logout.php

--- public/steemlogin/steem_logout.php
+++ public/steemlogin/steem_logout.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <head>
-  <title>Steem Logout</title>
+<title>Steem Logout</title>
+<h2>Loging out of steem Account....</h2>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/steemconnect"></script>
