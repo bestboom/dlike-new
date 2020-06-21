e1bd23a550fd6ae5666709b6d79751e624793f26
bestboom <certseek@gmail.com>
2020-06-21 22:46:16 +0500

    Update steem_logout.php

--- public/steemlogin/steem_logout.php
+++ public/steemlogin/steem_logout.php
@@ -1,8 +1,8 @@
 <!DOCTYPE html>
 <head>
   <title>Steem Logout</title>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.js"></script>
   <script src="/js/steemconnect.js"></script>
 </head>
 <script>
