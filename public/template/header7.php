f82352293654460e80de84074a72007824102516
bestboom <certseek@gmail.com>
2020-06-21 19:24:58 +0500

    up

--- public/template/header7.php
+++ public/template/header7.php
@@ -64,39 +64,35 @@ transition: .3s;font-weight: 600;cursor:pointer;}</style>
 <?php if (isset($_COOKIE['dlike_username']) || !empty($_COOKIE['dlike_username'])) { echo '<a id="logout_btn">Logout</a>';} else { } ?><br>
 <a href="/docs/dlike-paper.pdf">Whitepaper</a>
 </div>
-    <div class="banner-block">
-        <nav class="navbar main-nav navbar-expand-lg">
-            <div class="container">
-                <a class="navbar-brand" href="/">
-                    <img class="navbar-logo" src="/images/logo.png" alt="Dlike"/>
-                </a>
-                <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item" style="padding-top: 4px;">
-                            <div class="row" style="margin: 0px;">
-                                <div class="col-md-3 col-2">
-                                    <?php if (isset($_COOKIE['dlike_username']) || $_COOKIE['dlike_username']) { ?>
-                                    <button onclick="window.location.href='/rewards';" type="button" class="btn btn-default btn-circle-it btn-lg custom_btn_icon search_btn_hover"><i class="fas fa-user"></i>
-                                    </button>
-                                    <? } elseif (isset($_COOKIE['username']) || $_COOKIE['username'])  { ?>
-                                    <button onclick="window.location.href='/@<? echo $_COOKIE['username']; ?>';" type="button" class="btn btn-default btn-circle-it btn-lg custom_btn_icon search_btn_hover"><span class="img_profile"><img src="/images/user.png" alt="<? echo $_COOKIE['username']; ?>" title="<? echo $_COOKIE['username']; ?>" id="user_img" class="rounded-circle img-fluid" style="background: #fff;margin-top: -6px;"></span>
-                                    </button><? } else { ?>
-                                    <button onclick="window.location.href='/rewards';" type="button" class="btn btn-default btn-circle-it btn-lg custom_btn_icon search_btn_hover"><i class="fas fa-user"></i>
-                                    </button><? } ?>
-                                </div>
-                                <div class="col-md-4 col-2">
-                                    <button onclick="window.location.href='/share';" id="btn_share" type="button" class="btn btn-default btn-circle-it btn-lg custom_btn_icon edit_btn_hover"><i class="fa fa-pencil-alt"></i>
-                                    </button>
-                                </div>
-                                <div class="col-md-4 col-2" style="margin-top: 5px;">
-                                    <span class="nav_sticks" onclick="openNav()">&#9776;</span>
-                                </div>
+<div class="banner-block">
+    <nav class="navbar main-nav navbar-expand-lg">
+        <div class="container">
+            <a class="navbar-brand" href="/"><img class="navbar-logo" src="/images/logo.png" alt="Dlike"/></a>
+            <button class="navbar-toggler collapsed" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item" style="padding-top: 4px;">
+                        <div class="row" style="margin: 0px;">
+                            <div class="col-md-3 col-2">
+                                <?php if (isset($_COOKIE['dlike_username']) || $_COOKIE['dlike_username']) { ?>
+                                <button onclick="window.location.href='/rewards';" type="button" class="btn btn-default btn-circle-it btn-lg custom_btn_icon search_btn_hover"><i class="fas fa-user"></i>
+                                </button>
+                                <? } elseif (isset($_COOKIE['username']) || $_COOKIE['username'])  { ?>
+                                <button onclick="window.location.href='/@<? echo $_COOKIE['username']; ?>';" type="button" class="btn btn-default btn-circle-it btn-lg custom_btn_icon search_btn_hover"><span class="img_profile"><img src="/images/user.png" alt="<? echo $_COOKIE['username']; ?>" title="<? echo $_COOKIE['username']; ?>" id="user_img" class="rounded-circle img-fluid" style="background: #fff;margin-top: -6px;"></span>
+                                </button><? } else { ?>
+                                <button onclick="window.location.href='/rewards';" type="button" class="btn btn-default btn-circle-it btn-lg custom_btn_icon search_btn_hover"><i class="fas fa-user"></i>
+                                </button><? } ?>
                             </div>
-                        </li>
-                    </ul>
-                </div>
+                            <div class="col-md-4 col-2">
+                                <button onclick="window.location.href='/share';" id="btn_share" type="button" class="btn btn-default btn-circle-it btn-lg custom_btn_icon edit_btn_hover"><i class="fa fa-pencil-alt"></i>
+                                </button>
+                            </div>
+                            <div class="col-md-4 col-2" style="margin-top: 5px;">
+                                <span class="nav_sticks" onclick="openNav()">&#9776;</span>
+                            </div>
+                        </div>
+                    </li>
+                </ul>
             </div>
-        </nav><!-- main-nav-block -->
+        </div>
+    </nav><!-- main-nav-block -->
--- public/template/modals/upvotefail.php
+++ public/template/modals/upvotefail.php
@@ -1,10 +1,1 @@
-<div class="modal-body ">
-    <div class="mdStatusTitle sttError"><div class="iconTitle"><i class="fas fa-frown"></i></div></div>
-        <div class="mdStatusContent">
-            <h3 id="alert-title-error">Uh ho....</h3>
-                <p id="alert-content-error">You have already Upvoted This Post!</p>
-                <div class="actBtn">
-                	<button type="button" class="btn btn-danger" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">Ok.. I Understand!</span></button>
-                </div>
-        </div>
-</div>
+<div class="modal-body "><div class="mdStatusTitle sttError"><div class="iconTitle"><i class="fas fa-frown"></i></div></div><div class="mdStatusContent"><h3 id="alert-title-error">Uh ho....</h3><p id="alert-content-error">You have already Recommended this Share!</p><div class="actBtn"><button type="button" class="btn btn-danger" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">Ok.. I Understand!</span></button></div></div></div>
