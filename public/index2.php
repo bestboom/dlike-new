00092adcace8c5d3470a9b59ade50cc791fc982f
bestboom <certseek@gmail.com>
2020-06-25 20:32:34 +0500

    Update index2.php

--- public/index2.php
+++ public/index2.php
@@ -2,7 +2,7 @@
 <style>
     .latest-post-section{min-height:80vh;padding: 70px 0px 60px 0px;}
     .hov_vote{cursor:pointer;width: 21px;height: 21px;}
-    #post_likes{padding-right: 3px;font-weight: bold;padding-left: 3px;}
+    .post_likes{padding-right: 3px;font-weight: bold;padding-left: 3px;}
     .bottom_block{width:100%}
 </style>
 </div>
@@ -46,7 +46,7 @@ if ($result_T && $result_T->num_rows > 0)
     <h4 class="post-title"><a href="/post/@"><?php echo $row_T["title"]; ?></a></h4>
     <p class="post-entry post-tags"><?php echo $row_T["tags"]; ?></p>
     <div class="post-footer"><div class="post-author-block bottom_block">
-    <div class="post-comments"><a  class="hov_me" data-toggle="modal" data-target="" data-permlink="<?php echo $permlink; ?>" data-author="<?php echo $author; ?>"><img src="./images/post/dlike-hover.png" class="hov_vote"></a> | <div id="post_likes"><?php echo $postLikes; ?></div>LIKES</div>
+    <div class="post-comments"><a  class="hov_me" data-toggle="modal" data-target="" data-permlink="<?php echo $permlink; ?>" data-author="<?php echo $author; ?>"><img src="./images/post/dlike-hover.png" class="hov_vote"></a> | <div class="post_likes">0</div>LIKES</div>
     <div class="author-info"><span class="dlike_tokens"><?php echo $post_income; ?></span> <b>DLIKE</b></div>
     </div></div></div>
 </article></div>
@@ -85,8 +85,8 @@ if ($result_T && $result_T->num_rows > 0)
     //$('.latest-post-section').on("click", ".recomendme", function() {
     $('.recomendme').click(function() {
         if (dlike_username != null) {
-            var getpostlikes = $('.latest-post-section').find("#post_likes");
-            var likesofpost = $(this).html();
+            //var getpostlikes = $('.latest-post-section').find("#post_likes");
+            var likesofpost = $(".post_likes").html();
             var r_permlink = $("#r_permlink").val();
             var r_author = $("#r_author").val();
             var update = '1';
@@ -113,7 +113,7 @@ if ($result_T && $result_T->num_rows > 0)
                             toastr.success(response.message);
                             var newlikes = parseInt(likesofpost)+parseInt(update);
                             console.log(newlikes);
-                            $('#post_likes').html(newlikes);
+                            $('.post_likes').html(newlikes);
                             var post_income = response.post_income;
                             console.log(post_income);
                             var updatespostincome = newlikes * post_income;
