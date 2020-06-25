9d602aa4c5dc39ddf95df2f3015611d9f3eb4d70
bestboom <certseek@gmail.com>
2020-06-25 10:14:34 +0500

    Update index2.php

--- public/index2.php
+++ public/index2.php
@@ -2,7 +2,7 @@
 <style>
     .latest-post-section{min-height:80vh;padding: 70px 0px 60px 0px;}
     .hov_vote{cursor:pointer;width: 21px;height: 21px;}
-    .post_likes{padding-right: 3px;font-weight: bold;padding-left: 3px;}
+    #post_likes{padding-right: 3px;font-weight: bold;padding-left: 3px;}
     .bottom_block{width:100%}
 </style>
 </div>
@@ -45,7 +45,7 @@ if ($result_T && $result_T->num_rows > 0)
     <h4 class="post-title"><a href="/post/@"><?php echo $row_T["title"]; ?></a></h4>
     <p class="post-entry post-tags"><?php echo $row_T["tags"]; ?></p>
     <div class="post-footer"><div class="post-author-block bottom_block">
-    <div class="post-comments"><a  class="hov_me" data-toggle="modal" data-target="" data-permlink="<?php echo $permlink; ?>" data-author="<?php echo $author; ?>"><img src="./images/post/dlike-hover.png" class="hov_vote"></a> | <span class="post_likes"><?php echo $postLikes; ?></span>LIKES</div>
+    <div class="post-comments"><a  class="hov_me" data-toggle="modal" data-target="" data-permlink="<?php echo $permlink; ?>" data-author="<?php echo $author; ?>"><img src="./images/post/dlike-hover.png" class="hov_vote"></a> | <div id="post_likes"><?php echo $postLikes; ?></div>LIKES</div>
     <div class="author-info"><span id="dlike_tokens" data-popover="true" data-html="true" data-content="">0</span> <b>DLIKE</b></div>
     </div></div></div>
 </article></div>
@@ -105,7 +105,7 @@ if ($result_T && $result_T->num_rows > 0)
                             $('#up_vote').removeAttr('data-target');
                             $('#vote_icon').addClass("not-active");
                             toastr.success(response.message);
-                            $('.post_likes').html(response.data);
+                            $('#post_likes').html(response.data);
                             $('#recomendModal').modal('hide');
                             $('#recomend-status').hide();
                             $('#recomend-bar').show();
