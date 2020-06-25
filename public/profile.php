e87b80f3c3178dc7cc3469aa8c37aafee81bb65d
bestboom <certseek@gmail.com>
2020-06-25 18:57:07 +0500

    Update profile.php

--- public/profile.php
+++ public/profile.php
@@ -445,7 +445,7 @@ include('template/header5.php');
 					'\n' +
 					'<div class="post-contnet-wrap">\n' +
 					'\n' +
-					'<div class="row d-flex justify-content-center hov-it"><div class="hov-item"><img src="./images/post/dlike-hover.png" alt="img" class="img-responsive"><span class="hov_me" data-toggle="modal" data-target="" data-likes="" data-permlink="' + $post.permlink + '" data-author="' + $post.author + '"><div class="hov-txt"><h5><span id="hov-num" class="commentsDiv' + currentLikesDivElement + '"></span></h5></div></span></div></div>\n' +
+					'<div class="row d-flex justify-content-center hov-it"><div class="hov-item"><img src="./images/post/dlike-hover.png" alt="img" class="img-responsive"><span class="hov_me" data-toggle="modal" data-target="" data-likes="" data-permlink="' + $post.permlink + '" data-author="' + $post.author + '"><div class="hov-txt"><h5><span id="hov-num total_likes" class="commentsDiv' + currentLikesDivElement + '"></span></h5></div></span></div></div>\n' +
                     '\n' +
 					'<h4 class="post-title"><a href="/post/@' + $post.author + '/' + $post.permlink + '">' + $post.title + '</a></h4>\n' +
 					'\n' +
@@ -596,7 +596,7 @@ function getTotalLikes(thisAutor, thisPermlink, currentLikesDivElement){
         $("#r_likes").val(postsrec);
     });
 
-    
+
     $('.recomendme').click(function() {
         //alert('good');
         var r_permlink = $("#r_permlink").val();
