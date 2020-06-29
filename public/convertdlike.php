49eb5ceff5cc38d972a127a5498ceb5ccf742e9b
bestboom <certseek@gmail.com>
2020-06-29 21:05:24 +0500

    Update convertdlike.php

--- public/convertdlike.php
+++ public/convertdlike.php
@@ -49,28 +49,29 @@ $dlike_bal    = $rowIt['amount'];
                                 <div class="input-group-prepend">
                                     <div class="input-group-text mb-deck" style="background: #b6c9fb;"> ETH Address</div>
                                 </div>
-                                <input type="number" step="0.0001" min="0" max="10" name="dlike_convert_amount" id="dlike_convert_amount" placeholder="Enter ETH which has DLIKE Balance" class="form-control" style="padding: 8px;" />
+                                <input type="text" placeholder="Enter ETH which has DLIKE Balance" class="form-control" style="padding: 8px;" />
                             </div>
                             <div class="input-group mb-3" style="padding: 3px;">
                                 <div class="input-group-prepend">
                                     <div class="input-group-text mb-deck" style="background: #b6c9fb;">Convert Amount</div>
                                 </div>
-                                <input type="number" step="0.0001" min="0" max="10" name="dlike_convert_amount" id="dlike_convert_amount" placeholder="Amount to conver" class="form-control" style="padding: 8px;" />
+                                <input type="number" step="0.0001" min="0" max="10" name="dlike_convert_amount" id="dlike_convert_amount" placeholder="Amount to convert" class="form-control" style="padding: 8px;" />
                             </div>
                             <div class="input-group mb-3" style="padding: 3px;">
                                 <div class="input-group-prepend">
                                     <div class="input-group-text mb-deck" style="background: #b6c9fb;">Steem Username</div>
                                 </div>
-                                <input type="number" step="0.0001" min="0" max="10" name="dlike_convert_amount" id="dlike_convert_amount" placeholder="Enter steem address" class="form-control" style="padding: 8px;" />
+                                <input type="text" placeholder="Enter steem address" class="form-control" style="padding: 8px;" />
                             </div>
                             <div class="input-group mb-3" style="padding: 3px;">
                                 <div class="input-group-prepend">
                                     <div class="input-group-text mb-deck" style="background: #b6c9fb;">Earned By</div>
                                 </div>
-                                <input type="number" step="0.0001" min="0" max="10" name="dlike_convert_amount" id="dlike_convert_amount" placeholder="How you earned these tokens (IEO, Bounty)" class="form-control" style="padding: 8px;" />
+                                <input type="text" placeholder="How you earned tokens (IEO, Bounty)" class="form-control" style="padding: 8px;" />
                             </div>
+                            <div style="font-weight:500;text-align: left;padding-top:5px;padding-bottom: 5px;cursor: pointer;color:#fff;" class="eth_send_addr">Send DLIKE tokens to 131213212 to proceed conversion!</div>
                             <center>
-                                <button type="button" class="btn btn-default" style="width: 40%;margin-top: 15px;" id="with_tok">Submit</button>
+                                <button type="button" class="btn btn-default" style="width: 40%;margin-top: 15px;" id="with_eth_tok">Submit</button>
                             </center>
                         </form>
                     <? } 
