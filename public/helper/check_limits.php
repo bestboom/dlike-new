4e66b23830150a02d82e5f8f3b2b97b6cb74b546
bestboom <certseek@gmail.com>
2020-07-10 20:29:39 +0500

    Update check_limits.php

--- public/helper/check_limits.php
+++ public/helper/check_limits.php
@@ -1,18 +1,12 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require '../includes/config.php';
 
 if (isset($_POST['action']) && $_POST['action'] =='shares_limit' && isset($_POST['user']) && $_POST['user'] != '') {
-//if (isset($_POST["user"])){
     $user_name = $_POST['user'];
     $sql_post_limit = $conn->query("SELECT * FROM dlikeposts WHERE username = '$user_name' and created_at > now() - INTERVAL 24 HOUR");
         if ($sql_post_limit->num_rows >= 5) {
             die(json_encode(['error' => true, 'message' => 'Only 5 share allowed every 24 hours']));
-            } else {die(json_encode(['error' => false]));}   
+        } else {die(json_encode(['error' => false]));}   
 }
 
 if (isset($_POST['action']) && $_POST['action'] == 'unique_post' && isset($_POST['url']) && $_POST['url'] != '') {
