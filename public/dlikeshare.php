57911294db1c8dc77d160a694bf7f89c72ea51d4
bestboom <certseek@gmail.com>
2020-07-10 21:25:55 +0500

    Update dlikeshare.php

--- public/dlikeshare.php
+++ public/dlikeshare.php
@@ -32,18 +32,14 @@ if (!isset($_COOKIE['dlike_username']) || !$_COOKIE['dlike_username']) {
 <script type="text/javascript">
     $('#dlike_share').click(function() {
         if (dlike_username != null) {
+            $('.share_loader').show();
             let input_url = $("#url_field").val();
-            if (input_url == '') {
-                $("#url_field").css("border-color", "RED");
-                toastr.error('phew... You forgot to enter URL');
-                return false;
+            if (input_url == '') { $("#url_field").css("border-color", "RED");toastr.error('phew... You forgot to enter URL');$('#share_plus').show();$('.share_loader').hide();return false;
             }
             let verifyUrl = getDomain(input_url);
             let restricted_urls = ["dlike.io", "steemit.com", "wikipedia.org"];
             if (isValidURL(input_url)) {
-                if ($.inArray(verifyUrl, restricted_urls) > -1) {
-                    toastr.error('phew... Sharing from this url is not allowed');
-                    return false;
+                if ($.inArray(verifyUrl, restricted_urls) > -1) {toastr.error('phew... Sharing from this url is not allowed');$('#share_plus').show();$('.share_loader').hide(); return false;
                 }
 
                 $.ajax({
@@ -52,7 +48,7 @@ if (!isset($_COOKIE['dlike_username']) || !$_COOKIE['dlike_username']) {
                 data: { action : 'shares_limit',user: dlike_username },
                     success: function(data)  { 
                         try { var response = JSON.parse(data) 
-                            if (response.error == true) { toastr.error(response.message); return false;}
+                            if (response.error == true) { toastr.error(response.message); return false;$('#share_plus').show();$('.share_loader').hide();}
                             else {
                                 $.ajax({
                                     url: '/helper/check_limits.php',
@@ -60,10 +56,8 @@ if (!isset($_COOKIE['dlike_username']) || !$_COOKIE['dlike_username']) {
                                     data: { action : 'unique_post',newurl: input_url },
                                     success: function(data)  { 
                                         try { var response = JSON.parse(data) 
-                                            if (response.error == true) { toastr.error(response.message); return false;} 
-                                            else {
-                                                $('#share_plus').hide();
-                                                $('.share_loader').show();
+                                            if (response.error == true) { toastr.error(response.message); return false;$('#share_plus').show();$('.share_loader').hide();} 
+                                            else {$('#share_plus').hide();$('.share_loader').show();
                                                 fetch_data("helper/main.php", url);
                                             }
                                         } catch (err) {toastr.error('Sorry. Server response is malformed.');}
@@ -73,9 +67,7 @@ if (!isset($_COOKIE['dlike_username']) || !$_COOKIE['dlike_username']) {
                         } catch (err) {toastr.error('Sorry. Server response is malformed.');}
                     }
                 });
-            } else {
-                toastr.error('phew... URL is not Valid');
-            }
+            } else {toastr.error('phew... URL is not Valid');}
         } else { toastr.error('hmm... You must be login!'); return false; }
     });
     function fetch_data(apiUrl, webUrl) {
@@ -87,12 +79,8 @@ if (!isset($_COOKIE['dlike_username']) || !$_COOKIE['dlike_username']) {
     }
     function isValidURL(url) {
         var RegExp = /(http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/;
-        if (RegExp.test(url)) {
-            return true;
-        } else {
-            toastr.error('phew... Enter a valid url');
-            return false;
-        }
+        if (RegExp.test(url)) {return true;
+        } else {toastr.error('phew... Enter a valid url');return false;}
     }
     function getDomain(url) {
         let hostName = getHostName(url);
@@ -113,8 +101,6 @@ if (!isset($_COOKIE['dlike_username']) || !$_COOKIE['dlike_username']) {
         var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
         if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
             return match[2];
-        } else {
-            return false;
-        }
+        } else {return false;}
     }
 </script>
