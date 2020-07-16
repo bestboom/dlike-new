b29849f23958f88a538f2868491045454edcbfeb
bestboom <certseek@gmail.com>
2020-07-16 20:59:33 +0500

    up

--- public/dlike_post.php
+++ public/dlike_post.php
@@ -9,7 +9,12 @@ if ($sql_P && $sql_P->num_rows > 0){
     $post_time = strtotime($row_P["created_at"]);
     $permlink = $row_P["permlink"];
     $title = $row_P["title"];
+    $description = $row_P["description"];
 }
+
+$sql_W = $conn->query("SELECT * FROM dlikeaccounts where username = '$user'");
+    $row_W = $sql_W->fetch_assoc();
+    $profile_pic = $row_W["profile_pic"];
 ?>
 </div>
     <div class="latest-post-section">
@@ -19,27 +24,21 @@ if ($sql_P && $sql_P->num_rows > 0){
                     <?php echo '<a href="#"><img src="'.$imgUrl.'" alt="'.$permlink.'" class="img-responsive"></a>'; ?>
                 </div>
                 <div class="post-contnet-wrap">
-                    <span class="post-meta"><?php echo time_ago($post_time); ?></span>
-                    <h4 class="post-title">
+                    <div class="author-info">
+                        <h5><a href="#">@<?php echo $user; ?></a></h5>
+                        <span class="post-meta"><?php echo time_ago($post_time); ?></span>
+                    </div>
+                    <h4 class="post-title" style="line-height: 1.8rem;white-space: normal;">
                         <a href="#"><?php echo $title; ?></a>
                     </h4>
-                    <p class="post-entry">
-                        No third party can freeze or lose your funds! With enterprise-level 
-                        security superior to most other t is a long established fact that a 
-                        reader will be distracted by the readable content of a page when l
-                        ooking at its layout. The point of using...
-                    </p>
+                    <p class="post-entry"><?php echo $description; ?></p>
                     <div class="post-footer">
                         <div class="post-author-block">
                             <div class="author-thumb">
-                                <?php echo '<a href="#"><img src="'.$imgUrl.'" alt="'.$permlink.'" class="img-responsive"></a>'; ?>
+                                <?php echo '<a href="#"><img src="'.$profile_pic.'" alt="'.$user.'" class="img-responsive"></a>'; ?>
                             </div>
                             <div class="author-info">
-                                <h5>
-                                    <a href="#">
-                                        Nayn e Castro
-                                    </a>
-                                </h5>
+                                <h5><a href="#">@<?php echo $user; ?></a></h5>
                                 <a href="#">@<?php echo $user; ?></a>
                             </div>
                         </div>
@@ -306,137 +305,4 @@ if ($sql_P && $sql_P->num_rows > 0){
             </div>
         </div>
     </div>
-    <div class="colto-section">
-        <div class="container">
-            <div class="colto-content-wrap">
-                <div class="colto-content">
-                    <h3>Getting started</h3>
-                    <p>We provide professional and secure trading services for investors</p>
-                </div>
-                <div class="colto-btn-group">
-                    <button class="btn callto-btn">Log In</button>
-                    <span>or</span>
-                    <button class="btn callto-btn">Create Account</button>
-                </div>
-            </div>
-        </div>
-    </div><!-- colto-section -->
-    <!-- Call to Action End -->
-    <footer class="footer">
-        <div class="footer-upper-section">
-            <div class="container">
-                <div class="row">
-                    <div class="col-lg-3 col-md-4">
-                        <div class="footer-logo">
-                            <a href="#">
-                                <img src="./images/logo.png" alt="img" class="img-responsive"/>
-                            </a>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4">
-                        <div class="footer-info-list">
-                            <h4>About Us</h4>
-                            <ul>
-                                <li><a href="#">Our Team</a></li>
-                                <li><a href="#">Our Company</a></li>
-                                <li><a href="#">Contact Us</a></li>
-                                <li><a href="#">Token Listing</a></li>
-                                <li><a href="#">Join Us</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4">
-                        <div class="footer-info-list">
-                            <h4>Learn</h4>
-                            <ul>
-                                <li><a href="#">Legal</a></li>
-                                <li><a href="#">Terms of Use</a></li>
-                                <li><a href="#">AML&CFT</a></li>
-                                <li><a href="#">Privacy Policy</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-2 col-md-4">
-                        <div class="footer-info-list">
-                            <h4>Help</h4>
-                            <ul>
-                                <li><a href="#">Support</a></li>
-                                <li><a href="#">API Support</a></li>
-                                <li><a href="#">Coin/Token Listing</a></li>
-                                <li><a href="#">Partnership</a></li>
-                            </ul>
-                        </div>
-                    </div>
-                    <div class="col-lg-3 col-md-4">
-                        <div class="footer-info-list">
-                            <h4>Contact Us</h4>
-                            <ul class="contact-info">
-                                <li>Email:  <span>info.excoin@gmail.com</span></li>
-                                <li>Phone:   <span>+99 5589 54789</span></li>
-                            </ul>
-                            <ul class="social-style-two">
-                                <li>
-                                    <a href="#">
-                                        <i class="fab fa-facebook-f"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fab fa-twitter"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fab fa-google-plus-g"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fab fa-github"></i>
-                                    </a>
-                                </li>
-                                <li>
-                                    <a href="#">
-                                        <i class="fab fa-linkedin-in"></i>
-                                    </a>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="footer-icon-wrap">
-                <a href="#">
-                    <img src="./images/others/31.png" alt="img" class="img-responsive">
-                </a>
-            </div>
-        </div>
-        <div class="footer-bottom">
-            <div class="container">
-                <div class="footer-bottom-wrap">
-                    <div class="trade-volume-block">
-                        <ul>
-                            <li>
-                                <span>39151</span> Active Traders
-                            </li>
-                            <li>
-                                <span>4191 BTC</span> 24h Volume
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="copyright-text">
-                        © 2019 <a href="#">Excoin</a>. All Rights Reserved
-                    </div>
-                </div>
-            </div>
-        </div>
-    </footer>
-    <script src="./assets/js/jquery-1.12.4.min.js"></script>
-    <script src="./assets/js/popper.min.js"></script>
-    <script src="./assets/js/bootstrap.min.js"></script>
-    <script src="./assets/js/slick.min.js"></script>
-    <script src="./assets/js/jquery.peity.min.js"></script>
-    <script src="./assets/js/jquery.slimscroll.min.js"></script>
-    <script src="./js/custom.js"></script>
-</body>
-</html>
+<?php include ('template/dlike_footer.php'); ?>
