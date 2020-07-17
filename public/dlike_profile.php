a253c06a739f733287e08c66f197cbe3311419b5
bestboom <certseek@gmail.com>
2020-07-17 21:44:35 +0500

    Update dlike_profile.php

--- public/dlike_profile.php
+++ public/dlike_profile.php
@@ -120,15 +120,53 @@ include('template/header7.php');
 		                        	<div id="cmt_content">
 		                        	
 <?php
-$sql_T = $conn->query("SELECT * FROM dlike_upvotes where curator = '$prof_user' ORDER BY curation_time DESC");
-if ($sql_T && $sql_T->num_rows > 0)
-{   while ($row_T = $sql_T->fetch_assoc())
+$sql_M = $conn->query("SELECT * FROM dlike_upvotes where curator = '$prof_user' ORDER BY curation_time DESC");
+if ($sql_M && $sql_M->num_rows > 0)
+{   while ($row_M = $sql_M->fetch_assoc())
     {
-    	echo $author = $row_T["author"];
-        echo $permlink = $row_T["permlink"];
-        echo '<br>';
-} }
+    	$likes_author = $row_M["author"];
+        $likes_permlink = $row_M["permlink"];
+
+        $sql_T = $conn->query("SELECT * FROM dlikeposts ORDER BY created_at DESC LIMIT 60");
+		if ($sql_T && $sql_T->num_rows > 0)
+		{
+			$imgUrl = $row_T["img_url"];
+	        $author = $row_T["username"];
+	        $post_time = strtotime($row_T["created_at"]);
+	        $title = $row_T["title"];
+	        $post_tags = $row_T["tags"];
+	        $permlink = $row_T["permlink"];
+	        $post_hash_tags = preg_replace('/(\w+)/', '#$1', $post_tags);
+		}
+
+        $sql_W = $conn->query("SELECT * FROM dlikeaccounts where username = '$likes_author'");
+        if ($sql_W && $sql_W->num_rows > 0)
+        {   $row_W = $sql_W->fetch_assoc();
+            $profile_pic = $row_W["profile_pic"];
+            if (!empty($profile_pic)) { $user_profile_pic = $profile_pic; } else { $user_profile_pic = 'https://i.postimg.cc/rwbTkssy/dlike-user-profile.png';}
+        }
+
+        $checkLikes = $conn->query("SELECT * FROM postslikes WHERE author = '$likes_author' and permlink = '$likes_permlink'");
+        $row_L = $checkLikes->fetch_assoc();
+        if ($checkLikes->num_rows > 0){$postLikes = $row_L['likes'];}else{$postLikes = '0';}
+        $post_income = $postLikes * $post_reward;
 ?>
+<div class="col-lg-4 col-md-6 postsMainDiv"><article class="post-style-two">
+    <div class="post-contnet-wrap-top"><div class="post-footer"><div class="post-author-block">
+    <div class="author-thumb"><?php echo '<a href="/profile/'. $author.'"><img src="'.$user_profile_pic.'" alt="'.$row_T['username'].'" class="img-responsive"></a>'; ?></div>
+    <div class="author-info"><h5><?php echo '<a href="/profile/'. $author.'">'. $author.'</a>'; ?><div class="time"><?php echo time_ago($post_time); ?></div></h5> </div></div>
+    <div class="post-catg"><a href="/category/"><span class="post-meta"><?php echo ucfirst($row_T["ctegory"]); ?></span></a></div>
+    </div></div>
+    <div class="post-thumb img-fluid"><?php echo '<a href="/post/'.$author.'/'.$permlink.'"><img src=' . $imgUrl . ' class="card-img-top" /></a>'; ?></a></div>
+    <div class="post-contnet-wrap post_bottom">
+    <h4 class="post-title"><?php echo '<a href="/post/'.$author.'/'.$permlink.'">'.$title.'</a>'; ?></h4>
+    <p class="post-entry post-tags"><?php echo $post_hash_tags; ?></p>
+    <div class="post-comments bottom_block">
+        <div><img src="./images/post/dlike-hover.png" class="hov_vote" data-permlink="<?php echo $permlink; ?>" data-author="<?php echo $author; ?>"> | <span id="post_likes" class="post_likes<?php echo $permlink; ?><?php echo $author; ?>"><?php echo $postLikes; ?></span>LIKES</div>
+        <div><span class="dlike_tokens<?php echo $permlink; ?><?php echo $author; ?>"><?php echo $post_income; ?></span> <b>DLIKE</b></div>
+    </div>
+</article></div>
+<? } } ?>
 
 		                        	</div>
 		                        </div>
