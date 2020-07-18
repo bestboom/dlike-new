46b65903b8241f33a1dd853516307b83a3537c20
bestboom <certseek@gmail.com>
2020-07-18 21:56:31 +0500

    Update dlike_daily_reward.php

--- public/adminage/dlike_daily_reward.php
+++ public/adminage/dlike_daily_reward.php
@@ -6,6 +6,12 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 $sql_C = $conn->query("SELECT count(*) as total FROM dlike_upvotes where DATE(curation_time) = CURDATE()");
+
+//$sql_C = $conn->query("SELECT count(*) as total FROM dlike_upvotes where DATE(curation_time) = SUBDATE(CURRENT_DATE(), 1)");
 $row_C = $sql_C->fetch_assoc() or die($conn->error);
 
+
+
 ?>
+</div>
+<? echo $total_upvotes = $row_C["total"]; ?>
