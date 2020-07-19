dbad4e554b02b339b32bbb38bf97b1d29dbd1768
bestboom <certseek@gmail.com>
2020-07-19 22:48:57 +0500

    Update dlike_rewards.php

--- public/dlike_rewards.php
+++ public/dlike_rewards.php
@@ -11,6 +11,13 @@ if ($sql1 && $sql1->num_rows > 0)
 { 
     $row1 = $sql1->fetch_assoc();
     $yesterday_points = $row1["yesterday_upvotes"];
+    $staking = $row1["dlike_staking"];
+    $dao = $row1["dlike_dao"];
+    $team = $row1["dlike_team"];
+    $charity = $row1["dlike_charity"];
+    $foundation = $row1["dlike_foundation"];
+
+
 } else {
     $yesterday_points = 0;
 }
@@ -18,3 +25,122 @@ if ($sql1 && $sql1->num_rows > 0)
 echo $yesterday_points;
 
 ?>
+
+<div class="working-process-section" style="padding-top: 80px;">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-6  col-md-6">
+                <div class="working-process">
+                    <h4 class="pool_head">DLIKE Daily Reward Pool Distribution</h4>
+                    <p>
+                        You can earn tokens for your contributions to the DLIKE network. The more interactions on your content, the greater your share of the daily token reward pool to your OffChain wallet.
+                    </p>
+                    <p style="color:#c51d24;font-weight: 600;">
+                        <span class="fas fa-hand-point-right" style="padding-right: 10px;"></span>
+                        DLIKE reward pool is only available to PRO users
+                    </p>
+                    <div>
+                        <img src="/images/post/dlike-reward.png" class="img-fluid" alt="">
+                    </div>
+                </div>
+            </div>
+            <div class="offset-lg-1 col-lg-5 col-md-6">
+                <div class="user-connected-form-block" style="box-shadow: 0px 0px 10px 1px #cccccc;">
+                    <h3 style="text-align: center;">
+                        <div style="font-size: 1.1rem;">Reward Pool</div>
+                        <div class="reward_amount">Yesterday Likes: <?php echo $yesterday_points; ?></div>
+                    </h3>
+                    <form class="user-connected-from create-account-form reward_form" />
+                    <div class="form-group reward_fileds">
+                        <input type="text" class="form-control reward_input" value=" | Staking" readonly>
+                        <span class="fas fa-star inp_icon"></span>
+                        <span class="inp_text"><?php echo $staking; ?></span>
+                    </div>
+                    <div class="form-group reward_fileds">
+                        <input type="text" class="form-control reward_input" value=" | DAO" readonly>
+                        <span class="fas fa-bolt inp_icon"></span>
+                        <span class="inp_text"><?php echo $dao; ?></span>
+                    </div>
+                    <div class="form-group reward_fileds">
+                        <input type="text" class="form-control reward_input" value=" | Charity" readonly>
+                        <span class="fas fa-flask inp_icon"></span>
+                        <span class="inp_text"><?php echo $charity; ?></span>
+                    </div>
+                    <div class="form-group reward_fileds">
+                        <input type="text" class="form-control reward_input" value=" | AirDrop" readonly>
+                        <span class="fas fa-database inp_icon"></span>
+                        <span class="inp_text"><?php echo '0'; ?></span>
+                    </div>
+                    <div class="form-group reward_fileds">
+                        <input type="text" class="form-control reward_input" value=" | Team" readonly>
+                        <span class="fas fa-database inp_icon"></span>
+                        <span class="inp_text"><?php echo $team; ?></span>
+                    </div>
+                    <div class="form-group reward_fileds">
+                        <input type="text" class="form-control reward_input" value=" | Foundation" readonly>
+                        <span class="fas fa-database inp_icon"></span>
+                        <span class="inp_text"><?php echo $foundation; ?></span>
+                    </div>
+                    <p>Time Remaining for Next Reward Pool</p>
+                    <button type="button" class="btn btn-default reward_btn" disabled><span class="far fa-clock" style="font-size: 1.3rem;padding-right: 1rem;"></span><span class="dividendCountDown" style="font-size: 1.7rem;"></span></button>
+                    <p class="DlikeComments">Rewards are updated at certain intervals.</p>
+                    </form>
+                </div>
+            </div>
+        </div>
+        <div class="features-section" style="padding-top: 40px;">
+            <div class="container pool_box">
+                <div class="row">
+                    <div class="col-md-2 col-sm-3">
+                        <div class="features-block-icons">
+                            <span class="fab fa-gratipay reward_icon"></span>
+                            <p>Likes <br>
+                                <span style="font-size: 0.7rem;">Each like on your posts</span>
+                                <br><span class="head_color">20+</span>
+                            </p>
+                        </div><!-- features-block -->
+                    </div>
+                    <div class="col-md-2 col-sm-3">
+                        <div class="features-block-icons">
+                            <span class="fas fa-eye reward_icon"></span>
+                            <p>Views <br><span style="font-size: 0.7rem;">For 100 views on your posts</span><br>
+                                <span class="head_color">20+</span></p>
+                            </div><!-- features-block -->
+                        </div>
+                        <div class="col-md-2 col-sm-3">
+                            <div class="features-block-icons">
+                                <span class="fas fa-comment-alt reward_icon"></span>
+                                <p>comments <br><span style="font-size: 0.7rem;">Each one on your posts</span>
+                                    <br><span class="head_color">10+</span>
+                                </p>
+                            </div><!-- features-block -->
+                        </div>
+                        <div class="col-md-2 col-sm-3">
+                            <div class="features-block-icons">
+                                <span class="fas fa-chevron-circle-up reward_icon"></span>
+                                <p>upvotes <br><span style="font-size: 0.7rem;">$1 steem upvote</span>
+                                    <br><span class="head_color">100+</span>
+                                </p>
+                            </div><!-- features-block -->
+                        </div>
+                        <div class="col-md-2 col-sm-3">
+                            <div class="features-block-icons">
+                                <span class="fas fa-users reward_icon"></span>
+                                <p>Referralss <br><span style="font-size: 0.7rem;">Each New Referral </span>
+                                    <br><span class="head_color">50+</span>
+                                </p>
+                            </div><!-- features-block -->
+                        </div>
+                        <div class="col-md-2 col-sm-3">
+                            <div class="features-block-icons">
+                                <span class="fas fa-user-plus reward_icon"></span>
+                                <p>Referral Income <br><span style="font-size: 0.7rem;">Each Post By Your Referrals</span><br/><span class="head_color">5+</span></p>
+                            </div><!-- features-block -->
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div><!-- working-process-section-->
+<?php include('template/dlike_footer.php'); ?>
