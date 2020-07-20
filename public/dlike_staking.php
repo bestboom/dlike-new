4d89a6c14faacb4fd89901b435b5ee423b0a51ee
bestboom <certseek@gmail.com>
2020-07-20 16:57:06 +0500

    Update dlike_staking.php

--- public/dlike_staking.php
+++ public/dlike_staking.php
@@ -43,7 +43,89 @@ if(isset($_COOKIE['username']) && !empty($_COOKIE['username'])) { $staker =  $_C
                                 </div>
                             </li>
                         </ul>
-                        
+                    
+
+                    <div class="row row-cards">
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="queue-stats card p-3">
+                            <div class="d-flex align-items-center">
+                                <span class="stamp stamp-md bg-blue mr-3">
+                                  <i class="fa fa-list"></i>
+                                </span>
+                                <div>
+                                    <h4 class="m-0">
+                                        <small>In QUEUE</small>
+                                    </h4>
+                                    <small class="queue-stats-display text-muted">No posts in the Queue</small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="card p-3">
+                            <div class="d-flex align-items-center">
+                                <span class="stamp stamp-md bg-orange mr-3">
+                                  <i class="fa fa-money"></i>
+                                </span>
+                                <div>
+                                    <h4 class="m-0">
+                                        <small>Vote worth</small>
+                                    </h4>
+                                    <small class="text-muted">
+                                        <span class="run-bot-voteWorth">1.063 SBD</span>
+                                    </small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="bot-power card p-3">
+                            <div id="voting-power" class="d-flex align-items-center">
+                                <span class="stamp stamp-md bg-green mr-3">
+                                  <i class="fa fa-battery-half"></i>
+                                </span>
+                                <div style="width: 100%">
+                                    <h4 class="m-0">
+                                        <small>Dlike Voting Power</small>
+                                    </h4>
+                                    <div>
+                                        <div class="clearfix">
+                                            <div class="float-left">
+                                                <strong class="voting-power-display">95.23%</strong>
+                                            </div>
+                                            <div class="float-right">
+                                                <small class="text-muted"></small>
+                                            </div>
+                                        </div>
+                                        <div class="voting-power-bar progress progress-xs">
+                                            <div class="progress-bar bg-yellow" role="progressbar" style="width: 95.23%;" aria-valuenow="95.23" aria-valuemin="0" aria-valuemax="100"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-sm-6 col-lg-3">
+                        <div class="card p-3">
+                            <div class="d-flex align-items-center">
+                                <span class="stamp stamp-md bg-green mr-3">
+                                  <i class="fa fa-check"></i>
+                                </span>
+                                <div>
+                                    <h4 class="m-0">
+                                        <small>Last Vote</small>
+                                    </h4>
+                                    <small class="text-muted">
+                                        <span class="run-bot-lastVote">7/20/2020 1:17:34 PM</span>
+                                    </small>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                          
                         <div class="row">
                             <div class="col-sm-12 col-md-4">
