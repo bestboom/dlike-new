7f3642d106a9d37f803babb5757b1aae0a9ec35c
bestboom <certseek@gmail.com>
2020-07-22 09:19:36 +0500

    Update dlike_rewards.php

--- public/dlike_rewards.php
+++ public/dlike_rewards.php
@@ -232,28 +232,12 @@ chart.render();
 </script>
 <script src="http://code.highcharts.com/highcharts.js"></script>
 <script type="text/javascript">
-    var chart1 = Highcharts.chart({
-    chart: {
-        renderTo: 'highcontainer',
-        type: 'bar'
-    },
-    title: {
-        text: 'Fruit Consumption'
-    },
-    xAxis: {
-        categories: ['Apples', 'Bananas', 'Oranges']
-    },
-    yAxis: {
-        title: {
-            text: 'Fruit eaten'
-        }
-    },
-    series: [{
-        name: 'Jane',
-        data: [1, 0, 4]
-    }, {
-        name: 'John',
-        data: [5, 7, 3]
-    }]
+var chart = new Highcharts.Chart({
+      chart: {
+         renderTo: 'highContainer'
+      },
+      series: [{
+         data: <?php echo json_encode($data); ?>
+      }]
 });
 </script>
