3af75c91fc947a9580028ab5ffcbaafeebf99a41
bestboom <certseek@gmail.com>
2020-07-23 12:52:50 +0500

    Update dlike_staking.php

--- public/dlike_staking.php
+++ public/dlike_staking.php
@@ -231,9 +231,9 @@ $('#stake_me').click(function() {
 });
 
 function gettronweb(){
-    if(window.tronWeb && window.tronWeb.defaultAddress.base58){
-        document.write("Yes, catch it:",window.tronWeb.defaultAddress.base58)
-    } else {console.log('TronLink not working!')}
+    if(window.tronWeb && window.tronWeb.ready){
+        TRON.init();
+    } else {toastr.error('You must install Tronlink');return false;}
 }
     
 </script>
