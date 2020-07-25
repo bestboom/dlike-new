b7195126b3da87cec3a31b47ab7e5a83d084492f
bestboom <certseek@gmail.com>
2020-07-25 20:20:53 +0500

    Update today_rewards.php

--- public/crons/today_rewards.php
+++ public/crons/today_rewards.php
@@ -21,8 +21,8 @@ $foundation_reward = $total_upvotes * $foundation_percentage;
 $sql_W = $conn->query("SELECT * FROM dlike_daily_rewards where DATE(update_time) = CURDATE()");
 
 if ($sql_W->num_rows > 0){
-	$update_R = $conn->query("UPDATE dlike_daily_rewards SET today_upvotes = '$total_upvotes', dlike_staking = '$staking_reward', dlike_dao = '$dao_reward', dlike_charity = '$charity_reward', dlike_team = '$team_reward', dlike_foundation = '$foundation_reward', dlike_airdrop = '$airdrop_reward', update_time = 'date("Y-m-d H:i:s")' WHERE username = '$dlike_username'");
-
+	$date = date("Y-m-d H:i:s");
+	$update_R = $conn->query("UPDATE dlike_daily_rewards SET today_upvotes = '$total_upvotes', dlike_staking = '$staking_reward', dlike_dao = '$dao_reward', dlike_charity = '$charity_reward', dlike_team = '$team_reward', dlike_foundation = '$foundation_reward', dlike_airdrop = '$airdrop_reward', update_time = 'date' WHERE username = '$dlike_username'");
 } else {
 $sql_data = $conn->query("INSERT INTO dlike_daily_rewards (today_upvotes, dlike_staking, dlike_dao, dlike_charity, dlike_team, dlike_foundation, dlike_airdrop, update_time) VALUES ('".$total_upvotes."', '".$staking_reward."', '".$dao_reward."', '".$charity_reward."', '".$team_reward."', '".$foundation_reward."', '".$airdrop_reward."', '".date("Y-m-d H:i:s")."')");
 }
