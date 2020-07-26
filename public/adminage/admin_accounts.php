0d5fe7d036fd8e4bbbbd3580ce1fdeb1cb757082
bestboom <certseek@gmail.com>
2020-07-26 16:12:40 +0500

    Update admin_accounts.php

--- public/adminage/admin_accounts.php
+++ public/adminage/admin_accounts.php
@@ -118,5 +118,37 @@ $sql_U = $conn->query("SELECT * FROM dlikeaccounts where admin_account=1");
     }
 
 
+
 ?>
+<div class="container">
+    <div class="user-login-signup-form-wrap" style="padding: 7rem 0rem;">
+        <div class="modal-content" style="background: #1b1e63;border-radius: 14px;">
+            <div class="modal-body">
+                <div class="share-block">
+                    <p>Add New Account</p>
+                </div>
+                <div class="user-connected-form-block" style="background: #1b1e63;">
+                    <form class="user-connected-from password-reset-form">
+                    	<input type="hidden" id="reset_email_id" value="<?php echo $email; ?>" />
+                        <div class="input-group mb-3" style="padding: 3px;">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text mb-deck" style="background: #b6c9fb;"> <span class="fa fas fa-lock"></span></div>
+                            </div>
+                            <input type="password" name="reset_pass" id="reset_pass" placeholder="New Password" class="form-control" style="padding: 8px;" />
+                        </div>
+                        <div class="input-group mb-3" style="padding: 3px;">
+                            <div class="input-group-prepend">
+                                <div class="input-group-text mb-deck" style="background: #b6c9fb;"> <span class="fa fas fa-lock"></span></div>
+                            </div>
+                            <input type="password" name="confirm_reset_pass" id="confirm_reset_pass" placeholder="Confirm Password" class="form-control" style="padding: 8px;" />
+                        </div>
+                        <center>
+                            <button type="button" class="btn btn-default" style="width: 40%;margin-top: 15px;" id="reset_pass_btn">Reset Password</button>
+                        </center>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div> 
 <?php include('../template/dlike_footer.php'); ?>
