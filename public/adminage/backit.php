e1b2700a61abc4ecc30c83fe75410f49db5d448a
bestboom <certseek@gmail.com>
2020-07-26 19:03:28 +0500

    Update backit.php

--- public/adminage/backit.php
+++ public/adminage/backit.php
@@ -1,9 +1,9 @@
 <?php
 require '../includes/config.php';
 if (isset($_POST['action']) && $_POST['action'] == 'add_account' && isset($_POST['user']) && $_POST['user'] != ''){
-	$add_user = trim($_POST['user_acc']);
-    $add_pass = trim($_POST['acc_password']);
-    $add_email = trim($_POST['acc_email']);
+	$add_user = trim($_POST['user']);
+    $add_pass = trim($_POST['pass']);
+    $add_email = trim($_POST['email']);
     if(empty($add_user)){$acc_errors = "Username Shoould not be empty";}
     if(empty($add_pass)){$acc_errors = "Password Shoould not be empty";}
     if(empty($add_email)){$acc_errors = "Email Shoould not be empty";}
