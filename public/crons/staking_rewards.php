163d6782918cccdf07546630c1672b6c6f71a78c
bestboom <certseek@gmail.com>
2020-07-29 21:59:59 +0500

    Update staking_rewards.php

--- public/crons/staking_rewards.php
+++ public/crons/staking_rewards.php
@@ -6,7 +6,7 @@ if ($sql_u->num_rows > 0) {$count = mysqli_num_rows($sql_u);
     while($row_u = $sql_u->fetch_assoc()) {$users = $row_u["username"];
     	$sql_R=$conn->query("SELECT dlike_staking FROM dlike_rewards_history where DATE(update_time) = CURDATE()");
     	if ($sql_R->num_rows > 0) {$row_R = $sql_R->fetch_assoc();$staking_reward_amt = $row_R["dlike_staking"];$staking_paid = $row_R["staking_paid"];
-            if($staking_paid !=1){
+            if($staking_paid !== 1){
         		$reward = $staking_reward_amt / $receivers;
         		$sql_st = $conn->query("SELECT reward FROM dlike_staking_rewards WHERE username = '$users'");
         		if ($sql_st->num_rows > 0) {$row_st = $sql_st->fetch_assoc(); $user_bal = $row_st["reward"];
