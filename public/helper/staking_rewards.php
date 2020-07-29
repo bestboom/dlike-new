17cd2a568ab14e6754079d553e6ceb52a9558d26
bestboom <certseek@gmail.com>
2020-07-29 18:17:12 +0500

    Update staking_rewards.php

--- public/helper/staking_rewards.php
+++ public/helper/staking_rewards.php
@@ -9,8 +9,9 @@ if ($sql_u->num_rows > 0) {
     while($row_u = $sql_u->fetch_assoc()) {
     	echo $users = $row_u["username"];
     	$amount="300";
-
-    	$conn->query("UPDATE dlike_staking_rewards SET amount = '$amount' WHERE username = '$users'");
+    	$amount="10";
+    	$sqlm = $conn->query("INSERT INTO dlike_staking_rewards (username, reward) VALUES ('".$users."', '".$amounts."')");
+    	$sqlt = $conn->query("UPDATE dlike_staking_rewards SET reward = '$amount' WHERE username = '$users'");
 	} 
 }
 
