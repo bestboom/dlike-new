61417a103bc8dcf62e0e4ca34ef9de9c18064b52
bestboom <certseek@gmail.com>
2020-08-02 22:23:23 +0500

    Update dlike_wallet.php

--- public/dlike_wallet.php
+++ public/dlike_wallet.php
@@ -199,10 +199,11 @@ $offchain_address = $row_J["offchain_address"];
                     $.ajax({type: "POST", url: 'helper/tokenWithdraw.php',data:{ userwallet: tron_address, amount: trx_amt },
                         success: function(data) {
                             try {var response = JSON.parse(data)
+                                console.log(response)
                                 if (response.error == true) {toastr['error'](response.message);return false;
                                 } else {toastr['success'](response.message);setTimeout(function(){window.location.reload();}, 300);
                                 }
-                            } catch (err) {toastr.error('Sorry. it looks Server response is malformed');}
+                            } catch (err) {console.log(err); toastr.error('Sorry. it looks Server response is malformed');}
                         }
                     });
                                 //$("#dlike_tok_with").modal("hide");
