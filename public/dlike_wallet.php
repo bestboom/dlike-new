53cece11043f5e2aafaa102297fc203e2931d4dd
bestboom <certseek@gmail.com>
2020-08-07 12:27:58 +0500

    Update dlike_wallet.php

--- public/dlike_wallet.php
+++ public/dlike_wallet.php
@@ -179,6 +179,46 @@ $offchain_address = $row_J["offchain_address"];
         
         doAjax().then(function(data) { var response = JSON.parse(data);
             if (response.error == true) {$(".tok_out_btn").attr("disabled", false);toastr['error'](response.message);return false;}
+            else{
+                let user_address =false;
+                    if (window.tronWeb!=undefined) {user_address= await window.tronWeb.defaultAddress.base58;
+                    }else{toastr.error('Non-Tronlink browser detected. You should consider trying Tronlink Wallet!');return false;}
+                    if(user_address==false){toastr.error('Please Login to Tronlink Wallet.');return false;
+                    } else {
+                        if(tron_addresses.includes(user_address)) {toastr.error('This Tron address is being used by other user to stake!');enable_stake();return false;}
+                        var myContractInfo = await tronWeb.trx.getContract(mainContractAddress);
+                        var myContract = await tronWeb.contract(myContractInfo.abi.entrys, mainContractAddress);
+                        var balanceof = await myContract.balanceOf(user_address).call();
+                        balanceof = window.tronWeb.toDecimal(balanceof);stk_amt = stk_amt * 1e6;
+
+                        if(parseFloat(stk_amt) <=balanceof){
+                            await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+                            let result = await myContract.stake(stk_amt).send({ shouldPollResponse: false, feeLimit: 15000000, callValue: 0, from: user_address });
+                            console.log(result);
+                            if(result){
+                                $('#stakingStatus').modal('show');
+                                $(".st_trx_link").html('<a href="https://shasta.tronscan.org/#/transaction/'+result+'" target="_blank">Check Transaction Here</a>');
+                                var x = setInterval(function() {
+                                    $.get("https://api.shasta.trongrid.io/v1/transactions/"+result, function(data, status){
+                                        if(status=='success'){
+                                            var tx_result = data.data[0].ret[0].contractRet;  
+                                            if(tx_result=='SUCCESS'){
+                                                $.ajax({ type: "POST",url: "/helper/staking.php", data: {action : 'staking',amount: stk_amt,wallet: user_address,trx_id: result},});
+                                                $(".st_status_message").html('Tokens Staked Successfully!');
+                                                $(".iconTitle").find($('.fa')).removeClass('fa-spinner fa-pulse').addClass('fa-check-circle');setTimeout(function(){window.location.reload();},1000);
+                                            }else{
+                                                $(".st_status_message").html('Something Wrong ! Try Again.');
+                                                $(".iconTitle").find($(".fa")).removeClass('fa-spinner fa-pulse').addClass('fa-times-circle');setTimeout(function(){window.location.reload();},1000);
+                                            }
+                                        } 
+                                    }); 
+                                }, 15000);
+                            } else {toastr.error('some issue in staking.');enable_stake();return false;}
+                        }else{toastr.error('phew... Not enough amount you want to stake');enable_stake();return false;}
+                    }
+
+
+            }
             // do success stuff
         }).fail(function() {
             // do fail stuff
