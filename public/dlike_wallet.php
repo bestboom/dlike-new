900781ce1d67dac0fa61a43314401fe14eae7f3c
bestboom <certseek@gmail.com>
2020-08-07 12:33:51 +0500

    Update dlike_wallet.php

--- public/dlike_wallet.php
+++ public/dlike_wallet.php
@@ -174,7 +174,7 @@ $offchain_address = $row_J["offchain_address"];
         }
         if ((parseFloat(dlk_amount) <= 0) ||  (parseFloat(out_amount) <= 0)){$(".tok_out_btn").attr("disabled", false);toastr.error('phew... Not a valid withdraw amount!');return false;
         }
-        function doAjax() {return $.ajax({type: 'post',url: 'helper/dlk_withdraw.php',data: { action : 'withdraw',dlk_out_amount: out_amount },datatype: 'json',});
+        async function doAjax() {return $.ajax({type: 'post',url: 'helper/dlk_withdraw.php',data: { action : 'withdraw',dlk_out_amount: out_amount },datatype: 'json',});
         }
         
         doAjax().then(function(data) { var response = JSON.parse(data);
