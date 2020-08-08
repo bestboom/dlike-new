ec8b229a2b9d36607fdf4874264faca33c12c92d
bestboom <certseek@gmail.com>
2020-08-08 16:37:16 +0500

    Update welcome2.php

--- public/welcome2.php
+++ public/welcome2.php
@@ -2,7 +2,7 @@
 if (isset($_COOKIE['username']) || $_COOKIE['username'] || isset($_COOKIE['dlike_username']) || $_COOKIE['dlike_username']) {
     die('<script>window.location.replace("https://dlike.io","_self")</script>');
 } else {
-include('template/header5.php');
+include('template/header7.php');
 if (isset($_GET["ref"])){ $referrer = $_GET['ref'];} else { $referrer = 'dlike';}
 ?>
 </div>
@@ -294,11 +294,9 @@ if (isset($_GET["ref"])){ $referrer = $_GET['ref'];} else { $referrer = 'dlike';
 </div> 
 
 <div class="modal fade" id="copy_pass" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content modal-custom">
+    <div class="modal-dialog" role="document"><div class="modal-content modal-custom">
             <?php include('template/modals/password_modal.php'); ?>
-        </div>
-    </div>
+    </div></div>
 </div>
 <?php include('template/footer.php'); } ?>
 <script>
@@ -408,7 +406,6 @@ function signupemailverify() {
 }
 
 function resetemailpass() {
-
     var Signin_main_section  = document.querySelector('.signin_main_block');
     var signin_email_section = Signin_main_section.querySelector('.signin_email_block');
     var signin_forgot_section   = Signin_main_section.querySelector('.signin_forgot_block');
@@ -418,7 +415,6 @@ function resetemailpass() {
         top    : -20
     }, 300, function () {
         signin_email_section.style.display = 'none';
-
         signin_forgot_section.style.opacity = 0;
         signin_forgot_section.style.top     = '50px';
         signin_forgot_section.style.display = '';
@@ -429,20 +425,13 @@ function resetemailpass() {
         }, 300);
     });
 }
-$('.signin_email_btn').click(function() {
-    emailLogin();
-});
-$('.signup_steem_btn').click(function() {
-    signupwithsteem();
-});
-$('.signup_email_btn').click(function() {
-    signupwithemail();
-});
-$('.forgot_pass').click(function() {
-    resetemailpass();
-});
-$('.email_signup_btn').click(function() {
-    $('.signup_loader').show();
+
+$('.signin_email_btn').click(function() {emailLogin();});
+$('.signup_steem_btn').click(function() {signupwithsteem();});
+$('.signup_email_btn').click(function() {signupwithemail();});
+$('.forgot_pass').click(function() {resetemailpass();});
+
+$('.email_signup_btn').click(function() {$('.signup_loader').show();
     let signup_username = $('#username_signup_id').val();
     //console.log(signup_username);
     let signup_email = $('#signup_email').val();
@@ -451,109 +440,54 @@ $('.email_signup_btn').click(function() {
     let signup_loct_ip = $('#user_loc_email').val();
     let emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/;
 
-    if (signup_username == "") {
-        toastr.error('phew... username should not be empty');
-        return false;
-    }
-    if (signup_email == "") {
-        toastr.error('phew... Email should not be empty');
-        $('.signup_loader').hide();
-        return false;
+    if (signup_username == "") {toastr.error('phew... username should not be empty');return false;}
+    if(signup_email==""){toastr.error('phew...Email should not be empty');$('.signup_loader').hide();return false;
     } else {
-        if (!emailRegex.test(signup_email)) {
-            toastr.error('phew... email address is not valid');
-            $('.signup_loader').hide();
-            return false;
-        }
+        if (!emailRegex.test(signup_email)) {toastr.error('phew... email address is not valid'); $('.signup_loader').hide();return false;}
     }
-    if (signup_pass == "") {
-        toastr.error('phew... Password should not be empty');
-        $('.signup_loader').hide();
-        return false;
-    }
+    if (signup_pass=="") {toastr.error('phew... Password should not be empty');$('.signup_loader').hide();return false; }
 
     $.ajax({type: "POST",url: "/helper/email_signup.php",data: {action :'signup',signup_username: signup_username,signup_email: signup_email,signup_pass: signup_pass,signup_refer_by: signup_refer_by,signup_loct_ip:signup_loct_ip},
         success: function(data) {
-            try {
-                var response = JSON.parse(data)
-                if (response.error == true) {
-                    $('.signup_loader').hide();
-                    toastr['error'](response.message);
-                    return false;
-                } else {
-                    toastr['success'](response.message);
-                    $('#my_signup_email').html(signup_email);
-                    signupemailverify();
-                }
-            } catch (err) {
-                toastr.error('Sorry. Server response is malformed');
-            }
-        }
-    });
-});
-
-$('.email_login_btn').click(function() {
-    $('.login_loader').show();
-    $('#email_login_txt').hide();
-    $(".email_login_btn").attr("disabled", true);
-    let login_user_id = $('#login_user_id').val();
-    let login_pass = $('#email_pass').val();
-    let login_url = 'helper/email_login.php';
-    if (login_user_id == "") {
-        toastr.error('phew... Username should not be empty');
-        $('.login_loader').hide();
-        $('#email_login_txt').show();
-        $(".email_login_btn").attr("disabled", false);
-        return false;
-    }
-    if (login_pass == "") {toastr.error('phew... Password should not be empty');
-        $('.login_loader').hide();$('#email_login_txt').show();$(".email_login_btn").attr("disabled", false);
-        return false;
-    }
-    $.ajax({
-        type: "POST",
-        url: login_url,
-        data: { login_username: login_user_id,login_pass: login_pass },
-        success: function(data) {
-            try {
-                var response = JSON.parse(data)
-                if (response.error == true) {
-                    toastr['error'](response.message);
-                    $('.login_loader').hide();
-                    $('#email_login_txt').show();
-                    $(".email_login_btn").attr("disabled", false);
-                    return false;
-                } else {
-                    toastr['success'](response.message);
-                    $.cookie("dlike_username", response.dlikeuser, { expires: 7, path: '/' });
-                    setTimeout(function(){window.location.href = response.redirect;}, 700);
+            try {var response = JSON.parse(data)
+                if (response.error == true) {$('.signup_loader').hide();toastr['error'](response.message);return false;
+                } else {toastr['success'](response.message);$('#my_signup_email').html(signup_email);signupemailverify();
                 }
             } catch (err) {toastr.error('Sorry. Server response is malformed');}
         }
     });
 });
 
-$('.email_reset_pass_btn').click(function() {
-    let reset_email_id = $('#email_reset_pass').val();
-    let reset_url = 'helper/email_reset_pass.php';
-    if (reset_email_id == "") {
-        toastr.error('phew... Email should not be empty');
-        return false;
+$('.email_login_btn').click(function() {
+    $('.login_loader').show();$('#email_login_txt').hide();$(".email_login_btn").attr("disabled", true);
+    let login_user_id = $('#login_user_id').val();let login_pass = $('#email_pass').val();
+    if (login_user_id == "") {toastr.error('phew... Username should not be empty');$('.login_loader').hide();$('#email_login_txt').show();$(".email_login_btn").attr("disabled", false);return false;
     }
-    $.ajax({
-        type: "POST",
-        url: reset_url,
-        data: { reset_email: reset_email_id },
+    if (login_pass == "") {toastr.error('phew... Password should not be empty');
+        $('.login_loader').hide();$('#email_login_txt').show();$(".email_login_btn").attr("disabled", false);
+        return false;}
+    $.ajax({type: "POST",url: 'helper/email_login.php',data: { login_username: login_user_id,login_pass: login_pass },
         success: function(data) {
-            try {
-                var response = JSON.parse(data)
-                if (response.error == true) {
-                    toastr['error'](response.message);
-                    return false;
-                } else {
-                    toastr['success'](response.message);
-                    $(".email_reset_pass_btn").html('Email Sent');
-                    $(".email_reset_pass_btn").prop('disabled',true);
+            try {var response = JSON.parse(data)
+                if (response.error == true) {toastr['error'](response.message);
+                    $('.login_loader').hide();$('#email_login_txt').show();$(".email_login_btn").attr("disabled", false);return false;
+                } else {toastr['success'](response.message);
+                    $.cookie("dlike_username", response.dlikeuser, { expires: 7, path: '/' });
+                    setTimeout(function(){window.location.href = response.redirect;}, 500);
+                }
+            } catch (err) {toastr.error('Sorry. Server response is malformed');}
+        }
+    });
+});
+
+$('.email_reset_pass_btn').click(function() {let reset_email_id = $('#email_reset_pass').val();
+    if (reset_email_id == "") {toastr.error('phew... Email should not be empty');return false;}
+    $.ajax({
+        type: "POST",url: 'helper/email_reset_pass.php',data: { reset_email: reset_email_id },
+        success: function(data) {
+            try {var response = JSON.parse(data)
+                if (response.error == true) {toastr['error'](response.message);return false;
+                } else {toastr['success'](response.message);$(".email_reset_pass_btn").html('Email Sent');$(".email_reset_pass_btn").prop('disabled',true);
                 }
             } catch (err) {toastr.error('Sorry. Server response is malformed');}
         }
@@ -567,36 +501,19 @@ inputemailpin.addEventListener('keyup', function(){
     if(inputemailpin.value.length < 6 || inputemailpin.value.length > 6) {$(".signup-signup-email-verify .email_verify_pin_btn").prop('disabled',true);}
 });
 
-$('.email_verify_pin_btn').click(function() {
-    $('.verify_pin_loader').show();
-    $('.verify_email_txt').hide();
-    $(".email_verify_pin_btn").attr("disabled", true);
-    let login_user_id = $('#login_user_id').val();
-    let email_pin_code = $('#email_pin_code').val();
+$('.email_verify_pin_btn').click(function() {$(".email_verify_pin_btn").attr("disabled", true);
+    $('.verify_pin_loader').show();$('.verify_email_txt').hide();
+    let login_user_id = $('#login_user_id').val();let email_pin_code = $('#email_pin_code').val();
     let user_email = $('#my_signup_email').html();
-    let email_verify_url = 'helper/email_verify.php';
-    if (email_pin_code == "") {
-        toastr.error('phew... PIN value should not be empty');
-        $('.verify_pin_loader').hide();
-        $('.verify_email_txt').show();
-        $(".email_verify_pin_btn").attr("disabled", false);
-        return false;
-    }
+    if (email_pin_code == "") {toastr.error('phew... PIN value should not be empty');$('.verify_pin_loader').hide();$('.verify_email_txt').show();$(".email_verify_pin_btn").attr("disabled", false);return false;}
     var data_verify = { email_pin_code: email_pin_code, user_email: user_email};
-    $.ajax({
-        type: "POST",
-        url: email_verify_url,
+    $.ajax({type: "POST",url: 'helper/email_verify.php',
         data: data_verify,
         success: function(data) {
             try {var response = JSON.parse(data)
-                if (response.error == true) {
-                    toastr['error'](response.message);
-                    $('.verify_pin_loader').hide();
-                    $('.verify_email_txt').show();
-                    $(".email_verify_pin_btn").attr("disabled", false);
+                if (response.error == true) {toastr['error'](response.message);$('.verify_pin_loader').hide();$('.verify_email_txt').show();$(".email_verify_pin_btn").attr("disabled", false);
                     return false;
-                } else {toastr['success'](response.message);
-                    setTimeout(function(){window.location.href = response.redirect;}, 700);
+                } else {toastr['success'](response.message);setTimeout(function(){window.location.href = response.redirect;}, 700);
                 }
             } catch (err) {toastr.error('Sorry. Server response is malformed');}
         }
