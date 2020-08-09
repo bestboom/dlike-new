3496e6e6aab4dd7603b06a8a960d34a12f17b412
bestboom <certseek@gmail.com>
2020-08-09 20:05:19 +0500

    up

--- public/index2.php
+++ public/index2.php
@@ -104,7 +104,6 @@ $(document).ready(function () {
         }
         if (getLeftPosi_2()<0) {$('.scroller-left-2').show().css('display', 'flex');
         }else {$('.item').animate({left:"-="+getLeftPosi_2()+"px"},'slow');
-        //$('.scroller-left').hide();
         }
     }
     reAdjust_2();
@@ -119,34 +118,3 @@ $(document).ready(function () {
     });
 });
  </script>
-<script type="text/javascript">
-$(document).ready(function(){
-        windowOnScroll();
-});
-function windowOnScroll() {
-    $(window).on("scroll", function(e){
-        if ($(window).scrollTop() == $(document).height() - $(window).height()){
-            if($(".post-item").length < $("#total_count").val()) {
-                var lastId = $(".post-item:last").attr("id");
-                //getMoreData(lastId);
-            }
-        }
-    });
-}
-
-function getMoreData(lastId) {
-    $(window).off("scroll");
-    $.ajax({
-        url: '/helper/loadPosts.php?lastId=' + lastId,
-        type: "get",
-        beforeSend: function ()
-        {$('.ajax-loader').show();},
-        success: function (data) {
-            setTimeout(function() {$('.ajax-loader').hide();
-            $("#post-list").append(data);
-            windowOnScroll();
-            }, 1000);
-        }
-   });
-}
-</script>
