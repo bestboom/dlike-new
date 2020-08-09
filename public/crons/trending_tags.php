738c9e8c8527550bc21665055f9b5883fc317d05
bestboom <certseek@gmail.com>
2020-08-09 11:34:39 +0500

    Update trending_tags.php

--- public/crons/trending_tags.php
+++ public/crons/trending_tags.php
@@ -5,7 +5,7 @@ $sql_T = $conn->query("DELETE FROM dlike_trending_tags");
 
 $sql_C = $conn->query("SELECT count(*) as count, tag from dlike_tags group by tag Having count(*) >1 order by count(*) DESC LIMIT 12");
 if ($sql_C->num_rows > 0)
-{	while ($row = $sql_C->fetch_assoc())
+{	while ($row_C = $sql_C->fetch_assoc())
     {
         $tag_name = $row_C["tag"]; $tag_count = $row_C["count"]; 
         $sql_data=$conn->query("INSERT INTO dlike_trending_tags (tag, count) VALUES ('".$tag_name."', '".$tag_count."')");
