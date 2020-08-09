bb93d1806fac54a4120f30d0e506f9167b1564c5
bestboom <certseek@gmail.com>
2020-08-09 23:04:37 +0500

    up

--- public/helper/solve.php
+++ public/helper/solve.php
@@ -27,7 +27,7 @@ if (isset($_POST["ath"]) && isset($_POST["plink"]))
         $check_bot_likes = $conn->query("SELECT * FROM dlike_upvotes where ip_addr = '$thisip' and  curation_time > now() - INTERVAL 24 HOUR");
         if ($check_bot_likes->num_rows >= 10){die(json_encode(['error' => true, 'message' => 'Phew ...You can not do more likes!']));}
 
-        $check_time_likes = $conn->query("SELECT like_time FROM mylikes where username = '$userval' and TIMESTAMPDIFF(SECOND, like_time, $1) <= 10");
+        $check_time_likes = $conn->query("SELECT like_time FROM mylikes where username = '$userval' and TIMESTAMPDIFF(SECOND, like_time, now()) <= 10");
         if ($check_time_likes->num_rows > 0){die(json_encode(['error' => true, 'message' => 'Are you a Robot? Be patient while recomending posts to community with your Likes!']));}
 
         else {
