0844a7a8de9c21621cbf057e193bceb1e6c623ad
bestboom <certseek@gmail.com>
2020-08-13 10:39:11 +0500

    up

---
+++ public/steemposts.php
@@ -0,0 +1,28 @@
+<?php include('template/header7.php');?>
+</div>
+<div class="latest-post-section" style="min-height:80vh;padding: 70px 0px 60px 0px;">
+    <div class="container">
+        <div class="row" id="content">
+        </div>
+    </div>
+    <div class="container" style="padding-top:0px;">
+        <p>Dlike is a social sharing dapp (decentralized application) built on steem blockchain where you can share any news, story or tips which you consider worth sharing with community and if community likes your sharing then you get rewarded in the form of upvotes.</p>
+        <p>Dlike has 2 types of upvotes namely STEEM and DLIKER. Steem is the native token for steem blockchain while DLIKER is the native token for Dlike platform which is built on steem-engine exchange and is independently trade-able. </p>
+        <p>On dlike, we have different categories in which you can share according to your liking. Mainly these categories are health, business, food, cryptocurrency, sports, news, technology and general. You can share these news, stories and tips from any popular website or even from your personal blog but being an informative source, we do not allow personal promotion like selfies and other such content. So always share content that is information oriented and is useful as a source of information for all community members across the globe.</p>
+        <p>As compared to conventional social sharing sites like Reddit and Pinterest where you give your time for sharing and doesn’t get any part in earnings, here on dlike, you are rewarded for your time and efforts in the form of crypto earnings (steem and dliker) upvotes. Another advantage is of being censorship free platform as on dlike your content is saved on blockchain (steem) which is not editable or even remove able so there is no penalty in the form of censorship on dlike platform.</p>
+        <p>Dlike is building its own economy where any steem users can delegate his extra steem power to dlike which we use to upvote posts on dlike and the beneficiary reward out of this voting goes to delegators which was earlier in the form of steem and now in the form of DLIKER token buy back on steem engine exchange. So we are trying to build a system where very participant gets benefited as per his share in the platform and a solid economic base is being built in co-operation with community members.</p>
+        <p>We are also offering daily rewards for all dlike users in the form DLIKE tokens. These rewards are generated on the basis of user activities across the platform. These activities account the performance of user posts. Some of the basic metrics of this reward system includes the views and likes on the posts. In addition to this, users can invite new users which generate daily points for them from each of the post shared by their affiliates. In the long run, this reward system will be the main source of earning DLIKE tokens on dlike as it will be the major utility on dlike platform.</p>
+        <p>If you are looking for any help regarding dlike platform, sharing on dlike or anything related to dlike tokens, then always feel free to reach us on dlike discord channel.</p>
+    </div>
+</div>
+<?php include('template/modals/modal.php'); ?>    
+<?php include('template/dlike_footer.php'); ?>
+<script type="text/javascript">
+    $( document ).ready(function() {    
+        $('#loadings').delay(6000).fadeOut('slow');
+
+        //$(window).on('load',function(){
+        //    $('#dlikem_maket').modal('show');
+        //});
+    });
+</script>
