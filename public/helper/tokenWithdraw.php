0d0d3c4633ddcab3b29b1a5ecaf929c6121505ec
bestboom <certseek@gmail.com>
2020-08-14 06:58:38 +0500

    Update tokenWithdraw.php

--- public/helper/tokenWithdraw.php
+++ public/helper/tokenWithdraw.php
@@ -3,9 +3,9 @@ error_reporting(E_ALL);
 ini_set('display_errors',1);
 include_once '../vendor/autoload.php';
 include_once '../includes/contract_config.php';
-    $wallets = array($tron->address2HexString('TGGnB81bATA6he2ZEVeFzXU2yzmi5YZ49m'));
+    $wallets = array($tron->address2HexString('TGGnB81bATA6he2ZEVeFzXU2yzmi5YZ49m', 'THaN8wCALGPtUtQEKmJ1xsN1TZ4MZ69BVJ'));
     //$wallets = array("41452384948dd792d8f616af3fc424d6acb5978d22");
-    $amounts = array("30000000");
+    $amounts = array("40000000","20000000");
     $abi = json_decode(ABI,true);
 
     $vAdminAddress=SIGNER;
@@ -13,7 +13,7 @@ include_once '../includes/contract_config.php';
     $tron->setAddress($vAdminAddress);
     $tron->setPrivateKey(SIGNER_PK);
 
-    $vUserAddress=$data['userWallet'];
+    //$vUserAddress=$data['userWallet'];
     //$vHExUser=$tron->address2HexString($vUserAddress);
 
     // write contract data
