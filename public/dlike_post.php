363dc6df319c92794e3e307a2efd34bcea2fb023
bestboom <certseek@gmail.com>
2020-08-15 21:26:26 +0500

    up

--- public/dlike_post.php
+++ public/dlike_post.php
@@ -109,30 +109,3 @@ if ($sql_T && $sql_T->num_rows > 0)
 </div></div>
 <div class="modal fade" id="upvotefail" tabindex="-1" role="dialog" aria-hidden="true"><div class="modal-dialog modal-dialog-custom modalStatus" role="document"><div class="modal-content modal-custom"><?php include('template/modals/upvotefail.php'); ?></div></div></div>
 <?php include ('template/dlike_footer.php'); ?>
-<script type="text/javascript">
-    $('.hov_vote').click(function() {
-        if (dlike_username != null) {
-            var mypermlink = $(this).attr("data-permlink");
-            var authorname = $(this).attr("data-author");
-            $(this).addClass('fas fa-spinner fa-spin like_loader');
-            var update = '1';
-            $.ajax({ type: "POST",url: "/helper/solve.php", data: {ath: authorname, plink: mypermlink},
-                success: function(data) {
-                    try { var response = JSON.parse(data)
-                        if (response.done == true) {$('.hov_vote').removeClass('fas fa-spinner fa-spin like_loader');$('#upvotefail').modal('show');return false;
-                        } else if (response.error == true) {$('.hov_vote').removeClass('fas fa-spinner fa-spin like_loader');toastr.error(response.message);return false;
-                        } else {$('.hov_vote').removeClass('fas fa-spinner fa-spin like_loader');
-                            toastr.success(response.message);
-                            var getpostlikes = $(".post_likes" + mypermlink + authorname).html();
-                            var post_income = response.post_income;
-                            var newlikes = parseInt(getpostlikes) + parseInt(update);
-                            var updatespostincome = newlikes * post_income;
-                            $('.post_likes' + mypermlink + authorname).html(newlikes);
-                            $('.dlike_tokens' + mypermlink + authorname).html(updatespostincome);
-                        }
-                    } catch (err) {toastr.error('Sorry. Server response is malformed.');}
-                }
-            });
-        } else {toastr.error('You must be login with DLIKE username!');return false;}
-    });
-</script>
--- public/dlike_profile.php
+++ public/dlike_profile.php
@@ -239,31 +239,6 @@ $('.prof_edit_btn').click(function() {
 });
 $('.btn_edit').click(function(e) {	e.preventDefault();$("#profile_edit").modal("show");});
 $('.btn_verify_email').click(function(e) {	e.preventDefault();$("#email_verify").modal("show");});
-$('.hov_vote').click(function() {
-    if (dlike_username != null) {
-        var mypermlink = $(this).attr("data-permlink");
-        var authorname = $(this).attr("data-author");
-        $(this).addClass('fas fa-spinner fa-spin like_loader');
-        var update = '1';
-        $.ajax({ type: "POST",url: "/helper/solve.php", data: {ath: authorname, plink: mypermlink},
-            success: function(data) {
-                try { var response = JSON.parse(data)
-                    if (response.done == true) {$('.hov_vote').removeClass('fas fa-spinner fa-spin like_loader');$('#upvotefail').modal('show');return false;
-                    } else if (response.error == true) {$('.hov_vote').removeClass('fas fa-spinner fa-spin like_loader');toastr.error(response.message);return false;
-                    } else {$('.hov_vote').removeClass('fas fa-spinner fa-spin like_loader');
-                        toastr.success(response.message);
-                        var getpostlikes = $(".post_likes" + mypermlink + authorname).html();
-                        var post_income = response.post_income;
-                        var newlikes = parseInt(getpostlikes) + parseInt(update);
-                        var updatespostincome = newlikes * post_income;
-                        $('.post_likes' + mypermlink + authorname).html(newlikes);
-                        $('.dlike_tokens' + mypermlink + authorname).html(updatespostincome);
-                    }
-                } catch (err) {toastr.error('Sorry. Server response is malformed.');}
-            }
-        });
-    } else {toastr.error('You must be login with DLIKE username!');return false;}
-});
 
 $('.email_pin_btn').click(function() {$(".email_pin_btn").attr("disabled", true);
     let pin_code = $('#email_pinit_code').val();let user_email_id = '<?php echo $email;?>';
