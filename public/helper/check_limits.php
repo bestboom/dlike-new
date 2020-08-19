9462a81bff3eb7b3870a3333167d07b395b74c12
bestboom <certseek@gmail.com>
2020-08-19 14:58:13 +0500

    Update check_limits.php

--- public/helper/check_limits.php
+++ public/helper/check_limits.php
@@ -10,13 +10,13 @@ if (isset($_POST['action']) && $_POST['action'] =='shares_limit' && isset($_POST
     if(in_array($url, $restricted_urls)){ $errors = "phew... Sharing from this url is not allowed";}
     if (empty($errors)) {
         $sql_post_limit = $conn->query("SELECT * FROM dlikeposts WHERE username = '$user_name' and created_at > now() - INTERVAL 24 HOUR");
-        if ($sql_post_limit->num_rows >= 5) {die(json_encode(['error' => true, 'message' => 'Phew ... You reached max daily share limit!']));}
+        if ($sql_post_limit->num_rows >= 5) {die(json_encode(['error' => true, 'message' => 'Phew ... You reached max daily share limit!']));} else {die(json_encode(['error' => false, 'message' => 'You can post!']));}
     } else {die(json_encode(['error' => true,'message' => $errors]));}
 }
 
 if (isset($_POST['action']) && $_POST['action']=='unique_post' && isset($_POST['newurl']) && $_POST['newurl'] !='') {
     $url = trim(mysqli_real_escape_string($conn, $_POST["newurl"]));
     $sql_unique_url = $conn->query("SELECT ext_url FROM dlikeposts WHERE ext_url = '$url' and created_at > now() - INTERVAL 300 HOUR"); 
-    if ($sql_unique_url->num_rows > 0) {die(json_encode(['error' => true, 'message' => 'URL already shared. Can not be shared again!']));} 
+    if ($sql_unique_url->num_rows > 0) {die(json_encode(['error' => true, 'message' => 'URL already shared. Can not be shared again!']));}else{die(json_encode(['error' => false, 'message' => 'Url is unique']));}
 }
 ?>
