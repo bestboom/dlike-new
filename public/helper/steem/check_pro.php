76f9b757f9554f86a3271955d0936a9dd168e2eb
bestboom <certseek@gmail.com>
2020-08-19 18:39:58 +0500

    up

--- public/helper/steem/check_pro.php
+++ public/helper/steem/check_pro.php
@@ -1,58 +1,18 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
 require '../../includes/config.php';
-function validator($data){
-    return htmlspecialchars(strip_tags(trim($data)));
-}
+function validator($data){return htmlspecialchars(strip_tags(trim($data)));}
 
 if (isset($_POST["user"])){
     $user_name = $_POST['user'];
-    //$user_name = $_GET['user'];
-
-    $sqls = "SELECT * FROM prousers WHERE username = '$user_name'"; 
-    $resultAmount = $conn->query($sqls);
-        if ($resultAmount->num_rows > 0) {
-
-            $sql1 = "SELECT * FROM steemposts WHERE username = '$user_name' and created_at > now() - INTERVAL 24 HOUR"; 
-                $result1 = $conn->query($sql1);
-
-                if ($result1->num_rows >= 3) {
-
-                    die(json_encode([
-                        'error' => true,
-                        'message' => 'Can not post',
-                        'data' => 'PRO users can share 3 posts in 24 hours!'
-                    ]));
-
-                } else {
-                    die(json_encode([
-                        'error' => false,
-                        'message' => 'Lets Post'
-                    ]));
-                }
+    $sqls = $conn->query("SELECT * FROM prousers WHERE username = '$user_name'"); 
+        if ($sqls->num_rows > 0) {
+            $sql1 = $conn->query("SELECT * FROM steemposts WHERE username = '$user_name' and created_at > now() - INTERVAL 24 HOUR");
+                if ($sql1->num_rows >= 3) {die(json_encode([ 'error' => true, 'message' => 'PRO users can share 3 posts in 24 hours!']));
+                } else {die(json_encode(['error' => false,'message' => 'Lets Post']));}
         } else  {
-
-            $sql2 = "SELECT * FROM steemposts WHERE username = '$user_name' and created_at > now() - INTERVAL 24 HOUR"; 
-                $result2 = $conn->query($sql2);
-
-                if ($result2->num_rows > 0) {
-
-                    die(json_encode([
-                        'error' => true,
-                        'message' => 'Can not post',
-                        'data' => 'Only 1 share allowed every 24 hours. To share more become PRO!'
-                    ]));
-
-                } else {
-                    die(json_encode([
-                        'error' => false,
-                        'message' => 'Lets Post'
-                    ]));
-                }
+            $sql2 = $conn->query("SELECT * FROM steemposts WHERE username = '$user_name' and created_at > now() - INTERVAL 24 HOUR");
+                if ($sql2->num_rows > 0) { die(json_encode(['error' => true,'message' => 'Only 1 share allowed every 24 hours. To share more become PRO!']));
+                } else {die(json_encode(['error' => false, 'message' => 'Lets Post']));}
 	    }   
 } else { echo 'some issue'; } 
 ?>
--- public/helper/steem/check_share.php
+++ public/helper/steem/check_share.php
@@ -1,27 +1,8 @@
-<?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-require '../../includes/config.php';
-
+<?php require '../../includes/config.php';
 if (isset($_POST["url"])){
     $url = $_POST['url'];
-
-    $sqls = "SELECT ext_url FROM steemposts WHERE ext_url = '$url' and created_at > now() - INTERVAL 100 HOUR"; 
+    $sqls = "SELECT ext_url FROM steemposts WHERE ext_url = '$url' and created_at > now() - INTERVAL 100 HOUR";
     $resultAmount = $conn->query($sqls);
-        if ($resultAmount->num_rows > 0) {
-            die(json_encode([
-                'error' => true,
-                'message' => 'Can not post',
-                'data' => 'URL already shared. Can not be shared again!'
-            ]));
-        } else {
-            die(json_encode([
-                'error' => false,
-                'message' => 'Lets Post'
-            ]));
-	    }     
-} else { echo 'some issue'; }
-?>
+        if ($resultAmount->num_rows > 0) {die(json_encode(['error' => true,'message' => 'URL already shared. Can not be shared again!']));
+        } else {die(json_encode(['error' => false,'message' => 'Lets Post']));}     
+} else { echo 'some issue'; }?>
