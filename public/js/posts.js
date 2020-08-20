420ff4d73536384315f78b9a29418efca6d00cb3
bestboom <certseek@gmail.com>
2020-08-20 11:42:04 +0500

    up

--- public/js/posts.js
+++ public/js/posts.js
@@ -46,7 +46,7 @@ $(document).ready(function(){
 				'\n' +
 				'<div class="post-author-block">\n' +
 				'\n' +
-				'<div><a href="https://steemit.com/@' + $post.author + '" target="_blank"><img src="https://steemitimages.com/u/' + $post.author + '/avatar" alt="img" style="width: 32px;"></a></div>\n' +
+				'<div><a href="https://steemit.com/@' + $post.author + '" target="_blank"><img src="https://steemitimages.com/u/' + $post.author + '/avatar" alt="img" style="width: 32px;border-style: unset !important;"></a></div>\n' +
 				'\n' +
 				'<div class="author-info">\n' +
 				'\n' +
