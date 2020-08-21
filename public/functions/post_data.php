112e6ef606c4a90d2aedbeb7c26e8dbee2b85145
bestboom <certseek@gmail.com>
2020-08-21 16:17:22 +0500

    Update post_data.php

--- public/functions/post_data.php
+++ public/functions/post_data.php
@@ -7,7 +7,7 @@
 <div class="post-thumb img-fluid"><?php echo '<a href="/post/'.$author.'/'.$permlink.'"><img src=' . $imgUrl . ' class="card-img-top" /></a>'; ?></a></div>
 <div class="post-contnet-wrap post_bottom">
 <h4 class="post-title single_title"><?php echo '<a href="/post/'.$author.'/'.$permlink.'">'.$title.'</a>'; ?></h4>
-<p class="post-entry post-tags"><?php echo $post_tags; ?></p>
+<p class="post-entry post-tags"><?php echo $tags; ?></p>
 <div class="post-comments bottom_block">
 <div><img src="/images/post/dlike-hover.png" class="hov_vote" data-permlink="<?php echo $permlink; ?>" data-author="<?php echo $author; ?>"> | <span id="post_likes" class="post_likes<?php echo $permlink; ?><?php echo $author; ?>"><?php echo $postLikes; ?></span>LIKES</div>
 <div><span class="dlike_tokens<?php echo $permlink; ?><?php echo $author; ?>"><?php echo $post_income; ?></span> <b>DLIKE</b></div>
