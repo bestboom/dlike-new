0de24fc4e8793e8572781823ae6018274681af69
bestboom <certseek@gmail.com>
2020-08-21 22:21:47 +0500

    up

--- public/helper/email_login.php
+++ public/helper/email_login.php
@@ -11,11 +11,10 @@ if (isset($_POST['login_username'])  && $_POST['login_username'] != '' && isset(
 		$hashedPW = hash('sha256', $escapedPWN);
 
 		$check_user = $conn->query("SELECT * FROM dlikeaccounts where username = '$login_username' and password = '$hashedPW'");
-		if ($check_user->num_rows > 0) {$row_C = $check_user->fetch_assoc(); $dlike_username=$row_C['username'];
+		if ($check_user->num_rows>0){$row_C = $check_user->fetch_assoc(); $dlike_username=$row_C['username'];
 			$dlike_user_login_url = 'https://dlike.io';
     		die(json_encode(['error' => false,'message' => 'Login Successful!','redirect' => $dlike_user_login_url,'dlikeuser' => $dlike_username]));
 		} else {die(json_encode(['error' => true,'message' => 'Login details does not match!'])); }
-    } else {die(json_encode(['error' => true,'message' => $errors]));
-	}
+    } else {die(json_encode(['error' => true,'message' => $errors]));}
 } else {die('Some error');}
 ?>
