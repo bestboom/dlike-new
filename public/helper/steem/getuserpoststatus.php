faf286561c9ae8183de3b772df7b725c221532ed
bestboom <certseek@gmail.com>
2020-08-22 11:54:06 +0500

    Update getuserpoststatus.php

--- public/helper/steem/getuserpoststatus.php
+++ public/helper/steem/getuserpoststatus.php
@@ -1,7 +1,18 @@
-<?php require '../../includes/config.php';
-	$sql = $conn->query("SELECT * FROM userstatus where username = '".$_POST['author']."'");
-	if ($sql->num_rows > 0) {
-	while($row=$result->fetch_assoc()){$strReturn['setstatus']=$row['status'];$strReturn['status']='OK';}
-	} else {$strReturn['status'] = 'error';}
+<?php
+	ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(0);
+    	require '../includes/config.php';
+	  
+    	$sql = "SELECT * FROM userstatus where username = '".$_POST['author']."'";
+	$result = $conn->query($sql);
+	if ($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+			$strReturn['setstatus'] = $row['status'];
+			$strReturn['status'] = 'OK';
+		}
+	} else {
+		$strReturn['status'] = 'error';
+	}
   	echo json_encode($strReturn);die;
 ?>
