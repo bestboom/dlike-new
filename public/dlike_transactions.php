9db75e5b7e8f02b3bf58d7c6b17be75a3965c068
bestboom <certseek@gmail.com>
2020-08-30 22:40:57 +0500

    Update dlike_transactions.php

--- public/dlike_transactions.php
+++ public/dlike_transactions.php
@@ -52,64 +52,40 @@
             </div>
         </div>
     </div></div></div>
-    <div class="activity-section">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card h-100">
-                        <div class="card-header d-flex justify-content-between align-items-center">
-                            <div class="card-header-title"><h4>Latest Transactions</h4></div>
-                        </div>
-                        <table class="table coin-list table-hover" style="border: 1px solid #eee;">
-                        <tbody>
-                            <?php 
-                            $sql_T = $conn->query("SELECT * FROM dlike_transactions ORDER BY trx_time DESC LIMIT 30");
-                            if ($sql_T && $sql_T->num_rows > 0) {
-                                while ($row_T = $sql_T->fetch_assoc()) {
-                                    $start_time = strtotime($row_T["trx_time"]); 
-                                    $dlike_user = $row_T["username"];
-                                    $tx_type = $row_T["type"];
-                                    if($tx_type == 'a'){$trx_type = '<i class="fas fa-pencil-alt"></i>&nbsp;&nbsp;author';}elseif($tx_type == 'b'){$trx_type = '<i class="fas fa-coffee"></i>&nbsp;&nbsp;curation';}elseif($tx_type == 'c'){$trx_type = '<i class="fas fa-user"></i>&nbsp;&nbsp;affiliate';}
-
-
-                                    $sql_W = "SELECT * FROM dlikeaccounts where username = '$dlike_user'";
-                                    $result_W = $conn->query($sql_W);
-                                    if ($result_W && $result_W->num_rows > 0)
-                                    {
-                                        $profile_pic = $row_W["profile_pic"];
-                                        if (!empty($profile_pic))
-                                        { $user_profile_pic = $profile_pic; } else { $user_profile_pic = 'https://i.postimg.cc/rwbTkssy/dlike-user-profile.png';}
-                                    }
-                                    ?>
-                                    <tr>
-                                        <td class="exp-user wid_2" style="padding: 12px 22px;">
-                                            <span style="justify-content: left;"><?php echo '<img src="'.$user_profile_pic.'" style="padding-right:10px;width: 32px;"><a href="/profile/'. $dlike_user.'">'. $dlike_user.'</a>'; ?></span>
-                                        </td>
-                                        <td class="exp-amt wid_2" style="padding: 12px 22px;">
-                                            <span><?php echo $trx_type; ?></span>
-                                        </td>
-                                        <td class="exp-amt wid_2" style="padding: 12px 22px;">
-                                            <span><?php echo $row_T["amount"]; ?></span>
-                                        </td>
-                                        <td class="exp-amt wid_2" style="padding: 12px 22px;">
-                                            <span><?php echo '<a href="https://dlike.io/post/'.$row_T["reason"].'"><i class="fas fa-globe"></i></a>'; ?></span>
-                                        </td>
-                                        <td class="exp-amt wid_2" style="padding: 12px 22px;">
-                                            <?php echo time_ago($start_time); ?>
-                                        </td>
-                                    </tr>
-                                    <?php
-                                }
-                            }
-                            ?>
-                        </tbody>
-                    </table>
-                    </div>
+    <div class="activity-section"><div class="container"><div class="row">
+        <div class="col-md-12">
+            <div class="card h-100">
+                <div class="card-header d-flex justify-content-between align-items-center">
+                    <div class="card-header-title"><h4>Latest Transactions</h4></div>
                 </div>
+                <table class="table coin-list table-hover" style="border: 1px solid #eee;">
+                <tbody>
+                    <?php 
+                    $sql_T = $conn->query("SELECT * FROM dlike_transactions ORDER BY trx_time DESC LIMIT 30");
+                    if ($sql_T && $sql_T->num_rows > 0) {
+                        while ($row_T = $sql_T->fetch_assoc()) {
+                            $start_time = strtotime($row_T["trx_time"]); 
+                            $dlike_user = $row_T["username"];
+                            $tx_type = $row_T["type"];
+                            if($tx_type == 'a'){$trx_type = '<i class="fas fa-pencil-alt"></i>&nbsp;&nbsp;author';}elseif($tx_type == 'b'){$trx_type = '<i class="fas fa-coffee"></i>&nbsp;&nbsp;curation';}elseif($tx_type == 'c'){$trx_type = '<i class="fas fa-user"></i>&nbsp;&nbsp;affiliate';}
 
+                            $sql_W = $conn->query("SELECT * FROM dlikeaccounts where username = '$dlike_user'");
+                            if ($sql_W && $sql_W->num_rows > 0) { $row_W=$sql_W->fetch_assoc();
+                                $profile_pic = $row_W["profile_pic"];} ?>
+                            <tr>
+                                <td class="exp-user wid_2 ex_pad"><span style="justify-content: left;"><?php echo '<img src="'.$profile_pic.'" class="img-fluid ex_img"><a href="/profile/'. $dlike_user.'">'. $dlike_user.'</a>'; ?></span></td>
+                                <td class="exp-amt wid_2 ex_pad"><span><?php echo $trx_type; ?></span></td>
+                                <td class="exp-amt wid_2 ex_pad"><span><?php echo $row_T["amount"]; ?></span></td>
+                                <td class="exp-amt wid_2 ex_pad"><span><?php echo '<a href="https://dlike.io/post/'.$row_T["reason"].'"><i class="fas fa-globe"></i></a>'; ?></span></td>
+                                <td class="exp-amt wid_2 ex_pad"><?php echo time_ago($start_time); ?></td>
+                            </tr>
+                        <?php }
+                    } ?>
+                </tbody>
+            </table>
             </div>
         </div>
-    </div>
+    </div></div></div>
     <div class="activity-section"><div class="container"><div class="row"><div class="col-md-12">
         <div class="card h-100">
             <div class="card-header d-flex justify-content-between align-items-center">
@@ -126,12 +102,11 @@
                         $sql_F=$conn->query("SELECT * FROM dlikeaccounts where username='$dlike_user'");
                         if ($sql_F && $sql_F->num_rows > 0){$row_F=$sql_F->fetch_assoc();$profile_pic=$row_F["profile_pic"];} ?>
                         <tr>
-                            <td class="exp-user wid_2" style="padding: 12px 22px;"><span style="justify-content: left;"><?php echo '<img src="'.$profile_pic.'" style="padding-right:10px;width: 32px;"><a href="/profile/'. $dlike_user.'">'. $dlike_user.'</a>'; ?></span></td>
-                            <td class="exp-amt wid_2" style="padding: 12px 22px;"><span><?php echo $tx_type; ?></span></td>
-                            <td class="exp-amt wid_2" style="padding: 12px 22px;"><span><?php echo $row_T["amount"]; ?></span></td>
-                            <td class="exp-amt wid_2" style="padding: 12px 22px;"><span><?php echo '<a href="https://tronscan.org/#/transaction/'.$row_T["tron_trx"].'" target="_blank"><i class="fas fa-exchange-alt"></i></a>'; ?></span></td>
-                            <td class="exp-amt wid_2" style="padding: 12px 22px;">
-                                <?php echo time_ago($start_time); ?></td>
+                            <td class="exp-user wid_2 ex_pad"><span style="justify-content: left;"><?php echo '<img src="'.$profile_pic.'" class="img-fluid ex_img"><a href="/profile/'. $dlike_user.'">'. $dlike_user.'</a>'; ?></span></td>
+                            <td class="exp-amt wid_2 ex_pad"><span><?php echo $tx_type; ?></span></td>
+                            <td class="exp-amt wid_2 ex_pad"><span><?php echo $row_T["amount"]; ?></span></td>
+                            <td class="exp-amt wid_2 ex_pad"><span><?php echo '<a href="https://tronscan.org/#/transaction/'.$row_T["tron_trx"].'" target="_blank"><i class="fas fa-exchange-alt"></i></a>'; ?></span></td>
+                            <td class="exp-amt wid_2 ex_pad"><?php echo time_ago($start_time); ?></td>
                         </tr>
                     <?php } 
                 } ?>
@@ -154,7 +129,7 @@
                         $sql_F=$conn->query("SELECT * FROM dlikeaccounts where username='$dlike_user'");
                         if ($sql_F && $sql_F->num_rows > 0){$row_F=$sql_F->fetch_assoc();$profile_pic=$row_F["profile_pic"];} ?>
                         <tr>
-                            <td class="exp-user wid_2 ex_pad"><span style="justify-content: left;"><?php echo '<img src="'.$profile_pic.'" style="padding-right:10px;width: 32px;"><a href="/profile/'. $dlike_user.'">'. $dlike_user.'</a>'; ?></span></td>
+                            <td class="exp-user wid_2 ex_pad"><span style="justify-content: left;"><?php echo '<img src="'.$profile_pic.'" class="img-fluid ex_img"><a href="/profile/'. $dlike_user.'">'. $dlike_user.'</a>'; ?></span></td>
                             <td class="exp-amt wid_2 ex_pad"><span><?php echo $row_T["amount"]; ?></span></td>
                             <td class="exp-amt wid_2 ex_pad"><span><?php echo '<a href="https://tronscan.org/#/transaction/'.$row_T["status"].'" target="_blank"><i class="fas fa-exchange-alt"></i></a>'; ?></span></td>
                             <td class="exp-amt wid_2 ex_pad">
