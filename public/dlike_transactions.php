3d9049ca10b52ab1f9620959be9d79d60fa246cb
bestboom <certseek@gmail.com>
2020-08-30 21:49:18 +0500

    Update dlike_transactions.php

--- public/dlike_transactions.php
+++ public/dlike_transactions.php
@@ -110,7 +110,6 @@
             </div>
         </div>
     </div>
-
     <div class="activity-section"><div class="container"><div class="row"><div class="col-md-12">
         <div class="card h-100">
             <div class="card-header d-flex justify-content-between align-items-center">
@@ -124,13 +123,40 @@
                     while ($row_T = $sql_T->fetch_assoc()) {
                         $start_time = strtotime($row_T["trx_time"]);$dlike_user = $row_T["username"];$tx_type = $row_T["type"];
 
-                        $sql_W=$conn->query("SELECT * FROM dlikeaccounts where username='$dlike_user'");
-                        if ($sql_W && $sql_W->num_rows > 0){$profile_pic=$row_W["profile_pic"];} ?>
+                        $sql_F=$conn->query("SELECT * FROM dlikeaccounts where username='$dlike_user'");
+                        if ($sql_F && $sql_F->num_rows > 0){$row_F=$sql_F->fetch_assoc();$profile_pic=$row_F["profile_pic"];} ?>
                         <tr>
                             <td class="exp-user wid_2" style="padding: 12px 22px;"><span style="justify-content: left;"><?php echo '<img src="'.$profile_pic.'" style="padding-right:10px;width: 32px;"><a href="/profile/'. $dlike_user.'">'. $dlike_user.'</a>'; ?></span></td>
                             <td class="exp-amt wid_2" style="padding: 12px 22px;"><span><?php echo $tx_type; ?></span></td>
                             <td class="exp-amt wid_2" style="padding: 12px 22px;"><span><?php echo $row_T["amount"]; ?></span></td>
                             <td class="exp-amt wid_2" style="padding: 12px 22px;"><span><?php echo '<a href="https://tronscan.org/#/transaction/'.$row_T["tron_trx"].'" target="_blank"><i class="fas fa-exchange-alt"></i></a>'; ?></span></td>
+                            <td class="exp-amt wid_2" style="padding: 12px 22px;">
+                                <?php echo time_ago($start_time); ?></td>
+                        </tr>
+                    <?php } 
+                } ?>
+            </tbody>
+            </table>
+        </div>
+    </div></div></div></div>
+    <div class="activity-section"><div class="container"><div class="row"><div class="col-md-12">
+        <div class="card h-100">
+            <div class="card-header d-flex justify-content-between align-items-center">
+                <div class="card-header-title"><h4>Staking Transactions</h4></div>
+            </div>
+            <table class="table coin-list table-hover" style="border: 1px solid #eee;">
+            <tbody>
+                <?php 
+                $sql_T = $conn->query("SELECT * FROM dlike_withdrawals ORDER BY trx_time DESC LIMIT 30");
+                if ($sql_T && $sql_T->num_rows > 0) {
+                    while ($row_T = $sql_T->fetch_assoc()) {
+                        $start_time = strtotime($row_T["req_on"]);$dlike_user = $row_T["username"];
+                        $sql_F=$conn->query("SELECT * FROM dlikeaccounts where username='$dlike_user'");
+                        if ($sql_F && $sql_F->num_rows > 0){$row_F=$sql_F->fetch_assoc();$profile_pic=$row_F["profile_pic"];} ?>
+                        <tr>
+                            <td class="exp-user wid_2" style="padding: 12px 22px;"><span style="justify-content: left;"><?php echo '<img src="'.$profile_pic.'" style="padding-right:10px;width: 32px;"><a href="/profile/'. $dlike_user.'">'. $dlike_user.'</a>'; ?></span></td>
+                            <td class="exp-amt wid_2" style="padding: 12px 22px;"><span><?php echo $row_T["amount"]; ?></span></td>
+                            <td class="exp-amt wid_2" style="padding: 12px 22px;"><span><?php echo '<a href="https://tronscan.org/#/transaction/'.$row_T["status"].'" target="_blank"><i class="fas fa-exchange-alt"></i></a>'; ?></span></td>
                             <td class="exp-amt wid_2" style="padding: 12px 22px;">
                                 <?php echo time_ago($start_time); ?></td>
                         </tr>
