6972cd92d6d7d7f1ce438042cea1836f9d29028f
bestboom <certseek@gmail.com>
2020-09-09 16:31:54 +0500

    up

--- public/functions/post_data.php
+++ public/functions/post_data.php
@@ -4,7 +4,7 @@
 <div class="author-info"><h5><?php echo '<a href="/profile/'. $author.'">'. $author.'</a>'; ?><div class="time"><?php echo time_ago($post_time); ?></div></h5> </div></div>
 <div class="post-catg"><?php echo '<a href="/category/'. $category.'"><span class="post-meta">'. ucfirst($category).'</span></a>'; ?></div>
 </div></div>
-<div class="post-thumb img-fluid"><?php echo '<a href="/post/'.$author.'/'.$permlink.'"><img src=' . $imgUrl . ' class="card-img-top" /></a>'; ?></a></div>
+<div class="post-thumb img-fluid"><?php echo '<a href="/post/'.$author.'/'.$permlink.'"><img src=' . $imgUrl . ' onerror="postImgError(this);" class="card-img-top" /></a>'; ?></a></div>
 <div class="post-contnet-wrap post_bottom">
 <h4 class="post-title single_title"><?php echo '<a href="/post/'.$author.'/'.$permlink.'">'.$title.'</a>'; ?></h4>
 <p class="post-entry post-tags"><?php echo $tags; ?></p>
