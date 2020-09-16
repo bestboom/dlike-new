bd947852a7507074efb5f6efeb405af750d69080
bestboom <certseek@gmail.com>
2020-09-16 12:37:53 +0500

    Update dlk_withdraw.php

--- public/helper/dlk_withdraw.php
+++ public/helper/dlk_withdraw.php
@@ -76,7 +76,7 @@ if (isset($_POST['action']) && $_POST['action'] == 'paid' && isset($_POST['walle
 
 if (isset($_POST['action']) && $_POST['action'] == 'pay_user' && isset($_POST['wallet']) && $_POST['wallet'] != '') {
     $wallet = trim(mysqli_real_escape_string($conn, $_POST['wallet']));
-    $dlkamount = trim(mysqli_real_escape_string($conn, $_POST['dlk_out_amount']));
+    $out_amount = trim(mysqli_real_escape_string($conn, $_POST['dlk_out_amount']));
 
     $username = $_COOKIE['dlike_username'];
     $check_Bal = $conn->query("SELECT * FROM dlike_wallet WHERE username = '$username'");
@@ -104,8 +104,7 @@ if (isset($_POST['action']) && $_POST['action'] == 'pay_user' && isset($_POST['w
 	        $triggerContract = $tron->triggerContract($abi,$contract,$function,$params,$feeLimit,$address,$callValue ,$bandwidthLimit = 0);
 	        $signedTransaction = $tron->signTransaction($triggerContract);
 	        $response = $tron->sendRawTransaction($signedTransaction);
-	        if ($response['result'] == 1) { $status="0";
-	        	$sql_cur = $conn->query("INSERT INTO dlike_tokens_mapping (username, tron_address, amount, status, update_time) VALUES ('".$username."', '".$wallet."', '".$amount."', '".$status."', now())");
+	        if ($response['result'] == 1) {
 	             die(json_encode(['error' => false,'message' => 'All is fine to withdraw!']));
 	          }
 	        else{die(json_encode(['error' => true,'message' => $e->getMessage()]));}
