aaae3fda5844282e800ed5316c0282609928e5b8
bestboom <certseek@gmail.com>
2020-09-21 00:16:24 +0500

    Update dliker.php

--- public/dliker.php
+++ public/dliker.php
@@ -1,8 +1,8 @@
 <?phpif (!isset($_COOKIE['username']) || !$_COOKIE['username']) { die('<script>window.location.replace("https://dlike.io","_self")</script>');} else {$user_name = $_COOKIE['username'];}
-include('includes/config.php'); include "./template/header.php"; 
+include('includes/config.php'); include "template/header.php"; 
 
-require_once "./helper/steem/SteemEngine.php";
-require_once "./helper/steem/time_string.php";
+require_once "helper/steem/SteemEngine.php";
+require_once "helper/steem/time_string.php";
 use SnaddyvitchDispenser\SteemEngine\SteemEngine;
 $_STEEM_ENGINE = new SteemEngine();
 
