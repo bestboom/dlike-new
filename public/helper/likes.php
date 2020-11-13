606a2eb13e324df473442a04f220c30ce0da4293
bestboom <certseek@gmail.com>
2020-11-13 15:07:25 +0500

    up

--- public/helper/likes.php
+++ public/helper/likes.php
@@ -1,6 +1,7 @@
 <?php 
 require '../includes/config.php';
-if (isset($_POST['login_username'])  && $_POST['login_username'] != '' && isset($_POST['login_pass'])  && $_POST['login_pass'] != '') { 
+if (isset($_POST['login_username'])  && $_POST['login_username'] != '' && isset($_POST['login_pass'])  && $_POST['login_pass'] != '') {
+ 
     $token=$_POST["g-token"]; 
     $g_ip=$_SERVER['REMOTE_ADDR'];
     $url = "https://www.google.com/recaptcha/api/siteverify?".$recpatch_key."&response=".$token."&remoteip=".$g_ip;
@@ -19,7 +20,7 @@ if (isset($_POST['login_username'])  && $_POST['login_username'] != '' && isset(
     		$check_user = $conn->query("SELECT * FROM dlikeaccounts where username = '$login_username' and password = '$hashedPW'");
     		if ($check_user->num_rows>0){$row_C = $check_user->fetch_assoc(); $dlike_username=$row_C['username'];
     			$dlike_user_login_url = 'https://dlike.io';
-        		die(json_encode(['error' => false,'message' => 'Login Successful!','redirect' => $dlike_user_login_url,'dlikeuser' => $dlike_username,'catch' => $token]));
+        		die(json_encode(['error' => false,'message' => 'Login Successful!','redirect' => $dlike_user_login_url,'dlikeuser' => $dlike_username,'catch' => $g_response]));
     		} else {die(json_encode(['error' => true,'message' => 'Login details does not match!'])); }
         } else {die(json_encode(['error' => true,'message' => $errors]));}
         //}else {die(json_encode(['error' => true,'message' => 'Verification failed, please try again']));}
