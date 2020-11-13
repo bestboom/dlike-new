9fd4759cc2feca57be4d41de8c55cb63cd11449a
bestboom <certseek@gmail.com>
2020-11-13 13:30:58 +0500

    Update email_login.php

--- public/helper/email_login.php
+++ public/helper/email_login.php
@@ -6,21 +6,22 @@ if (isset($_POST['login_username'])  && $_POST['login_username'] != '' && isset(
     $url = "https://www.google.com/recaptcha/api/siteverify?".$recpatch_key."&response=".$token."&remoteip=".$g_ip;
     $request = file_get_contents($url);
     $g_response = json_decode($request);
-    if(!$g_response->success){$errors = "Verification failed, please try again";}
-	$login_username = trim($_POST["login_username"]);
-	$login_pass = trim($_POST["login_pass"]);
-	if(empty($login_username)){ $errors = "Username Shoould not be empty";}
-    if(empty($login_pass)){ $errors = "Password Shoould not be empty";}
-    if (empty($errors)) {
-    	$escapedPW = mysqli_real_escape_string($conn, $login_pass);
-		$escapedPWN = md5($escapedPW);
-		$hashedPW = hash('sha256', $escapedPWN);
+    if($g_response->success){
+    	$login_username = trim($_POST["login_username"]);
+    	$login_pass = trim($_POST["login_pass"]);
+    	if(empty($login_username)){ $errors = "Username Shoould not be empty";}
+        if(empty($login_pass)){ $errors = "Password Shoould not be empty";}
+        if (empty($errors)) {
+        	$escapedPW = mysqli_real_escape_string($conn, $login_pass);
+    		$escapedPWN = md5($escapedPW);
+    		$hashedPW = hash('sha256', $escapedPWN);
 
-		$check_user = $conn->query("SELECT * FROM dlikeaccounts where username = '$login_username' and password = '$hashedPW'");
-		if ($check_user->num_rows>0){$row_C = $check_user->fetch_assoc(); $dlike_username=$row_C['username'];
-			$dlike_user_login_url = 'https://dlike.io';
-    		die(json_encode(['error' => false,'message' => 'Login Successful!','redirect' => $dlike_user_login_url,'dlikeuser' => $dlike_username]));
-		} else {die(json_encode(['error' => true,'message' => 'Login details does not match!'])); }
-    } else {die(json_encode(['error' => true,'message' => $errors]));}
+    		$check_user = $conn->query("SELECT * FROM dlikeaccounts where username = '$login_username' and password = '$hashedPW'");
+    		if ($check_user->num_rows>0){$row_C = $check_user->fetch_assoc(); $dlike_username=$row_C['username'];
+    			$dlike_user_login_url = 'https://dlike.io';
+        		die(json_encode(['error' => false,'message' => 'Login Successful!','redirect' => $dlike_user_login_url,'dlikeuser' => $dlike_username]));
+    		} else {die(json_encode(['error' => true,'message' => 'Login details does not match!'])); }
+        } else {die(json_encode(['error' => true,'message' => $errors]));}
+    }else {die(json_encode(['error' => true,'message' => 'Verification failed, please try again']));}
 } else {die('Some error');}
 ?>
