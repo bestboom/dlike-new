dc67b4210be6a2b433af431467ce7f413fe959ad
bestboom <certseek@gmail.com>
2020-11-13 22:12:31 +0500

    Update email_signup.php

--- public/helper/email_signup.php
+++ public/helper/email_signup.php
@@ -31,8 +31,8 @@ if (isset($_POST['action']) && $_POST['action'] == 'signup' && isset($_POST['sig
     $url = "https://www.google.com/recaptcha/api/siteverify?";
     $data = array('secret' => $recpatch_key, 'response' => $g_token, 'remoteip'=> $gs_ip);
     $options = array('http' => array('method'  => 'POST','content' => http_build_query($data)));
-    //$context  = stream_context_create($options);
-    //$result = file_get_contents($url, false, $context);
+    $context  = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
     //die(json_encode(['error' => true,'message' => $result]));
     //$gs_response = json_decode($result);
     
