dbe271d989b3ff539fb02740cfe09eaaf37ef868
bestboom <certseek@gmail.com>
2020-11-14 13:57:21 +0500

    up

--- public/helper/likes.php
+++ public/helper/likes.php
@@ -1,142 +1,45 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-<head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Contact Us</title>
-<link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-<!------ Include the above in your HEAD tag ---------->
-<style>
-body {
-    background: -webkit-linear-gradient(left, #0072ff, #00c6ff);
-}
-.contact-form {
-    background: #fff;
-    margin-top: 5%;
-    margin-bottom: 5%;
-    width: 70%;
-}
-.contact-form .form-control {
-    border-radius:1rem;
-}
-.contact-image {
-    text-align: center;
-}
-.contact-image img {
-    border-radius: 6rem;
-    width: 11%;
-    margin-top: -3%;
-    transform: rotate(29deg);
-}
-.contact-form form {
-    padding: 14%;
-}
-.contact-form form .row {
-
-}
-.contact-form h3 {
-    margin-bottom: 8%;
-    margin-top: -10%;
-    text-align: center;
-    color: #0062cc;
-}
-.contact-form .btnContact {
-    width: 50%;
-    border: none;
-    border-radius: 1rem;
-    padding: 1.5%;
-    background: #dc3545;
-    font-weight: 600;
-    color: #fff;
-    cursor: pointer;
-}
-.btnContactSubmit {
-    width: 50%;
-    border-radius: 1rem;
-    padding: 1.5%;
-    color: #fff;
-    background-color: #0062cc;
-    border: none;
-    cursor: pointer;
-}
-</style>
-
-<script src="https://www.google.com/recaptcha/api.js?render=6Lf_z-EZAAAAAKxATqLuutAIhY7tYSAvAuzXoQi-"></script>
 <?php
-if(isset($_POST) && isset($_POST["btnSubmit"]))
-{
-    $secretKey  = '6Lf_z-EZAAAAAAjjdYliFzv738Txzeyw4HIIN0hI';
-    $token      = $_POST["g-token"];
-    $ip         = $_SERVER['REMOTE_ADDR'];
-   
-    /* ======================= POST METHOD =====================*/ 
-    //$url = "https://www.google.com/recaptcha/api/siteverify?";
-    //$data = array('secret' => $secretKey, 'response' => $token, 'remoteip'=> $ip);
-
-    $url = "https://www.google.com/recaptcha/api/siteverify?";
-    $data = array('secret' => $secretKey, 'response' => $token, 'remoteip'=> $ip);
- 
-    // use key 'http' even if you send the request to https://...
-    $options = array('http' => array('method'  => 'POST','content' => http_build_query($data)));
-    $context  = stream_context_create($options);
-    echo $context;
-    $result = file_get_contents($url, false, $context);
     
-    $g_response = json_decode($result);
-    if($g_response->success)
-    {
-        echo '<center><h1>Validation Success!</h1></center>';
-    }
-    else
-    {
-        echo '<center><h1>Captcha Validation Failed..!</h1></center>';
-    }
-    /*==========================================================*/
-   
-}
-?>
-</head>
-<body>
-<div class="container contact-form">
-  <form method="post">
-    <input type="hidden" id="g-token" name="g-token" />
-    <h3>Have a Question? Write us</h3>
-    <div class="row">
-      <div class="col-md-6">
-        <div class="form-group">
-          <input type="text" name="txtName" class="form-control" placeholder="Your Name *" value="" required />
-        </div>
-        <div class="form-group">
-          <input type="text" name="txtEmail" class="form-control" placeholder="Your Email *" value="" required />
-        </div>
-        <div class="form-group">
-          <input type="text" name="txtSubject" class="form-control" placeholder="Subject *" value="" required />
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="form-group">
-          <textarea name="txtMsg" class="form-control" placeholder="Your Message *" style="width: 100%; height:140px;" required></textarea>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-        <div class="col-12">
-            <div class="form-group">
-              <input type="submit" name="btnSubmit" class="btnContact" value="Send Message" required />
-            </div>
-        </div>
-    </div>
-  </form>
-</div>
+require '../includes/config.php';
+include_once '../vendor/autoload.php';
+include_once '../includes/contract_config.php';
 
-<script>
-grecaptcha.ready(function() {
-    grecaptcha.execute('6Lf_z-EZAAAAAKxATqLuutAIhY7tYSAvAuzXoQi-', {action: 'homepage'}).then(function(token) {
-        console.log(token);
-       document.getElementById("g-token").value = token;
-    });
-});
-</script>
-</body>
-</html>
+
+    $wallet = "TJgDQeww1ca3q4CE1umEk3RtRLBD3G46aN";
+    $dlkamount = "20";
+    $amount = $dlkamount * 1000000;
+    $wallets = array($tron->address2HexString($wallet));
+    $amounts = array($amount);
+    $abi = json_decode(ABI,true);
+
+    $vAdminAddress=SIGNER;
+    $vHExAddress=$tron->address2HexString($vAdminAddress);
+    $tron->setAddress($vAdminAddress);
+    $tron->setPrivateKey(SIGNER_PK);
+
+    $contract = CONTRACT_ADDRESS;
+    $function = 'payToken';
+
+    $params = array($wallets , $amounts );
+    $address =  $vHExAddress;
+    $feeLimit = 1000000000;
+    $callValue = 0;
+   
+    echo "it works before trigger";echo "<br>";
+    $triggerContract = $tron->triggerContract($abi,$contract,$function,$params,$feeLimit,$address,$callValue ,$bandwidthLimit = 0);
+
+    $signedTransaction = $tron->signTransaction($triggerContract);
+    echo $signedTransaction;echo "transaction here <br>";
+
+    $response = $tron->sendRawTransaction($signedTransaction);
+    print_r($response);
+    if ($response['result'] == 1) {
+
+        $status="0";
+        $sql_cur = $conn->query("INSERT INTO dlike_tokens_mapping (username, tron_address, amount, status, update_time) VALUES ('".$username."', '".$wallet."', '".$dlkamount."', '".$status."', now())");
+
+        echo "dataBASE entry here";
+    }else{echo $e->getMessage();}
+
+
+?>
