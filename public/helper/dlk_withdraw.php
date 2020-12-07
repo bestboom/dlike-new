cf6fd9e30e34b3c6716e0d797870a10bf9b549c9
bestboom <certseek@gmail.com>
2020-12-07 13:15:59 +0500

    Update dlk_withdraw.php

--- public/helper/dlk_withdraw.php
+++ public/helper/dlk_withdraw.php
@@ -26,7 +26,7 @@ if (isset($_POST['action'])  && $_POST['action'] == 'withdraw' && isset($_POST['
 	$verified = $row_add['verified'];$user_status = $row_add['status'];
 	if (empty($tron_address)){$errors = 'Phew... You must add your tron wallet address!';}
 	if ($verified !='1'){$errors = 'Phew... You must verify your email before withdrawing!';}
-	if ($user_status ='1'){$errors = 'Phew... Issue in withdrawal. Please contact support';}
+	if ($user_status =='1'){$errors = 'Phew... Issue in withdrawal. Please contact support';}
     if (empty($errors)) { 
     	die(json_encode(['error' => false,'message' => 'All is fine to withdraw!','user_wallet_add' => $user_wallet_add]));
     } else {die(json_encode(['error' => true,'message' => $errors]));}
