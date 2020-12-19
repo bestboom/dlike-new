0383f13fe83e73217cf141ed157635dd44fa70bd
bestboom <certseek@gmail.com>
2020-12-19 11:59:41 +0500

    Update wallet.js

--- public/js/wallet.js
+++ public/js/wallet.js
@@ -1,56 +1,53 @@
-function enable_draw(){$(".tok_out_btn").attr("disabled", false).html('Withdraw');}
+function enable_draw(){$(".withd_btn").attr("disabled", false).html('Withdraw');}
 let withdraw_val = document.getElementById('withdraw_amount');
 withdraw_val.onkeydown = function(e) {
     if(!((e.keyCode > 95 && e.keyCode < 106) || (e.keyCode > 47 && e.keyCode < 58) || e.keyCode == 8)) {return false;}
 }
-$('.withd_btn').click(function(e) {  e.preventDefault();$("#dlike_tok_with").modal("show");});
+$('.withd_btn').click(async function() {
+    if (dlike_username != null) {
+        $(".withd_btn").attr("disabled", true).html('Processing...');let user_bal_amt=$('.user_bal').html();
 
-$('.tok_out_btn').click(async function() {
-    $(".tok_out_btn").attr("disabled", true).html('Processing...');;
-    let out_amount = $('#withdraw_amount').val();let dlk_amount = $('.user_bal').html();
-    if (out_amount == "") {toastr.error('phew... Please enter valid amount to withdraw');enable_draw();return false;
-    }
-    if (parseFloat(out_amount) > parseFloat(dlk_amount)) {toastr.error('phew... Not enough balance');enable_draw();return false;
-    }
-    if ((parseFloat(dlk_amount) <= 0) ||  (parseFloat(out_amount) <= 0)){toastr.error('phew... Not a valid withdraw amount!');enable_draw();return false;
-    }
-    async function doAjax() {return $.ajax({type: 'post',url: '/helper/dlk_withdraw.php',data: { action : 'withdraw',dlk_out_amount: out_amount },datatype: 'json',});
-    }
-    doAjax().then(async function(data) { var response = JSON.parse(data);
-        if (response.error == true) {toastr['error'](response.message);enable_draw();return false;}
-        else{ let user_address =false;var my_address = response.user_wallet_add;
-            if (window.tronWeb!=undefined) {user_address= await window.tronWeb.defaultAddress.base58;
-            }else{toastr.error('Non-Tronlink browser detected. You should consider trying Tronlink Wallet!');return false;}
-            if(user_address != my_address) {toastr.error('You are trying to withdraw with a different tron address which is not in your DLIKE wallet!');return false;}
-            if(user_address==false){toastr.error('Please Login to Tronlink Wallet.');return false;
-            } else { 
-                $.ajax({type: 'post',url:'helper/dlk_withdraw.php',data:{action : 'pay_user',dlk_out_amount: out_amount,wallet: user_address},});
-                let payout_amount = out_amount * 1e6;
-                let myContractInfo = await tronWeb.trx.getContract(mainContractAddress);
-                let myContract = await tronWeb.contract(myContractInfo.abi.entrys, mainContractAddress);
-                let result = await myContract.getToken(payout_amount).send({ shouldPollResponse: false, feeLimit: 15000000, callValue: 0, from: user_address });console.log(result);
-                if(result){
-                    $("#dlike_tok_with").modal("hide");$('#withdrawStatus').modal('show');
-                    $(".wd_trx_link").html('<a href="https://tronscan.org/#/transaction/'+result+'" target="_blank">Check Transaction Here</a>');
-                    var x = setInterval(function() {
-                        $.get("https://api.trongrid.io/wallet/gettransactioninfobyid?value="+result, function(data, status){
-                            if(status=='success'){var wid_data = JSON.parse(data);
-                                var tx_result = wid_data.receipt["result"]; 
-                                if(tx_result=='SUCCESS'){
-                                    $.ajax({type: 'post',url:'helper/dlk_withdraw.php',data:{action : 'paid',dlk_out_amount: out_amount,wallet: user_address,trx_id: result},});
-                                    $(".wd_status_message").html('Tokens Withdraw Successfully!');
-                                    $(".iconTitle").find($(".fa")).removeClass('fa-spinner fa-pulse').addClass('fa-check-circle');setTimeout(function(){window.location.reload();}, 1000);
-                                }else{
-                                    $(".wd_status_message").html('Something Wrong! Try Again.');
-                                    $(".iconTitle").find($(".fa")).removeClass('fa-spinner fa-pulse').addClass('fa-times-circle');setTimeout(function(){window.location.reload();}, 1000);
-                                }
-                            } 
-                        }); 
-                    }, 9000);
+        if (user_bal_amt == "") {toastr.error('phew... Looks like balance issue');enable_draw();return false;}
+        if (parseFloat(user_bal_amt) <= 0){toastr.error('phew... Not a valid withdraw amount!');enable_draw();return false;}
+        
+        async function doAjax() {return $.ajax({type: 'post',url: '/helper/dlk_withdraw.php',data: { action : 'withdraw',dlk_out_amount: user_bal_amt},datatype: 'json',});}
+        doAjax().then(async function(data) { var response = JSON.parse(data);
+            if (response.error == true) {toastr['error'](response.message);enable_draw();return false;}
+            else{ let user_address =false;var my_address = response.user_wallet_add;
+                if (window.tronWeb!=undefined) {user_address= await window.tronWeb.defaultAddress.base58;
+                }else{toastr.error('Non-Tronlink browser detected. You should consider trying Tronlink Wallet!');return false;}
+                if(user_address != my_address) {toastr.error('You are trying to withdraw with a different tron address which is not in your DLIKE wallet!');return false;}
+                if(user_address==false){toastr.error('Please Login to Tronlink Wallet.');return false;
+                } else { 
+                    $.ajax({type: 'post',url:'helper/dlk_withdraw.php',data:{action : 'pay_user',dlk_out_amount: user_bal_amt,wallet: user_address},});
+                    let payout_amount = user_bal_amt * 1e6;
+                    let myContractInfo = await tronWeb.trx.getContract(mainContractAddress);
+                    let myContract = await tronWeb.contract(myContractInfo.abi.entrys, mainContractAddress);
+                    let result = await myContract.getToken(payout_amount).send({ shouldPollResponse: false, feeLimit: 15000000, callValue: 0, from: user_address });console.log(result);
+                    if(result){
+                        //$("#dlike_tok_with").modal("hide");
+                        $('#withdrawStatus').modal('show');
+                        $(".wd_trx_link").html('<a href="https://tronscan.org/#/transaction/'+result+'" target="_blank">Check Transaction Here</a>');
+                        var x = setInterval(function() {
+                            $.get("https://api.trongrid.io/wallet/gettransactioninfobyid?value="+result, function(data, status){
+                                if(status=='success'){var wid_data = JSON.parse(data);
+                                    var tx_result = wid_data.receipt["result"]; 
+                                    if(tx_result=='SUCCESS'){
+                                        $.ajax({type: 'post',url:'helper/dlk_withdraw.php',data:{action : 'paid',dlk_out_amount: user_bal_amt,wallet: user_address,trx_id: result},});
+                                        $(".wd_status_message").html('Tokens Withdraw Successfully!');
+                                        $(".iconTitle").find($(".fa")).removeClass('fa-spinner fa-pulse').addClass('fa-check-circle');setTimeout(function(){window.location.reload();}, 1000);
+                                    }else{
+                                        $(".wd_status_message").html('Something Wrong! Try Again.');
+                                        $(".iconTitle").find($(".fa")).removeClass('fa-spinner fa-pulse').addClass('fa-times-circle');setTimeout(function(){window.location.reload();}, 1000);
+                                    }
+                                } 
+                            }); 
+                        }, 5000);
+                    }
                 }
             }
-        }
-    })
+        })
+    } else {toastr.error('You must be login with DLIKE username!');enable_draw();return false;}
 });
 $('.add_address').click(function() { let offchain_add = $('#offchain_add').val();
     if (offchain_add == "") { toastr.error('phew... You forgot to enter address');return false;}
