b2fe4f7b8d80155c5506b7b50c2c4b817eda0da1
bestboom <certseek@gmail.com>
2021-01-31 17:28:08 +0500

    Update testit.php

--- public/testit.php
+++ public/testit.php
@@ -6,9 +6,9 @@ error_reporting(E_ALL);
 
 include_once 'helper/spatie/image-optimizer/src/OptimizerChainFactory.php';
 include_once 'helper/spatie/image-optimizer/src/OptimizerChain.php';
-include_once 'helper/spatie/image-optimizer/src/DummyLogger.php';
 include_once 'helper/spatie/Psr/Log/LoggerInterface.php';
-require_once("../vendor/autoload.php"); 
+include_once 'helper/spatie/image-optimizer/src/DummyLogger.php';
+//require_once("../vendor/autoload.php"); 
 
 //use Spatie\ImageOptimizer\OptimizerChainFactory;
 
