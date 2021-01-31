0e889b6dbab48e6ffee38697948683d1eb54586a
bestboom <certseek@gmail.com>
2021-01-31 16:44:07 +0500

    Update testit.php

--- public/testit.php
+++ public/testit.php
@@ -3,7 +3,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //include '../vendor/spatie/image-optimizer/src/OptimizerChainFactory.php';
-//require_once("../vendor/autoload.php"); 
+require_once("../vendor/autoload.php"); 
 include_once 'helper/spatie/image-optimizer/src/OptimizerChainFactory.php';
 include_once 'helper/spatie/image-optimizer/src/OptimizerChain.php';
 include_once 'helper/spatie/image-optimizer/src/DummyLogger.php';
