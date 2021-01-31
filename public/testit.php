7cf2c60e3a31c7f355a118c6071d2476055270fe
bestboom <certseek@gmail.com>
2021-01-31 16:25:24 +0500

    up

---
+++ public/testit.php
@@ -0,0 +1,19 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+//include '../vendor/spatie/image-optimizer/src/OptimizerChainFactory.php';
+//require_once("../vendor/autoload.php"); 
+include_once 'spatie/image-optimizer/src/OptimizerChainFactory.php';
+include_once 'spatie/image-optimizer/src/OptimizerChain.php';
+include_once 'spatie/image-optimizer/src/DummyLogger.php';
+
+use Spatie\ImageOptimizer\OptimizerChainFactory;
+
+$optimizerChain = OptimizerChainFactory::create();
+
+echo $pathToImage = "https://aniportalimages.s3.amazonaws.com/media/details/__sized__/Palestine_covid_jan_30-thumbnail-154x87-70.jpg";
+
+echo $optimizerChain->optimize($pathToImage);
+
+?>	
