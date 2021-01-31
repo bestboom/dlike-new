239e2c9e75dd0f4f3a0a9496a1cf9e1f3985c5ca
bestboom <certseek@gmail.com>
2021-01-31 19:50:34 +0500

    Update testit.php

--- public/testit.php
+++ public/testit.php
@@ -4,7 +4,8 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 //include '../vendor/spatie/image-optimizer/src/OptimizerChainFactory.php';
 require_once("../vendor/autoload.php"); 
-include 'helper/spatie/image-optimizer/src/Optimizers';
+include 'helper/spatie/image-optimizer/src/OptimizerChainFactory.php';
+include 'helper/spatie/image-optimizer/src/OptimizerChain.php';
 include 'helper/spatie/image-optimizer/src/Optimizers/BaseOptimizer.php';
 include 'helper/spatie/image-optimizer/src/Optimizers/Optipng.php';
 include 'helper/spatie/image-optimizer/src/Optimizers/Gifsicle.php';
@@ -12,8 +13,7 @@ include 'helper/spatie/image-optimizer/src/Optimizers/Jpegoptim.php';
 include 'helper/spatie/image-optimizer/src/Optimizers/Cwebp.php';
 include 'helper/spatie/image-optimizer/src/Optimizers/Svgo.php';
 include 'helper/spatie/image-optimizer/src/Optimizers/Pngquant.php';
-include 'helper/spatie/image-optimizer/src/OptimizerChainFactory.php';
-include 'helper/spatie/image-optimizer/src/OptimizerChain.php';
+
 include 'helper/spatie/Psr/Log/LoggerInterface.php';
 include 'helper/spatie/image-optimizer/src/DummyLogger.php';
 
