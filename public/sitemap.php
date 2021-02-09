3d9a570ee926eb4e3a927a0ca48383803ab2642d
bestboom <certseek@gmail.com>
2021-02-09 13:23:27 +0500

    up

--- public/sitemap.php
+++ public/sitemap.php
@@ -3,34 +3,37 @@ $filename = 'helper/sitemap_urls.txt';
 $urls = file($filename);
 $filectime = filectime($filename);
 $urls = array_map('trim',$urls);
-$sitemap = array();
+
+
+$priority = '0.9';
+$changefreq = 'daily';
+$datem = date("Y-m-d\TH:i:sP");
+
+$codtgetdata='<?xml version="1.0" encoding="iso-8859-1"?>
+<urlset
+      xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+      xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+      xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
+      http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
+
 
 foreach($urls as $url) {
-	if ($url != '') {
-		$priority = '0.9';
-		$sitemap[] = array(
-			'loc' => $url,
-			'lastmod' => date('Y-m-d\TH:i:sP',$filectime),
-			'changefreq' => 'daily',
-			'priority' => $priority,
-		);
-	}
+$codtgetdata .='<url>
+<loc>'.$url.'</loc>
+<lastmod>'.$datem.'</lastmod>
+<changefreq>'.$changefreq.'</changefreq>
+<priority>'.$priority.'</priority>
+</url>';
 }
-header('Content-Type: text/xml');
-echo '<?xml version=\'1.0\' encoding=\'UTF-8\'?>';
-echo "\n";
-echo '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
-	xsi:schemaLocation="http://www.sitemaps.org/schemas/sitemap/0.9
-			    http://www.sitemaps.org/schemas/sitemap/0.9/sitemap.xsd">';
-echo "\n";
-foreach ($sitemap as $link) {
-	echo "\t<url>\n";
-	echo "\t\t<loc>" . htmlentities($link['loc']) . "</loc>\n";
-	echo "\t\t<lastmod>{$link['lastmod']}</lastmod>\n";
-	echo "\t\t<changefreq>{$link['changefreq']}</changefreq>\n";
-	echo "\t\t<priority>{$link['priority']}</priority>\n";
-	echo "\t</url>\n";
+$codtgetdata .='</urlset> ';
+
+$path = "sitemap.xml";
+$modo = "w+";
+
+if ($fpen=fopen($path,$modo)){
+   fwrite ($fpen,$codtgetdata);
+   echo "<p><b>Sitemap successfuly created and saved to sitemap.xml!</b>";
+}else{
+   echo "<p><b>There has been a problem and the file has not been created.</b>";
 }
-echo '</urlset>';
 ?>
