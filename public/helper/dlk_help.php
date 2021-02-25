6a14cb2484e1db5c14ee887e0993a9383eb42ea7
bestboom <certseek@gmail.com>
2021-02-25 12:24:06 +0500

    up

--- public/helper/dlk_help.php
+++ public/helper/dlk_help.php
@@ -7,9 +7,9 @@ error_reporting(E_ALL);
 
 
 
-$sql = "ALTER TABLE dlike_daily_rewards ADD dlike_mining varchar(255) NOT NULL AFTER dlike_foundation";
+$sql = "ALTER TABLE dlike_daily_rewards ADD dlike_nodes varchar(255) NOT NULL AFTER dlike_mining";
 if ($conn->query($sql) === TRUE) {
-    echo "new field dlike_mining added to wallet table";
+    echo "new field dlike_nodes added to wallet table";
 } else {
     echo "Error updating table: " . $conn->error;
 }
