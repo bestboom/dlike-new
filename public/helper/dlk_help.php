5c2ad4260e3f6ee126a3b77bd0ccb0919d997d5f
bestboom <certseek@gmail.com>
2021-03-24 22:25:46 +0500

    Update dlk_help.php

--- public/helper/dlk_help.php
+++ public/helper/dlk_help.php
@@ -5,8 +5,26 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+$sql = "ALTER TABLE dlike_mining_rewards ADD to_address varchar(255) NOT NULL AFTER id";
+if ($conn->query($sql) === TRUE) {
+    echo "new field added to wallet table";
+} else {
+    echo "Error updating table: " . $conn->error;
+}
+
+
+$sql = "ALTER TABLE dlike_mining_rewards ADD status varchar(255) NOT NULL AFTER amount";
+if ($conn->query($sql) === TRUE) {
+    echo "new field added to wallet table";
+} else {
+    echo "Error updating table: " . $conn->error;
+}
+
+
+/*
 $sql = "CREATE TABLE dlike_mining_rewards (
 id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
+to_address VARCHAR(255) NOT NULL,
 trx_id VARCHAR(255) NOT NULL,
 amount FLOAT(12,2) NOT NULL,
 trx_time TIMESTAMP
@@ -18,7 +36,7 @@ if ($conn->query($sql) === TRUE) {
 }
 
 
-/*
+
 $sql = "ALTER TABLE dlike_rewards_history ADD dlike_nodes varchar(255) NOT NULL AFTER dlike_mining";
 if ($conn->query($sql) === TRUE) {
     echo "new field dlike_nodes added to wallet table";
